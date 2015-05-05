5f705ed405e4af80f209d34a91612706b23ee1a7
Alexander Garagatyi <agaragatyi@codenvy.com>
2015-05-05 12:56:26 +0300

    IDEX-2511: move docker connector and docker machine impl to che

--- assembly-platform-api/src/main/java/org/eclipse/che/api/deploy/ApiModule.java
+++ assembly-platform-api/src/main/java/org/eclipse/che/api/deploy/ApiModule.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.che.api.deploy;
 
-import com.codenvy.machine.docker.local.LocalDockerModule;
 import com.google.inject.AbstractModule;
 
 import org.eclipse.che.api.analytics.AnalyticsModule;
@@ -31,6 +30,7 @@ import org.eclipse.che.ide.ext.ssh.server.KeyService;
 import org.eclipse.che.ide.ext.ssh.server.SshKeyStore;
 import org.eclipse.che.ide.ext.ssh.server.UserProfileSshKeyStore;
 import org.eclipse.che.inject.DynaModule;
+import org.eclipse.che.plugin.docker.machine.local.LocalDockerModule;
 import org.eclipse.che.security.oauth.OAuthAuthenticationService;
 import org.eclipse.che.security.oauth.OAuthAuthenticatorProvider;
 import org.eclipse.che.security.oauth.OAuthAuthenticatorProviderImpl;
