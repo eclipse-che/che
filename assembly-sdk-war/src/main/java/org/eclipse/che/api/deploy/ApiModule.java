36ef3a872e879c3bcfe0dc12d14c1978214d6471
Evgen Vidolob <evidolob@codenvy.com>
2015-05-18 16:29:43 +0300

    IDEX-2549 fix build, disable building code assistant, while  org.eclipse.ltk.core don't present in public maven repository

--- assembly-sdk-war/src/main/java/org/eclipse/che/api/deploy/ApiModule.java
+++ assembly-sdk-war/src/main/java/org/eclipse/che/api/deploy/ApiModule.java
@@ -29,10 +29,6 @@ import org.eclipse.che.ide.ext.ssh.server.KeyService;
 import org.eclipse.che.ide.ext.ssh.server.SshKeyStore;
 import org.eclipse.che.ide.ext.ssh.server.UserProfileSshKeyStore;
 import org.eclipse.che.inject.DynaModule;
-import org.eclipse.che.jdt.JavaNavigationService;
-import org.eclipse.che.jdt.JavadocService;
-import org.eclipse.che.jdt.rest.JavaClasspathService;
-import org.eclipse.che.jdt.rest.JavaReconcileService;
 import org.eclipse.che.plugin.docker.machine.local.LocalDockerModule;
 import org.eclipse.che.security.oauth.OAuthAuthenticationService;
 import org.eclipse.che.security.oauth.OAuthAuthenticatorProvider;
@@ -106,9 +102,9 @@ public class ApiModule extends AbstractModule {
 //        install(new FactoryModule());
         install(new LocalDockerModule());
 
-        bind(JavadocService.class);
-        bind(JavaNavigationService.class);
-        bind(JavaReconcileService.class);
-        bind(JavaClasspathService.class);
+//        bind(JavadocService.class);
+//        bind(JavaNavigationService.class);
+//        bind(JavaReconcileService.class);
+//        bind(JavaClasspathService.class);
     }
 }
