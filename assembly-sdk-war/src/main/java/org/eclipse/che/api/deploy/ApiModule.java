c9264c4e757b52d4d1fc7e06245d2b7a17d526d2
Evgen Vidolob <evidolob@codenvy.com>
2015-05-18 15:04:04 +0300

    IDEX-2549 merge 'sever_jdt' branch to 4.0, remove unused code

--- assembly-sdk-war/src/main/java/org/eclipse/che/api/deploy/ApiModule.java
+++ assembly-sdk-war/src/main/java/org/eclipse/che/api/deploy/ApiModule.java
@@ -29,6 +29,10 @@ import org.eclipse.che.ide.ext.ssh.server.KeyService;
 import org.eclipse.che.ide.ext.ssh.server.SshKeyStore;
 import org.eclipse.che.ide.ext.ssh.server.UserProfileSshKeyStore;
 import org.eclipse.che.inject.DynaModule;
+import org.eclipse.che.jdt.JavaNavigationService;
+import org.eclipse.che.jdt.JavadocService;
+import org.eclipse.che.jdt.rest.JavaClasspathService;
+import org.eclipse.che.jdt.rest.JavaReconcileService;
 import org.eclipse.che.plugin.docker.machine.local.LocalDockerModule;
 import org.eclipse.che.security.oauth.OAuthAuthenticationService;
 import org.eclipse.che.security.oauth.OAuthAuthenticatorProvider;
@@ -101,5 +105,10 @@ public class ApiModule extends AbstractModule {
         install(new VirtualFileSystemFSModule());
 //        install(new FactoryModule());
         install(new LocalDockerModule());
+
+        bind(JavadocService.class);
+        bind(JavaNavigationService.class);
+        bind(JavaReconcileService.class);
+        bind(JavaClasspathService.class);
     }
 }
