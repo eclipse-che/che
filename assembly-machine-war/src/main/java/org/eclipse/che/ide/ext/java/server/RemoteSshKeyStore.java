f0494600d5494b927929d4b98e8217c76fca92df
Vitaly Parfonov <vparfonov@codenvy.com>
2015-08-12 01:16:13 +0300

    Tempoarry add few dummy component to make servuce startable

---
+++ assembly-machine-war/src/main/java/org/eclipse/che/ide/ext/java/server/RemoteSshKeyStore.java
@@ -0,0 +1,21 @@
+/**
+ * ****************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * <p/>
+ * Contributors:
+ * Codenvy, S.A. - initial API and implementation
+ * *****************************************************************************
+ */
+package org.eclipse.che.ide.ext.java.server;
+
+/**
+ * //
+ *
+ * @author Vitalii Parfonov
+ */
+public class RemoteSshKeyStore {
+}
