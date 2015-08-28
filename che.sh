8c5074fb634b165ab2aeae703f0e2b1109a4ba12
Florent BENOIT <fbenoit@codenvy.com>
2015-08-28 14:40:33 +0200

    IDEX-2977 Simplify and standardize scripts

--- che.sh
+++ che.sh
@@ -1,52 +1,25 @@
 #!/bin/sh
 #
-# Startup script for Codenvy SDK.  Downloads Tomcat for running projects within the IDE if necessary.
+# Copyright (c) 2012-2015 Codenvy, S.A.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Codenvy, S.A. - initial API and implementation
+#
 
+BASEDIR=$(dirname $0)
+VERSION=`basename $BASEDIR/assembly-sdk/target/assembly-sdk-*/`
 
-TOMCAT="tomcat"
-TOMCAT_IDE_DIR="assembly-sdk/target/tomcat-ide"
+ASSEMBLY_BIN_DIR=$BASEDIR/assembly-sdk/target/$VERSION/$VERSION/bin
 
-if [ ! -d "${TOMCAT_IDE_DIR}" ]
+if [ ! -d "${ASSEMBLY_BIN_DIR}" ]
 then
-  unzip assembly-sdk/target/*.zip -d assembly-sdk/target/tomcat-ide
- # echo "$(tput setaf 1)ERROR: Looks like you have not installed the Codenvy SDK."$(tput sgr0)
- # echo "$(tput setaf 1)ERROR: Please run 'mvn clean install' and try again."$(tput sgr0)
- # echo "$(tput setaf 1)ERROR: For more information, please see the Codenvy SDK README:"$(tput sgr0)
- # echo "$(tput setaf 1)ERROR:     https://github.com/codenvy/che"$(tput sgr0)
- # exit 1
+  echo "$(tput setaf 1)The command 'mvn clean install' needs to be run first. This will build the Eclipse Che assembly."$(tput sgr0)
+  exit 1
 fi
 
-cd "${TOMCAT_IDE_DIR}"
+$ASSEMBLY_BIN_DIR/che.sh $*
 
-if [ ! -d "$TOMCAT" ]
-then
-  echo "$(tput setaf 2)INFO: No Tomcat found for runners"$(tput sgr0)
-  echo "$(tput setaf 2)INFO: Downloading Apache Tomcat for runners"$(tput sgr0)
-
-  sleep 2
-
-  tomcatVersion="7.0.50"
-  tomcatDir="apache-tomcat-"${tomcatVersion}
-  tomcatBinUrl="http://archive.apache.org/dist/tomcat/tomcat-7/v${tomcatVersion}/bin/apache-tomcat-${tomcatVersion}.zip"
-
-  curl -# -f -o ${tomcatDir}.zip ${tomcatBinUrl}
-
-  if [ $? -ne 0 ]
-  then
-    echo "$(tput setaf 1)ERROR: Unable to download Tomcat ${tomcatVersion} from ${tomcatBinUrl}"$(tput sgr0)
-    exit 1
-  fi
-
-  unzip -q ${tomcatDir}
-  rm apache-tomcat-${tomcatVersion}.zip
-  mv ${tomcatDir} ${TOMCAT}
-  rm -rf ${tomcatDir}
-
-  echo "$(tput setaf 2)INFO: Tomcat ${tomcatVersion} for runners successfully downloaded"$(tput sgr0)
-fi
-
-echo "$(tput setaf 2)INFO: Launching Eclipse Che"$(tput sgr0)
-sleep 1
-
-cd bin
-./che.sh $*
