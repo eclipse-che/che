961b7235d7b8f9deceb91d523b8c54abd64b0751
Vitaly Parfonov <vparfonov@codenvy.com>
2015-09-02 18:29:34 +0300

    Fix starting script

--- che.sh
+++ che.sh
@@ -17,34 +17,9 @@ ASSEMBLY_BIN_DIR=$BASEDIR/assembly-sdk/target/$VERSION/$VERSION/bin
 
 if [ ! -d "${ASSEMBLY_BIN_DIR}" ]
 then
-  echo "$(tput setaf 2)INFO: No Tomcat found for runners"$(tput sgr0)
-  echo "$(tput setaf 2)INFO: Downloading Apache Tomcat for runners"$(tput sgr0)
-
-  sleep 2
-
-  tomcatVersion="7.0.50"
-  tomcatDir="apache-tomcat-"${tomcatVersion}
-  tomcatBinUrl="http://archive.apache.org/dist/tomcat/tomcat-7/v${tomcatVersion}/bin/apache-tomcat-${tomcatVersion}.zip"
-
-#  curl -# -f -o ${tomcatDir}.zip ${tomcatBinUrl}
-
-  if [ $? -ne 0 ]
-  then
-    echo "$(tput setaf 1)ERROR: Unable to download Tomcat ${tomcatVersion} from ${tomcatBinUrl}"$(tput sgr0)
-    exit 1
-  fi
-
-  unzip -q ${tomcatDir}
-  rm apache-tomcat-${tomcatVersion}.zip
-  mv ${tomcatDir} ${TOMCAT}
-  rm -rf ${tomcatDir}
-
-  echo "$(tput setaf 2)INFO: Tomcat ${tomcatVersion} for runners successfully downloaded"$(tput sgr0)
+  echo "$(tput setaf 1)The command 'mvn clean install' needs to be run first. This will build the Eclipse Che assembly."$(tput sgr0)
+  exit 1
 fi
 
-echo "$(tput setaf 2)INFO: Launching Eclipse Che"$(tput sgr0)
-sleep 1
-pwd
-cd bin
-pwd
-./che.sh $*
+$ASSEMBLY_BIN_DIR/che.sh $*
+
