5a27c85da755027f6545c69dd2d9197b4b27eaea
Stéphane Tournié <stephane.tournie@serli.com>
2015-10-22 17:17:26 +0200

    IDEX-3288 Update Che examples  - fixing MyProjectGenerator  - renaming example package as examples  - setting my project type to use proper custom runner
    Change-Id: Ifa77d5fa04d1c48f936810b7d09c6c104dcc979f
    Signed-off-by: Stéphane Tournié <stephane.tournie@serli.com>

---
+++ examples/my-theme/src/main/java/examples/MyTheme.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package examples;
+
+import org.eclipse.che.ide.theme.DarkTheme;
+
+/**
+ * @author Evgen Vidolob
+ */
+public class MyTheme extends DarkTheme {
+
+    @Override
+    public String getId() {
+        return "My Theme id";
+    }
+
+    @Override
+    public String getDescription() {
+        return "My new Che Theme";
+    }
+
+    @Override
+    public String getMainFontColor() {
+        return "red";
+    }
+
+    @Override
+    public String getPartBackground() {
+        return "white";
+    }
+
+    @Override
+    public String getTabsPanelBackground() {
+        return "white";
+    }
+}
