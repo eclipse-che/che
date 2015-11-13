db5b0bc2a1e2b4f3d7a7a23210cbf34c2f15992f
Anton Korneta <akorneta@codenvy.com>
2015-11-13 11:07:40 +0200

    IDEX-3428: Change license to EPL

--- assembly-sdk/src/assembly/bin/setenv.sh
+++ assembly-sdk/src/assembly/bin/setenv.sh
@@ -1,19 +1,13 @@
+#!/bin/sh
 #
-# CODENVY CONFIDENTIAL
-# __________________
+# Copyright (c) 2012-2015 Codenvy, S.A.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
 #
-#  [2012] - [2015] Codenvy, S.A.
-#  All Rights Reserved.
-#
-# NOTICE:  All information contained herein is, and remains
-# the property of Codenvy S.A. and its suppliers,
-# if any.  The intellectual and technical concepts contained
-# herein are proprietary to Codenvy S.A.
-# and its suppliers and may be covered by U.S. and Foreign Patents,
-# patents in process, and are protected by trade secret or copyright law.
-# Dissemination of this information or reproduction of this material
-# is strictly forbidden unless prior written permission is obtained
-# from Codenvy S.A..
+# Contributors:
+#   Codenvy, S.A. - initial API and implementation
 #
 
 #Global Conf dir
