5ecf4bd2b99d015f386f94c48c19bf00e8d94a0f
Sergii Kabashniuk <skabashnyuk@codenvy.com>
2015-11-30 10:40:12 +0200

    IDEX-1991 Fix licenses

--- assembly-sdk/src/assembly/bin/setenv.bat
+++ assembly-sdk/src/assembly/bin/setenv.bat
@@ -1,20 +1,14 @@
 @REM
-@REM CODENVY CONFIDENTIAL
-@REM __________________
+@REM Copyright (c) 2012-2015 Codenvy, S.A.
+@REM All rights reserved. This program and the accompanying materials
+@REM are made available under the terms of the Eclipse Public License v1.0
+@REM which accompanies this distribution, and is available at
+@REM http://www.eclipse.org/legal/epl-v10.html
 @REM
-@REM [2012] - [2015] Codenvy, S.A.
-@REM All Rights Reserved.
+@REM Contributors:
+@REM   Codenvy, S.A. - initial API and implementation
 @REM
-@REM NOTICE: All information contained herein is, and remains
-@REM the property of Codenvy S.A. and its suppliers,
-@REM if any. The intellectual and technical concepts contained
-@REM herein are proprietary to Codenvy S.A.
-@REM and its suppliers and may be covered by U.S. and Foreign Patents,
-@REM patents in process, and are protected by trade secret or copyright law.
-@REM Dissemination of this information or reproduction of this material
-@REM is strictly forbidden unless prior written permission is obtained
-@REM from Codenvy S.A..
-@REM
+
 @echo off
 set CHE_LOCAL_CONF_DIR=%CATALINA_HOME%\conf
 
