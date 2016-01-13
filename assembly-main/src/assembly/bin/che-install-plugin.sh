001e614304b92f2be7e2b21e530913c5f4300a05
Tyler Jewell <tjewell@codenvy.com>
2016-01-12 20:03:44 -0800

    New syntax

--- assembly-main/src/assembly/bin/che-install-plugin.sh
+++ assembly-main/src/assembly/bin/che-install-plugin.sh
@@ -29,24 +29,33 @@ function init_global_variables {
 Looks like something went wrong. Possible issues: 
 "
 
-  CHE_VARIABLES="
-Che Environment Variables:
-  (OPTIONAL) DOCKER_MACHINE_HOST                   ==> (Linux) Docker host IP - set if browser clients remote
-  "
-
   USAGE="
 Usage: 
-  che [OPTIONS] [run | start | stop]
-     -i,      --image        Launches Che within a Docker container using latest image
-     -i:tag,  --image:tag    Launches Che within a Docker container using specific image tag
-
-."
+  che-install-plugin [OPTIONS]         
+     -a            --assembly          Creates new distributable Che assembly with your plugins 
+     -s:deps,      --skip:deps         Skips automatic injection of POM dependencies of your plugins 
+     -s:maven,     --skip:maven        Skips running maven to inject your plugins into local repository
+     -s:update,    --skip:update       Skips updating this assembly with new packages; leaves them in /temp build dir 
+     -s:wsagent,   --skip:wsagent      Skips creating new ws agent 
+     -s:wsmaster,  --skip:wsmaster     Skips creating new ws master, which contains IDE & ws manager
+     -d,           --debug             Additional verbose logging for this program
+     -h,           --help              This help message
+"
 
   # Sets value of operating system
   WIN=false
   MAC=false
   LINUX=false
 
+
+  USE_DEBUG=false
+  USE_HELP=false
+  SKIP_MAVEN=false
+  SKIP_DEPENDENCIES=false
+  SKIP_UPDATE=false
+  SKIP_WSMASTER=false
+  SKIP_WSAGENT=false
+  ASSEMBLY=false
 }
 
 function usage {
@@ -55,69 +64,63 @@ function usage {
 
 function error_exit {
   echo
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   echo "$1"
-  echo "$WRONG $CHE_VARIABLES $USAGE"
+  echo "$WRONG $USAGE"
+  USE_HELP=true
 }
 
 function parse_command_line {
 
   for command_line_option in "$@"
   do
-  case $command_line_option in
-    -i|--image)
-      USE_DOCKER=true
-    ;;
-    -i:*|--image:*)
-      USE_DOCKER=true
-      if [ "${command_line_option#*:}" != "" ]; then
-        CHE_DOCKER_TAG="${command_line_option#*:}"
-      fi
-    ;;
-    -p:*|--port:*)
-      if [ "${command_line_option#*:}" != "" ]; then
-        CHE_PORT="${command_line_option#*:}"
-      fi
-    ;;
-    -r:*|--remote:*)
-      if [ "${command_line_option#*:}" != "" ]; then
-        CHE_IP="${command_line_option#*:}"
-      fi
-    ;;
-    -m:*|--machine:*)
-      if [ "${command_line_option#*:}" != "" ]; then
-        VM="${command_line_option#*:}"
-      fi
-    ;;
-    -s|--suppress)
-      PRINT_CLIENT_CONNECT=false
-    ;;
-    -h|--help)
-      USE_HELP=true
-      usage
-      return
-    ;;
-    -d|--debug)
-      USE_DEBUG=true
-    ;;    
-    start|stop|run)
-      CHE_SERVER_ACTION=${command_line_option}
-    ;;
-    *)
-      # unknown option
-      error_exit "!!! You passed an unknown command line option."
-    ;;
-  esac
+    case $command_line_option in
+      -a|--assembly)
+        ASSEMBLY=true
+      ;;
+      -s:*|--skip:*)
+        if [ "${command_line_option#*:}" != "" ]; then
+          case "${command_line_option#*:}" in
+            maven)
+              SKIP_MAVEN=true
+            ;;
+            deps)
+              SKIP_DEPENDENCIES=true
+            ;;
+            update)
+              SKIP_UPDATE=true
+            ;;
+            wsagent)
+              SKIP_WSAGENT=true
+            ;;
+            wsmaster)
+              SKIP_WSMASTER=true
+            ;;
+          esac
+        fi
+      ;;
+      -d|--debug)
+        USE_DEBUG=true
+      ;;
+      -h|--help)
+        USE_HELP=true
+        usage
+      ;;
+      *)
+        # unknown option
+        error_exit "!!! You passed an unknown command line option."
+      ;;
+    esac
   done
 
   if $USE_DEBUG; then
-    echo "USE_DOCKER: ${USE_DOCKER}"
-    echo "CHE_DOCKER_TAG: ${CHE_DOCKER_TAG}"
-    echo "CHE_PORT: ${CHE_PORT}"
-    echo "CHE_IP: \"${CHE_IP}\""
-    echo "CHE_DOCKER_MACHINE: ${VM}"
-    echo "PRINT_CLIENT_CONNECT: ${PRINT_CLIENT_CONNECT}"
+    echo "ASSEMBLY: ${ASSEMBLY}"
+    echo "SKIP_MAVEN: ${SKIP_MAVEN}"
+    echo "SKIP_DEPENDENCIES: ${SKIP_DEPENDENCIES}"
+    echo "SKIP_UPDATE: ${SKIP_UPDATE}"
+    echo "SKIP_WSMASTER: ${SKIP_WSMASTER}"
+    echo "SKIP_WSAGENT: ${SKIP_WSMAGENT}"
     echo "USE_HELP: ${USE_HELP}"
-    echo "CHE_SERVER_ACTION: ${CHE_SERVER_ACTION}"
     echo "USE_DEBUG: ${USE_DEBUG}"
   fi
 }
@@ -146,8 +149,8 @@ function set_environment_variables {
   SDK_DIR="${CHE_HOME}/sdk"
 
   PLUGIN_IDE_DIR="${PLUGIN_DIR}/ide"
-  PLUGIN_CHE_DIR="${PLUGIN_DIR}/che"
-  PLUGIN_WORKSPACE_DIR="${PLUGIN_DIR}/workspace"
+  PLUGIN_WSMASTER_DIR="${PLUGIN_DIR}/ws-master"
+  PLUGIN_WSAGENT_DIR="${PLUGIN_DIR}/ws-agent"
   
   # The IDE project that will be built with the extension
   PLUGIN_IDE_WAR_DIR="${SDK_DIR}/assembly-ide-war"
@@ -155,68 +158,156 @@ function set_environment_variables {
   # The machine web app project that will be built with the extension
   PLUGIN_MACHINE_WAR_DIR="${SDK_DIR}/assembly-machine-war"
 
+  # Creates the ws-agent.zip artifact for a Che assembly, which packages Tomcat + machine war into single package
+  PLUGIN_MACHINE_SERVER_DIR="${SDK_DIR}/assembly-machine-server"
+
+  # Generates a new Che assembly that contains new IDE, ws-master, and ws-agent.
+  PLUGIN_ASSEMBLY_DIR="${SDK_DIR}/assembly-main"
+
 }
 
+function echo_stage {
+
+    echo
+    echo "#####################################################################"
+    echo "$1"
+    echo "#####################################################################"
+    echo
+
+}
+
+init_global_variables
+parse_command_line "$@"
 set_environment_variables
 
-echo
-echo "#################################################################"
-echo "### CHE SDK: Installing each extension into local maven"
-echo "#################################################################"
-echo
+if [ "${USE_HELP}" == "false" ]; then
 
-# Install every 3rd-party extension into local Maven repository
-for file in $PLUGIN_DIR/*.jar
-do
+
+
+  if [ "${SKIP_MAVEN}" == "false" ]; then
+    echo_stage "CHE SDK: Installing each extension into local maven repository"
+  fi
+
+  # Install every 3rd-party extension into local Maven repository
+  for file in $PLUGIN_DIR/*.jar
+  do
     if [ -f $file ]; then
         cp $file $PLUGIN_IDE_DIR
         cp $file $PLUGIN_CHE_DIR
         cp $file $PLUGIN_WORKSPACE_DIR
-        mvn org.apache.maven.plugins:maven-install-plugin:2.5.1:install-file -Dfile=$file
+      
+        if [ "${SKIP_MAVEN}" == "false" ]; then
+          mvn org.apache.maven.plugins:maven-install-plugin:2.5.1:install-file -Dfile=$file
+        fi
     fi
-done
+  done
+  
 
-echo
-echo "#################################################################"
-echo "### CHE SDK: Adding client-side extensions as deps to IDE web app"
-echo "#################################################################"
-echo
+  if [ "${SKIP_DEPENDENCIES}" == "false" ]; then
 
-# Prepare to re-build Codenvy IDE
-#java -cp "${CHE_HOME}/lib/che-plugin-sdk-tools.jar" org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_MACHINE_WAR_DIR}" --extResourcesDir="${PLUGIN_MACHINE_WAR_DIR}/temp"
-java -cp c:/codenvy/che-plugins/plugin-sdk/che-plugin-sdk-tools/target/che-plugin-sdk-tools-4.0.0-beta-8-SNAPSHOT-jar-with-dependencies.jar org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_IDE_DIR}" --extResourcesDir="${PLUGIN_IDE_WAR_DIR}"
+    echo_stage "CHE SDK: Adding IDE extensions as dependencies"
 
-echo
-echo "#################################################################"
-echo "### CHE SDK: Adding server-side extensions as deps to IDE web app"
-echo "#################################################################"
-echo
+    # Prepare to re-build Codenvy IDE
+    java -cp "${CHE_HOME}/sdk/che-plugin-sdk-tools.jar" org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_IDE_DIR}" --extResourcesDir="${PLUGIN_IDE_WAR_DIR}"
+    #java -cp c:/codenvy/che-plugins/plugin-sdk/che-plugin-sdk-tools/target/che-plugin-sdk-tools-4.0.0-beta-8-SNAPSHOT-jar-with-dependencies.jar org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_IDE_DIR}" --extResourcesDir="${PLUGIN_IDE_WAR_DIR}"
 
-# Prepare to re-build Codenvy IDE
-#java -cp "${CHE_HOME}/lib/che-plugin-sdk-tools.jar" org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_MACHINE_WAR_DIR}" --extResourcesDir="${PLUGIN_MACHINE_WAR_DIR}/temp"
-java -cp c:/codenvy/che-plugins/plugin-sdk/che-plugin-sdk-tools/target/che-plugin-sdk-tools-4.0.0-beta-8-SNAPSHOT-jar-with-dependencies.jar org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_CHE_DIR}" --extResourcesDir="${PLUGIN_IDE_WAR_DIR}"
+    echo_stage "CHE SDK: Adding extensions as dependencies to ws-master"
 
-echo
-echo "#################################################################"
-echo "### CHE SDK: Compiling new IDE web app with your plugins"
-echo "#################################################################"
-echo
+    # Prepare to re-build Codenvy IDE
+    java -cp "${CHE_HOME}/sdk/che-plugin-sdk-tools.jar" org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_WSMASTER_DIR}" --extResourcesDir="${PLUGIN_IDE_WAR_DIR}"
+    #java -cp c:/codenvy/che-plugins/plugin-sdk/che-plugin-sdk-tools/target/che-plugin-sdk-tools-4.0.0-beta-8-SNAPSHOT-jar-with-dependencies.jar org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_CHE_DIR}" --extResourcesDir="${PLUGIN_IDE_WAR_DIR}"
 
-# Re-build the che web application with extensions from ide/ and che/ directories included. This artifact is deployed into Che server.
-cd "${PLUGIN_IDE_WAR_DIR}/temp"
-mvn sortpom:sort
-mvn -Denforcer.skip=true clean package -Dskip-validate-sources=true
-cd "${CHE_HOME}"
-cp "${PLUGIN_IDE_WAR_DIR}/temp/target/*.war tomcat/webapps/ide.war"
+    echo_stage "CHE SDK: Adding extensions as dependencies to ws-agent"
 
-# Re-build the machine web application with custom extension included from workspace/ directories included. This artifact is packaged into ws-agent.zip and deployed into workspace machine.
+    # Prepare to re-build Codenvy IDE
+    java -cp "${CHE_HOME}/sdk/che-plugin-sdk-tools.jar" org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_WSAGENT_DIR}" --extResourcesDir="${PLUGIN_MACHINE_WAR_DIR}"
+    #java -cp c:/codenvy/che-plugins/plugin-sdk/che-plugin-sdk-tools/target/che-plugin-sdk-tools-4.0.0-beta-8-SNAPSHOT-jar-with-dependencies.jar org.eclipse.che.ide.sdk.tools.InstallExtension --extDir="${PLUGIN_CHE_DIR}" --extResourcesDir="${PLUGIN_IDE_WAR_DIR}"
 
-echo
-echo "#################################################################"
-echo "### CHE SDK: Restart Che - it will start with your plugins"
-echo "#################################################################"
-echo
+  fi
 
-# cd ../..
-# cp $EXT_RES_WORK_DIR_REL_PATH/target/*.war webapps/che.war
-# rm -rf webapps/che
+  if [ "${SKIP_WSMASTER}" == "false" ]; then
+
+    echo_stage "CHE SDK: Compiling IDE & server-side plug-ins into new workspace master. ~5 minutes."
+      
+    # Re-build the che web application with extensions from ide/ and che/ directories included. This artifact is deployed into Che server.
+    cd "${PLUGIN_IDE_WAR_DIR}/temp"
+    mvn sortpom:sort
+    mvn -Denforcer.skip=true clean package install -Dskip-validate-sources=true
+    cd "${CHE_HOME}"
+
+    if [ "${SKIP_UPDATE}" == "false" ]; then
+      cp "${PLUGIN_IDE_WAR_DIR}/temp/target/*.war tomcat/webapps/ide.war"
+    fi
+  fi 
+
+  # Re-build the machine web application with custom extension included from workspace/ directories included. This artifact is packaged into ws-agent.zip and deployed into workspace machine.
+  if [ "${SKIP_WSAGENT}" == "false" ]; then
+
+    echo_stage "CHE SDK: Compiling ws-agent plug-ins into new workspace agent."
+      
+    # Re-build the che web application with extensions from ide/ and che/ directories included. This artifact is deployed into Che server.
+    cd "${PLUGIN_MACHINE_WAR_DIR}/temp"
+    mvn sortpom:sort
+    mvn -Denforcer.skip=true clean package install -Dskip-validate-sources=true
+
+    echo_stage "CHE SDK: Packaging ws-agent web app and Tomcat into ws-agent.zip."
+
+    cd "${PLUGIN_MACHINE_SERVER_DIR}"
+    mvn -Denforcer.skip=true clean package install -Dskip-validate-sources=true
+    cd "${CHE_HOME}"
+
+
+    if [ "${SKIP_UPDATE}" == "false" ]; then
+      cp "${PLUGIN_MACHINE_SERVER_DIR}/target/*.zip lib/ws-agent.zip"
+    fi
+
+  fi  
+
+  if [ "${ASSEMBLY}" == "true" ]; then
+    echo_stage "CHE SDK: Creating new distributable Che assembly in /plugins/assembly"
+
+    cd "${PLUGIN_ASSEMBLY_DIR}"
+    mvn -Denforcer.skip=true clean install -Dskip-validate-sources=true
+
+    cd "${CHE_HOME}"
+    rm -rf plugins/assembly
+    mkdir plugins/assembly
+    cp -r --target-directory=plugins/assembly "${PLUGIN_ASSEMBLY_DIR}"/target/eclipse-che*
+  fi  
+
+
+  echo 
+  echo "#####################################################################"
+  echo "Good Job! Your build has completed."
+  echo "#####################################################################"
+  echo 
+
+  if [ "${SKIP_WSMASTER}" == "false" ] && [ "${SKIP_UPDATE}" == "true" ]; then
+    echo "New Workspace Master Web App: 
+    ${PLUGIN_IDE_WAR_DIR}/temp/target/ide.war"
+  fi
+
+  if [ "${SKIP_WSMASTER}" == "false" ] && [ "${SKIP_UPDATE}" == "false" ]; then
+    echo "New Workspace Master Web App: 
+    ${CHE_HOME}/tomcat/webapps/ide.war"
+  fi
+
+  if [ "${SKIP_WSAGENT}" == "false" ] && [ "${SKIP_UPDATE}" == "true" ]; then
+    echo "New Workspace Agent: 
+    ${PLUGIN_MACHINE_SERVER_DIR}/target/*.zip"
+  fi
+
+  if [ "${SKIP_WSAGENT}" == "false" ] && [ "${SKIP_UPDATE}" == "false" ]; then
+    echo "New Workspace Agent: 
+    ${CHE_HOME}/lib/ws-agent.zip"
+  fi
+
+  if [ "${SKIP_UPDATE}" == "false" ]; then
+    echo "Restart this Che assembly to see your plugins."
+  fi
+
+  if [ "${ASSEMBLY}" == "true" ]; then
+    echo "New Che Assembly 
+    ${CHE_HOME}/plugins/assembly/"
+  fi
+fi
