660d58d51cb627c81ae33313c6564872a083032c
Florent BENOIT <fbenoit@codenvy.com>
2016-02-25 10:48:33 +0100

    CHE-603 allow to plug new route
    Change-Id: Iac866c734f71f35d405cced67feccb756ea44cea
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- dashboard/src/components/routing/route-history.service.js
+++ dashboard/src/components/routing/route-history.service.js
@@ -27,6 +27,14 @@ export class RouteHistory {
     });
   }
 
+  /**
+   * Add a new path on top of all existing paths
+   * @param path the path on which we will we redirecting when we pop current path
+   */
+  pushPath(path) {
+    this.history.push(path);
+  }
+
   getPaths() {
     return this.history;
   }
