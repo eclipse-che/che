f639877a9a9b9eec626d1a049bb342ae72722a20
Artem Zatsarynnyi <azatsarynnyy@codenvy.com>
2016-03-23 11:55:23 +0200

    Remove 'Create Maven module' feature

--- plugins/plugin-java/che-plugin-java-ext-maven/src/main/java/org/eclipse/che/ide/extension/maven/client/MavenExtension.java
+++ plugins/plugin-java/che-plugin-java-ext-maven/src/main/java/org/eclipse/che/ide/extension/maven/client/MavenExtension.java
@@ -19,7 +19,6 @@ import org.eclipse.che.api.workspace.shared.dto.ProjectConfigDto;
 import org.eclipse.che.ide.api.action.ActionManager;
 import org.eclipse.che.ide.api.action.DefaultActionGroup;
 import org.eclipse.che.ide.api.action.IdeActions;
-import org.eclipse.che.ide.api.constraints.Anchor;
 import org.eclipse.che.ide.api.constraints.Constraints;
 import org.eclipse.che.ide.api.event.project.ProjectReadyEvent;
 import org.eclipse.che.ide.api.event.project.ProjectReadyHandler;
@@ -33,7 +32,6 @@ import org.eclipse.che.ide.ext.java.client.dependenciesupdater.DependenciesUpdat
 import org.eclipse.che.ide.ext.java.client.project.node.JavaNodeManager;
 import org.eclipse.che.ide.extension.machine.client.machine.events.MachineStateEvent;
 import org.eclipse.che.ide.extension.machine.client.machine.events.MachineStateHandler;
-import org.eclipse.che.ide.extension.maven.client.actions.CreateMavenModuleAction;
 import org.eclipse.che.ide.extension.maven.client.actions.UpdateDependencyAction;
 import org.eclipse.che.ide.extension.maven.shared.MavenAttributes;
 import org.eclipse.che.ide.part.explorer.project.ProjectExplorerPresenter;
@@ -47,7 +45,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.eclipse.che.ide.api.action.IdeActions.GROUP_BUILD_CONTEXT_MENU;
-import static org.eclipse.che.ide.api.action.IdeActions.GROUP_FILE_NEW;
 
 /**
  * Maven extension entry point.
@@ -146,18 +143,13 @@ public class MavenExtension {
 
     @Inject
     private void prepareActions(ActionManager actionManager,
-                                UpdateDependencyAction updateDependencyAction,
-                                CreateMavenModuleAction createMavenModuleAction) {
+                                UpdateDependencyAction updateDependencyAction) {
         // register actions
         actionManager.registerAction("updateDependency", updateDependencyAction);
-        actionManager.registerAction("createMavenModule", createMavenModuleAction);
 
         // add actions in main menu
         DefaultActionGroup assistantGroup = (DefaultActionGroup)actionManager.getAction(IdeActions.GROUP_ASSISTANT);
         assistantGroup.add(updateDependencyAction, Constraints.LAST);
-
-        DefaultActionGroup newGroup = (DefaultActionGroup)actionManager.getAction(GROUP_FILE_NEW);
-        newGroup.add(createMavenModuleAction, new Constraints(Anchor.AFTER, "newJavaPackage"));
 
         // add actions in context menu
         DefaultActionGroup buildContextMenuGroup = (DefaultActionGroup)actionManager.getAction(GROUP_BUILD_CONTEXT_MENU);
