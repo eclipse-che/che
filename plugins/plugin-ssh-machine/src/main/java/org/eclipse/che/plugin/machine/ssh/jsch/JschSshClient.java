70e92f25ff41711424fd1b13a3cd7ac6b09d91cc
Alexander Garagatyi <agaragatyi@codenvy.com>
2016-04-18 21:25:32 +0300

    Fix websocket terminal launching in ssh machine

--- plugins/plugin-ssh-machine/src/main/java/org/eclipse/che/plugin/machine/ssh/jsch/JschSshClient.java
+++ plugins/plugin-ssh-machine/src/main/java/org/eclipse/che/plugin/machine/ssh/jsch/JschSshClient.java
@@ -211,14 +211,14 @@ public class JschSshClient implements SshClient {
             // apply permissions
             File file = new File(sourcePath);
             // read
-            int permissions = 4;
+            int permissions = 256;
             // execute
             if (file.canExecute()) {
-                permissions += 1;
+                permissions += 64;
             }
             // write
             if (file.canWrite()) {
-                permissions += 2;
+                permissions += 128;
             }
             channelSftp.chmod(permissions, absoluteTargetPath);
         } catch (SftpException e) {
