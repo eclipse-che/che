a5244958d564c30697c85712c589464fba23522e
Mykola Morhun <mmorhun@codenvy.com>
2016-04-20 09:34:36 +0300

    CODENVY-350: Set up workspaces to work behind the proxy
    Signed-off-by: Mykola Morhun <mmorhun@codenvy.com>

---
+++ plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/local/provider/HttpsProxyEnvVariableProvider.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.plugin.docker.machine.local.provider;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+
+/**
+ * Add env variable to docker environment with https proxy settings.
+ *
+ * @author Mykola Morhun
+ */
+public class HttpsProxyEnvVariableProvider implements Provider<String> {
+
+    private static final String HTTPS_PROXY = "https_proxy=";
+
+    @Inject
+    @Named("https.proxy")
+    private String httpsProxy;
+
+    @Override
+    public String get() {
+        return httpsProxy.isEmpty() ? "" : HTTPS_PROXY + httpsProxy;
+    }
+
+}
