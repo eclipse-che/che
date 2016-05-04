caeb5f38f82040a0e885dc86dc75260c93fcec55
Vitaly Parfonov <vparfonov@codenvy.com>
2016-05-04 15:24:21 +0300

    Rename share to shared
    Signed-off-by: Vitaly Parfonov <vparfonov@codenvy.com>

---
+++ wsagent/che-core-api-project-shared/src/main/java/org/eclipse/che/api/project/shared/dto/SourceEstimation.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.api.project.shared.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import org.eclipse.che.dto.shared.DTO;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author andrew00x
+ */
+@DTO
+public interface SourceEstimation {
+
+
+    /** Gets unique id of type of project. */
+    @ApiModelProperty(value = "type ID", position = 1)
+    String getType();
+
+    SourceEstimation withType(String type);
+
+
+    /** Gets attributes of this project. */
+    @ApiModelProperty(value = "Project attributes", position = 2)
+    Map<String, List<String>> getAttributes();
+
+
+    SourceEstimation withAttributes(Map<String, List<String>> attributes);
+
+    @ApiModelProperty(value = "if matched", position = 3)
+    boolean isMatched();
+
+    SourceEstimation withMatched(boolean matched);
+
+}
