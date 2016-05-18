1026c7e8d6d84d7ac48b3446a0b5b556b90b8ee5
Artem Zatsarynnyi <azatsarynnyy@codenvy.com>
2016-05-18 18:17:01 +0300

    CHE-1083: Refactor modules registration

--- plugins/plugin-maven/che-plugin-maven-server/src/main/java/org/eclipse/che/plugin/maven/server/inject/MavenModule.java
+++ plugins/plugin-maven/che-plugin-maven-server/src/main/java/org/eclipse/che/plugin/maven/server/inject/MavenModule.java
@@ -17,6 +17,8 @@ import com.google.inject.multibindings.Multibinder;
 import org.eclipse.che.api.project.server.handlers.ProjectHandler;
 import org.eclipse.che.api.project.server.type.ProjectTypeDef;
 import org.eclipse.che.api.project.server.type.ValueProviderFactory;
+import org.eclipse.che.inject.DynaModule;
+import org.eclipse.che.maven.server.MavenTerminal;
 import org.eclipse.che.plugin.maven.server.core.MavenProgressNotifier;
 import org.eclipse.che.plugin.maven.server.core.MavenServerNotifier;
 import org.eclipse.che.plugin.maven.server.core.MavenTerminalImpl;
@@ -26,15 +28,15 @@ import org.eclipse.che.plugin.maven.server.projecttype.MavenValueProviderFactory
 import org.eclipse.che.plugin.maven.server.projecttype.handler.ArchetypeGenerationStrategy;
 import org.eclipse.che.plugin.maven.server.projecttype.handler.GeneratorStrategy;
 import org.eclipse.che.plugin.maven.server.projecttype.handler.MavenProjectGenerator;
-import org.eclipse.che.plugin.maven.server.projecttype.handler.ProjectBecomeMavenHandler;
+import org.eclipse.che.plugin.maven.server.projecttype.handler.MavenProjectInitHandler;
 import org.eclipse.che.plugin.maven.server.rest.MavenServerService;
-import org.eclipse.che.maven.server.MavenTerminal;
 
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
 
 /** @author Artem Zatsarynnyi */
-// TODO: rework after new Project API
+@DynaModule
 public class MavenModule extends AbstractModule {
+
     @Override
     protected void configure() {
         newSetBinder(binder(), ValueProviderFactory.class).addBinding().to(MavenValueProviderFactory.class);
@@ -43,26 +45,14 @@ public class MavenModule extends AbstractModule {
 
         Multibinder<ProjectHandler> projectHandlerMultibinder = newSetBinder(binder(), ProjectHandler.class);
         projectHandlerMultibinder.addBinding().to(MavenProjectGenerator.class);
-//        projectHandlerMultibinder.addBinding().to(AddMavenModuleHandler.class);
-//        projectHandlerMultibinder.addBinding().to(RemoveMavenModuleHandler.class);
-        projectHandlerMultibinder.addBinding().to(ProjectBecomeMavenHandler.class);
-//        projectHandlerMultibinder.addBinding().to(ProjectHasBecomeMaven.class);
-//        projectHandlerMultibinder.addBinding().to(MavenProjectCreatedHandler.class);
+        projectHandlerMultibinder.addBinding().to(MavenProjectInitHandler.class);
 
         newSetBinder(binder(), GeneratorStrategy.class).addBinding().to(ArchetypeGenerationStrategy.class);
-//        bind(ClassPathBuilder.class).to(MavenClassPathBuilder.class).in(Singleton.class);
-
-//        Multibinder<VirtualFileFilter> multibinder = newSetBinder(binder(), VirtualFileFilter.class, Names.named("vfs.index_filter"));
-//        multibinder.addBinding().to(MavenTargetFilter.class);
-//        Multibinder<VirtualFileFilter> multibinder = newSetBinder(binder(), VirtualFileFilter.class, Names.named("vfs.index_filter"));
-//        multibinder.addBinding().to(MavenTargetFilter.class);
 
         bind(MavenTerminal.class).to(MavenTerminalImpl.class).in(Singleton.class);
         bind(MavenProgressNotifier.class).to(MavenServerNotifier.class).in(Singleton.class);
 
         bind(MavenServerService.class);
-
-//        bind(IWorkspace.class).toProvider(EclipseWorkspaceProvider.class).in(Singleton.class);
 
         bind(PomChangeListener.class).asEagerSingleton();
     }
