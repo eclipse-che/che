f28a2bbf974884c70365a927f97d8cf1d097d970
Sven Efftinge <sven.efftinge@typefox.io>
2016-05-31 08:52:13 +0200

    [Issue #1178] Added hooks to register content type and highlighting configuration for Orion editor.
    Signed-off-by: Sven Efftinge <sven.efftinge@typefox.io>

---
+++ plugins/plugin-orion/che-plugin-orion-editor/src/main/java/org/eclipse/che/ide/editor/orion/client/OrionContentTypeRegistrant.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.ide.editor.orion.client;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+import org.eclipse.che.ide.api.editor.filetype.MultipleMethodFileIdentifier;
+import org.eclipse.che.ide.api.editor.texteditor.EditorModule.EditorModuleReadyCallback;
+import org.eclipse.che.ide.editor.orion.client.jso.OrionCodeEditWidgetOverlay;
+import org.eclipse.che.ide.editor.orion.client.jso.OrionContentTypeOverlay;
+import org.eclipse.che.ide.editor.orion.client.jso.OrionHighlightingConfigurationOverlay;
+import org.eclipse.che.ide.editor.orion.client.jso.OrionServiceRegistryOverlay;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArrayString;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+public class OrionContentTypeRegistrant {
+
+    final MultipleMethodFileIdentifier fileTypeIdentifier;
+    final Provider<OrionCodeEditWidgetOverlay> codeEditWidgetProvider;
+    final OrionEditorModule editorModule;
+    
+    @Inject
+    public OrionContentTypeRegistrant(
+            final MultipleMethodFileIdentifier fileTypeIdentifier,
+            final Provider<OrionCodeEditWidgetOverlay> codeEditWidgetProvider,
+            final OrionEditorModule editorModule) {
+        this.fileTypeIdentifier = fileTypeIdentifier;
+        this.codeEditWidgetProvider = codeEditWidgetProvider;
+        this.editorModule = editorModule;
+    }
+
+    public void registerFileType(final OrionContentTypeOverlay contentType, final OrionHighlightingConfigurationOverlay config) {
+        // register content type and configure orion
+        JsArrayString extensions = contentType.getExtensions();
+        for (int i = 0; i < extensions.length(); i++) {
+            String extension = extensions.get(i);
+            fileTypeIdentifier.registerNewExtension(extension, newArrayList(contentType.getId()));
+        }
+        
+        editorModule.waitReady(new EditorModuleReadyCallback() {
+            
+            @Override
+            public void onEditorModuleReady() {
+                OrionServiceRegistryOverlay serviceRegistry = codeEditWidgetProvider.get().getServiceRegistry();
+                serviceRegistry.doRegisterService("orion.core.contenttype", JavaScriptObject.createObject(), contentType.toServiceObject());
+                serviceRegistry.doRegisterService("orion.edit.highlighter", JavaScriptObject.createObject(), config);
+            }
+            
+            @Override
+            public void onEditorModuleError() {
+            }
+        });
+        
+    }
+}
