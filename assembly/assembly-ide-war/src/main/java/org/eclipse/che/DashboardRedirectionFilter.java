9c68742d0aae943e921d96bca526b455cf7484b2
Max Shaposhnik <mshaposhnik@codenvy.com>
2016-06-08 14:27:51 +0300

    fix imports DashboardRedirectionFilter

--- assembly/assembly-ide-war/src/main/java/org/eclipse/che/DashboardRedirectionFilter.java
+++ assembly/assembly-ide-war/src/main/java/org/eclipse/che/DashboardRedirectionFilter.java
@@ -11,7 +11,12 @@
 package org.eclipse.che;
 
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
