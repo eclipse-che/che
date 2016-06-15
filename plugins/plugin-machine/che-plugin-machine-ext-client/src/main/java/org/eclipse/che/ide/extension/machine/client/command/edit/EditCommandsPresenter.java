a8e489face98b5236678a2c9b0be71703c2c4cb8
Vitaly Parfonov <vparfonov@codenvy.com>
2016-06-15 16:31:40 +0300

    CHE-1328: fix cheking on equals in case checking null and empty string. Code clean up
    Signed-off-by: Vitaly Parfonov <vparfonov@codenvy.com>

--- plugins/plugin-machine/che-plugin-machine-ext-client/src/main/java/org/eclipse/che/ide/extension/machine/client/command/edit/EditCommandsPresenter.java
+++ plugins/plugin-machine/che-plugin-machine-ext-client/src/main/java/org/eclipse/che/ide/extension/machine/client/command/edit/EditCommandsPresenter.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.che.ide.extension.machine.client.command.edit;
 
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
@@ -396,10 +397,9 @@ public class EditCommandsPresenter implements EditCommandsView.ActionDelegate, F
     }
 
     private String getPreviewUrlOrNull(CommandConfiguration configuration) {
-        if (configuration.getAttributes() != null) {
+        if (configuration.getAttributes() != null && configuration.getAttributes().containsKey(PREVIEW_URL_ATTR)) {
             return configuration.getAttributes().get(PREVIEW_URL_ATTR);
         }
-
         return null;
     }
 
@@ -540,7 +540,7 @@ public class EditCommandsPresenter implements EditCommandsView.ActionDelegate, F
         }
         return editedPage.isDirty()
                || !editedCommandOriginName.equals(view.getConfigurationName())
-               || !editedCommandOriginPreviewUrl.equals(view.getConfigurationPreviewUrl());
+               || !Strings.nullToEmpty(editedCommandOriginPreviewUrl).equals(Strings.nullToEmpty(view.getConfigurationPreviewUrl()));
     }
 
     private void fireConfigurationAdded(CommandConfiguration command) {
