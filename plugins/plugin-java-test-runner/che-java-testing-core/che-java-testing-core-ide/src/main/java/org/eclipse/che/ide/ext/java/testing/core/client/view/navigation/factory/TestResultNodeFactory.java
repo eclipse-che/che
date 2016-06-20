b9379ddd25c042948a672e839b379acc09a719bc
Mirage Abeysekara <mirage.12@cse.mrt.ac.lk>
2016-06-20 15:07:50 +0530

    formatted license header
    Signed-off-by: Mirage Abeysekara <mirage.12@cse.mrt.ac.lk>

--- plugins/plugin-java-test-runner/che-java-testing-core/che-java-testing-core-ide/src/main/java/org/eclipse/che/ide/ext/java/testing/core/client/view/navigation/factory/TestResultNodeFactory.java
+++ plugins/plugin-java-test-runner/che-java-testing-core/che-java-testing-core-ide/src/main/java/org/eclipse/che/ide/ext/java/testing/core/client/view/navigation/factory/TestResultNodeFactory.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.che.ide.ext.java.testing.core.client.view.navigation.factory;
 
 import com.google.inject.assistedinject.Assisted;
