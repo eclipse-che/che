e0b89db65226720215fc404d54635944a6088aab
Anatoliy Bazko <abazko@codenvy.com>
2016-06-23 17:35:33 +0300

    Small fixes

--- plugins/plugin-languageserver/che-plugin-languageserver-server/src/main/java/org/eclipse/che/plugin/languageserver/server/LanguageServerModule.java
+++ plugins/plugin-languageserver/che-plugin-languageserver-server/src/main/java/org/eclipse/che/plugin/languageserver/server/LanguageServerModule.java
@@ -3,7 +3,6 @@ package org.eclipse.che.plugin.languageserver.server;
 import com.google.inject.AbstractModule;
 
 import org.eclipse.che.inject.DynaModule;
-import org.eclipse.che.plugin.languageserver.server.csharp.CSharpLanguageServerRegistrant;
 import org.eclipse.che.plugin.languageserver.server.json.JsonLanguageServerRegistrant;
 import org.eclipse.che.plugin.languageserver.server.lsapi.PublishDiagnosticsParamsMessenger;
 
@@ -14,7 +13,7 @@ public class LanguageServerModule extends AbstractModule {
     protected void configure() {
         bind(FatJarBasedLanguageServerRegistrant.class);
         bind(JsonLanguageServerRegistrant.class).asEagerSingleton();
-        bind(CSharpLanguageServerRegistrant.class).asEagerSingleton();
+//        bind(CSharpLanguageServerRegistrant.class).asEagerSingleton();
         bind(LanguageRegistryService.class);
         bind(TextDocumentServiceImpl.class);
         bind(WorkspaceServiceImpl.class);
