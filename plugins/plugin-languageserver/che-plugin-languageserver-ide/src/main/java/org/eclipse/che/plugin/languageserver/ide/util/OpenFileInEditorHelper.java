cc437d75a9bb24239bfde4685caa2380fbf63d11
Evgen Vidolob <evidolob@codenvy.com>
2016-07-18 14:08:06 +0300

    Merge branch 'master' of github.com:eclipse/che into language-server
    # Conflicts:
    #	plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/resources/org/eclipse/che/ide/ext/java/Java.gwt.xml

--- plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/util/OpenFileInEditorHelper.java
+++ plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/util/OpenFileInEditorHelper.java
@@ -10,22 +10,22 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.languageserver.ide.util;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Strings;
 import com.google.gwt.core.client.Scheduler;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import org.eclipse.che.api.promises.client.Function;
-import org.eclipse.che.ide.api.data.tree.Node;
+import org.eclipse.che.api.promises.client.Operation;
+import org.eclipse.che.api.promises.client.OperationException;
+import org.eclipse.che.ide.api.app.AppContext;
 import org.eclipse.che.ide.api.editor.EditorAgent;
 import org.eclipse.che.ide.api.editor.EditorPartPresenter;
 import org.eclipse.che.ide.api.editor.OpenEditorCallbackImpl;
 import org.eclipse.che.ide.api.editor.text.TextRange;
 import org.eclipse.che.ide.api.editor.texteditor.TextEditorPresenter;
-import org.eclipse.che.ide.api.project.node.HasStorablePath;
-import org.eclipse.che.ide.api.resources.VirtualFile;
+import org.eclipse.che.ide.api.resources.File;
 import org.eclipse.che.ide.part.explorer.project.ProjectExplorerPresenter;
-import org.eclipse.che.ide.project.node.FileReferenceNode;
 import org.eclipse.che.ide.resource.Path;
 
 /**
@@ -37,15 +37,17 @@ public class OpenFileInEditorHelper {
 
     private final EditorAgent              editorAgent;
     private final ProjectExplorerPresenter projectExplorer;
+    private final AppContext               appContext;
 
     @Inject
     public OpenFileInEditorHelper(EditorAgent editorAgent,
-                                  ProjectExplorerPresenter projectExplorer) {
+                                  ProjectExplorerPresenter projectExplorer, AppContext appContext) {
         this.editorAgent = editorAgent;
         this.projectExplorer = projectExplorer;
+        this.appContext = appContext;
     }
 
-    public void openFile(String filePath, TextRange selectionRange) {
+    public void openFile(String filePath, final TextRange selectionRange) {
         if (Strings.isNullOrEmpty(filePath)) {
             return;
         }
@@ -57,45 +59,58 @@ public class OpenFileInEditorHelper {
             return;
         }
 
-        projectExplorer.getNodeByPath(new HasStorablePath.StorablePath(filePath))
-                       .then(selectNode())
-                       .then(openNode(selectionRange));
+//        projectExplorer.getNodeByPath(new HasStorablePath.StorablePath(filePath))
+//                       .then(selectNode())
+//                       .then(openNode(selectionRange));
+        appContext.getWorkspaceRoot().getFile(filePath).then(new Operation<Optional<File>>() {
+            @Override
+            public void apply(Optional<File> file) throws OperationException {
+                if (file.isPresent()) {
+                    editorAgent.openEditor(file.get(), new OpenEditorCallbackImpl() {
+                        @Override
+                        public void onEditorOpened(EditorPartPresenter editor) {
+                            fileOpened(editor, selectionRange);
+                        }
+                    });
+                }
+            }
+        });
     }
 
     public void openFile(String filePath) {
         openFile(filePath, null);
     }
 
-    private Function<Node, Node> openNode(final TextRange selectionRange) {
-        return new Function<Node, Node>() {
-            @Override
-            public Node apply(Node node) {
-                if (node instanceof FileReferenceNode) {
-                    openFile((VirtualFile)node, selectionRange);
-                }
-                return node;
-            }
-        };
-    }
-
-    private Function<Node, Node> selectNode() {
-        return new Function<Node, Node>() {
-            @Override
-            public Node apply(Node node){
-                projectExplorer.select(node, false);
-                return node;
-            }
-        };
-    }
-
-    private void openFile(VirtualFile result, final TextRange selectionRange) {
-        editorAgent.openEditor(result, new OpenEditorCallbackImpl() {
-            @Override
-            public void onEditorOpened(EditorPartPresenter editor) {
-                fileOpened(editor, selectionRange);
-            }
-        });
-    }
+//    private Function<Node, Node> openNode(final TextRange selectionRange) {
+//        return new Function<Node, Node>() {
+//            @Override
+//            public Node apply(Node node) {
+//                if (node instanceof FileReferenceNode) {
+//                    openFile((VirtualFile)node, selectionRange);
+//                }
+//                return node;
+//            }
+//        };
+//    }
+//
+//    private Function<Node, Node> selectNode() {
+//        return new Function<Node, Node>() {
+//            @Override
+//            public Node apply(Node node){
+//                projectExplorer.select(node, false);
+//                return node;
+//            }
+//        };
+//    }
+//
+//    private void openFile(VirtualFile result, final TextRange selectionRange) {
+//        editorAgent.openEditor(result, new OpenEditorCallbackImpl() {
+//            @Override
+//            public void onEditorOpened(EditorPartPresenter editor) {
+//                fileOpened(editor, selectionRange);
+//            }
+//        });
+//    }
 
     private void fileOpened(final EditorPartPresenter editor, final TextRange selectionRange) {
         if (editor instanceof TextEditorPresenter && selectionRange != null) {
