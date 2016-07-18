c811bbf0f258cb0db9d6852c8c05b60d376ca8c1
Anton Korneta <akorneta@codenvy.com>
2016-07-18 11:29:17 +0300

    fixup! rewrite preferences with @Transactional

--- wsmaster/che-core-api-user/src/test/java/org/eclipse/che/api/user/server/jpa/PreferenceJpaTckRepository.java
+++ wsmaster/che-core-api-user/src/test/java/org/eclipse/che/api/user/server/jpa/PreferenceJpaTckRepository.java
@@ -10,14 +10,16 @@
  *******************************************************************************/
 package org.eclipse.che.api.user.server.jpa;
 
+import com.google.inject.persist.Transactional;
+
 import org.eclipse.che.api.user.server.model.impl.UserImpl;
 import org.eclipse.che.commons.lang.Pair;
 import org.eclipse.che.commons.test.tck.repository.TckRepository;
 import org.eclipse.che.commons.test.tck.repository.TckRepositoryException;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
@@ -27,34 +29,27 @@ import java.util.Map;
  *
  * @author Anton Korneta
  */
+@Transactional
 public class PreferenceJpaTckRepository implements TckRepository<Pair<String, Map<String, String>>> {
 
     @Inject
-    private EntityManagerFactory factory;
+    private Provider<EntityManager> managerProvider;
 
     @Override
     public void createAll(Collection<? extends Pair<String, Map<String, String>>> entities) throws TckRepositoryException {
-        final EntityManager manager = factory.createEntityManager();
-
-        manager.getTransaction().begin();
-
+        final EntityManager manager = managerProvider.get();
         int i = 0;
         for (Pair<String, Map<String, String>> pair : entities) {
             manager.persist(new UserImpl(pair.first, "email_" + i, "name_" + i, "password", Collections.emptyList()));
             manager.persist(new PreferenceEntity(pair.first, pair.second));
             i++;
         }
-        manager.getTransaction().commit();
-        manager.close();
     }
 
     @Override
     public void removeAll() throws TckRepositoryException {
-        final EntityManager manager = factory.createEntityManager();
-        manager.getTransaction().begin();
+        final EntityManager manager = managerProvider.get();
         manager.createQuery("DELETE FROM Preference").executeUpdate();
         manager.createQuery("DELETE FROM User").executeUpdate();
-        manager.getTransaction().commit();
-        manager.close();
     }
 }
