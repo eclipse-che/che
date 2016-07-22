e77e4ba7ccc4f3c28b3d1483af660ceb91f03c8f
Alexander Garagatyi <agaragatyi@codenvy.com>
2016-07-22 12:04:38 +0300

    CHE-1369: fix JSON field fetching after fix on server-side
    Signed-off-by: Alexander Garagatyi <agaragatyi@codenvy.com>

--- dashboard/src/app/projects/create-project/create-project.controller.js
+++ dashboard/src/app/projects/create-project/create-project.controller.js
@@ -440,7 +440,7 @@ export class CreateProjectCtrl {
   getDisplayMachineLog(log) {
     log = angular.fromJson(log);
     if (angular.isObject(log)) {
-      return '[' + log.machine + '] ' + log.content;
+      return '[' + log.machineName + '] ' + log.content;
     } else {
       return log;
     }
