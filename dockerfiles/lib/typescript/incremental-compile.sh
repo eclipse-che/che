0be2173cce49eff9e4caa4bd4c84069d3d82fc9b
Florent BENOIT <fbenoit@codenvy.com>
2016-08-01 16:19:36 +0200

    Bunch of changes for allowing to build on CI with Docker/Linux
    Change-Id: I1e7c20bd6ff591692bd5e47dfee60094e60b1687
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- dockerfiles/lib/typescript/incremental-compile.sh
+++ dockerfiles/lib/typescript/incremental-compile.sh
@@ -1,4 +1,11 @@
-docker run --rm -v $(pwd):/usr/src/app -w /usr/src/app node:6  /bin/bash -c "/usr/src/app/dependencies/compile/node_modules/typescript/bin/tsc --outDir /usr/src/app/lib /usr/src/app/src/index.ts"
+#!/bin/sh
+# Copyright (c) 2012-2016 Codenvy, S.A., Red Hat, Inc
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+
+docker run --rm -v $(pwd):/usr/src/app -w /usr/src/app node:6  /bin/bash -c "/usr/src/app/dependencies/compile/node_modules/typescript/bin/tsc --outDir /usr/src/app/lib /usr/src/app/src/index.ts && groupadd user && useradd -g user user && (chown --silent -R user.user /usr/src/app || true)"
 
 if [ $? -eq 0 ]; then
     echo 'Compilation of TypeScript is OK'
