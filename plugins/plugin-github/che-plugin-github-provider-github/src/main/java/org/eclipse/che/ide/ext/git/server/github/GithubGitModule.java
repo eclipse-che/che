34593e127347b88f57634a6d219c75412e62d84e
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2016-08-04 10:04:54 +0300

    CHE-2033 Rename Plugin Github package

--- plugins/plugin-github/che-plugin-github-provider-github/src/main/java/org/eclipse/che/ide/ext/git/server/github/GithubGitModule.java
+++ plugins/plugin-github/che-plugin-github-provider-github/src/main/java/org/eclipse/che/ide/ext/git/server/github/GithubGitModule.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   Codenvy, S.A. - initial API and implementation
  *******************************************************************************/
-package org.eclipse.che.ide.ext.git.server.nativegit;
+package org.eclipse.che.ide.ext.git.server.github;
 
 import org.eclipse.che.api.git.CredentialsProvider;
 import org.eclipse.che.inject.DynaModule;
