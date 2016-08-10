7d01a7a3f0d019bed21b3e3d02d00328cd9d98ad
Florent BENOIT <fbenoit@codenvy.com>
2016-08-10 01:19:05 -0700

    Add bat scripts for #1895 allowing to compile typescript library and generate docker image
    Change-Id: Ib86083aaa151fe46de57bb02e8d23eb8d55830bc
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

---
+++ dockerfiles/lib/typescript/incremental-compile.bat
@@ -0,0 +1,12 @@
+@echo off
+echo "Incremental compiling from directory %cd%"
+docker run --rm -v %cd%:/usr/src/app -w /usr/src/app node:6 /bin/bash -c "/usr/src/app/dependencies/compile/node_modules/typescript/bin/tsc --outDir /usr/src/app/lib /usr/src/app/src/index.ts"
+if %ERRORLEVEL% == 0 goto :end
+
+:error
+echo "Typescript compilation failed"
+exit /b 1
+
+
+:end
+echo "Successfully compiled"
