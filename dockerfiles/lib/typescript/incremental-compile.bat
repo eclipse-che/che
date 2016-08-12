ac55628c32e297b65a61dca9997a15e7904bde04
Florent BENOIT <fbenoit@codenvy.com>
2016-08-12 19:19:27 +0200

    #1895 enable decorators support
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- dockerfiles/lib/typescript/incremental-compile.bat
+++ dockerfiles/lib/typescript/incremental-compile.bat
@@ -1,6 +1,6 @@
 @echo off
 echo "Incremental compiling from directory %cd%"
-docker run --rm -v %cd%:/usr/src/app -w /usr/src/app node:6 /bin/bash -c "/usr/src/app/dependencies/compile/node_modules/typescript/bin/tsc --outDir /usr/src/app/lib /usr/src/app/src/index.ts"
+docker run --rm -v %cd%:/usr/src/app -w /usr/src/app node:6 /bin/bash -c "/usr/src/app/dependencies/compile/node_modules/typescript/bin/tsc --target ES5 --experimentalDecorators --emitDecoratorMetadata --outDir /usr/src/app/lib /usr/src/app/src/index.ts"
 if %ERRORLEVEL% == 0 goto :end
 
 :error
