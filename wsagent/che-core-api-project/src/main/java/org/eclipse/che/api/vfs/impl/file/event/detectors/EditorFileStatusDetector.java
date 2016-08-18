c181fd49daa9e8521dd571d57b72e3550c4909e0
Dmitry Kuleshov <dkuleshov@codenvy.com>
2016-08-18 18:31:54 +0300

    che#1912: added file content checksum regisrty
    Signed-off-by: Dmitry Kuleshov <dkuleshov@codenvy.com>

--- wsagent/che-core-api-project/src/main/java/org/eclipse/che/api/vfs/impl/file/event/detectors/EditorFileStatusDetector.java
+++ wsagent/che-core-api-project/src/main/java/org/eclipse/che/api/vfs/impl/file/event/detectors/EditorFileStatusDetector.java
@@ -11,17 +11,20 @@
 package org.eclipse.che.api.vfs.impl.file.event.detectors;
 
 import com.google.common.annotations.Beta;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
 
+import org.eclipse.che.api.core.ForbiddenException;
+import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.core.jsonrpc.JsonRpcRequestReceiver;
 import org.eclipse.che.api.core.jsonrpc.JsonRpcRequestTransmitter;
 import org.eclipse.che.api.core.jsonrpc.shared.JsonRpcRequest;
-import org.eclipse.che.api.core.notification.EventService;
-import org.eclipse.che.api.core.notification.EventSubscriber;
-import org.eclipse.che.api.project.server.notification.ProjectItemModifiedEvent;
-import org.eclipse.che.api.project.server.notification.ProjectItemModifiedEvent.EventType;
 import org.eclipse.che.api.project.shared.dto.event.FileInEditorStatusDto;
 import org.eclipse.che.api.project.shared.dto.event.FileInVfsStatusDto;
 import org.eclipse.che.api.project.shared.dto.event.FileInVfsStatusDto.Status;
+import org.eclipse.che.api.project.shared.dto.event.FileWatcherEventType;
+import org.eclipse.che.api.vfs.Path;
+import org.eclipse.che.api.vfs.VirtualFileSystemProvider;
 import org.eclipse.che.api.vfs.impl.file.event.EventTreeNode;
 import org.eclipse.che.api.vfs.impl.file.event.HiEvent;
 import org.eclipse.che.api.vfs.impl.file.event.HiEventDetector;
@@ -29,20 +32,20 @@ import org.eclipse.che.dto.server.DtoFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
+import static java.nio.charset.Charset.defaultCharset;
 import static java.util.Optional.empty;
-import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 import static org.eclipse.che.api.project.shared.dto.event.FileInEditorStatusDto.Status.CLOSED;
 import static org.eclipse.che.api.project.shared.dto.event.FileInEditorStatusDto.Status.OPENED;
@@ -57,37 +60,19 @@ import static org.eclipse.che.dto.server.DtoFactory.newDto;
  */
 @Beta
 @Singleton
-public class EditorFileStatusDetector implements HiEventDetector<FileInVfsStatusDto>,
-                                                 JsonRpcRequestReceiver,
-                                                 EventSubscriber<ProjectItemModifiedEvent> {
-
+public class EditorFileStatusDetector implements HiEventDetector<FileInVfsStatusDto>, JsonRpcRequestReceiver {
     private static final Logger LOG = LoggerFactory.getLogger(EditorFileStatusDetector.class);
 
-    private final Map<String, Set<Integer>> endpointRegistry = new HashMap<>();
-    private final Set<String>                skipping         = new HashSet<>();
+    private final Receiver     receiver     = new Receiver();
+    private final FileRegistry fileRegistry = new FileRegistry();
 
-    private final ProjectItemModifiedEventSubscriber subscriber = new ProjectItemModifiedEventSubscriber();
-    private final NotificationReceiver               receiver   = new NotificationReceiver();
-
+    private final VirtualFileSystemProvider vfsProvider;
     private final JsonRpcRequestTransmitter transmitter;
-    private final EventService              eventService;
 
     @Inject
-    public EditorFileStatusDetector(JsonRpcRequestTransmitter transmitter, EventService eventService) {
+    public EditorFileStatusDetector(VirtualFileSystemProvider vfsProvider, JsonRpcRequestTransmitter transmitter) {
+        this.vfsProvider = vfsProvider;
         this.transmitter = transmitter;
-        this.eventService = eventService;
-    }
-
-    @PostConstruct
-    public void subscribe() {
-        LOG.debug("Subscribing {}", ProjectItemModifiedEventSubscriber.class);
-        eventService.subscribe(subscriber);
-    }
-
-    @PreDestroy
-    public void unsubscribe() {
-        LOG.debug("Unsubscribing {}", ProjectItemModifiedEventSubscriber.class);
-        eventService.unsubscribe(subscriber);
     }
 
     @Override
@@ -95,44 +80,39 @@ public class EditorFileStatusDetector implements HiEventDetector<FileInVfsStatus
         if (!eventTreeNode.isRoot() || eventTreeNode.getChildren().isEmpty()) {
             return empty();
         }
-
         final Set<EventTreeNode> files = eventTreeNode.stream()
                                                       .filter(EventTreeNode::modificationOccurred)
                                                       .filter(EventTreeNode::isFile)
-                                                      .filter(event -> endpointRegistry.keySet().contains(event.getPath()))
+                                                      .filter(event -> fileRegistry.isTracked(event.getPath()))
                                                       .collect(toSet());
 
-        transmit(files.stream().filter(event -> MODIFIED == event.getLastEventType()).collect(toList()),
-                 UPDATED,
-                 true);
-
-        transmit(files.stream().filter(event -> DELETED == event.getLastEventType()).collect(toList()),
-                 REMOVED,
-                 false);
+        filterAndTransmit(files, UPDATED);
+        filterAndTransmit(files, REMOVED);
 
         return Optional.empty();
     }
 
-    private void transmit(List<EventTreeNode> files, Status status, boolean checkForSkipping) {
-        files.forEach(file -> {
-            final String path = file.getPath();
+    private void filterAndTransmit(Set<EventTreeNode> files, Status status) {
+        final FileWatcherEventType type = status == UPDATED ? MODIFIED : DELETED;
 
-            if (checkForSkipping && skipping.remove(path)) {
-                return;
-            }
-
-            final JsonRpcRequest request = newDto(JsonRpcRequest.class)
-                    .withMethod("event:file-in-vfs-status-changed")
-                    .withJsonrpc("2.0")
-                    .withParams(newDto(FileInVfsStatusDto.class).withPath(path).withStatus(status).toString());
-
-            endpointRegistry.get(path).forEach(endpoint -> transmitter.transmit(request, endpoint));
-        });
+        files.stream()
+             .filter(event -> type == event.getLastEventType())
+             .forEach(file -> prepareAndTransmit(file, status));
     }
 
-    @Override
-    public void onEvent(ProjectItemModifiedEvent event) {
-        subscriber.onEvent(event);
+    private void prepareAndTransmit(EventTreeNode file, Status status) {
+        final String path = file.getPath();
+
+        final JsonRpcRequest request = newDto(JsonRpcRequest.class)
+                .withMethod("event:file-in-vfs-status-changed")
+                .withJsonrpc("2.0")
+                .withParams(newDto(FileInVfsStatusDto.class).withPath(path).withStatus(status).toString());
+
+        final boolean contentUpdated = fileRegistry.updateHash(path);
+
+        if (contentUpdated) {
+            fileRegistry.getEndpoints(path).forEach(endpoint -> transmitter.transmit(request, endpoint));
+        }
     }
 
     @Override
@@ -140,46 +120,79 @@ public class EditorFileStatusDetector implements HiEventDetector<FileInVfsStatus
         receiver.receive(request, endpoint);
     }
 
-    private class NotificationReceiver implements JsonRpcRequestReceiver {
-
+    private class Receiver implements JsonRpcRequestReceiver {
         @Override
         public void receive(JsonRpcRequest request, Integer endpoint) {
             final FileInEditorStatusDto dto = DtoFactory.getInstance().createDtoFromJson(request.getParams(), FileInEditorStatusDto.class);
             final FileInEditorStatusDto.Status status = dto.getStatus();
             final String path = dto.getPath();
 
-            skipping.remove(path);
-
             if (status == OPENED) {
-                track(endpoint, path);
+                fileRegistry.add(path, endpoint);
             } else if (status == CLOSED) {
-                untrack(endpoint, path);
+                fileRegistry.remove(path, endpoint);
             }
-        }
-
-        private void track(int endpoint, String path) {
-            Optional.ofNullable(endpointRegistry.get(path))
-                    .orElseGet(() -> {
-                        final Set<Integer> endpoints = new HashSet<>();
-                        endpointRegistry.put(path, endpoints);
-                        return endpoints;
-                    })
-                    .add(endpoint);
-
-        }
-
-        private void untrack(int endpoint, String path) {
-            Optional.ofNullable(endpointRegistry.get(path))
-                    .ifPresent(endpoints -> endpoints.remove(endpoint));
         }
     }
 
-    private class ProjectItemModifiedEventSubscriber implements EventSubscriber<ProjectItemModifiedEvent> {
-        @Override
-        public void onEvent(ProjectItemModifiedEvent event) {
-            if (EventType.UPDATED == event.getType()) {
-                skipping.add(event.getPath());
+    private class FileRegistry {
+        private final Map<String, HashCode>      hashRegistry     = new HashMap<>();
+        private final Map<String, List<Integer>> endpointRegistry = new HashMap<>();
+
+        private void add(String path, int endpoint) {
+            List<Integer> endpoints = endpointRegistry.get(path);
+
+            if (endpoints == null) {
+                endpoints = new LinkedList<>();
+                endpointRegistry.put(path, endpoints);
+                hashRegistry.put(path, getHash(path));
             }
+
+            endpoints.add(endpoint);
+        }
+
+        private void remove(String path, int endpoint) {
+            final List<Integer> endpoints = endpointRegistry.get(path);
+
+            if (endpoints == null) {
+                return;
+            }
+
+            endpoints.remove((Integer)endpoint);
+
+            if (endpoints.isEmpty()) {
+                hashRegistry.remove(path);
+                endpointRegistry.remove(path);
+            }
+        }
+
+        private boolean updateHash(String path) {
+            final HashCode newHash = getHash(path);
+            final HashCode oldHash = hashRegistry.put(path, newHash);
+
+            return !Objects.equals(oldHash, newHash);
+        }
+
+        private boolean isTracked(String path) {
+            return endpointRegistry.keySet().contains(path);
+        }
+
+        private Set<Integer> getEndpoints(String path) {
+            return Collections.unmodifiableSet(new HashSet<>(endpointRegistry.get(path)));
+        }
+
+        private HashCode getHash(String path) {
+            try {
+                final String content = vfsProvider.getVirtualFileSystem()
+                                                  .getRoot()
+                                                  .getChild(Path.of(path))
+                                                  .getContentAsString();
+
+                return Hashing.sha1().hashString(content, defaultCharset());
+            } catch (ServerException | ForbiddenException e) {
+                LOG.error("Error trying to read {} file and broadcast it", path, e);
+            }
+            return null;
         }
     }
 }
