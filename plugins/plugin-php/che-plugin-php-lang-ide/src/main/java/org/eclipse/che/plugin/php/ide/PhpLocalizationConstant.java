43265118a8cf0f933ac4c1903476a068cdb49fca
Kaloyan Raev <kaloyan.r@zend.com>
2016-08-18 13:55:54 +0300

    Basic PHP plugin
    This is the first step of providing better PHP language support in Che.
    This basic plugin is created after the existing Python plugin and
    provides the PHP project type and the "New PHP File" action.
    
    Signed-off-by: Kaloyan Raev <kaloyan.r@zend.com>

---
+++ plugins/plugin-php/che-plugin-php-lang-ide/src/main/java/org/eclipse/che/plugin/php/ide/PhpLocalizationConstant.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.plugin.php.ide;
+
+import com.google.gwt.i18n.client.Messages;
+
+/**
+ * Localization constants. Interface to represent the constants defined in resource bundle:
+ * 'PhpLocalizationConstant.properties'.
+ *
+ * @author Kaloyan Raev
+ */
+public interface PhpLocalizationConstant extends Messages {
+
+
+    @Key("php.action.create.php.file.title")
+    @DefaultMessage("PHP File")
+    String createPhpFileActionTitle();
+
+    @Key("php.action.create.php.file.description")
+    @DefaultMessage("Create PHP File")
+    String createPhpFileActionDescription();
+
+}
