1e337f57c433fada415444ad925d2358d3a0a0aa
Florent BENOIT <fbenoit@codenvy.com>
2016-08-19 08:35:41 +0200

    #1895 use of static constants Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- dockerfiles/lib/typescript/src/log.ts
+++ dockerfiles/lib/typescript/src/log.ts
@@ -21,16 +21,16 @@ export class Log {
     static logger : Log;
     static disabledPrefix : boolean = false;
 
-    BLUE: string = '\u001b[34m';
-    GREEN : string = '\u001b[32m';
-    RED : string = '\u001b[31m';
-    ORANGE: string = '\u001b[33m';
-    NC : string = '\u001b[39m';
+    static BLUE: string = '\u001b[34m';
+    static GREEN : string = '\u001b[32m';
+    static RED : string = '\u001b[31m';
+    static ORANGE: string = '\u001b[33m';
+    static NC : string = '\u001b[39m';
 
-    INFO_PREFIX: string = this.GREEN +  'INFO:' + this.NC;
-    DEBUG_PREFIX: string = this.BLUE +  'DEBUG:' + this.NC;
-    WARN_PREFIX: string = this.ORANGE +  'WARN:' + this.NC;
-    ERROR_PREFIX: string = this.RED +  'ERROR:' + this.NC;
+    static INFO_PREFIX: string = Log.GREEN +  'INFO:' + Log.NC;
+    static DEBUG_PREFIX: string = Log.BLUE +  'DEBUG:' + Log.NC;
+    static WARN_PREFIX: string = Log.ORANGE +  'WARN:' + Log.NC;
+    static ERROR_PREFIX: string = Log.RED +  'ERROR:' + Log.NC;
     
     static getLogger() : Log {
         if (!Log.logger) {
@@ -60,13 +60,13 @@ export class Log {
     log(type: LogType, message: string, optional: Array<any>) {
         var prefix: String;
         if ('info' === type) {
-            prefix = this.INFO_PREFIX;
+            prefix = Log.INFO_PREFIX;
         } else if ('debug' === type) {
-            prefix = this.DEBUG_PREFIX;
+            prefix = Log.DEBUG_PREFIX;
         } else if ('warn' === type) {
-            prefix = this.WARN_PREFIX;
+            prefix = Log.WARN_PREFIX;
         } else if ('error' === type) {
-            prefix = this.ERROR_PREFIX;
+            prefix = Log.ERROR_PREFIX;
         }
 
         if (Log.context) {
