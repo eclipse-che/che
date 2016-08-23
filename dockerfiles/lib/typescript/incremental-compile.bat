d4462f59ad47ce273dd3dfc2be276df0e9e7de9c
Florent BENOIT <fbenoit@codenvy.com>
2016-08-23 11:58:50 +0200

    #1895 add missing commands for windows
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- dockerfiles/lib/typescript/incremental-compile.bat
+++ dockerfiles/lib/typescript/incremental-compile.bat
@@ -1,6 +1,6 @@
 @echo off
 echo "Incremental compiling from directory %cd%"
-docker run --rm -v %cd%:/usr/src/app -w /usr/src/app node:6 /bin/bash -c "/usr/src/app/dependencies/compile/node_modules/typescript/bin/tsc --target ES5 --experimentalDecorators --emitDecoratorMetadata --outDir /usr/src/app/lib /usr/src/app/src/index.ts"
+docker run --rm -v %cd%:/usr/src/app -w /usr/src/app node:6 /bin/bash -c "cd /usr/src/app/src && find . -name "*.properties" -exec install -D {} /usr/src/app/lib/{} \; && cd /usr/src/app && /usr/src/app/dependencies/compile/node_modules/typescript/bin/tsc --target ES5 --experimentalDecorators --emitDecoratorMetadata --outDir /usr/src/app/lib /usr/src/app/src/index.ts"
 if %ERRORLEVEL% == 0 goto :end
 
 :error
