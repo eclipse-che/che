543eeb40f60e97055821cb61a17373cdf88e7e41
RomanNikitenko <rnikitenko@codenvy.com>
2016-09-12 16:02:41 +0300

    CHE-1232. Add mapper for RuntimeException (#1547)
    Signed-off-by: Roman Nikitenko <rnikitenko@codenvy.com>

---
+++ core/che-core-api-core/src/test/java/org/eclipse/che/api/core/rest/RuntimeExceptionMapperTest.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.api.core.rest;
+
+import org.everrest.assured.EverrestJetty;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import static com.jayway.restassured.RestAssured.expect;
+
+@Listeners(value = {EverrestJetty.class})
+public class RuntimeExceptionMapperTest {
+    @Path("/runtime-exception")
+    public static class RuntimeExceptionService {
+        @GET
+        @Path("/re-empty-msg")
+        public String reWithEmptyMessage() {
+            throw new NullPointerException();
+        }
+    }
+
+    RuntimeExceptionService service;
+
+    RuntimeExceptionMapper mapper;
+
+    @Test
+    public void shouldHandleRuntimeException() {
+        final String expectedErrorMessage = "{\"message\":\"Internal Server Error occurred, error time:";
+
+        expect()
+                .statusCode(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
+                .body(Matchers.startsWith(expectedErrorMessage))
+                .when().get("/runtime-exception/re-empty-msg");
+    }
+}
