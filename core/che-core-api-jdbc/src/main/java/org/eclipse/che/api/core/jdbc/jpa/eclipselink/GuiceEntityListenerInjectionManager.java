b8f0e147f6508ed5f13b52287a2690fe6533fe44
Yevhenii Voevodin <evoevodin@codenvy.com>
2016-09-19 18:38:55 +0300

    Initialize Jpa before EntityListenerInjectionManager

--- core/che-core-api-jdbc/src/main/java/org/eclipse/che/api/core/jdbc/jpa/eclipselink/GuiceEntityListenerInjectionManager.java
+++ core/che-core-api-jdbc/src/main/java/org/eclipse/che/api/core/jdbc/jpa/eclipselink/GuiceEntityListenerInjectionManager.java
@@ -14,6 +14,7 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
 
+import org.eclipse.che.api.core.jdbc.jpa.guice.JpaInitializer;
 import org.eclipse.persistence.internal.sessions.AbstractSession;
 import org.eclipse.persistence.internal.sessions.cdi.EntityListenerInjectionManager;
 
@@ -45,6 +46,9 @@ import javax.naming.NamingException;
  */
 @Singleton
 public class GuiceEntityListenerInjectionManager implements EntityListenerInjectionManager {
+
+    @Inject
+    private JpaInitializer jpaInitializer;
 
     @Inject
     private Injector injector;
