55fbae1a287dda97c0dc1a4b995a8b2b45797486
Florent BENOIT <fbenoit@codenvy.com>
2016-09-28 15:36:14 +0200

    - Add integration tests that test the TypeScript generated DTOs - Add toJson() method on DTO - Add support of constructor with empty object (like new MyDTOImpl().withName("hello")) - Integration tests can be disabled with -DskipIntegrationTests flag
    Change-Id: I3bcc2de6bb549ef8e29a266270ebcf0735e696d1
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- core/che-core-typescript-dto-maven-plugin/src/test/java/org/eclipse/che/plugin/typescript/dto/MyOtherDTO.java
+++ core/che-core-typescript-dto-maven-plugin/src/test/java/org/eclipse/che/plugin/typescript/dto/MyOtherDTO.java
@@ -12,28 +12,14 @@ package org.eclipse.che.plugin.typescript.dto;
 
 import org.eclipse.che.dto.shared.DTO;
 
-import java.util.Map;
-
 /**
  * @author Florent Benoit
  */
 @DTO
-public interface MyCustomDTO {
+public interface MyOtherDTO {
 
     String getName();
     void setName(String name);
-    MyCustomDTO withName(String name);
-
-    MyOtherDTO getConfig();
-    MyCustomDTO withConfig(MyOtherDTO otherDTO);
-    void setConfig(MyOtherDTO otherDTO);
-
-    void setStatus(Status status);
-    MyCustomDTO withStatus(Status status);
-    Status getStatus();
-
-    Map<String, MyOtherDTO> getCustomMap();
-    void setCustomMap(Map<String, MyOtherDTO> map);
-    MyCustomDTO withCustomMap(Map<String, MyOtherDTO> map);
+    MyOtherDTO withName(String name);
 
 }
