88ce750fdb096fa8ff9367fbd1b0929456a5c274
Vitalii Parfonov <vparfonov@codenvy.com>
2016-10-05 16:31:50 +0300

    Add default content for PHP project
    Signed-off-by: Vitalii Parfonov <vparfonov@codenvy.com>

--- plugins/plugin-php/che-plugin-php-lang-server/src/main/java/org/eclipse/che/plugin/php/inject/PhpModule.java
+++ plugins/plugin-php/che-plugin-php-lang-server/src/main/java/org/eclipse/che/plugin/php/inject/PhpModule.java
@@ -14,10 +14,14 @@ import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 
 import org.eclipse.che.api.languageserver.launcher.LanguageServerLauncher;
+import org.eclipse.che.api.project.server.handlers.ProjectHandler;
 import org.eclipse.che.api.project.server.type.ProjectTypeDef;
 import org.eclipse.che.inject.DynaModule;
 import org.eclipse.che.plugin.php.languageserver.PhpLanguageServerLauncher;
+import org.eclipse.che.plugin.php.projecttype.PhpProjectGenerator;
 import org.eclipse.che.plugin.php.projecttype.PhpProjectType;
+
+import static com.google.inject.multibindings.Multibinder.newSetBinder;
 
 /**
  * @author Kaloyan Raev
@@ -28,6 +32,9 @@ public class PhpModule extends AbstractModule {
     protected void configure() {
         Multibinder<ProjectTypeDef> projectTypeMultibinder = Multibinder.newSetBinder(binder(), ProjectTypeDef.class);
         projectTypeMultibinder.addBinding().to(PhpProjectType.class);
+
+        Multibinder<ProjectHandler> projectHandlerMultibinder = newSetBinder(binder(), ProjectHandler.class);
+        projectHandlerMultibinder.addBinding().to(PhpProjectGenerator.class);
 
         Multibinder.newSetBinder(binder(), LanguageServerLauncher.class).addBinding().to(PhpLanguageServerLauncher.class);
     }
