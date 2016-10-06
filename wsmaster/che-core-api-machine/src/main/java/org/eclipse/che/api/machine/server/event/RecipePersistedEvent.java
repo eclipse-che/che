40892068735b4c32bbb11960fb5a0037931c802a
Anton Korneta <akorneta@codenvy.com>
2016-10-06 16:17:38 +0300

    Add recipe persisted event

---
+++ wsmaster/che-core-api-machine/src/main/java/org/eclipse/che/api/machine/server/event/RecipePersistedEvent.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.api.machine.server.event;
+
+import org.eclipse.che.api.core.notification.EventOrigin;
+import org.eclipse.che.api.machine.shared.ManagedRecipe;
+
+/**
+ * Published after recipe instance is persisted.
+ *
+ * @author Anton Korneta.
+ */
+@EventOrigin("recipe")
+public class RecipePersistedEvent {
+    private final ManagedRecipe recipe;
+
+    public RecipePersistedEvent(ManagedRecipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public ManagedRecipe getRecipe() {
+        return recipe;
+    }
+}
