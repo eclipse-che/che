dfa28dd85950b989775a300e1342d0c90db8534e
Mario Loriedo <mloriedo@redhat.com>
2016-10-10 14:43:42 +0200

    Add build args support in compose files (CHE-2661)
    Signed-off-by: Mario Loriedo <mloriedo@redhat.com>

--- plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/MachineProviderImpl.java
+++ plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/MachineProviderImpl.java
@@ -402,7 +402,8 @@ public class MachineProviderImpl implements MachineInstanceProvider {
                             .withAuthConfigs(dockerCredentials.getCredentials())
                             .withDoForcePull(doForcePullOnBuild)
                             .withMemoryLimit(service.getMemLimit())
-                            .withMemorySwapLimit(-1);
+                            .withMemorySwapLimit(-1)
+                            .withBuildArgs(service.getBuild().getArgs());
 
             docker.buildImage(buildImageParams, progressMonitor);
         } catch (IOException e) {
