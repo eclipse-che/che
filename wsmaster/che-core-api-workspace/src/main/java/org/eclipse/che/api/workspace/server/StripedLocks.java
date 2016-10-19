6dbe14bde0ac2e965cbbdc68c69840e6e1b5d127
Alexander Garagatyi <agaragatyi@codenvy.com>
2016-10-19 10:32:09 -0600

    Fix locking in StrippedLocks class

--- wsmaster/che-core-api-workspace/src/main/java/org/eclipse/che/api/workspace/server/StripedLocks.java
+++ wsmaster/che-core-api-workspace/src/main/java/org/eclipse/che/api/workspace/server/StripedLocks.java
@@ -95,12 +95,12 @@ public class StripedLocks {
 
         private WriteLock(String key) {
             this.key = key;
-            striped.get(key).readLock().lock();
+            striped.get(key).writeLock().lock();
         }
 
         @Override
         public void close() {
-            striped.get(key).readLock().unlock();
+            striped.get(key).writeLock().unlock();
         }
     }
 
