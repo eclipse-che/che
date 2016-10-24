71f057f3c1fbbd274771f4b950912a7fd50c3acf
Bartlomiej Laczkowski <bartlomiej.l@zend.com>
2016-10-24 23:47:39 +0200

    Renamed packages, improved "get file contents" engine request & overall connection handling.
    Signed-off-by: Bartlomiej Laczkowski <bartlomiej.l@zend.com>

---
+++ plugins/plugin-php-zend-debugger/che-plugin-php-zend-debugger-ide/src/main/java/org/eclipse/che/plugin/php/zdb/ide/ZendDebugger.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Rogue Wave Software, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Rogue Wave Software, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.plugin.php.zdb.ide;
+
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+
+import org.eclipse.che.api.debug.shared.model.Location;
+import org.eclipse.che.ide.api.debug.BreakpointManager;
+import org.eclipse.che.ide.api.debug.DebuggerServiceClient;
+import org.eclipse.che.ide.api.filetypes.FileTypeRegistry;
+import org.eclipse.che.ide.api.resources.VirtualFile;
+import org.eclipse.che.ide.debug.DebuggerDescriptor;
+import org.eclipse.che.ide.debug.DebuggerManager;
+import org.eclipse.che.ide.dto.DtoFactory;
+import org.eclipse.che.ide.util.storage.LocalStorageProvider;
+import org.eclipse.che.ide.websocket.MessageBusProvider;
+import org.eclipse.che.plugin.debugger.ide.debug.AbstractDebugger;
+import org.eclipse.che.plugin.debugger.ide.fqn.FqnResolver;
+import org.eclipse.che.plugin.debugger.ide.fqn.FqnResolverFactory;
+import org.eclipse.che.plugin.php.zdb.ide.configuration.ZendDbgConfigurationType;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+/**
+ * Zend PHP debugger.
+ *
+ * @author Bartlomiej Laczkowski
+ */
+public class ZendDebugger extends AbstractDebugger {
+
+	public static final String ID = "zend-debugger";
+
+	public final FqnResolverFactory fqnResolverFactory;
+	public final FileTypeRegistry fileTypeRegistry;
+
+	@Inject
+	public ZendDebugger(DebuggerServiceClient service, DtoFactory dtoFactory, LocalStorageProvider localStorageProvider,
+			MessageBusProvider messageBusProvider, EventBus eventBus, FqnResolverFactory fqnResolverFactory,
+			ZendDbgFileHandler zendDebuggerFileHandler, DebuggerManager debuggerManager,
+			FileTypeRegistry fileTypeRegistry, BreakpointManager breakpointManager) {
+		super(service, dtoFactory, localStorageProvider, messageBusProvider, eventBus, zendDebuggerFileHandler,
+				debuggerManager, breakpointManager, ID);
+		this.fqnResolverFactory = fqnResolverFactory;
+		this.fileTypeRegistry = fileTypeRegistry;
+	}
+
+	@Override
+	protected String fqnToPath(@NotNull Location location) {
+		String resourcePath = location.getResourcePath();
+		return resourcePath != null ? resourcePath : location.getTarget();
+	}
+
+	@Override
+	protected String pathToFqn(VirtualFile file) {
+		String fileExtension = fileTypeRegistry.getFileTypeByFile(file).getExtension();
+		FqnResolver resolver = fqnResolverFactory.getResolver(fileExtension);
+		if (resolver != null) {
+			return resolver.resolveFqn(file);
+		}
+		return null;
+	}
+
+	@Override
+	protected DebuggerDescriptor toDescriptor(Map<String, String> connectionProperties) {
+		return new DebuggerDescriptor("Zend Debugger", "Zend Debugger client, port: "
+				+ connectionProperties.get(ZendDbgConfigurationType.ATTR_DEBUG_PORT));
+	}
+
+}
---
+++ plugins/plugin-php-zend-debugger/che-plugin-php-zend-debugger-server/src/main/java/org/eclipse/che/plugin/php/zdb/server/utils/ZendDbgUtils.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Rogue Wave Software, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Rogue Wave Software, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.plugin.php.zdb.server.utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.project.server.ProjectManager;
+import org.eclipse.che.api.project.server.VirtualFileEntry;
+import org.eclipse.che.api.vfs.Path;
+import org.eclipse.che.api.vfs.search.QueryExpression;
+import org.eclipse.che.api.vfs.search.SearchResult;
+import org.eclipse.che.api.vfs.search.Searcher;
+import org.eclipse.che.plugin.php.zdb.server.ZendDebugger;
+
+import com.google.inject.Singleton;
+
+/**
+ * Zend debug utils.
+ * 
+ * @author Bartlomiej Laczkowski
+ */
+@Singleton
+public class ZendDbgUtils {
+
+	private static ProjectManager projectManager;
+
+	public static final String[] SUPER_GLOBAL_NAMES = new String[] { "$GLOBALS", "$_SERVER", "$_GET", "$_POST",
+			"$_FILES", "$_COOKIE", "$_SESSION", "$_REQUEST", "$_ENV" };
+
+	public static final String THIS = "$this";
+	public static final String CLASS_INDICATOR = "<class>";
+
+	@Inject
+	public ZendDbgUtils(ProjectManager projectManager) {
+		ZendDbgUtils.projectManager = projectManager;
+	}
+
+	/**
+	 * Checks if given variable name is a name of super global variable.
+	 * 
+	 * @param name
+	 * @return <code>true</code> if given variable name is a name of super
+	 *         global variable, <code>false</code> otherwise
+	 */
+	public static boolean isSuperGlobal(String name) {
+		for (int i = 0; i < SUPER_GLOBAL_NAMES.length; i++)
+			if (SUPER_GLOBAL_NAMES[i].equalsIgnoreCase(name))
+				return true;
+		return false;
+	}
+
+	/**
+	 * Checks if given variable name is a name of "this" pseudo-variable.
+	 * 
+	 * @param name
+	 * @return <code>true</code> if given variable name is a name of "this"
+	 *         pseudo-variable, <code>false</code> otherwise
+	 */
+	public static boolean isThis(String name) {
+		return THIS.equalsIgnoreCase(name);
+	}
+
+	/**
+	 * Finds local file path that corresponds to remote file path.
+	 * 
+	 * @param remotePath
+	 * @return corresponding local file path
+	 */
+	public static String getLocalPath(String remotePath) {
+		Path remoteFilePath = Path.of(remotePath);
+		String remoteFileName = remoteFilePath.getName();
+		SearchResult searchResult = null;
+		try {
+			Searcher searcher = projectManager.getSearcher();
+			QueryExpression searchQuery = new QueryExpression().setName(remoteFileName);
+			searchResult = searcher.search(searchQuery);
+		} catch (Exception e) {
+			ZendDebugger.LOG.error(e.getMessage(), e);
+			return null;
+		}
+		if (searchResult == null) {
+			return null;
+		}
+		List<Path> resultPaths = new ArrayList<>();
+		for (String resultPath : searchResult.getFilePaths()) {
+			resultPaths.add(Path.of(resultPath));
+		}
+		// Dummy best match for now...
+		Path bestMatchPath = null;
+		int i = 1;
+		while (remoteFilePath.length() - i >= 0) {
+			String remoteSegment = remoteFilePath.element(remoteFilePath.length() - i);
+			for (Path localFilePath : resultPaths) {
+				if (localFilePath.length() - i < 0) {
+					continue;
+				}
+				String localSegment = localFilePath.element(localFilePath.length() - i);
+				if (localSegment.equals(remoteSegment)) {
+					bestMatchPath = localFilePath;
+				}
+			}
+			i++;
+		}
+		return bestMatchPath != null ? bestMatchPath.toString() : null;
+	}
+
+	/**
+	 * Returns local file absolute path.
+	 * 
+	 * @param vfsPath
+	 * @return local file absolute path
+	 */
+	public static String getAbsolutePath(String vfsPath) {
+		VirtualFileEntry virtualFileEntry = getVirtualFileEntry(vfsPath);
+		if (virtualFileEntry != null) {
+			File ioFile = virtualFileEntry.getVirtualFile().toIoFile();
+			if (ioFile != null) {
+				return ioFile.getAbsolutePath();
+			}
+			return virtualFileEntry.getVirtualFile().getPath().toString();
+		}
+		return vfsPath;
+	}
+
+	/**
+	 * Finds and returns VFS file entry for given local path.
+	 * 
+	 * @param path
+	 * @return VFS file entry for given local path
+	 */
+	public static VirtualFileEntry getVirtualFileEntry(String path) {
+		VirtualFileEntry virtualFileEntry = null;
+		try {
+			virtualFileEntry = projectManager.getProjectsRoot().getChild(path);
+		} catch (ServerException e) {
+			ZendDebugger.LOG.error(e.getMessage(), e);
+		}
+		return virtualFileEntry;
+	}
+
+}
