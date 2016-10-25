50af6524b736d9bd3561d8a0467333b550e14f46
Igor Vinokur <ivinokur@codenvy.com>
2016-10-25 14:22:24 +0300

    CHE-2524: Change parameter type in JpaUserDao (#2743)

--- wsmaster/che-core-api-user/src/main/java/org/eclipse/che/api/user/server/jpa/JpaUserDao.java
+++ wsmaster/che-core-api-user/src/main/java/org/eclipse/che/api/user/server/jpa/JpaUserDao.java
@@ -39,6 +39,7 @@ import static java.util.stream.Collectors.toList;
  *
  * @author Yevhenii Voevodin
  * @author Anton Korneta
+ * @author Igor Vinokur
  */
 @Singleton
 public class JpaUserDao implements UserDao {
@@ -175,15 +176,16 @@ public class JpaUserDao implements UserDao {
 
     @Override
     @Transactional
-    public Page<UserImpl> getAll(int maxItems, int skipCount) throws ServerException {
+    public Page<UserImpl> getAll(int maxItems, long skipCount) throws ServerException {
         // TODO need to ensure that 'getAll' query works with same data as 'getTotalCount'
         checkArgument(maxItems >= 0, "The number of items to return can't be negative.");
-        checkArgument(skipCount >= 0, "The number of items to skip can't be negative.");
+        checkArgument(skipCount >= 0 && skipCount <= Integer.MAX_VALUE,
+                      "The number of items to skip can't be negative or greater than " + Integer.MAX_VALUE);
         try {
             final List<UserImpl> list = managerProvider.get()
                                                        .createNamedQuery("User.getAll", UserImpl.class)
                                                        .setMaxResults(maxItems)
-                                                       .setFirstResult(skipCount)
+                                                       .setFirstResult((int)skipCount)
                                                        .getResultList()
                                                        .stream()
                                                        .map(JpaUserDao::erasePassword)
