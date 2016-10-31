ea57ef39d485dc55b1292ea07afef9f207cdec85
Dmytro Nochevnov <dnochevnov@codenvy.com>
2016-10-31 12:48:01 +0200

    fixup! fixup! fixup! fixup! fixup! Support array format in environment in compose file
    Signed-off-by: Dmytro Nochevnov <dnochevnov@codenvy.com>

--- wsmaster/che-core-api-workspace/src/main/java/org/eclipse/che/api/environment/server/compose/deserializer/EnvironmentDeserializer.java
+++ wsmaster/che-core-api-workspace/src/main/java/org/eclipse/che/api/environment/server/compose/deserializer/EnvironmentDeserializer.java
@@ -14,7 +14,6 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.google.common.collect.ImmutableMap;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -30,7 +29,7 @@ import static java.lang.String.format;
  */
 public class EnvironmentDeserializer extends JsonDeserializer<Map<String, String>> {
 
-    public static final String ARRAY_ITEM_DIVIDER        = "=";
+    public static final String ARRAY_ITEM_VALUE_DIVIDER  = "=";
     public static final String UNSUPPORTED_VALUE_MESSAGE = "Unsupported value '%s'.";
 
     @Override
@@ -53,13 +52,7 @@ public class EnvironmentDeserializer extends JsonDeserializer<Map<String, String
         */
         try {
             if (environment instanceof List) {
-                // convert array to Map<String, String>{ key1: value1, key2: value2 }
-                Map<String, String> map = new HashMap<>();
-                for (String item : (List<String>) environment) {
-                    map.putAll(parseEnvironmentItem(item));
-                }
-
-                return map;
+                return parseArrayEnvironment((List<String>) environment);
             }
         } catch (IllegalArgumentException ie) {
             throw ctxt.mappingException(ie.getMessage());
@@ -85,19 +78,27 @@ public class EnvironmentDeserializer extends JsonDeserializer<Map<String, String
     }
 
     /**
-     * Parse "key=value" item.
-     * @param item - environment array item to parse
+     * Convert list [key1=value1, key2=value2] into Map<String, String>{ key1: value1, key2: value2 }.
+     *
+     * @param environment
+     *          list form of compose environment to parse
      * @return map of key/value which are divided by "=" sign in item.
-     * @throws IllegalArgumentException if there is no "=" sign in item, or if there is empty key in item
+     * @throws IllegalArgumentException if there is no "=" sign in environment item, or if there is empty key in item
      */
-    private Map<String, String> parseEnvironmentItem(String item) throws IllegalArgumentException {
-        if (item.indexOf(ARRAY_ITEM_DIVIDER) < 1) {
-            throw new IllegalArgumentException(format(UNSUPPORTED_VALUE_MESSAGE, item));
+    private Map<String, String> parseArrayEnvironment(List<String> environment) throws IllegalArgumentException {
+        Map<String, String> map = new HashMap<>();
+        for (String item : environment) {
+            int valueDividerIndex = item.indexOf(ARRAY_ITEM_VALUE_DIVIDER);
+            if (valueDividerIndex < 1) {
+                throw new IllegalArgumentException(format(UNSUPPORTED_VALUE_MESSAGE, item));
+            }
+
+            String key = item.substring(0, valueDividerIndex);
+            String value = item.substring(valueDividerIndex + 1, item.length());
+
+            map.put(key, value);
         }
 
-        String key = item.substring(0, item.indexOf(ARRAY_ITEM_DIVIDER));
-        String value = item.substring(item.indexOf(ARRAY_ITEM_DIVIDER) + 1, item.length());
-
-        return ImmutableMap.of(key, value);
+        return map;
     }
 }
