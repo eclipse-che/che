a1cc91faf7de9101544c5536a15f444ebf159751
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2016-11-08 17:32:06 +0200

    Replace deprecated junit.framework.Assert with org.junit.Assert

--- ide/commons-gwt/src/test/java/org/eclipse/che/ide/resource/PathTest.java
+++ ide/commons-gwt/src/test/java/org/eclipse/che/ide/resource/PathTest.java
@@ -14,12 +14,12 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNotSame;
-import static junit.framework.Assert.assertNull;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for the {@link Path}.
