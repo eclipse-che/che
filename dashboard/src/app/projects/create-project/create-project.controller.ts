bdd272bafc67fd99fb5dabe7c3d30605a1f6e906
Ann Shumilova <ashumilova@codenvy.com>
2016-11-10 13:16:43 +0200

    CHE-2937: add ability to import multi-projects from dashboard
    Signed-off-by: Ann Shumilova <ashumilova@codenvy.com>

--- dashboard/src/app/projects/create-project/create-project.controller.ts
+++ dashboard/src/app/projects/create-project/create-project.controller.ts
@@ -592,7 +592,8 @@ export class CreateProjectController {
       let deferredResolve = this.$q.defer();
       let deferredResolvePromise = deferredResolve.promise;
 
-      let importPromise = this.cheAPI.getWorkspace().getWorkspaceAgent(workspaceId).getProject().importProject(projectName, projectData.source);
+      let projects = this.processMultiproject(projectData);
+      let importPromise = this.cheAPI.getWorkspace().getWorkspaceAgent(workspaceId).getProject().createProjects(projects);
 
       importPromise.then(() => {
         // add commands if there are some that have been defined
@@ -643,7 +644,31 @@ export class CreateProjectController {
 
   }
 
-  resolveProjectType(workspaceId: string, projectName: string, projectData: any, deferredResolve: ng.IDeferred<any>) {
+  /**
+   * Process multi-project and prepare batch of projects to be created.
+   *
+   * @param projectData project data to process
+   * @returns {Array|any} array of projects
+   */
+  processMultiproject(projectData) {
+    let currentPath = '/' + projectData.project.name;
+
+    let projects = projectData.projects;
+    let project = angular.copy(projectData.project);
+    project.path = currentPath;
+    project.source = projectData.source;
+
+    //Update path of sub-projects:
+    projects.forEach((project : any) => {
+      let index = project.path.indexOf('/' + project.name);
+      project.path = currentPath + project.path.substr(index);
+    });
+
+    projects.push(project);
+    return projects;
+  }
+
+  resolveProjectType(workspaceId, projectName, projectData, deferredResolve) {
     let projectDetails = projectData.project;
     if (!projectDetails.attributes) {
       projectDetails.source = projectData.source;
