28d21574028e797602a5606a0c508c6dd430abda
Sergii Kabashniuk <skabashnyuk@codenvy.com>
2016-11-14 18:01:52 +0200

    fixup! Introduced after user remove event

--- wsmaster/che-core-api-user/src/main/java/org/eclipse/che/api/user/server/event/PostUserRemovedEvent.java
+++ wsmaster/che-core-api-user/src/main/java/org/eclipse/che/api/user/server/event/PostUserRemovedEvent.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.che.api.user.server.event;
 
+import org.eclipse.che.api.core.notification.EventOrigin;
 import org.eclipse.che.api.user.server.model.impl.UserImpl;
 
 /**
@@ -17,6 +18,7 @@ import org.eclipse.che.api.user.server.model.impl.UserImpl;
  *
  * @author Sergii Kabashniuk
  */
+@EventOrigin("user")
 public class PostUserRemovedEvent {
 
     private final String userId;
