3f34354254464f52f40ffab9516a9302e2630616
Christian Dietrich <christian.dietrich@itemis.de>
2016-11-18 09:32:09 +0100

    more stupid stuff
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- wsmaster/che-core-api-agent/src/main/resources/agents/scripts/org.eclipse.che.ls.mydsl.script.sh
+++ wsmaster/che-core-api-agent/src/main/resources/agents/scripts/org.eclipse.che.ls.mydsl.script.sh
@@ -1,4 +1,13 @@
-
+#
+# Copyright (c) 2012-2016 Codenvy, S.A.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Codenvy, S.A. - initial API and implementation
+#
 unset PACKAGES
 unset SUDO
 command -v tar >/dev/null 2>&1 || { PACKAGES=${PACKAGES}" tar"; }
