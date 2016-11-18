de89c19b1a099e8aa603075382c6673bbbe75791
Christian Dietrich <christian.dietrich@itemis.de>
2016-11-18 11:50:05 +0100

    fixed script
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- wsmaster/che-core-api-agent/src/main/resources/agents/scripts/org.eclipse.che.ls.mydsl.script.sh
+++ wsmaster/che-core-api-agent/src/main/resources/agents/scripts/org.eclipse.che.ls.mydsl.script.sh
@@ -15,21 +15,9 @@ command -v curl >/dev/null 2>&1 || { PACKAGES=${PACKAGES}" curl"; }
 test "$(id -u)" = 0 || SUDO="sudo"
 
 AGENT_BINARIES_URI=http://cdietrich.github.io/mydsl-full.jar
+CHE_DIR=$HOME/che
+LS_DIR=${CHE_DIR}/ls-mydsl
 LS_LAUNCHER=${LS_DIR}/launch.sh
-
-if [ -f /etc/centos-release ]; then
-    FILE="/etc/centos-release"
-    LINUX_TYPE=$(cat $FILE | awk '{print $1}')
- elif [ -f /etc/redhat-release ]; then
-    FILE="/etc/redhat-release"
-    LINUX_TYPE=$(cat $FILE | cut -c 1-8)
- else
-    FILE="/etc/os-release"
-    LINUX_TYPE=$(cat $FILE | grep ^ID= | tr '[:upper:]' '[:lower:]')
-    LINUX_VERSION=$(cat $FILE | grep ^VERSION_ID=)
-fi
-
-MACHINE_TYPE=$(uname -m)
 
 mkdir -p ${CHE_DIR}
 mkdir -p ${LS_DIR}
@@ -42,4 +30,4 @@ curl --create-dirs -o ${LS_DIR}/mydsl-full.jar ${AGENT_BINARIES_URI}
 
 touch ${LS_LAUNCHER}
 chmod +x ${LS_LAUNCHER}
-echo "java -jar mydsl-full.jar" > ${LS_LAUNCHER}
+echo "java -jar ${LS_DIR}/mydsl-full.jar" > ${LS_LAUNCHER}
