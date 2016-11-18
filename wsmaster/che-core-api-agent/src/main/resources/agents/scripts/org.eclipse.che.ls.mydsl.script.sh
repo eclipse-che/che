3fe260f87f1e10781ce00d6468f7f9643e4b9dd6
Christian Dietrich <christian.dietrich@itemis.de>
2016-11-18 12:15:09 +0100

    better version of script
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- wsmaster/che-core-api-agent/src/main/resources/agents/scripts/org.eclipse.che.ls.mydsl.script.sh
+++ wsmaster/che-core-api-agent/src/main/resources/agents/scripts/org.eclipse.che.ls.mydsl.script.sh
@@ -8,6 +8,7 @@
 # Contributors:
 #   Codenvy, S.A. - initial API and implementation
 #
+
 unset PACKAGES
 unset SUDO
 command -v tar >/dev/null 2>&1 || { PACKAGES=${PACKAGES}" tar"; }
@@ -19,15 +20,94 @@ CHE_DIR=$HOME/che
 LS_DIR=${CHE_DIR}/ls-mydsl
 LS_LAUNCHER=${LS_DIR}/launch.sh
 
+if [ -f /etc/centos-release ]; then
+    FILE="/etc/centos-release"
+    LINUX_TYPE=$(cat $FILE | awk '{print $1}')
+ elif [ -f /etc/redhat-release ]; then
+    FILE="/etc/redhat-release"
+    LINUX_TYPE=$(cat $FILE | cut -c 1-8)
+ else
+    FILE="/etc/os-release"
+    LINUX_TYPE=$(cat $FILE | grep ^ID= | tr '[:upper:]' '[:lower:]')
+    LINUX_VERSION=$(cat $FILE | grep ^VERSION_ID=)
+fi
+
+MACHINE_TYPE=$(uname -m)
+
 mkdir -p ${CHE_DIR}
 mkdir -p ${LS_DIR}
 
-#####################
-### Install MyDsl LS ###
-#####################
+########################
+### Install packages ###
+########################
 
-curl --create-dirs -o ${LS_DIR}/mydsl-full.jar ${AGENT_BINARIES_URI}
+# Red Hat Enterprise Linux 7
+############################
+if echo ${LINUX_TYPE} | grep -qi "rhel"; then
+    test "${PACKAGES}" = "" || {
+        ${SUDO} yum install ${PACKAGES};
+    }
+
+# Red Hat Enterprise Linux 6
+############################
+elif echo ${LINUX_TYPE} | grep -qi "Red Hat"; then
+    test "${PACKAGES}" = "" || {
+        ${SUDO} yum install ${PACKAGES};
+    }
+
+# Ubuntu 14.04 16.04 / Linux Mint 17
+####################################
+elif echo ${LINUX_TYPE} | grep -qi "ubuntu"; then
+    test "${PACKAGES}" = "" || {
+        ${SUDO} apt-get update;
+        ${SUDO} apt-get -y install ${PACKAGES};
+    }
+
+
+# Debian 8
+##########
+elif echo ${LINUX_TYPE} | grep -qi "debian"; then
+    test "${PACKAGES}" = "" || {
+        ${SUDO} apt-get update;
+        ${SUDO} apt-get -y install ${PACKAGES};
+    }
+
+# Fedora 23
+###########
+elif echo ${LINUX_TYPE} | grep -qi "fedora"; then
+    PACKAGES=${PACKAGES}" procps-ng"
+    test "${PACKAGES}" = "" || {
+        ${SUDO} dnf -y install ${PACKAGES};
+    }
+
+
+# CentOS 7.1 & Oracle Linux 7.1
+###############################
+elif echo ${LINUX_TYPE} | grep -qi "centos"; then
+    test "${PACKAGES}" = "" || {
+        ${SUDO} yum -y install ${PACKAGES};
+    }
+
+# openSUSE 13.2
+###############
+elif echo ${LINUX_TYPE} | grep -qi "opensuse"; then
+    test "${PACKAGES}" = "" || {
+        ${SUDO} zypper install -y ${PACKAGES};
+    }
+
+else
+    >&2 echo "Unrecognized Linux Type"
+    >&2 cat /etc/os-release
+    exit 1
+fi
+
+
+######################
+### Install Mydsl LS ###
+######################
+
+curl ${LS_DIR} -s ${AGENT_BINARIES_URI}
 
 touch ${LS_LAUNCHER}
 chmod +x ${LS_LAUNCHER}
-echo "java -jar ${LS_DIR}/mydsl-full.jar" > ${LS_LAUNCHER}
+echo "java -jar ${LS_DIR}/mydsl-full.jar" >> ${LS_LAUNCHER}
