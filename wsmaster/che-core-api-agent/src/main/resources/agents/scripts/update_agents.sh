382ae92e43719f58b390d7cb44eade27b8441a80
Evgen Vidolob <evidolob@codenvy.com>
2016-11-30 09:33:22 +0200

    Merge with master
    Signed-off-by: Vitalii Parfonov <vparfonov@codenvy.com>

--- wsmaster/che-core-api-agent/src/main/resources/agents/scripts/update_agents.sh
+++ wsmaster/che-core-api-agent/src/main/resources/agents/scripts/update_agents.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2012-2017 Codenvy, S.A.
+# Copyright (c) 2012-2016 Codenvy, S.A.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
@@ -36,3 +36,4 @@ updateAgentScript ".." "org.eclipse.che.ls.csharp"
 updateAgentScript ".." "org.eclipse.che.ls.php"
 updateAgentScript ".." "org.eclipse.che.unison"
 updateAgentScript ".." "org.eclipse.che.ls.typescript"
+updateAgentScript ".." "org.eclipse.che.ls.python"
