a5d0a5e242b29c17ae12c4bafe1b2a29b4ce552b
Florent BENOIT <fbenoit@codenvy.com>
2016-12-02 01:41:50 -0700

    - Rename base-cli folder to base (#3255)
    - Rename lib-typescript folder to lib
    - Update docker image eclipse/che-lib-typescript to be eclipse/che-lib
    
    Change-Id: I878b52853a424f14cdec74074055d52dc8e198fd
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- dockerfiles/lib/build.sh
+++ dockerfiles/lib/build.sh
@@ -5,7 +5,7 @@
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 
-IMAGE_NAME="eclipse/che-lib-typescript"
+IMAGE_NAME="eclipse/che-lib"
 . $(cd "$(dirname "$0")"; pwd)/../build.include
 
 DIR=$(cd "$(dirname "$0")"; pwd)
