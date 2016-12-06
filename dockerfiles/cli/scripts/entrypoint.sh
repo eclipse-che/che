961f477150c598881450f5ba0fbb8e5c29dac09e
Tyler Jewell <tjewell@codenvy.com>
2016-12-06 14:49:30 -0800

    usage

--- dockerfiles/cli/scripts/entrypoint.sh
+++ dockerfiles/cli/scripts/entrypoint.sh
@@ -8,16 +8,17 @@
 
 init_usage() {
   USAGE="
-Usage: docker run <DOCKER_PARAMETERS> ${CHE_IMAGE_FULLNAME} [COMMAND]
+USAGE: 
+  docker run <DOCKER_PARAMETERS> ${CHE_IMAGE_FULLNAME} [COMMAND]
 
-Mandatory Docker Parameters:
-  -v <LOCAL_PATH>:${CHE_CONTAINER_ROOT}                Where user, instance, and log data saved
+MANDATORY DOCKER PARAMETERS:
+  -v <LOCAL_PATH>:${CHE_CONTAINER_ROOT}         Where user, instance, and log data saved
   -v /var/run/docker.sock:/var/run/docker.sock
   -it
 
-Optional Docker Parameters:
-  -e CHE_HOST                          IP address or hostname where ${CHE_FORMAL_PRODUCT_NAME} will serve its users
-  -e CHE_PORT                          Port that $CHE_FORMAL_PRODUCT_NAME} will bind to
+OPTIONAL DOCKER PARAMETERS:
+  -e CHE_HOST=<YOUR_HOST>              IP address or hostname where ${CHE_FORMAL_PRODUCT_NAME} will serve its users
+  -e CHE_PORT=<YOUR_PORT>              Port where ${CHE_FORMAL_PRODUCT_NAME} will bind itself to
   -v <LOCAL_PATH>:/data/instance       Where instance, user, log data will be saved
   -v <LOCAL_PATH>:/data/backup         Where backup files will be saved
   -v <LOCAL_PATH>:/cli                 Where the CLI trace log is saved
@@ -25,37 +26,26 @@ Optional Docker Parameters:
   -v <LOCAL_PATH>:/sync                Where remote ws files will be copied with sync command
   -v <LOCAL_PATH>:/unison              Where unison profile for optimzing sync command resides
     
-Commands:
+COMMANDS:
   help                                 This message
   version                              Installed version and upgrade paths
   init                                 Initializes a directory with a ${CHE_FORMAL_PRODUCT_NAME} install
-       [--no-force                         Default - uses cached local Docker images
-        --pull                             Checks for newer images from DockerHub
-        --force                            Removes all images and re-pulls all images from DockerHub
-        --offline                          Uses images saved to disk from the offline command
-        --accept-license                   Auto accepts the ${CHE_FORMAL_PRODUCT_NAME} license during installation
-        --reinit]                          Reinstalls using existing $CHE_MINI_PRODUCT_NAME.env configuration
-  start [--pull | --force | --offline] Starts ${CHE_FORMAL_PRODUCT_NAME} services
+  start                                Starts ${CHE_FORMAL_PRODUCT_NAME} services
   stop                                 Stops ${CHE_FORMAL_PRODUCT_NAME} services
-  restart [--pull | --force]           Restart ${CHE_FORMAL_PRODUCT_NAME} services
+  restart                              Restart ${CHE_FORMAL_PRODUCT_NAME} services
   destroy                              Stops services, and deletes ${CHE_FORMAL_PRODUCT_NAME} instance data
-          [--quiet                         Does not ask for confirmation before destroying instance data
-           --cli]                          If :/cli is mounted, will destroy the cli.log
-  rmi [--quiet]                        Removes the Docker images for <version>, forcing a repull
+  rmi                                  Removes the Docker images for <version>, forcing a repull
   config                               Generates a ${CHE_FORMAL_PRODUCT_NAME} config from vars; run on any start / restart
   upgrade                              Upgrades ${CHE_FORMAL_PRODUCT_NAME} from one version to another with migrations and backups
-  download [--pull|--force|--offline]  Pulls Docker images for the current ${CHE_FORMAL_PRODUCT_NAME} version
-  backup [--quiet | --skip-data]       Backups ${CHE_FORMAL_PRODUCT_NAME} configuration and data to ${CHE_CONTAINER_ROOT}/backup volume mount
-  restore [--quiet]                    Restores ${CHE_FORMAL_PRODUCT_NAME} configuration and data from ${CHE_CONTAINER_ROOT}/backup mount
+  download                             Pulls Docker images for the current ${CHE_FORMAL_PRODUCT_NAME} version
+  backup                               Backups ${CHE_FORMAL_PRODUCT_NAME} configuration and data to ${CHE_CONTAINER_ROOT}/backup volume mount
+  restore                              Restores ${CHE_FORMAL_PRODUCT_NAME} configuration and data from ${CHE_CONTAINER_ROOT}/backup mount
   offline                              Saves ${CHE_FORMAL_PRODUCT_NAME} Docker images into TAR files for offline install
   info                                 Displays info about ${CHE_FORMAL_PRODUCT_NAME} and the CLI
-       [--all                              Run all debugging tests
-        --debug                            Displays system information
-        --network]                         Test connectivity between ${CHE_FORMAL_PRODUCT_NAME} sub-systems
   ssh <wksp-name> [machine-name]       SSH to a workspace if SSH agent enabled
   sync <wksp-name>                     Synchronize workspace with current working directory
-  action <action-name> [--help]        Start action on ${CHE_FORMAL_PRODUCT_NAME} instance
-  test <test-name> [--help]            Start test on ${CHE_FORMAL_PRODUCT_NAME} instance
+  action <action-name>                 Start action on ${CHE_FORMAL_PRODUCT_NAME} instance
+  test <test-name>                     Start test on ${CHE_FORMAL_PRODUCT_NAME} instance
 "
 }
 
