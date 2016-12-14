fbc32b8b1135324554c7cd095e5c6d918ad9ee45
Ann Shumilova <ashumilova@codenvy.com>
2016-12-14 13:31:12 +0200

    CHE-3257: display version number in CHE dashboard from API request
    Signed-off-by: Ann Shumilova <ashumilova@codenvy.com>

--- dashboard/src/components/api/che-service.factory.ts
+++ dashboard/src/components/api/che-service.factory.ts
@@ -16,24 +16,43 @@
  * @author Ann Shumilova
  */
 export class CheService {
+  /**
+   * Service for performing HTTP requests.
+   */
+  private $http: ng.IHttpService;
+
+  private servicesPromise: ng.IPromise;
+  /**
+   * The list of available services.
+   */
+  private services: Array<string>;
+  /**
+   * Information about services.
+   */
+  private servicesInfo: any;
 
   /**
    * Default constructor that is using resource
    * @ngInject for Dependency injection
    */
-  constructor ($http) {
+  constructor ($http: ng.IHttpService) {
     this.$http = $http;
   }
 
-  fetchServices() {
+  /**
+   * Fetches all available services.
+   *
+   * @returns {ng.IPromise}
+   */
+  fetchServices(): ng.IPromise {
     if (this.servicesPromise) {
       return this.servicesPromise;
     }
 
     let promise = this.$http.get('/api/');
-    this.servicesPromise = promise.then((response) => {
+    this.servicesPromise = promise.then((response: any) => {
       this.services = [];
-      response.data.rootResources.forEach((service) => {
+      response.data.rootResources.forEach((service: any) => {
         let path = service.path.charAt(0) === '/' ? service.path.substr(1) : service.path;
         this.services.push(path);
       });
@@ -44,13 +63,36 @@ export class CheService {
 
   /**
    * Checks whether service is available.
+   * @param name service name
    * @returns {Boolean|*}
    */
-  isServiceAvailable(name) {
+  isServiceAvailable(name: string): boolean {
     if (!this.services) {
       return false;
     }
 
     return this.services.indexOf(name) > -1;
+  }
+
+  /**
+   * Fetches the services info.
+   *
+   * @returns {IHttpPromise<T>}
+   */
+  fetchServicesInfo(): ng.IPromise {
+    let promise = this.$http({'method': 'OPTIONS', 'url': '/api/'});
+    let infoPromise = promise.then((response: any) => {
+      this.servicesInfo = response.data;
+    });
+    return infoPromise;
+  }
+
+  /**
+   * Returns services information.
+   *
+   * @returns {any} servies information
+   */
+  getServicesInfo(): any {
+    return this.servicesInfo;
   }
 }
