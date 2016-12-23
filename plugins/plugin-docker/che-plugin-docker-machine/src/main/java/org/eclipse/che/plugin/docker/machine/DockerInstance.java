6b5c10ce74b7a5bb5131c18f8c0d9b6c0751372d
Angel Misevski <amisevsk@redhat.com>
2016-12-23 17:58:52 -0500

    CHE-2030: Replace getServers() implementation
    Replaces DockerInstanceRuntimeInfo#getServers() to use
    ServerEvaluationStrategy. Deletes LocalDockerInstanceRuntimeInfo
    class as it is no longer needed. Adds MapBinder of
    ServerEvaluationStrategy to LocalDockerModule.
    
    Updates DockerInstanceRuntimeInfo tests to be more
    readable and removes now unnecessary tests.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/DockerInstance.java
+++ plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/DockerInstance.java
@@ -146,9 +146,8 @@ public class DockerInstance extends AbstractInstance {
             try {
                 final ContainerInfo containerInfo = docker.inspectContainer(container);
                 machineRuntime = new MachineRuntimeInfoImpl(dockerMachineFactory.createMetadata(containerInfo,
-                                                                                                null,
-                                                                                                node.getHost(),
-                                                                                                getConfig()));
+                                                                                                getConfig(),
+                                                                                                node.getHost()));
             } catch (IOException e) {
                 LOG.error(e.getLocalizedMessage(), e);
                 return null;
