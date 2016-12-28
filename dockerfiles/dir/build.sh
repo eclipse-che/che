af14a2e91f19ad19308a2772ddae925fdd293593
Mario Loriedo <mloriedo@redhat.com>
2016-12-28 12:41:18 +0100

    Minor dockerfiles build fixes
    Signed-off-by: Mario Loriedo <mloriedo@redhat.com>

--- dockerfiles/dir/build.sh
+++ dockerfiles/dir/build.sh
@@ -9,4 +9,4 @@ IMAGE_NAME="eclipse/che-dir"
 . $(cd "$(dirname "$0")"; pwd)/../build.include
 
 init "$@"
-build "$@"
+build
--- dockerfiles/mount/build.sh
+++ dockerfiles/mount/build.sh
@@ -9,4 +9,4 @@ IMAGE_NAME="eclipse/che-mount"
 . $(cd "$(dirname "$0")"; pwd)/../build.include
 
 init "$@"
-build "$@"
+build
