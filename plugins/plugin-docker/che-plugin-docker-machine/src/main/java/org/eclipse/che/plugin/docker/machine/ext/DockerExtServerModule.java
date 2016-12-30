f4ade80ec12160a09a462a0273720b5f27b5c22f
Alexander Garagatyi <agaragatyi@codenvy.com>
2016-12-30 12:18:13 +0200

    Refactor workspace infrastructure provisioning
    Move agents volume addition into infrastructure provisioner.
    Suppress some false positive warnings.
    Add some javadocs.
    Signed-off-by: Alexander Garagatyi <agaragatyi@codenvy.com>

--- plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/ext/DockerExtServerModule.java
+++ plugins/plugin-docker/che-plugin-docker-machine/src/main/java/org/eclipse/che/plugin/docker/machine/ext/DockerExtServerModule.java
@@ -14,9 +14,6 @@ import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 import com.google.inject.name.Names;
 
-import org.eclipse.che.inject.CheBootstrap;
-import org.eclipse.che.plugin.docker.machine.ext.provider.WsAgentVolumeProvider;
-
 /**
  * Guice module for extension servers feature in docker machines
  *
@@ -28,12 +25,6 @@ import org.eclipse.che.plugin.docker.machine.ext.provider.WsAgentVolumeProvider;
 public class DockerExtServerModule extends AbstractModule {
     @Override
     protected void configure() {
-        Multibinder<String> volumesMultibinder = Multibinder.newSetBinder(binder(),
-                                                                          String.class,
-                                                                          Names.named("machine.docker.dev_machine.machine_volumes"));
-        volumesMultibinder.addBinding()
-                          .toProvider(WsAgentVolumeProvider.class);
-
         Multibinder<String> devMachineEnvVars = Multibinder.newSetBinder(binder(),
                                                                          String.class,
                                                                          Names.named("machine.docker.dev_machine.machine_env"))
@@ -41,21 +32,5 @@ public class DockerExtServerModule extends AbstractModule {
         devMachineEnvVars.addBinding().toProvider(org.eclipse.che.plugin.docker.machine.ext.provider.ApiEndpointEnvVariableProvider.class);
         devMachineEnvVars.addBinding().toProvider(org.eclipse.che.plugin.docker.machine.ext.provider.ProjectsRootEnvVariableProvider.class);
         devMachineEnvVars.addBinding().toProvider(org.eclipse.che.plugin.docker.machine.ext.provider.JavaOptsEnvVariableProvider.class);
-        devMachineEnvVars.addBinding()
-                         .toInstance(CheBootstrap.CHE_LOCAL_CONF_DIR
-                                     + '='
-                                     + org.eclipse.che.plugin.docker.machine.ext.provider.DockerExtConfBindingProvider
-                                             .EXT_CHE_LOCAL_CONF_DIR);
-        Multibinder<String> allMachinesEnvVars = Multibinder.newSetBinder(binder(),
-                                                                          String.class,
-                                                                          Names.named("machine.docker.machine_env"))
-                                                            .permitDuplicates();
-        allMachinesEnvVars.addBinding()
-                          .toProvider(org.eclipse.che.plugin.docker.machine.ext.provider.ApiEndpointEnvVariableProvider.class);
-        org.eclipse.che.plugin.docker.machine.ext.provider.DockerExtConfBindingProvider extConfBindingProvider =
-                new org.eclipse.che.plugin.docker.machine.ext.provider.DockerExtConfBindingProvider();
-        if (extConfBindingProvider.get() != null) {
-            volumesMultibinder.addBinding().toProvider(extConfBindingProvider);
-        }
     }
 }
