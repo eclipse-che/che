0db9ec377e3ef6d13533f1970fdf245162006c92
Oleksii Kurinnyi <okurinnyi@codenvy.com>
2017-01-04 10:58:46 +0200

    fixup! make field 'Runtime name' readonly

--- dashboard/src/app/workspaces/workspace-details/environments/environments.controller.ts
+++ dashboard/src/app/workspaces/workspace-details/environments/environments.controller.ts
@@ -109,6 +109,39 @@ export class WorkspaceEnvironmentsController {
   }
 
   /**
+   * Returns true if environment name is unique
+   *
+   * @param name {string} environment name to validate
+   * @returns {boolean}
+   */
+  isUnique(name: string): boolean {
+    return name === this.environmentName || !this.workspaceConfig.environments || !this.workspaceConfig.environments[name];
+  }
+
+  /**
+   * Updates name of environment
+   * @param isFormValid {boolean}
+   */
+  updateEnvironmentName(isFormValid: boolean): void {
+    if (!isFormValid || this.newEnvironmentName === this.environmentName) {
+      return;
+    }
+
+    this.workspaceConfig.environments[this.newEnvironmentName] = this.environment;
+    delete this.workspaceConfig.environments[this.environmentName];
+
+    if (this.workspaceConfig.defaultEnv === this.environmentName) {
+      this.workspaceConfig.defaultEnv = this.newEnvironmentName;
+    }
+
+    this.machinesViewStatus[this.newEnvironmentName] = this.machinesViewStatus[this.environmentName];
+
+    this.environmentName = this.newEnvironmentName;
+
+    this.doUpdateEnvironments();
+  }
+
+  /**
    * Returns name of machine which includes 'ws-agent'
    *
    * @returns {string} name of dev machine
