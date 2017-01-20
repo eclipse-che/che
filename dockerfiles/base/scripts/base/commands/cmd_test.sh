80d12172d0aa7c636f390578b3efb8d23f336e64
Florent BENOIT <fbenoit@codenvy.com>
2017-01-20 05:23:38 -0700

    Fix codenvy 1562 : We can execute commands on remote side so we shouldn't check state of the local server (which is BTW checked by the command itself) (#3825)
    Change-Id: I4ab90b19e5d060d4518ca26c93810df81be67d81
    Signed-off-by: Florent BENOIT <fbenoit@codenvy.com>

--- dockerfiles/base/scripts/base/commands/cmd_test.sh
+++ dockerfiles/base/scripts/base/commands/cmd_test.sh
@@ -9,18 +9,8 @@
 cmd_test() {
   debug $FUNCNAME
 
-  if container_exist_by_name $CHE_SERVER_CONTAINER_NAME; then
-    CURRENT_CHE_SERVER_CONTAINER_ID=$(get_server_container_id $CHE_SERVER_CONTAINER_NAME)
-    if container_is_running ${CURRENT_CHE_SERVER_CONTAINER_ID} && \
-       server_is_booted ${CURRENT_CHE_SERVER_CONTAINER_ID}; then
+  # Not loaded as part of the init process to save on download time
+  update_image_if_not_found eclipse/che-test:nightly
+  docker_run -it eclipse/che-test:nightly "$@"
 
-        # Not loaded as part of the init process to save on download time
-        update_image_if_not_found eclipse/che-test:nightly
-        docker_run -it eclipse/che-test:nightly "$@"
-
-       return
-    fi
-  fi
-
-  info "test" "The system is not running."
 }
