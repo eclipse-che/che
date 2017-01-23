8fe7fa323259578db7d969f1d06d3bfcb6967f27
Oleksii Kurinnyi <okurinnyi@codenvy.com>
2017-01-23 11:04:31 +0200

    fix bug which prevents to show workspace-details page (#3850)
    Signed-off-by: Oleksii Kurinnyi <okurinnyi@codenvy.com>

--- dashboard/src/app/workspaces/workspace-details/workspace-details.controller.ts
+++ dashboard/src/app/workspaces/workspace-details/workspace-details.controller.ts
@@ -117,7 +117,7 @@ export class WorkspaceDetailsController {
 
   init(): void {
     let routeParams = this.$route.current.params;
-    this.workspaceNamespace = this.$location.search().namespace || (this.getNamespaces() ? this.getNamespaces()[0].id : undefined);
+    this.workspaceNamespace = this.$location.search().namespace || (this.getNamespaces().length ? this.getNamespaces()[0].id : undefined);
     if (routeParams && routeParams.namespace && routeParams.workspaceName) {
       this.isCreationFlow = false;
       this.namespace = routeParams.namespace;
