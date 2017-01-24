eed7549e97e0f922f600eb5755fea15db5992ef4
Marian Labuda <mlabuda@redhat.com>
2017-01-24 13:25:40 +0100

    Added functional tests for CLI start command
    Signed-off-by: Marian Labuda <mlabuda@redhat.com>

--- dockerfiles/base/test.sh
+++ dockerfiles/base/test.sh
@@ -17,6 +17,7 @@ IMAGE_NAME="eclipse/che-bats:$TAG"
 echo "Running tests in container from image $IMAGE_NAME"
 echo "Running functional bats tests for CLI prompts and usage"
 docker run -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/cli_prompts_usage_tests.bats
-echo "Running functional bats tests for init and destroy"
+echo "Running functional bats tests for init and destroy commands"
 docker run -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/cmd_init_destroy_tests.bats
-
+echo "Running functionals bats tests for start command"
+docker run -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/cmd_start_tests.bats
--- dockerfiles/base/tests/test_base.sh
+++ dockerfiles/base/tests/test_base.sh
@@ -19,15 +19,39 @@ fi
 mkdir $TESTRUN_DIR -p
 
 kill_running_named_container() {
-  if [ $(docker ps --format '{{.Names}}' | grep $1 | wc -l) -eq 1 ]; then
+  if [[ $(docker ps --format '{{.Names}}' | grep $1 | wc -l) -eq 1 ]]; then
     echo "Stopping named container $1"
     docker kill $1 1>/dev/null
   fi
 }
 
 remove_named_container() {
-  if [ $(docker ps -a --format '{{.Names}}' | grep $1 | wc -l) -eq 1 ]; then
+  if [[ $(docker ps -a --format '{{.Names}}' | grep $1 | wc -l) -eq 1 ]]; then
     echo "Removing named container $1"
     docker rm $1 1>/dev/null
   fi
 }
+
+# Pass a port as an argument to check whether is free or not
+# Returns 0 if port is free (not listening), 1 otherwise
+port_is_free() {
+  if [[ $(netstat -lnt | awk -v port=$1 '$6 == "LISTEN" && $4 ~ "."port' | wc -l) -gt 0 ]]; then
+    return 1
+  else 
+    return 0
+  fi
+}
+
+# Get first free port from range of dynamic/private ports
+get_free_port() {
+  local port=49200
+  while [[ $(port_is_free $port) -eq 1 ]]; do
+    if [[ $port -eq 65535 ]]; then
+       echo ""
+       return 1
+    fi
+    port=$((port+1))
+  done
+  echo $port
+}
+
