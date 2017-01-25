8d1304320f202b34cda13756075c12aadf041bb5
Marian Labuda <mlabuda@redhat.com>
2017-01-25 12:57:30 +0100

    fixup! fixup! Added functional tests for CLI start command

--- dockerfiles/base/test.sh
+++ dockerfiles/base/test.sh
@@ -16,9 +16,10 @@ IMAGE_NAME="eclipse/che-bats:$TAG"
 
 # Runs functional CLI tests in a docker container.
 # Pass a file name of functional bats tests as an argument.
-# The file has to be placed in tests folder in directory containing this script
+#   The file has to be placed in tests folder in directory containing this script
+# (Optional) second argument is options for a docker run command.
 run_test_in_docker_container() {
-  docker run -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/$1
+  docker run $2 -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/$1
 }
 
 echo "Running tests in container from image $IMAGE_NAME"
@@ -27,5 +28,5 @@ run_test_in_docker_container cli_prompts_usage_tests.bats
 echo "Running functional bats tests for init and destroy commands"
 run_test_in_docker_container cmd_init_destroy_tests.bats
 echo "Running functionals bats tests for start command"
-run_test_in_docker_container cmd_start_tests.bats
+run_test_in_docker_container cmd_start_tests.bats --net=host
 
