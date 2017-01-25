a20e0984651ddd5b4d05590ae25cf11cea829421
Yevhenii Voevodin <evoevodin@codenvy.com>
2017-01-25 18:54:31 +0200

    Add system service that allows to prepare system to shutdown
    Resolves #3892.
    
    Two API methods are available:
    
    * _POST /api/system/stop_ - stops corresponding system services, for now it's workspace service.
    Basically it stops all the RUNNING workspaces snapshotting them before if configured to do so,
    also it interrupts start of currently STARTING workspaces.
    
    * _GET /api/system/state_ - returns current system state(only status for now).
    By default system status is RUNNING, but after stop is called or che server is
    stopped directly system status is changed like the following:
    ```
    RUNNING -> PREPARING_TO_SHUTDOWN -> READY_TO_SHUTDOWN
    ```
    Status changes are followed by status changed events, to recieve these
    events client has to subscribe on *system:state* channel.
    
    So client can use different ways to track system status changes.
    Doesn't matter whether API method is used or che server is stopped directly,
    events will be sent anyway.

---
+++ wsmaster/che-core-api-system/src/main/java/org/eclipse/che/api/system/server/SystemManager.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.api.system.server;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import org.eclipse.che.api.core.ConflictException;
+import org.eclipse.che.api.core.notification.EventService;
+import org.eclipse.che.api.system.shared.SystemStatus;
+import org.eclipse.che.api.system.shared.event.SystemStatusChangedEvent;
+import org.eclipse.che.api.workspace.server.WorkspaceManager;
+import org.eclipse.che.commons.lang.concurrent.LoggingUncaughtExceptionHandler;
+import org.eclipse.che.commons.lang.concurrent.ThreadLocalPropagateContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.eclipse.che.api.system.shared.SystemStatus.PREPARING_TO_SHUTDOWN;
+import static org.eclipse.che.api.system.shared.SystemStatus.READY_TO_SHUTDOWN;
+import static org.eclipse.che.api.system.shared.SystemStatus.RUNNING;
+
+/**
+ * Facade for system operations.
+ *
+ * @author Yevhenii Voevodin
+ */
+@Singleton
+public class SystemManager {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SystemManager.class);
+
+    private final AtomicReference<SystemStatus> statusRef;
+    private final WorkspaceManager              workspaceManager;
+    private final EventService                  eventService;
+
+    private final CountDownLatch shutdownLatch = new CountDownLatch(1);
+
+    @Inject
+    public SystemManager(WorkspaceManager workspaceManager, EventService eventService) {
+        this.workspaceManager = workspaceManager;
+        this.eventService = eventService;
+        this.statusRef = new AtomicReference<>(RUNNING);
+    }
+
+    /**
+     * Stops some of the system services preparing system to lighter shutdown.
+     * System status is changed from {@link SystemStatus#RUNNING} to
+     * {@link SystemStatus#PREPARING_TO_SHUTDOWN}.
+     *
+     * @throws ConflictException
+     *         when system status is different from running
+     */
+    public void stopServices() throws ConflictException {
+        if (!statusRef.compareAndSet(RUNNING, PREPARING_TO_SHUTDOWN)) {
+            throw new ConflictException("System shutdown has been already called, system status: " + statusRef.get());
+        }
+        ExecutorService exec = Executors.newSingleThreadExecutor(
+                new ThreadFactoryBuilder().setDaemon(false)
+                                          .setNameFormat("ShutdownSystemServicesPool")
+                                          .setUncaughtExceptionHandler(LoggingUncaughtExceptionHandler.getInstance())
+                                          .build());
+        exec.execute(ThreadLocalPropagateContext.wrap(this::doStopServices));
+        exec.shutdown();
+    }
+
+    /**
+     * Gets current system status.
+     *
+     * @see SystemStatus
+     */
+    public SystemStatus getSystemStatus() {
+        return statusRef.get();
+    }
+
+    /** Synchronously stops corresponding services. */
+    private void doStopServices() {
+        LOG.info("Preparing system to shutdown");
+        eventService.publish(new SystemStatusChangedEvent(RUNNING, PREPARING_TO_SHUTDOWN));
+        try {
+            workspaceManager.shutdown();
+            statusRef.set(READY_TO_SHUTDOWN);
+            eventService.publish(new SystemStatusChangedEvent(PREPARING_TO_SHUTDOWN, READY_TO_SHUTDOWN));
+            LOG.info("System is ready to shutdown");
+        } catch (InterruptedException x) {
+            LOG.error("Interrupted while waiting for system service to shutdown components");
+            Thread.currentThread().interrupt();
+        } finally {
+            shutdownLatch.countDown();
+        }
+    }
+
+    @PreDestroy
+    @VisibleForTesting
+    void shutdown() throws InterruptedException {
+        if (!statusRef.compareAndSet(RUNNING, PREPARING_TO_SHUTDOWN)) {
+            shutdownLatch.await();
+        } else {
+            doStopServices();
+        }
+    }
+}
