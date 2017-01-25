93db309e804babf27d1a455d8dd4402554ec0720
Marian Labuda <mlabuda@redhat.com>
2017-01-25 11:03:57 +0100

    fixup! Added functional tests for CLI start command

--- dockerfiles/base/test.sh
+++ dockerfiles/base/test.sh
@@ -14,10 +14,18 @@ BATS_BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 init "$@"
 IMAGE_NAME="eclipse/che-bats:$TAG"
 
+# Runs functional CLI tests in a docker container.
+# Pass a file name of functional bats tests as an argument.
+# The file has to be placed in tests folder in directory containing this script
+run_test_in_docker_container() {
+  docker run -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/$1
+}
+
 echo "Running tests in container from image $IMAGE_NAME"
 echo "Running functional bats tests for CLI prompts and usage"
-docker run -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/cli_prompts_usage_tests.bats
+run_test_in_docker_container cli_prompts_usage_tests.bats
 echo "Running functional bats tests for init and destroy commands"
-docker run -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/cmd_init_destroy_tests.bats
+run_test_in_docker_container cmd_init_destroy_tests.bats
 echo "Running functionals bats tests for start command"
-docker run -v $BATS_BASE_DIR:$BATS_BASE_DIR -e CLI_IMAGE_TAG=$TAG -e BATS_BASE_DIR=$BATS_BASE_DIR -v /var/run/docker.sock:/var/run/docker.sock $IMAGE_NAME bats $BATS_BASE_DIR/tests/cmd_start_tests.bats
+run_test_in_docker_container cmd_start_tests.bats
+
