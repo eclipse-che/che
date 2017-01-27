25592337bf193df63f1f20eedc750420665d3eb6
Max Shaposhnik <mshaposhnik@codenvy.com>
2017-01-27 12:50:48 +0200

    fixup! Merge branch 'master' into CHE-3621

--- plugins/plugin-ssh-machine/src/main/java/org/eclipse/che/plugin/machine/ssh/SshMachineModule.java
+++ plugins/plugin-ssh-machine/src/main/java/org/eclipse/che/plugin/machine/ssh/SshMachineModule.java
@@ -24,7 +24,7 @@ public class SshMachineModule extends AbstractModule {
     protected void configure() {
         bind(SshMachineInstanceProvider.class);
 
-        bind(SshMachineFactory.class).to(SshMachineFactory.class);
+        bind(SshMachineFactory.class);
 
         bindConstant().annotatedWith(Names.named("machine.ssh.server.terminal.location")).to("~/che");
 
