50f641d2263adedc9e814ced4b765ff027229d5b
Vitalii Parfonov <vparfonov@codenvy.com>
2017-02-03 12:12:18 +0200

    Decouple testing framework API from Java test specific frameworks (#3999)
    * Decouple testing framework core from Java specific test frameworks.
    
    Signed-off-by: Bartlomiej Laczkowski <bartlomiej.l@zend.com>

---
+++ plugins/plugin-testing-java/plugin-testing-testng/che-plugin-testing-testng-ide/src/main/java/org/eclipse/che/plugin/testing/testng/ide/inject/TestNGGinModule.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.plugin.testing.testng.ide.inject;
+
+import org.eclipse.che.ide.api.extension.ExtensionGinModule;
+import org.eclipse.che.plugin.testing.ide.TestAction;
+import org.eclipse.che.plugin.testing.testng.ide.TestNGTestAction;
+
+import com.google.gwt.inject.client.AbstractGinModule;
+import com.google.gwt.inject.client.multibindings.GinMultibinder;
+
+/**
+ *
+ * @author Mirage Abeysekara
+ */
+@ExtensionGinModule
+public class TestNGGinModule extends AbstractGinModule {
+    @Override
+    protected void configure() {
+        GinMultibinder.newSetBinder(binder(), TestAction.class).addBinding().to(TestNGTestAction.class);
+    }
+}
