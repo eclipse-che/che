66b7dbbed2efc7000224dc69543e5b1eb0343bb3
Tyler Jewell <tjewell@codenvy.com>
2017-02-11 13:48:33 -0800

    fixup
    Signed-off-by: Tyler Jewell <tjewell@codenvy.com>

--- dockerfiles/cli/scripts/post_init.sh
+++ dockerfiles/cli/scripts/post_init.sh
@@ -9,12 +9,5 @@
 post_init() {
   GLOBAL_HOST_IP=${GLOBAL_HOST_IP:=$(docker_run --net host ${BOOTSTRAP_IMAGE_CHEIP})}
   DEFAULT_CHE_HOST=$GLOBAL_HOST_IP
-
-  if [ -z ${CHE_HOST+x} ]; then
-    CHE_HOST_SET_ON_COMMAND_LINE=false
-  else
-    CHE_HOST_SET_ON_COMMAND_LINE=true
-  fi
-
   CHE_HOST=${CHE_HOST:-${DEFAULT_CHE_HOST}}
 }
