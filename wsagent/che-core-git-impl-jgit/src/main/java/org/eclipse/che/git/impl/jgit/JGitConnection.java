d7c93c49a4af127c9d21a4426a608e717e07534c
Mykola Morhun <mmorhun@codenvy.com>
2017-02-21 13:18:12 +0200

    CHE-4015: move adding of deleted files to index in separate method
    Signed-off-by: Mykola Morhun <mmorhun@codenvy.com>

--- wsagent/che-core-git-impl-jgit/src/main/java/org/eclipse/che/git/impl/jgit/JGitConnection.java
+++ wsagent/che-core-git-impl-jgit/src/main/java/org/eclipse/che/git/impl/jgit/JGitConnection.java
@@ -277,21 +277,25 @@ class JGitConnection implements GitConnection {
         try {
             addCommand.call();
 
-            // to add deleted files in index it is required to perform git rm on them
-            Set<String> deletedFiles = getGit().status().call().getMissing();
-            if (!deletedFiles.isEmpty()) {
-                RmCommand rmCommand = getGit().rm();
-                if (filePatterns.contains(".")) {
-                    deletedFiles.forEach(rmCommand::addFilepattern);
-                } else {
-                    filePatterns.forEach(filePattern -> deletedFiles.stream()
-                                                                    .filter(deletedFile -> deletedFile.startsWith(filePattern))
-                                                                    .forEach(rmCommand::addFilepattern));
-                }
-                rmCommand.call();
-            }
+            addDeletedFilesToIndex(filePatterns);
         } catch (GitAPIException exception) {
             throw new GitException(exception.getMessage(), exception);
+        }
+    }
+
+    // to add deleted files in index it is required to perform git rm on them
+    private void addDeletedFilesToIndex(List<String> filePatterns) throws GitAPIException {
+        Set<String> deletedFiles = getGit().status().call().getMissing();
+        if (!deletedFiles.isEmpty()) {
+            RmCommand rmCommand = getGit().rm();
+            if (filePatterns.contains(".")) {
+                deletedFiles.forEach(rmCommand::addFilepattern);
+            } else {
+                filePatterns.forEach(filePattern -> deletedFiles.stream()
+                                                                .filter(deletedFile -> deletedFile.startsWith(filePattern))
+                                                                .forEach(rmCommand::addFilepattern));
+            }
+            rmCommand.call();
         }
     }
 
