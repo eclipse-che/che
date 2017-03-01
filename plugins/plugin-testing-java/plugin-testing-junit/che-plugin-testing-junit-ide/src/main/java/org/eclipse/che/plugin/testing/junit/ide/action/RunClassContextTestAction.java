c7a7fc8dd0c903723bd97cd74a7e9c0ebdd8f6c4
David Festal <dfestal@redhat.com>
2017-03-01 19:01:52 +0100

    Abstract the real test run in a single class used by all action classes
    Signed-off-by: David Festal <dfestal@redhat.com>

--- plugins/plugin-testing-java/plugin-testing-junit/che-plugin-testing-junit-ide/src/main/java/org/eclipse/che/plugin/testing/junit/ide/action/RunClassContextTestAction.java
+++ plugins/plugin-testing-java/plugin-testing-junit/che-plugin-testing-junit-ide/src/main/java/org/eclipse/che/plugin/testing/junit/ide/action/RunClassContextTestAction.java
@@ -10,10 +10,6 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.testing.junit.ide.action;
 
-import static org.eclipse.che.ide.api.notification.StatusNotification.DisplayMode.FLOAT_MODE;
-import static org.eclipse.che.ide.api.notification.StatusNotification.Status.FAIL;
-import static org.eclipse.che.ide.api.notification.StatusNotification.Status.PROGRESS;
-import static org.eclipse.che.ide.api.notification.StatusNotification.Status.SUCCESS;
 import static org.eclipse.che.ide.workspace.perspectives.project.ProjectPerspective.PROJECT_PERSPECTIVE_ID;
 
 import java.util.Arrays;
@@ -22,17 +18,10 @@ import java.util.Map;
 
 import javax.validation.constraints.NotNull;
 
-import org.eclipse.che.api.promises.client.Operation;
-import org.eclipse.che.api.promises.client.OperationException;
-import org.eclipse.che.api.promises.client.Promise;
-import org.eclipse.che.api.promises.client.PromiseError;
-import org.eclipse.che.api.testing.shared.TestResult;
 import org.eclipse.che.ide.api.action.AbstractPerspectiveAction;
 import org.eclipse.che.ide.api.action.ActionEvent;
 import org.eclipse.che.ide.api.app.AppContext;
 import org.eclipse.che.ide.api.notification.NotificationManager;
-import org.eclipse.che.ide.api.notification.StatusNotification;
-import org.eclipse.che.ide.api.resources.Project;
 import org.eclipse.che.ide.api.resources.VirtualFile;
 import org.eclipse.che.ide.api.selection.Selection;
 import org.eclipse.che.ide.api.selection.SelectionAgent;
@@ -48,14 +37,17 @@ import com.google.inject.Inject;
 /**
  *
  * @author Mirage Abeysekara
+ * @author David Festal
  */
-public class RunClassContextTestAction extends AbstractPerspectiveAction {
+public class RunClassContextTestAction extends AbstractPerspectiveAction
+	implements RunTestActionDelegate.Source {
 
     private final NotificationManager notificationManager;
     private final TestResultPresenter presenter;
     private final TestServiceClient service;
     private final AppContext appContext;
     private final SelectionAgent selectionAgent;
+    private RunTestActionDelegate delegate;
 
     @Inject
     public RunClassContextTestAction(JUnitTestResources resources,
@@ -72,45 +64,20 @@ public class RunClassContextTestAction extends AbstractPerspectiveAction {
         this.service = service;
         this.appContext = appContext;
         this.selectionAgent = selectionAgent;
+        this.delegate = new RunTestActionDelegate(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        final StatusNotification notification = new StatusNotification("Running Tests...", PROGRESS, FLOAT_MODE);
-        notificationManager.notify(notification);
         final Selection<?> selection = selectionAgent.getSelection();
         final Object possibleNode = selection.getHeadElement();
         if (possibleNode instanceof FileNode) {
             VirtualFile file = ((FileNode) possibleNode).getData();
-            final Project project = appContext.getRootProject();
             String fqn = JavaUtil.resolveFQN(file);
             Map<String, String> parameters = new HashMap<>();
             parameters.put("fqn", fqn);
             parameters.put("runClass", "true");
-            parameters.put("updateClasspath", "true");
-            Promise<TestResult> testResultPromise = service.getTestResult(project.getPath(), "junit", parameters);
-            testResultPromise.then(new Operation<TestResult>() {
-                @Override
-                public void apply(TestResult result) throws OperationException {
-                    notification.setStatus(SUCCESS);
-                    if (result.isSuccess()) {
-                        notification.setTitle("Test runner executed successfully");
-                        notification.setContent("All tests are passed");
-                    } else {
-                        notification.setTitle("Test runner executed successfully with test failures.");
-                        notification.setContent(result.getFailureCount() + " test(s) failed.\n");
-                    }
-                    presenter.handleResponse(result);
-                }
-            }).catchError(new Operation<PromiseError>() {
-                @Override
-                public void apply(PromiseError exception) throws OperationException {
-                    final String errorMessage = (exception.getMessage() != null) ? exception.getMessage()
-                            : "Failed to run test cases";
-                    notification.setContent(errorMessage);
-                    notification.setStatus(FAIL);
-                }
-            });
+            delegate.doRunTests(e, parameters);
         }
     }
 
@@ -135,4 +102,24 @@ public class RunClassContextTestAction extends AbstractPerspectiveAction {
                 && "java".equals(((FileNode) possibleNode).getData().getExtension());
         e.getPresentation().setEnabled(enable);
     }
+
+	@Override
+	public NotificationManager getNotificationManager() {
+		return notificationManager;
+	}
+
+	@Override
+	public AppContext getAppContext() {
+		return appContext;
+	}
+
+	@Override
+	public TestServiceClient getService() {
+		return service;
+	}
+
+	@Override
+	public TestResultPresenter getPresenter() {
+		return presenter;
+	}
 }
