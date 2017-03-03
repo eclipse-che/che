7bb31e6c78cc92549d592dc344467e03c61bd69f
Oleksii Kurinnyi <okurinnyi@codenvy.com>
2017-03-03 15:42:03 +0200

    CHE-4247: fix bug when project template isn't selected in templates list
    Signed-off-by: Oleksii Kurinnyi <okurinnyi@codenvy.com>

--- dashboard/src/app/projects/create-project/samples/create-project-samples.controller.ts
+++ dashboard/src/app/projects/create-project/samples/create-project-samples.controller.ts
@@ -10,24 +10,23 @@
  */
 'use strict';
 import {CheAPI} from '../../../../components/api/che-api.factory';
-import {CreateProjectController} from '../create-project.controller';
 
 /**
  * This class is handling the controller for the samples part
  * @author Florent Benoit
  */
 export class CreateProjectSamplesController {
-  $rootScope: ng.IRootScopeService;
-  templates: Array<any>;
+  templates: Array<che.IProjectTemplate>;
   selectedTemplateName: string = '';
+
+  currentStackTags: string[];
+  projectSampleOnSelect: Function;
 
   /**
    * Default constructor that is using resource
    * @ngInject for Dependency injection
    */
-  constructor($rootScope: ng.IRootScopeService, cheAPI: CheAPI) {
-    this.$rootScope = $rootScope;
-
+  constructor(cheAPI: CheAPI) {
     this.templates = cheAPI.getProjectTemplate().getAllProjectTemplates();
     if (!this.templates.length) {
       cheAPI.getProjectTemplate().fetchTemplates();
@@ -35,50 +34,47 @@ export class CreateProjectSamplesController {
   }
 
   /**
-   * Callback when a template is selected and also give the controller on which to select the data
-   * @param template: che.IProject -  the selected template
-   * @param createProjectCtrl: CreateProjectController - callback controller
+   * Returns filtered list of templates.
+   *
+   * @return {che.IProjectTemplate[]}
    */
-  selectTemplate(template: che.IProject, createProjectCtrl: CreateProjectController) {
-    // selected item
-    this.selectedTemplateName = template.name;
-    // update source details
-    createProjectCtrl.importProjectData.source.type = template.source.type;
-    createProjectCtrl.importProjectData.source.location = template.source.location;
-    createProjectCtrl.importProjectData.source.parameters = template.source.parameters;
-    // update name, type, description
-    createProjectCtrl.setProjectDescription(template.description);
-    createProjectCtrl.importProjectData.project.type = template.projectType;
-    createProjectCtrl.importProjectData.project.commands = template.commands;
-    createProjectCtrl.importProjectData.project.attributes = template.attributes;
-    createProjectCtrl.importProjectData.project.options = template.options;
-    createProjectCtrl.importProjectData.projects = template.projects;
+  getFilteredTemplates(): che.IProjectTemplate[] {
+    let stackTags = !this.currentStackTags ? [] : this.currentStackTags.map((tag: string) => tag.toLowerCase());
+    return this.templates.filter((template: che.IProjectTemplate) => {
+      let templateTags = template.tags.map((tag: string) => tag.toLowerCase());
+      return stackTags.some((tag: string) => templateTags.indexOf(tag) > -1);
+    });
+  }
 
-    let name: string = template.displayName;
-    // strip space
-    name = name.replace(/\s/g, '_');
-    // strip dot
-    name = name.replace(/\./g, '_');
-    createProjectCtrl.setProjectName(name);
-    // broadcast event
-    this.$rootScope.$broadcast('create-project-samples:selected');
+  /**
+   * Callback when a template is selected and also give the controller on which to select the data
+   * @param template: che.IProjectTemplate -  the selected template
+   */
+  selectTemplate(template: che.IProjectTemplate): void {
+    if (!template) {
+      return;
+    }
+
+    // set selected item
+    this.selectedTemplateName = template.name;
+
+    this.projectSampleOnSelect({template: template});
   }
 
   /**
    * Select the first element in the list
-   * @param templateName: che.IProject
-   * @param createProjectCtrl: CreateProjectController
+   * @param {che.IProjectTemplate} template
    */
-  initItem(template: che.IProject, createProjectCtrl: CreateProjectController): void {
+  initItem(template: che.IProjectTemplate): void {
     if (!template || this.selectedTemplateName === template.name) {
       return;
     }
-    this.selectTemplate(template, createProjectCtrl);
+    this.selectTemplate(template);
   }
 
   /**
    * Helper method used to get the length of keys of the given object
-   * @param items: Array<any>
+   * @param {Array<any>} items
    * @returns {number} length of keys
    */
   getItemsSize(items: Array<any>): number {
