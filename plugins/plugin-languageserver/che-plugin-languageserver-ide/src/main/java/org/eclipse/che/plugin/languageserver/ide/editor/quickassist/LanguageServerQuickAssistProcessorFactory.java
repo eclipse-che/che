4feee7fc4d026d6b1922d82d0d463cb6cf1ec978
Thomas Mäder <tmader@redhat.com>
2017-03-07 14:19:10 +0100

    CHE-3957: Code Action Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/editor/quickassist/LanguageServerQuickAssistProcessorFactory.java
+++ plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/editor/quickassist/LanguageServerQuickAssistProcessorFactory.java
@@ -1,23 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2012-2017 Codenvy, S.A.
+ * Copyright (c) 2017 Red Hat.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Codenvy, S.A. - initial API and implementation
+ *     Red Hat - Initial Contribution
  *******************************************************************************/
-package org.eclipse.che.plugin.languageserver.ide.editor;
+package org.eclipse.che.plugin.languageserver.ide.editor.quickassist;
 
-import org.eclipse.che.ide.api.editor.texteditor.TextEditor;
-
-import io.typefox.lsapi.ServerCapabilities;
+import org.eclipse.che.ide.api.editor.EditorPartPresenter;
 
 /**
- * @author Evgen Vidolob
+ * @author Thomas Mäder
  */
-public interface LanguageServerEditorConfigurationFactory {
-
-    LanguageServerEditorConfiguration build(TextEditor editor, ServerCapabilities capabilities);
+public interface LanguageServerQuickAssistProcessorFactory {
+    LanguageServerQuickAssistProcessor create(EditorPartPresenter editor);
 }
