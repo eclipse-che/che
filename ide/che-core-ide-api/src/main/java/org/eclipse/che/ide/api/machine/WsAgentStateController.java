0d26699f94439f97374923d5b5316b75535936c1
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2017-03-09 14:37:37 +0200

    Test
    Signed-off-by: Vladyslav Zhukovskii <vzhukovskii@codenvy.com>

--- ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/machine/WsAgentStateController.java
+++ ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/machine/WsAgentStateController.java
@@ -193,6 +193,9 @@ public class WsAgentStateController implements ConnectionOpenedHandler, Connecti
         }
         messageBusCallbacks.clear();
 
+
+
+
         for (AsyncCallback<DevMachine> callback : devMachineCallbacks) {
             callback.onSuccess(devMachine);
         }
