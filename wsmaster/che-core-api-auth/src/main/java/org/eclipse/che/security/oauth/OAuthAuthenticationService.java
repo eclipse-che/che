7f2c70179dfb0a6f0f1a2202aabebef62d5fe074
Sun Seng David TAN <sutan@redhat.com>
2017-03-16 13:27:20 +0100

    Set Github token through workspace master Rest API. Added a force act… (#4438)
    Set Github token through workspace master Rest API. Added a force activation property variable to register Github Oauth provider even without client id/secret
    
    Signed-off-by: Sun Seng David Tan <sutan@redhat.com>

--- wsmaster/che-core-api-auth/src/main/java/org/eclipse/che/security/oauth/OAuthAuthenticationService.java
+++ wsmaster/che-core-api-auth/src/main/java/org/eclipse/che/security/oauth/OAuthAuthenticationService.java
@@ -27,9 +27,11 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HttpMethod;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -202,5 +204,28 @@ public class OAuthAuthenticationService {
             throw new BadRequestException("Unsupported OAuth provider " + oauthProviderName);
         }
         return oauth;
+    }
+
+    @POST
+    @Path("token")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void setToken(@Required @QueryParam("oauth_provider") String oauthProvider, //
+                         OAuthToken token) throws ServerException {
+        if (token == null) {
+            throw new ServerException("No token provided");
+        }
+
+        OAuthAuthenticator provider = providers.getAuthenticator(oauthProvider);
+        if (provider == null) {
+            throw new ServerException("\"" + oauthProvider + "\" oauth provider not registered");
+        }
+
+        String userId = EnvironmentContext.getCurrent().getSubject().getUserId();
+
+        try {
+            provider.setToken(userId, token);
+        } catch (IOException e) {
+            throw new ServerException(e.getMessage());
+        }
     }
 }
