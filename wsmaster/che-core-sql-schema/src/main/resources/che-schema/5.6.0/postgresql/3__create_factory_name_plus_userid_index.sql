9cc84e959b90df6135a1c4a6006b7d36ff7b871f
Max Shaposhnik <mshaposhnik@codenvy.com>
2017-03-22 18:02:46 +0200

    fixup! fixup! fixup! CHE-4372; add unique constraing for factory name + user

--- wsmaster/che-core-sql-schema/src/main/resources/che-schema/5.6.0/postgresql/3__create_factory_name_plus_userid_index.sql
+++ wsmaster/che-core-sql-schema/src/main/resources/che-schema/5.6.0/postgresql/3__create_factory_name_plus_userid_index.sql
@@ -17,22 +17,22 @@ UPDATE che_factory
 SET name = concat('f', right(id, 9)) WHERE name IS NULL OR name = '';
 
 WITH dupes AS
-   ( SELECT id, user_id, name
-     FROM che_factory
-     WHERE (name,user_id)
-     IN (SELECT name, user_id
-        FROM che_factory
-        GROUP BY name, user_id
-        HAVING count(*) > 1)
-   ),
-   uniques AS
-   ( WITH q as
-     ( SELECT *, row_number()
-       OVER (PARTITION BY name,user_id ORDER BY name,user_id)
-       AS rn FROM che_factory
-     )
-   SELECT id FROM q WHERE rn = 1
-   )
+  ( SELECT id, user_id, name
+    FROM che_factory
+    WHERE (name,user_id)
+    IN (SELECT name, user_id
+      FROM che_factory
+      GROUP BY name, user_id
+      HAVING count(*) > 1)
+  ),
+  uniques AS
+  ( WITH q as
+    ( SELECT *, row_number()
+      OVER (PARTITION BY name,user_id ORDER BY name,user_id)
+      AS rn FROM che_factory
+    )
+    SELECT id FROM q WHERE rn = 1
+  )
 UPDATE che_factory
 SET name = concat(che_factory.name, '-', right(dupes.id, 9))
 FROM dupes, uniques
