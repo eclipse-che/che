3320d90e7a3ed6d03084655e070d19f2955a59e8
Alexander Garagatyi <agaragatyi@codenvy.com>
2017-03-24 13:22:27 +0200

    fix router Signed-off-by: Alexander Garagatyi <agaragatyi@codenvy.com>

--- agents/go-agents/src/main/go/core/rest/default_router.go
+++ agents/go-agents/src/main/go/core/rest/default_router.go
@@ -25,65 +25,67 @@ type DefaultRouter struct {
 	router *httprouter.Router
 }
 
-type toHandleFunc func(HTTPRouteHandlerFunc) httprouter.Handle
+type basePathChoppingRouter struct {
+	DefaultRouter
+	reg *regexp.Regexp
+}
 
 // NewDefaultRouter creates new instance of default implementation of router.
 // basePath is path that should be cut from origin HTTP path and then matched to restRoutes.
 func NewDefaultRouter(basePath string, restRoutes []RoutesGroup) Router {
 	var jrouter = httprouter.New()
-	defaultRouter := DefaultRouter{
-		router: jrouter,
-	}
 
+	// base path chopping is not needed
 	if basePath == "" {
-		defaultRouter.addRoutes(restRoutes, toHandle)
-	} else {
-		if reg, err := regexp.Compile(basePath); err == nil {
-			defaultRouter.addRoutes(restRoutes, getBasePathChoppingToHandleFunc(reg))
-		} else {
-			panic(fmt.Errorf("Base path '%s' is not a regexp. Error: %s", basePath, err))
+		defaultRouter := &DefaultRouter{
+			router: jrouter,
 		}
+		defaultRouter.addRoutes(restRoutes)
+		return defaultRouter
 	}
 
-	return &defaultRouter
+	// base path chopping is needed
+	reg, err := regexp.Compile(basePath)
+	if err == nil {
+		panic(fmt.Errorf("Base path '%s' is not a regexp. Error: %s", basePath, err))
+	}
+	defaultRouter := &basePathChoppingRouter{
+		DefaultRouter: DefaultRouter{router: jrouter},
+		reg:           reg,
+	}
+	defaultRouter.addRoutes(restRoutes)
+	return defaultRouter
 }
 
 func (router *DefaultRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	router.router.ServeHTTP(w, r)
 }
 
-func (router *DefaultRouter) addRoutes(restRoutes []RoutesGroup, f toHandleFunc) {
+func (router *DefaultRouter) addRoutes(restRoutes []RoutesGroup) {
 	for _, routesGroup := range restRoutes {
 		for _, route := range routesGroup.Items {
 			router.router.Handle(
 				route.Method,
 				route.Path,
-				f(route.HandleFunc),
+				toHandle(route.HandleFunc),
 			)
 		}
 	}
+}
+
+func (router *basePathChoppingRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// if reg is prefix of request path remove the prefix
+	if idx := router.reg.FindStringSubmatchIndex(r.URL.Path); len(idx) != 0 && idx[0] == 0 {
+		r.URL.Path = r.URL.Path[idx[1]:]
+		r.RequestURI = r.RequestURI[idx[1]:]
+	}
+	router.router.ServeHTTP(w, r)
 }
 
 func toHandle(f HTTPRouteHandlerFunc) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		if err := f(w, r, routerParamsAdapter{params: p}); err != nil {
 			WriteError(w, err)
-		}
-	}
-}
-
-func getBasePathChoppingToHandleFunc(reg *regexp.Regexp) toHandleFunc {
-	return func(f HTTPRouteHandlerFunc) httprouter.Handle {
-		return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-			// if reg is prefix of request path remove the prefix
-			if idx := reg.FindStringSubmatchIndex(r.URL.Path); len(idx) != 0 && idx[0] == 0 {
-				r.URL.Path = r.URL.Path[idx[1]:]
-				r.RequestURI = r.RequestURI[idx[1]:]
-			}
-
-			if err := f(w, r, routerParamsAdapter{params: p}); err != nil {
-				WriteError(w, err)
-			}
 		}
 	}
 }
