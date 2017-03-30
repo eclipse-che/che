b9ebdc2cb4c15441536298733ed594d33b7665cd
Alexander Garagatyi <agaragatyi@codenvy.com>
2017-03-30 18:08:42 +0300

    CODENVY-1734: improve docker client (#4620)
    Throw specific exception in docker client
    if exec not found on exec start.
    Signed-off-by: Alexander Garagatyi <agaragatyi@codenvy.com>

--- plugins/plugin-docker/che-plugin-docker-client/src/main/java/org/eclipse/che/plugin/docker/client/json/ExecInfo.java
+++ plugins/plugin-docker/che-plugin-docker-client/src/main/java/org/eclipse/che/plugin/docker/client/json/ExecInfo.java
@@ -94,11 +94,11 @@ public class ExecInfo {
                "id='" + id + '\'' +
                ", container=" + container +
                ", processConfig=" + processConfig +
-               ", openStdout=" + openStdout +
-               ", openStderr=" + openStderr +
-               ", openStdin=" + openStdin +
-               ", running=" + running +
-               ", exitCode=" + exitCode +
+               ", openStdout='" + openStdout + '\'' +
+               ", openStderr='" + openStderr + '\'' +
+               ", openStdin='" + openStdin + '\'' +
+               ", running='" + running + '\'' +
+               ", exitCode='" + exitCode + '\'' +
                '}';
     }
 }
