b15a46385735efff214805ff026ccfbcd2b3d078
Max Shaposhnik <mshaposhnik@codenvy.com>
2017-04-06 11:54:45 +0300

    CHE-3876; Added indexing scripts

---
+++ wsmaster/che-core-sql-schema/src/main/resources/che-schema/5.8.0/2__add_account_pattern_index.sql
@@ -0,0 +1,17 @@
+--
+--  [2012] - [2017] Codenvy, S.A.
+--  All Rights Reserved.
+--
+-- NOTICE:  All information contained herein is, and remains
+-- the property of Codenvy S.A. and its suppliers,
+-- if any.  The intellectual and technical concepts contained
+-- herein are proprietary to Codenvy S.A.
+-- and its suppliers and may be covered by U.S. and Foreign Patents,
+-- patents in process, and are protected by trade secret or copyright law.
+-- Dissemination of this information or reproduction of this material
+-- is strictly forbidden unless prior written permission is obtained
+-- from Codenvy S.A..
+--
+
+CREATE INDEX index_account_pattern ON account(name text_pattern_ops);
+
