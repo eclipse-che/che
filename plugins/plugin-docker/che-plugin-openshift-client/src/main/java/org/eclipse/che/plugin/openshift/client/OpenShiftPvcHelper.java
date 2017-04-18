2510ba885dbe49ffde2d698a535b62834e152907
Mario Loriedo <mloriedo@redhat.com>
2017-04-18 15:45:57 +0200

    Rework PVC management on OpenShift
    - Change how subdirectories are created in pods to
      use a short, terminating job instead of a full deployment
    - Add OpenShiftWorkspaceFilesCleaner class to properly
      notice workspace deleted events
    - Add helper class to manage job pods. For creation, some
      effort is made to avoid attempting to create workspaces
      unnecessarily, but only exists in-memory
    - Workspace deletions are batched together so that removing
      workspaces directories can be done when server is idled,
      avoiding unnecessary PVC mounts
    - Add two new properties: che.openshift.jobs.image and
      che.openshift.jobs.memorylimit, which are used by
      OpenShiftPvcHelper to set up pods
    
    Current issues:
    - Since workspace directories are not deleted immediately,
      attempting to re-create a workspace with the same name
      will result in the previous instance's project to already
      be there. This should have a minor impact.
    - Memory for which workspace dirs have been created is not
      persisted, resulting in potentially unnecessary jobs
    - Openshift workspace files cleaner is included by overwriting
      binding in WsMasterModule instead of using a provider. This
      could be better, but OpenShift integration may be reaching a
      point where a custom module is a better solution.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>
    Signed-off-by: Sun Seng David Tan <sutan@redhat.com>

---
+++ plugins/plugin-docker/che-plugin-openshift-client/src/main/java/org/eclipse/che/plugin/openshift/client/OpenShiftPvcHelper.java
@@ -0,0 +1,232 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.che.plugin.openshift.client;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.ContainerBuilder;
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaimVolumeSource;
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaimVolumeSourceBuilder;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.Quantity;
+import io.fabric8.kubernetes.api.model.Volume;
+import io.fabric8.kubernetes.api.model.VolumeBuilder;
+import io.fabric8.kubernetes.api.model.VolumeMount;
+import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftClient;
+
+/**
+ * Helper class for executing simple commands in a Persistent Volume on Openshift.
+ * <p>
+ * Creates a short-lived Pod using a CentOS image which mounts a specified PVC and
+ * executes a command (either {@code mkdir -p <path>} or {@code rm -rf <path}). Reports
+ * back whether the pod succeeded or failed. Supports multiple paths for one command.
+ * <p>
+ * For mkdir commands, an in-memory list of created workspaces is stored and used to avoid
+ * calling mkdir unnecessarily. However, this list is not persisted, so dir creation is
+ * not tracked between restarts.
+ *
+ * @author amisevsk
+ */
+final class OpenShiftPvcHelper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OpenShiftPvcHelper.class);
+
+    private static final String POD_PHASE_SUCCEEDED        = "Succeeded";
+    private static final String POD_PHASE_FAILED           = "Failed";
+    private static final String[] MKDIR_WORKSPACE_COMMAND  = new String[] {"mkdir", "-p"};
+    private static final String[] RMDIR_WORKSPACE_COMMAND  = new String[] {"rm", "-rf"};
+
+    private static final Set<String> createdWorkspaces = ConcurrentHashMap.newKeySet();
+
+    private final String jobImage;
+    private final String jobMemoryLimit;
+
+    protected enum Command {REMOVE, MAKE}
+
+    @Inject
+    protected OpenShiftPvcHelper(@Named("che.openshift.jobs.image") String jobImage,
+                                 @Named("che.openshift.jobs.memorylimit") String jobMemoryLimit) {
+        this.jobImage = jobImage;
+        this.jobMemoryLimit = jobMemoryLimit;
+    }
+
+    /**
+     * Creates a pod with {@code command} and reports whether it succeeded
+     * @param workspacesPvcName
+     *            name of the PVC to mount
+     * @param projectNamespace
+     *            OpenShift namespace
+     * @param jobNamePrefix
+     *            prefix used for pod metadata name. Name structure will normally
+     *            be {@code <prefix><workspaceDirs>} if only one path is passed, or
+     *            {@code <prefix>batch} if multiple paths are provided
+     * @param command
+     *            command to execute in PVC.
+     * @param workspaceDirs
+     *            list of arguments attached to command. A list of directories to
+     *            create/delete.
+     * @return true if Pod terminates with phase "Succeeded" or mkdir command issued
+     *            for already created worksapce, false otherwise.
+     *
+     * @see Command
+     */
+    protected boolean createJobPod(String workspacesPvcName,
+                                   String projectNamespace,
+                                   String jobNamePrefix,
+                                   Command command,
+                                   String... workspaceDirs) {
+
+        if (workspaceDirs.length == 0) {
+            return true;
+        }
+
+        if (Command.MAKE.equals(command)) {
+            String[] dirsToCreate = filterDirsToCreate(workspaceDirs);
+            if (dirsToCreate.length == 0) {
+                return true;
+            }
+            workspaceDirs = dirsToCreate;
+        }
+
+        VolumeMount vm = new VolumeMountBuilder()
+                .withMountPath("/projects")
+                .withName(workspacesPvcName)
+                .build();
+
+        PersistentVolumeClaimVolumeSource pvcs = new PersistentVolumeClaimVolumeSourceBuilder()
+                .withClaimName(workspacesPvcName)
+                .build();
+
+        Volume volume = new VolumeBuilder()
+                .withPersistentVolumeClaim(pvcs)
+                .withName(workspacesPvcName)
+                .build();
+
+        String[] jobCommand = getCommand(command, "/projects/", workspaceDirs);
+        LOG.info("Executing command {} in PVC {} for {} dirs", jobCommand[0], workspacesPvcName, workspaceDirs.length);
+
+        Map<String, Quantity> limit = Collections.singletonMap("memory", new Quantity(jobMemoryLimit));
+
+        String podName = workspaceDirs.length > 1 ? jobNamePrefix + "batch"
+                                                  : jobNamePrefix + workspaceDirs[0];
+
+        Container container = new ContainerBuilder().withName(podName)
+                                                    .withImage(jobImage)
+                                                    .withImagePullPolicy("IfNotPresent")
+                                                    .withNewSecurityContext()
+                                                        .withPrivileged(false)
+                                                    .endSecurityContext()
+                                                    .withCommand(jobCommand)
+                                                    .withVolumeMounts(vm)
+                                                    .withNewResources()
+                                                        .withLimits(limit)
+                                                    .endResources()
+                                                    .build();
+
+        Pod podSpec = new PodBuilder().withNewMetadata()
+                                       .withName(podName)
+                                   .endMetadata()
+                                   .withNewSpec()
+                                       .withContainers(container)
+                                       .withVolumes(volume)
+                                       .withRestartPolicy("Never")
+                                   .endSpec()
+                                   .build();
+
+
+        try (OpenShiftClient openShiftClient = new DefaultOpenShiftClient()){
+            openShiftClient.pods().inNamespace(projectNamespace).create(podSpec);
+            boolean completed = false;
+            while(!completed) {
+                Pod pod = openShiftClient.pods().inNamespace(projectNamespace).withName(podName).get();
+                String phase = pod.getStatus().getPhase();
+                switch (phase) {
+                    case POD_PHASE_FAILED:
+                        LOG.info("Pod command {} failed", Arrays.toString(jobCommand));
+                    case POD_PHASE_SUCCEEDED:
+                        openShiftClient.resource(pod).delete();
+                        updateCreatedDirs(command, phase, workspaceDirs);
+                        return POD_PHASE_SUCCEEDED.equals(phase);
+                    default:
+                        Thread.sleep(1000);
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return false;
+    }
+
+    private String[] getCommand(Command commandType, String mountPath, String... dirs) {
+        String[] command = new String[0];
+        switch (commandType) {
+            case MAKE :
+                command = MKDIR_WORKSPACE_COMMAND;
+                break;
+            case REMOVE :
+                command = RMDIR_WORKSPACE_COMMAND;
+                break;
+        }
+
+        String[] dirsWithPath = Arrays.asList(dirs).stream()
+                .map(dir -> mountPath + dir)
+                .toArray(String[]::new);
+
+        String[] fullCommand = new String[command.length + dirsWithPath.length];
+
+        System.arraycopy(command, 0, fullCommand, 0, command.length);
+        System.arraycopy(dirsWithPath, 0, fullCommand, command.length, dirsWithPath.length);
+        return fullCommand;
+    }
+
+    private void updateCreatedDirs(Command command, String phase, String... workspaceDirs) {
+        if (!POD_PHASE_SUCCEEDED.equals(phase)) {
+            return;
+        }
+        List<String> dirs = Arrays.asList(workspaceDirs);
+        switch (command) {
+            case MAKE:
+                createdWorkspaces.addAll(dirs);
+                break;
+            case REMOVE:
+                createdWorkspaces.removeAll(dirs);
+                break;
+        }
+    }
+
+    private String[] filterDirsToCreate(String[] allDirs) {
+        List<String> dirs = Arrays.asList(allDirs);
+        List<String> dirsToCreate = new ArrayList<>();
+        for(String dir : dirs) {
+            if (!createdWorkspaces.contains(dir)) {
+                dirsToCreate.add(dir);
+            }
+        }
+        return dirsToCreate.toArray(new String[dirsToCreate.size()]);
+    }
+}
---
+++ plugins/plugin-docker/che-plugin-openshift-client/src/main/java/org/eclipse/che/plugin/openshift/client/OpenShiftWorkspaceFilesCleaner.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.che.plugin.openshift.client;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.core.event.ServerIdleEvent;
+import org.eclipse.che.api.core.model.workspace.Workspace;
+import org.eclipse.che.api.core.notification.EventService;
+import org.eclipse.che.api.core.notification.EventSubscriber;
+import org.eclipse.che.api.workspace.server.WorkspaceFilesCleaner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class used to remove workspace directories in Persistent Volume when a workspace
+ * is delete while running on OpenShift. Deleted workspace directories are stored
+ * in a list. Upon Che server idling, all of these workspaces are deleted simultaneously
+ * from the PVC using a {@link OpenShiftPvcHelper} job.
+ * <p>
+ * Since deleting a workspace does not immediately remove its files, re-creating a workspace
+ * with a previously used name can result in files from the previous workspace still being
+ * present.
+ *
+ * @see WorkspaceFilesCleaner
+ * @author amisevsk
+ */
+@Singleton
+public class OpenShiftWorkspaceFilesCleaner implements WorkspaceFilesCleaner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OpenShiftConnector.class);
+    private static final Set<String> deleteQueue = ConcurrentHashMap.newKeySet();
+    private final String projectNamespace;
+    private final String workspacesPvcName;
+    private final OpenShiftPvcHelper openShiftPvcHelper;
+
+    @Inject
+    public OpenShiftWorkspaceFilesCleaner(EventService eventService,
+                                          OpenShiftPvcHelper openShiftPvcHelper,
+                                          @Named("che.openshift.project") String projectNamespace,
+                                          @Named("che.openshift.workspaces.pvc.name") String workspacesPvcName) {
+        this.projectNamespace = projectNamespace;
+        this.workspacesPvcName = workspacesPvcName;
+        this.openShiftPvcHelper = openShiftPvcHelper;
+        eventService.subscribe(new EventSubscriber<ServerIdleEvent>() {
+            @Override
+            public void onEvent(ServerIdleEvent event) {
+                deleteWorkspacesInQueue(event);
+            }
+        });
+    }
+
+    @Override
+    public void clear(Workspace workspace) throws IOException, ServerException {
+        String workspaceName = workspace.getConfig().getName();
+        if (isNullOrEmpty(workspaceName)) {
+            LOG.error("Could not get workspace name for files removal.");
+            return;
+        }
+        deleteQueue.add(workspaceName);
+    }
+
+    private void deleteWorkspacesInQueue(ServerIdleEvent event) {
+        List<String> deleteQueueCopy = new ArrayList<>(deleteQueue);
+        String[] dirsToDelete = deleteQueueCopy.toArray(new String[deleteQueueCopy.size()]);
+
+        LOG.info("Deleting {} workspaces on PVC {}", deleteQueueCopy.size(), workspacesPvcName);
+        boolean successful = openShiftPvcHelper.createJobPod(workspacesPvcName,
+                                                             projectNamespace,
+                                                             "delete-",
+                                                             OpenShiftPvcHelper.Command.REMOVE,
+                                                             dirsToDelete);
+        if (successful) {
+            deleteQueue.removeAll(deleteQueueCopy);
+        }
+    }
+}
