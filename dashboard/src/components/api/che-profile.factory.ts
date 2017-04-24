5ab52d4a41fa56a3bbcb2186b41882221901b82f
Oleksii Orel <oorel@codenvy.com>
2017-04-24 10:25:57 +0300

    improve paging for factories
    Signed-off-by: Oleksii Orel <oorel@codenvy.com>

--- dashboard/src/components/api/che-profile.factory.ts
+++ dashboard/src/components/api/che-profile.factory.ts
@@ -10,6 +10,10 @@
  */
 'use strict';
 
+interface IProfileResource<T> extends ng.resource.IResourceClass<T> {
+  getById(data: { userId: string }): ng.resource.IResource<T>;
+  setAttributes(data: { attributes: che.IProfileAttributes }): ng.resource.IResource<T>;
+}
 
 /**
  * This class is handling the profile API retrieval
@@ -18,17 +22,35 @@
 export class CheProfile {
 
   /**
+   * Angular Promise service.
+   */
+  private $q: ng.IQService;
+  /**
+   * Angular Resource service.
+   */
+  private $resource: ng.resource.IResourceService;
+  /**
+   * Angular Http service.
+   */
+  private $http: ng.IHttpService;
+
+  private profile: che.IProfile;
+  private profileIdMap: Map<string, che.IProfile>;
+  private remoteProfileAPI: IProfileResource<che.IProfile>;
+
+  /**
    * Default constructor that is using resource
    * @ngInject for Dependency injection
    */
-  constructor($resource, $http) {
+  constructor($q: ng.IQService, $resource: ng.resource.IResourceService, $http: ng.IHttpService) {
+    this.$q = $q;
     this.$resource = $resource;
 
     // http is used for sending data with DELETE method (angular is not sending any data by default with DELETE)
     this.$http = $http;
 
     // remote call
-    this.remoteProfileAPI = this.$resource('/api/profile', {}, {
+    this.remoteProfileAPI = <IProfileResource<che.IProfile>>this.$resource('/api/profile', {}, {
       getById: {method: 'GET', url: '/api/profile/:userId'},
       setAttributes: {method: 'PUT', url: '/api/profile/attributes'}
     });
@@ -49,29 +71,13 @@ export class CheProfile {
   }
 
   /**
-   * Gets the full name if it possible
-   * @returns {string} full name
-   */
-  getFullName(attributes) {
-    if (!attributes) {
-      return '';
-    }
-    let firstName = attributes.firstName;
-    if (!firstName) {
-      firstName = '';
-    }
-    let lastName = attributes.lastName;
-    if (!lastName) {
-      lastName = '';
-    }
-
-    return firstName + ' ' + lastName;
-  }
-
-  /**
    * Gets the profile data
+   * @returns {ng.IPromise<che.IProfile>} the promise
    */
-  fetchProfile() {
+  fetchProfile(): ng.IPromise<che.IProfile> {
+    if (this.profile && !this.profile.$resolved) {
+      return this.$q.when(this.profile);
+    }
     let profile = this.remoteProfileAPI.get();
     // if we don't yet have data
     if (!this.profile) {
@@ -79,23 +85,26 @@ export class CheProfile {
       this.profile = profile;
     }
 
-    let profilePromise = profile.$promise;
-
-    profilePromise.then((profile) => {
-      // update profile data if we have new value
-      this.profile = profile;
+    return profile.$promise.then(() => {
+      if (!angular.equals(this.profile, profile)) {
+        this.profile = profile;
+      }
       this.profileIdMap.set(profile.userId, profile);
+      return this.profile;
+    }, (error: any) => {
+      if (error && error.status === 304) {
+        return this.profile;
+      }
+      return this.$q.reject(error);
     });
-
-    return profilePromise;
   }
 
   /**
    * Set the profile attributes data
-   * @param attributes
-   * @returns {*} the promise
+   * @param attributes {che.IProfileAttributes}
+   * @returns {ng.IPromise<any>} the promise
    */
-  setAttributes(attributes) {
+  setAttributes(attributes: che.IProfileAttributes): ng.IPromise<any> {
     let promise = this.remoteProfileAPI.setAttributes(attributes).$promise;
 
     return promise;
@@ -104,21 +113,23 @@ export class CheProfile {
   /**
    * Fetch the profile from the given userId
    * @param userId the user for which we will call remote api and get promise
-   * @returns {*} the promise
+   * @returns {ng.IPromise<che.IProfile>} the promise
    */
-  fetchProfileId(userId) {
+  fetchProfileId(userId: string): ng.IPromise<che.IProfile> {
     let promise = this.remoteProfileAPI.getById({userId: userId}).$promise;
-    let parsedResultPromise = promise.then((profile) => {
+
+    return promise.then((profile: che.IProfile) => {
       this.profileIdMap.set(userId, profile);
+      return profile;
+    }, (error: any) => {
+      if (error && error.status === 304) {
+        return this.profileIdMap.get(userId);
+      }
+      return this.$q.reject(error);
     });
-
-    return parsedResultPromise;
-
   }
 
-  getProfileFromId(userId) {
+  getProfileFromId(userId: string): che.IProfile {
     return this.profileIdMap.get(userId);
   }
-
-
 }
