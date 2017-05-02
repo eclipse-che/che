d082b32eeb844af88faea4323866618272d9404f
David Festal <dfestal@redhat.com>
2017-05-02 14:29:19 +0200

    Rework top test menu items
    https://issues.jboss.org/browse/CHE-227
    
    
    Signed-off-by: David Festal <dfestal@redhat.com>

--- plugins/plugin-testing-java/plugin-testing-junit/che-plugin-testing-junit-ide/src/main/java/org/eclipse/che/plugin/testing/junit/ide/action/RunAllTestAction.java
+++ plugins/plugin-testing-java/plugin-testing-junit/che-plugin-testing-junit-ide/src/main/java/org/eclipse/che/plugin/testing/junit/ide/action/RunAllTestAction.java
@@ -10,15 +10,28 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.testing.junit.ide.action;
 
+import static org.eclipse.che.ide.workspace.perspectives.project.ProjectPerspective.PROJECT_PERSPECTIVE_ID;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.constraints.NotNull;
+
+import org.eclipse.che.ide.api.action.AbstractPerspectiveAction;
 import org.eclipse.che.ide.api.action.ActionEvent;
 import org.eclipse.che.ide.api.app.AppContext;
-import org.eclipse.che.ide.api.editor.EditorAgent;
-import org.eclipse.che.ide.api.filetypes.FileTypeRegistry;
 import org.eclipse.che.ide.api.notification.NotificationManager;
-import org.eclipse.che.ide.ext.java.client.action.JavaEditorAction;
+import org.eclipse.che.ide.api.resources.Container;
+import org.eclipse.che.ide.api.resources.File;
+import org.eclipse.che.ide.api.resources.Project;
+import org.eclipse.che.ide.api.resources.Resource;
+import org.eclipse.che.ide.api.resources.VirtualFile;
+import org.eclipse.che.ide.api.selection.Selection;
+import org.eclipse.che.ide.api.selection.SelectionAgent;
+import org.eclipse.che.ide.ext.java.client.util.JavaUtil;
+import org.eclipse.che.ide.resources.tree.ContainerNode;
+import org.eclipse.che.ide.resources.tree.FileNode;
 import org.eclipse.che.plugin.testing.ide.TestServiceClient;
 import org.eclipse.che.plugin.testing.ide.action.RunTestActionDelegate;
 import org.eclipse.che.plugin.testing.ide.view.TestResultPresenter;
@@ -31,35 +44,63 @@ import com.google.inject.Inject;
  * @author Mirage Abeysekara
  * @author David Festal
  */
-public class RunAllTestAction extends JavaEditorAction
-                              implements RunTestActionDelegate.Source {
+public class RunAllTestAction extends AbstractPerspectiveAction
+                                     implements RunTestActionDelegate.Source {
 
     private final NotificationManager   notificationManager;
     private final TestResultPresenter   presenter;
     private final TestServiceClient     service;
+    private final AppContext            appContext;
+    private final SelectionAgent        selectionAgent;
     private final RunTestActionDelegate delegate;
 
     @Inject
     public RunAllTestAction(JUnitTestResources resources,
-                            NotificationManager notificationManager,
-                            EditorAgent editorAgent,
-                            TestResultPresenter presenter,
-                            FileTypeRegistry fileTypeRegistry,
-                            TestServiceClient service,
-                            JUnitTestLocalizationConstant localization) {
-        super(localization.actionRunAllTitle(), localization.actionRunAllDescription(), resources.testAllIcon(),
-              editorAgent, fileTypeRegistry);
+                                   NotificationManager notificationManager,
+                                   AppContext appContext,
+                                   TestResultPresenter presenter,
+                                   TestServiceClient service,
+                                   SelectionAgent selectionAgent,
+                                   JUnitTestLocalizationConstant localization) {
+        super(Arrays.asList(PROJECT_PERSPECTIVE_ID), localization.actionRunAllTitle(),
+              localization.actionRunAllDescription(), null, resources.testAllIcon());
         this.notificationManager = notificationManager;
-        this.editorAgent = editorAgent;
         this.presenter = presenter;
         this.service = service;
+        this.appContext = appContext;
+        this.selectionAgent = selectionAgent;
         this.delegate = new RunTestActionDelegate(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Map<String, String> parameters = new HashMap<>();
-        delegate.doRunTests(e, parameters);
+        Resource resource = appContext.getResource();
+        if (resource != null) {
+            Project project = resource.getProject();
+            if (project != null) {
+                Map<String, String> parameters = new HashMap<>();
+                delegate.doRunTests(e, parameters);
+            }
+        }
+    }
+
+    @Override
+    public void updateInPerspective(@NotNull ActionEvent e) {
+        Resource resource = appContext.getResource();
+        if (resource == null) {
+            e.getPresentation().setEnabledAndVisible(false);
+        }
+        
+        Project project = resource.getProject();
+        if (project == null) {
+            e.getPresentation().setEnabledAndVisible(false);
+        }
+        
+        e.getPresentation().setVisible(true);
+
+        String projectType = project.getType();
+        boolean enable = "maven".equals(projectType);
+        e.getPresentation().setEnabled(enable);
     }
 
     @Override
@@ -81,7 +122,7 @@ public class RunAllTestAction extends JavaEditorAction
     public TestResultPresenter getPresenter() {
         return presenter;
     }
-    
+
     @Override
     public String getTestingFramework() {
         return "junit";
