47da1cd7398161dd16e2cc678d10d792c39e23a0
Bartlomiej Laczkowski <bartlomiej.l@zend.com>
2017-05-31 16:16:19 +0200

    Small fixup after merge with master.
    Signed-off-by: Bartlomiej Laczkowski <bartlomiej.l@zend.com>

--- plugins/plugin-testing-java/plugin-testing-junit/che-plugin-testing-junit-ide/src/main/java/org/eclipse/che/plugin/testing/junit/ide/inject/JUnitGinModule.java
+++ plugins/plugin-testing-java/plugin-testing-junit/che-plugin-testing-junit-ide/src/main/java/org/eclipse/che/plugin/testing/junit/ide/inject/JUnitGinModule.java
@@ -12,7 +12,7 @@ package org.eclipse.che.plugin.testing.junit.ide.inject;
 
 import org.eclipse.che.ide.api.extension.ExtensionGinModule;
 import org.eclipse.che.plugin.testing.ide.TestAction;
-import org.eclipse.che.plugin.testing.junit.ide.JUnitTestActionGroup;
+import org.eclipse.che.plugin.testing.junit.ide.JUnitTestAction;
 
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.gwt.inject.client.multibindings.GinMultibinder;
@@ -25,6 +25,6 @@ import com.google.gwt.inject.client.multibindings.GinMultibinder;
 public class JUnitGinModule extends AbstractGinModule {
     @Override
     protected void configure() {
-        GinMultibinder.newSetBinder(binder(), TestAction.class).addBinding().to(JUnitTestActionGroup.class);
+        GinMultibinder.newSetBinder(binder(), TestAction.class).addBinding().to(JUnitTestAction.class);
     }
 }
