2c9f78fa7a26950729fe11b3c5f23ea5a463ccd6
Anatoliy Bazko <abazko@codenvy.com>
2017-06-08 16:41:59 +0300

    Don't share breakpoints between workspaces (#5288)
    CHE-5288: Don't share breakpoints between workspaces

---
+++ ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/debug/BreakpointStorage.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.ide.api.debug;
+
+import org.eclipse.che.api.promises.client.Promise;
+
+import java.util.List;
+
+/**
+ * Preserves and loads breakpoints for the active workspace.
+ *
+ * @author Anatolii Bazko
+ */
+public interface BreakpointStorage {
+
+    /**
+     * Preserves breakpoints into a storage.
+     *
+     * @param breakpoints
+     *      the list of the breakpoints
+     */
+    void addAll(List<Breakpoint> breakpoints);
+
+    /**
+     * Preserve a single breakpoints into a storage.
+     *
+     * @param breakpoint
+     *      the breakpoint
+     */
+    void add(Breakpoint breakpoint);
+
+    /**
+     * Removes breakpoints out of the storage.
+     *
+     * @param breakpoint
+     *      the breakpoint
+     */
+    void delete(Breakpoint breakpoint);
+
+
+    /**
+     * Removes breakpoints out of the storage.
+     *
+     * @param breakpoints
+     *      the list of the breakpoints
+     */
+    void deleteAll(List<Breakpoint> breakpoints);
+
+    /**
+     * Clears storage.
+     */
+    void clear();
+
+    /**
+     * Loads all breakpoints out of the storage.
+     *
+     * @return the list of the breakpoints
+     */
+    Promise<List<Breakpoint>> readAll();
+}
