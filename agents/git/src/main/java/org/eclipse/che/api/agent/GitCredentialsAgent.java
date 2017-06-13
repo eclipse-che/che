7b6cdeb04cdb7dca5d0b4e4e152276606a72301c
Igor Vinokur <ivinokur@codenvy.com>
2017-06-13 11:35:34 +0300

    fixup! CHE-5169: Add Git agent

--- agents/git/src/main/java/org/eclipse/che/api/agent/GitCredentialsAgent.java
+++ agents/git/src/main/java/org/eclipse/che/api/agent/GitCredentialsAgent.java
@@ -19,22 +19,21 @@ import org.eclipse.che.api.agent.shared.model.impl.BasicAgent;
 import java.io.IOException;
 
 /**
- * Git agent.
- * Creates sh script that retrieves ssh keys,
- * Git username and email from user preferences via API.
- * The script executes on each git ssh operation.
+ * Git credentials agent.
+ * Creates sh script that retrieves SSH keys from user preferences for console Git SSH operations.
+ * Injects Git username and email from user preferences to console Git preferences.
  *
  * @see Agent
  *
  * @author Igor Vinokur
  */
 @Singleton
-public class GitAgent extends BasicAgent {
+public class GitCredentialsAgent extends BasicAgent {
     private static final String AGENT_DESCRIPTOR = "org.eclipse.che.git.json";
     private static final String AGENT_SCRIPT     = "org.eclipse.che.git.script.sh";
 
     @Inject
-    public GitAgent() throws IOException {
+    public GitCredentialsAgent() throws IOException {
         super(AGENT_DESCRIPTOR, AGENT_SCRIPT);
     }
 }
