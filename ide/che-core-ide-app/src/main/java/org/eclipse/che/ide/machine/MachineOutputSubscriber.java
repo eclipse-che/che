7ec4dacb8192bdc9ba36579e6706989236ae63ba
Artem Zatsarynnyi <azatsary@redhat.com>
2017-06-15 11:47:05 +0300

    fix typo

--- ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/machine/MachineOutputSubscriber.java
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/machine/MachineOutputSubscriber.java
@@ -18,7 +18,7 @@ import org.eclipse.che.api.core.jsonrpc.commons.RequestTransmitter;
 import org.eclipse.che.ide.api.app.AppContext;
 import org.eclipse.che.ide.api.machine.events.MachineStartingEvent;
 
-/** Subscribes to the output of every started machine. */
+/** Subscribes to the output of each started machine. */
 @Singleton
 class MachineOutputSubscriber {
 
