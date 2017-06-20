1c36c241c1583d02f019448e82117a82e5ee9740
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2017-06-20 11:41:46 +0300

    Refactor method body

--- core/che-core-api-core/src/main/java/org/eclipse/che/api/core/jsonrpc/commons/RequestHandlerManager.java
+++ core/che-core-api-core/src/main/java/org/eclipse/che/api/core/jsonrpc/commons/RequestHandlerManager.java
@@ -135,18 +135,42 @@ public class RequestHandlerManager {
     }
 
     public synchronized boolean deregister(String method) {
+        Category category = methodToCategory.remove(method);
 
-        oneToOneHandlers.remove(method);
-        oneToManyHandlers.remove(method);
-        oneToNoneHandlers.remove(method);
-        manyToOneHandlers.remove(method);
-        manyToManyHandlers.remove(method);
-        manyToNoneHandlers.remove(method);
-        noneToOneHandlers.remove(method);
-        noneToManyHandlers.remove(method);
-        noneToNoneHandlers.remove(method);
+        if (category == null) {
+            return false;
+        }
 
-        return methodToCategory.remove(method) != null;
+        switch (category) {
+            case ONE_TO_ONE:
+                oneToOneHandlers.remove(method);
+                break;
+            case ONE_TO_MANY:
+                oneToManyHandlers.remove(method);
+                break;
+            case ONE_TO_NONE:
+                oneToNoneHandlers.remove(method);
+                break;
+            case MANY_TO_ONE:
+                manyToOneHandlers.remove(method);
+                break;
+            case MANY_TO_MANY:
+                manyToManyHandlers.remove(method);
+                break;
+            case MANY_TO_NONE:
+                manyToNoneHandlers.remove(method);
+                break;
+            case NONE_TO_ONE:
+                noneToOneHandlers.remove(method);
+                break;
+            case NONE_TO_MANY:
+                noneToManyHandlers.remove(method);
+                break;
+            case NONE_TO_NONE:
+                noneToNoneHandlers.remove(method);
+        }
+
+        return true;
     }
 
     public void handle(String endpointId, String requestId, String method, JsonRpcParams params) {
