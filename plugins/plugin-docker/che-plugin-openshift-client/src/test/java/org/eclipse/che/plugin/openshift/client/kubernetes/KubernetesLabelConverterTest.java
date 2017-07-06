8c25c2ac7db8befa551758337b7b6070ee881d96
Florent BENOIT <fbenoit@redhat.com>
2017-07-06 14:01:46 +0200

    Sort imports
    Change-Id: Ia8e2b2facb565458a972f0bd42da6cc4a1d525c1
    Signed-off-by: Florent BENOIT <fbenoit@redhat.com>

--- plugins/plugin-docker/che-plugin-openshift-client/src/test/java/org/eclipse/che/plugin/openshift/client/kubernetes/KubernetesLabelConverterTest.java
+++ plugins/plugin-docker/che-plugin-openshift-client/src/test/java/org/eclipse/che/plugin/openshift/client/kubernetes/KubernetesLabelConverterTest.java
@@ -10,13 +10,13 @@
  *******************************************************************************/
 package org.eclipse.che.plugin.openshift.client.kubernetes;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class KubernetesLabelConverterTest {
 
--- plugins/plugin-docker/che-plugin-openshift-client/src/test/java/org/eclipse/che/plugin/openshift/client/kubernetes/KubernetesStringUtilsTest.java
+++ plugins/plugin-docker/che-plugin-openshift-client/src/test/java/org/eclipse/che/plugin/openshift/client/kubernetes/KubernetesStringUtilsTest.java
@@ -11,11 +11,11 @@
 
 package org.eclipse.che.plugin.openshift.client.kubernetes;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-
 import org.apache.commons.lang.RandomStringUtils;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class KubernetesStringUtilsTest {
 
