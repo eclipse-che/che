6d05f07c6031a2cdf7fcce71d9ecfcc850b5e31f
sudaraka94 <sudarakayasindu@gmail.com>
2017-07-07 14:13:36 +0530

    created a project for testing rest

---
+++ wsagent/che-datasource-api/src/main/java/DatasourceModule.java
@@ -0,0 +1,12 @@
+import com.google.inject.AbstractModule;
+import org.eclipse.che.inject.DynaModule;
+/**
+ * Created by sudaraka on 7/7/17.
+ */
+@DynaModule
+public class DatasourceModule extends AbstractModule{
+    @Override
+    protected void configure() {
+        bind(DatasourceService.class);
+    }
+}
