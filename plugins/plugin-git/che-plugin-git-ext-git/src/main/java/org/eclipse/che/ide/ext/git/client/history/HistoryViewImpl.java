3b58ee1b68955daabc4f1fd7b6069e491d8bdab6
Mykola Morhun <mmorhun@redhat.com>
2017-07-19 12:06:34 +0300

    CHE-5152: Apply max scroll fix for Git History window
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- plugins/plugin-git/che-plugin-git-ext-git/src/main/java/org/eclipse/che/ide/ext/git/client/history/HistoryViewImpl.java
+++ plugins/plugin-git/che-plugin-git-ext-git/src/main/java/org/eclipse/che/ide/ext/git/client/history/HistoryViewImpl.java
@@ -208,7 +208,9 @@ public class HistoryViewImpl extends Window implements HistoryView {
 
     @UiHandler("revisionsPanel")
     public void onPanelScrolled(ScrollEvent ignored) {
-        if (revisionsPanel.getVerticalScrollPosition() == revisionsPanel.getMaximumVerticalScrollPosition()) {
+        // We cannot rely on exact equality of scroll positions because GWT sometimes round such values
+        // and it is possible that the actual max scroll position is a pixel less then declared.
+        if (revisionsPanel.getMaximumVerticalScrollPosition() - revisionsPanel.getVerticalScrollPosition() <= 1) {
             // to avoid autoscrolling to selected item
             revisionsPanel.getElement().focus();
 
