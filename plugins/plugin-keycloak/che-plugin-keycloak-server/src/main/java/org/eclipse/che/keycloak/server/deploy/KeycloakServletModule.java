01d06f3d35d207607cb149645fceeafb1c0fb53f
Max Shaposhnik <mshaposh@redhat.com>
2017-08-16 15:08:45 +0300

    Fix regexp

--- plugins/plugin-keycloak/che-plugin-keycloak-server/src/main/java/org/eclipse/che/keycloak/server/deploy/KeycloakServletModule.java
+++ plugins/plugin-keycloak/che-plugin-keycloak-server/src/main/java/org/eclipse/che/keycloak/server/deploy/KeycloakServletModule.java
@@ -16,15 +16,17 @@ import org.eclipse.che.keycloak.server.KeycloakAuthenticationFilter;
 import org.eclipse.che.keycloak.server.KeycloakEnvironmentInitalizationFilter;
 
 import javax.inject.Singleton;
+import java.util.regex.Pattern;
 
 
 public class KeycloakServletModule extends ServletModule {
     @Override
     protected void configureServlets() {
         bind(KeycloakAuthenticationFilter.class).in(Singleton.class);
+        Pattern compile = Pattern.compile("^(?!.*(/websocket/|/docs/))(?!.*(/ws/?|/eventbus/?|/settings/?)$).*");
 
         // Not contains '/websocket' and not ends with '/ws' or '/eventbus' or '/settings/'
-        filterRegex("^(?!.*/websocket)(?!.*(/ws|/eventbus|/settings)$).*").through(KeycloakAuthenticationFilter.class);
-        filterRegex("^(?!.*/websocket)(?!.*(/ws|/eventbus|/settings)$).*").through(KeycloakEnvironmentInitalizationFilter.class);
+        filterRegex("^(?!.*(/websocket/|/docs/))(?!.*(/ws/?|/eventbus/?|/settings/?)$).*").through(KeycloakAuthenticationFilter.class);
+        filterRegex("^(?!.*(/websocket/|/docs/))(?!.*(/ws/?|/eventbus/?|/settings/?)$).*").through(KeycloakEnvironmentInitalizationFilter.class);
     }
 }
