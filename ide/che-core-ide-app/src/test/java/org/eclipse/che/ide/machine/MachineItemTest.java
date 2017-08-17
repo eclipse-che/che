f728a9f2cd8d7cac6a8d1caaa711010c67a9eaa3
Mihail Kuznyetsov <mkuznets@redhat.com>
2017-08-17 16:53:12 +0300

    Change copyright owner on remaining files

--- ide/che-core-ide-app/src/test/java/org/eclipse/che/ide/machine/MachineItemTest.java
+++ ide/che-core-ide-app/src/test/java/org/eclipse/che/ide/machine/MachineItemTest.java
@@ -10,14 +10,14 @@
  *******************************************************************************/
 package org.eclipse.che.ide.machine;
 /*******************************************************************************
- * Copyright (c) 2012-2017 Codenvy, S.A.
+ * Copyright (c) 2012-2017 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Codenvy, S.A. - initial API and implementation
+ *   Red Hat, Inc. - initial API and implementation
  *******************************************************************************/
 
 import org.eclipse.che.api.core.rest.shared.dto.Link;
