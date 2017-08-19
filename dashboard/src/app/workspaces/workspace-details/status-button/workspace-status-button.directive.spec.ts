8615a37809e0213de7225491d460a61ec699c032
Oleksii Orel <oorel@redhat.com>
2017-08-19 12:58:41 +0300

    Change copyright owner to "Red Hat, Inc" on remaining files
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- dashboard/src/app/workspaces/workspace-details/status-button/workspace-status-button.directive.spec.ts
+++ dashboard/src/app/workspaces/workspace-details/status-button/workspace-status-button.directive.spec.ts
@@ -1,14 +1,13 @@
 /*
- * Copyright (c) 2015-2017 Codenvy, S.A.
+ * Copyright (c) 2015-2017 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Codenvy, S.A. - initial API and implementation
+ *   Red Hat, Inc. - initial API and implementation
  */
-
 import {CheHttpBackend} from '../../../../components/api/test/che-http-backend';
 interface ITestScope extends ng.IRootScopeService {
   model: {
