0a9a53205742471c7c48b1948caaff623eb5e426
Thomas Mäder <tmader@redhat.com>
2017-08-21 12:01:47 +0200

    Turn off Java editor

--- plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/JavaEditorExtension.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/JavaEditorExtension.java
@@ -29,7 +29,7 @@ public class JavaEditorExtension {
       final JavaResources javaResources,
       Resources resources) {
     // register editor provider
-    editorRegistry.registerDefaultEditor(javaFile, javaEditorProvider);
+    //editorRegistry.registerDefaultEditor(javaFile, javaEditorProvider);
     editorRegistry.registerDefaultEditor(classFile, javaEditorProvider);
 
     javaResources.css().ensureInjected();
