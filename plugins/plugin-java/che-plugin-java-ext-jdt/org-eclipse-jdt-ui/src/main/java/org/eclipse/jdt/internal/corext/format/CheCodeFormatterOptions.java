cedea7d2fe718b3dd5ac593faf364fc829677b0e
Valeriy Svydenko <vsvydenk@redhat.com>
2017-08-23 16:12:30 +0300

    CHE-5721: fix license

--- plugins/plugin-java/che-plugin-java-ext-jdt/org-eclipse-jdt-ui/src/main/java/org/eclipse/jdt/internal/corext/format/CheCodeFormatterOptions.java
+++ plugins/plugin-java/che-plugin-java-ext-jdt/org-eclipse-jdt-ui/src/main/java/org/eclipse/jdt/internal/corext/format/CheCodeFormatterOptions.java
@@ -1,10 +1,10 @@
 /**
  * ***************************************************************************** Copyright (c)
- * 2012-2015 Codenvy, S.A. All rights reserved. This program and the accompanying materials are made
+ * 2012-2015 Red Hat, Inc. All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
- * <p>Contributors: Codenvy, S.A. - initial API and implementation
+ * <p>Contributors: Red Hat, Inc. - initial API and implementation
  * *****************************************************************************
  */
 package org.eclipse.jdt.internal.corext.format;
