c500bb4ce7b095e0b8260d5623c1c8e313b673c9
Thomas Mäder <tmader@redhat.com>
2017-08-24 10:08:38 +0200

    License header, formatting

--- plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaLanguageServerLauncher.java
+++ plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaLanguageServerLauncher.java
@@ -1,23 +1,22 @@
-/*******************************************************************************
- * Copyright (c) 2012-2017 Codenvy, S.A.
+/*
+ * Copyright (c) 2012-2017 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Codenvy, S.A. - initial API and implementation
- *******************************************************************************/
+ *   Red Hat, Inc. - initial API and implementation
+ */
 package org.eclipse.che.plugin.java.languageserver;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-
-import javax.security.auth.callback.LanguageCallback;
-
 import org.eclipse.che.api.languageserver.exception.LanguageServerException;
 import org.eclipse.che.api.languageserver.launcher.LanguageServerLauncherTemplate;
 import org.eclipse.che.api.languageserver.registry.DocumentFilter;
@@ -27,10 +26,6 @@ import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.LanguageServer;
 
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
-
-
 /**
  * @author Evgen Vidolob
  * @author Anatolii Bazko
@@ -38,48 +33,55 @@ import com.google.inject.Singleton;
  */
 @Singleton
 public class JavaLanguageServerLauncher extends LanguageServerLauncherTemplate {
-    private static final String                    REGEX       = ".*\\.java";
-    private static final LanguageServerDescription DESCRIPTION = createServerDescription();
+  private static final String REGEX = ".*\\.java";
+  private static final LanguageServerDescription DESCRIPTION = createServerDescription();
 
-    private final Path launchScript;
+  private final Path launchScript;
 
-    @Inject
-    public JavaLanguageServerLauncher() {
-        launchScript = Paths.get(System.getenv("HOME"), "che/ls-java/launch.sh");
+  @Inject
+  public JavaLanguageServerLauncher() {
+    launchScript = Paths.get(System.getenv("HOME"), "che/ls-java/launch.sh");
+  }
+
+  @Override
+  public boolean isAbleToLaunch() {
+    return Files.exists(launchScript);
+  }
+
+  protected LanguageServer connectToLanguageServer(
+      final Process languageServerProcess, LanguageClient client) {
+    Launcher<LanguageServer> launcher =
+        Launcher.createLauncher(
+            client,
+            LanguageServer.class,
+            languageServerProcess.getInputStream(),
+            languageServerProcess.getOutputStream());
+    launcher.startListening();
+    return launcher.getRemoteProxy();
+  }
+
+  protected Process startLanguageServerProcess(String projectPath) throws LanguageServerException {
+    ProcessBuilder processBuilder = new ProcessBuilder(launchScript.toString());
+    processBuilder.directory(launchScript.getParent().toFile());
+    processBuilder.redirectInput(ProcessBuilder.Redirect.PIPE);
+    processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
+    try {
+      return processBuilder.start();
+    } catch (IOException e) {
+      throw new LanguageServerException("Can't start Java language server", e);
     }
+  }
 
-    @Override
-    public boolean isAbleToLaunch() {
-        return Files.exists(launchScript);
-    }
+  public LanguageServerDescription getDescription() {
+    return DESCRIPTION;
+  }
 
-    protected LanguageServer connectToLanguageServer(final Process languageServerProcess, LanguageClient client) {
-        Launcher<LanguageServer> launcher = Launcher.createLauncher(client, LanguageServer.class,
-                                                                    languageServerProcess.getInputStream(),
-                                                                    languageServerProcess.getOutputStream());
-        launcher.startListening();
-        return launcher.getRemoteProxy();
-    }
-
-    protected Process startLanguageServerProcess(String projectPath) throws LanguageServerException {
-        ProcessBuilder processBuilder = new ProcessBuilder(launchScript.toString());
-        processBuilder.directory(launchScript.getParent().toFile());
-        processBuilder.redirectInput(ProcessBuilder.Redirect.PIPE);
-        processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
-        try {
-            return processBuilder.start();
-        } catch (IOException e) {
-            throw new LanguageServerException("Can't start Java language server", e);
-        }
-    }
-
-    public LanguageServerDescription getDescription() {
-        return DESCRIPTION;
-    }
-
-    private static LanguageServerDescription createServerDescription() {
-        LanguageServerDescription description = new LanguageServerDescription("org.eclipse.che.plugin.java.languageserver", null,
-                        Arrays.asList(new DocumentFilter(JavaModule.LANGUAGE_ID, REGEX, null)));
-        return description;
-    }
+  private static LanguageServerDescription createServerDescription() {
+    LanguageServerDescription description =
+        new LanguageServerDescription(
+            "org.eclipse.che.plugin.java.languageserver",
+            null,
+            Arrays.asList(new DocumentFilter(JavaModule.LANGUAGE_ID, REGEX, null)));
+    return description;
+  }
 }
