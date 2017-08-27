0a4908d88c8b44c15cce53f15abebc0627100ccd
sudaraka94 <sudarakayasindu@gmail.com>
2017-08-27 22:40:00 +0530

    Cleaned datasource-shared code and added license header

--- wsagent/che-datasource-shared/src/main/java/org/eclipse/che/datasource/shared/DatabaseType.java
+++ wsagent/che-datasource-shared/src/main/java/org/eclipse/che/datasource/shared/DatabaseType.java
@@ -1,21 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2017 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.che.datasource.shared;
 
-/**
- * Created by test on 7/15/17.
- */
+
 public enum DatabaseType {
 
-    POSTGRES("postgres", 5432, "postgres", ""),
-
-    MYSQL("mysql", 3306, "root", ""),
-
-    ORACLE("oracle", 1521, "SYSTEM", ""),
-
-    JTDS("sqlserver", 1433, "sa", "Password123"),
-
-    NUODB("nuodb", 48004, "dba", "goalie"),
-
-    GOOGLECLOUDSQL("googleCloudSql", 3306, "root", "");
+    MYSQL("mysql", 3306, "root", "");
 
     private String connectorId;
     private int defaultPort;
--- wsagent/che-datasource-shared/src/main/java/org/eclipse/che/datasource/shared/DefaultDatasourceDefinitionDTO.java
+++ wsagent/che-datasource-shared/src/main/java/org/eclipse/che/datasource/shared/DefaultDatasourceDefinitionDTO.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012-2015 Codenvy, S.A.
+ * Copyright (c) 2012-2017 Codenvy, S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,16 +14,6 @@ import org.eclipse.che.dto.shared.DTO;
 
 @DTO
 public interface DefaultDatasourceDefinitionDTO extends DatabaseConfigurationDTO {
-
-
-    // String getHostName();
-    //
-    // int getPort();
-    //
-    //
-    // void setHostName(String hostname);
-    //
-    // void setPort(int port);
 
 
     DefaultDatasourceDefinitionDTO withHostName(String hostname);
