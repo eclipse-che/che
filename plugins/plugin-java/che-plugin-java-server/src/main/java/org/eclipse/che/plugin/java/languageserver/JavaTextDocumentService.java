a1ed8b167bd265f8cd6dd787b3b3e709b3074b7b
Thomas Mäder <tmader@redhat.com>
2017-08-31 15:39:57 +0200

    First cut of opening references in jars

---
+++ plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaTextDocumentService.java
@@ -0,0 +1,7 @@
+package org.eclipse.che.plugin.java.languageserver;
+
+import org.eclipse.lsp4j.services.TextDocumentService;
+
+public interface JavaTextDocumentService extends TextDocumentService {
+  
+}
