af9a04ab0bd927dd35ca1a7b7e42a0b636d44a34
David Festal <dfestal@redhat.com>
2017-08-31 14:13:31 +0200

    Add Javadoc
    Signed-off-by: David Festal <dfestal@redhat.com>

--- plugins/plugin-keycloak/che-plugin-keycloak-server/src/main/java/org/eclipse/che/keycloak/server/AbstractKeycloakFilter.java
+++ plugins/plugin-keycloak/che-plugin-keycloak-server/src/main/java/org/eclipse/che/keycloak/server/AbstractKeycloakFilter.java
@@ -5,6 +5,13 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Base abstract class for the Keycloak-related servlet filters.
+ * 
+ * In particular it defines commnon use-cases when the
+ * authentication / multi-user logic should be skipped
+ *
+ */
 public abstract class AbstractKeycloakFilter implements Filter {
 
   protected boolean shouldSkipAuthentication(HttpServletRequest request, String token) {
