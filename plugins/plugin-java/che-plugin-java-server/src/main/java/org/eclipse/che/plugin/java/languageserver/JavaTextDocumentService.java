6f56e49be3db5fb45bf973bdd627b41adadb823c
Thomas Mäder <tmader@redhat.com>
2017-09-01 14:00:42 +0200

    Makes support for class file contents work
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaTextDocumentService.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.che.plugin.java.languageserver;
-
-import org.eclipse.lsp4j.services.TextDocumentService;
-
-public interface JavaTextDocumentService extends TextDocumentService {
-  
-}
