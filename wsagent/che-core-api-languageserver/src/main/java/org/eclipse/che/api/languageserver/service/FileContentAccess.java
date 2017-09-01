1fd2231405273fd472b067b7872cf292abe37464
Thomas Mäder <tmader@redhat.com>
2017-09-01 14:00:42 +0200

    Makes support for class file contents work
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- wsagent/che-core-api-languageserver/src/main/java/org/eclipse/che/api/languageserver/service/FileContentAccess.java
+++ wsagent/che-core-api-languageserver/src/main/java/org/eclipse/che/api/languageserver/service/FileContentAccess.java
@@ -3,5 +3,5 @@ package org.eclipse.che.api.languageserver.service;
 import java.util.concurrent.CompletableFuture;
 
 public interface FileContentAccess {
-  CompletableFuture<String> getFileContent();
+  CompletableFuture<String> getFileContent(String uri);
 }
