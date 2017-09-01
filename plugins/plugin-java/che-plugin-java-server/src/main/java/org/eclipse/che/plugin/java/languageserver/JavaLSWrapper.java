d7eb8148cdc6e0c017e45ceb9815cee41890f4b0
Thomas Mäder <tmader@redhat.com>
2017-09-01 14:00:42 +0200

    Makes support for class file contents work
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaLSWrapper.java
+++ plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaLSWrapper.java
@@ -1,22 +1,27 @@
 package org.eclipse.che.plugin.java.languageserver;
 
 import java.lang.reflect.Proxy;
+import java.util.concurrent.CompletableFuture;
+import org.eclipse.che.api.languageserver.service.FileContentAccess;
 import org.eclipse.che.api.languageserver.util.DynamicWrapper;
-import org.eclipse.lsp4j.services.LanguageServer;
 import org.eclipse.lsp4j.services.TextDocumentService;
 
 public class JavaLSWrapper {
-  private LanguageServer wrapped;
+  private JavaLanguageServer wrapped;
 
-  public JavaLSWrapper(LanguageServer wrapped) {
+  public JavaLSWrapper(JavaLanguageServer wrapped) {
     this.wrapped = wrapped;
+  }
+
+  public CompletableFuture<String> getFileContent(String uri) {
+    return wrapped.classFileContents(new org.eclipse.lsp4j.TextDocumentIdentifier(uri));
   }
 
   public TextDocumentService getTextDocumentService() {
     return (TextDocumentService)
         Proxy.newProxyInstance(
             getClass().getClassLoader(),
-            new Class[] {TextDocumentService.class},
+            new Class[] {TextDocumentService.class, FileContentAccess.class},
             new DynamicWrapper(
                 new JavaTextDocumentServiceWraper(wrapped.getTextDocumentService()),
                 wrapped.getTextDocumentService()));
