f707e20ec8bb00180a2e4d8bbe4546a54ba91c24
Masaki Muranaka <monaka@monami-ya.com>
2017-09-07 07:05:41 +0000

    Fix the invalid null check.
    fixes #6163.
    
    Signed-off-by: Masaki Muranaka <monaka@monami-ya.com>

--- ide/che-core-orion-editor/src/main/java/org/eclipse/che/ide/editor/orion/client/OrionContentTypeRegistrant.java
+++ ide/che-core-orion-editor/src/main/java/org/eclipse/che/ide/editor/orion/client/OrionContentTypeRegistrant.java
@@ -66,7 +66,7 @@ public class OrionContentTypeRegistrant {
                     "orion.core.contenttype",
                     JavaScriptObject.createObject(),
                     contentType.toServiceObject());
-                if (config.getPatterns() != null || config.getPatterns().length() != 0) {
+                if (config.getPatterns() != null && config.getPatterns().length() != 0) {
                   serviceRegistry.doRegisterService(
                       "orion.edit.highlighter", JavaScriptObject.createObject(), config);
                 }
