c9d553a3773e00b8ec38aab028c9c3009f630c4a
Max Shaposhnik <mshaposh@redhat.com>
2017-09-11 15:15:39 +0300

    Code fmt

--- wsmaster/che-core-api-machine/src/main/java/org/eclipse/che/api/machine/server/jpa/JpaRecipeDao.java
+++ wsmaster/che-core-api-machine/src/main/java/org/eclipse/che/api/machine/server/jpa/JpaRecipeDao.java
@@ -107,14 +107,28 @@ public class JpaRecipeDao implements RecipeDao {
   /**
    * Translated query should look like:
    *
-   * <p>SELECT recipe.ID, recipe.CREATOR, recipe.DESCRIPTION, recipe.NAME, recipe.SCRIPT,
-   * recipe.TYPE FROM {oj RECIPEPERMISSIONS permission LEFT OUTER JOIN RECIPE recipe ON (recipe.ID =
-   * permission.RECIPEID) LEFT OUTER JOIN Recipe_TAGS tag ON (tag.Recipe_ID = recipe.ID)},
-   * RECIPEPERMISSIONS_ACTIONS permissionActions WHERE ((tag.tag IN (?)) AND ((? IS NULL) OR
-   * (recipe.TYPE IS NULL) OR (recipe.TYPE = ?)) AND ((permission.USERID IS NULL) OR
-   * (permission.USERID = ?)) AND (permissionActions.actions = ?) AND
-   * (permissionActions.RECIPEPERMISSIONS_ID = permission.ID)) GROUP BY recipe.ID HAVING
-   * (COUNT(tag.tag) = ?)
+   * <pre>
+   * SELECT recipe.ID,
+   *        recipe.CREATOR,
+   *        recipe.DESCRIPTION,
+   *        recipe.NAME,
+   *        recipe.SCRIPT,
+   *        recipe.TYPE
+   * FROM {oj RECIPEPERMISSIONS permission
+   * LEFT OUTER JOIN RECIPE recipe ON (recipe.ID = permission.RECIPEID)
+   * LEFT OUTER JOIN Recipe_TAGS tag ON (tag.Recipe_ID = recipe.ID)},
+   *     RECIPEPERMISSIONS_ACTIONS permissionActions
+   * WHERE ((tag.tag IN (?))
+   *     AND ((? IS NULL)
+   *           OR (recipe.TYPE IS NULL)
+   *           OR (recipe.TYPE = ?))
+   *     AND ((permission.USERID IS NULL)
+   *           OR (permission.USERID = ?))
+   *     AND (permissionActions.actions = ?)
+   *     AND (permissionActions.RECIPEPERMISSIONS_ID = permission.ID))
+   * GROUP BY recipe.ID
+   * HAVING (COUNT(tag.tag) = ?)
+   * </pre>
    */
   @Override
   @Transactional
