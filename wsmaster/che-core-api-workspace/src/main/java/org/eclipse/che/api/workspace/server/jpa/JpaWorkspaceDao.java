12a765e44dee9f5dd49adb4a4263930d76225f29
Max Shaposhnik <mshaposh@redhat.com>
2017-09-12 12:27:21 +0300

    Minor code fix

--- wsmaster/che-core-api-workspace/src/main/java/org/eclipse/che/api/workspace/server/jpa/JpaWorkspaceDao.java
+++ wsmaster/che-core-api-workspace/src/main/java/org/eclipse/che/api/workspace/server/jpa/JpaWorkspaceDao.java
@@ -51,6 +51,12 @@ public class JpaWorkspaceDao implements WorkspaceDao {
   @Inject private EventService eventService;
   @Inject private Provider<EntityManager> managerProvider;
 
+  private static final String findByWorkerQuery =
+      "SELECT ws FROM Worker worker  "
+          + "          LEFT JOIN worker.workspace ws "
+          + "          WHERE worker.userId = :userId "
+          + "          AND 'read' MEMBER OF worker.actions";
+
   @Override
   public WorkspaceImpl create(WorkspaceImpl workspace) throws ConflictException, ServerException {
     requireNonNull(workspace, "Required non-null workspace");
@@ -154,16 +160,10 @@ public class JpaWorkspaceDao implements WorkspaceDao {
   @Transactional
   public List<WorkspaceImpl> getWorkspaces(String userId) throws ServerException {
 
-    final String query =
-        "SELECT ws FROM Worker worker  "
-            + "          LEFT JOIN worker.workspace ws "
-            + "          WHERE worker.userId = :userId "
-            + "          AND 'read' MEMBER OF worker.actions";
-
     try {
       return managerProvider
           .get()
-          .createQuery(query, WorkspaceImpl.class)
+          .createQuery(findByWorkerQuery, WorkspaceImpl.class)
           .setParameter("userId", userId)
           .getResultList();
     } catch (RuntimeException x) {
