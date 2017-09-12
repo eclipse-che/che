dd5a1666969717da86d511709699286d80f8af6d
Max Shaposhnik <mshaposh@redhat.com>
2017-09-12 15:21:10 +0300

    Property fix

--- dockerfiles/init/manifests/che.pp
+++ dockerfiles/init/manifests/che.pp
@@ -61,7 +61,7 @@ node default {
   $che_pg_password = getValue("CHE_POSTGRES_PASSWORD", "pgchepassword")
   $che_pg_database = getValue("CHE_POSTGRES_DATABASE", "dbche")
 
-  $system.super_privileged_mode=getValue("CHE_SYSTEM_SUPER_PRIVILEGED_MODE", "false")
+  $system_super_privileged_mode=getValue("SYSTEM_SUPER__PRIVILEGED__MODE", "false")
 
   ###############################
   # Include base module
