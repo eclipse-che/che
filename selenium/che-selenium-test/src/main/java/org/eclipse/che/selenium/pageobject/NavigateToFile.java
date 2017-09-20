dd8d35b3930ff05c8c0c4a2665349df95834cc2d
Sergey Skorik <sskorik@codenvy.com>
2017-09-20 17:35:35 +0300

    added debugId for NavigateToFile feature

--- selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/NavigateToFile.java
+++ selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/NavigateToFile.java
@@ -45,21 +45,21 @@ public class NavigateToFile {
 
   private interface Locators {
     String NAVI_TO_FILE_FORM = "gwt-debug-file-navigateToFile-mainPanel";
-    String FILE_NAME_FIELD = "//div[text()='Enter file name:']/following::input[1]";
-    String FILE_NAME_POPUP = "//div[text()='Enter file name:']/following::table[last()]";
+    String FILE_NAME_FIELD = "gwt-debug-navigateToFile-fileName";
+    String FILE_NAME_POPUP = "gwt-debug-navigateToFile-suggestionPanel";
     String FILE_NAME_LIST_SELECT_WITH_PATH =
-        "//div[text()='Enter file name:']//following::tr[contains(., '%s')][last()]";
+        "//div[@id='gwt-debug-navigateToFile-suggestionPanel']//div[last()]//tr[contains(., '%s')]";
     String FILE_NAME_LIST_SELECT =
-        "//div[text()='Enter file name:']/following::table[last()]//td[text()='%s']";
+        "//div[@id='gwt-debug-navigateToFile-suggestionPanel']//div[last()]//td[text()='%s']";
   }
 
   @FindBy(id = Locators.NAVI_TO_FILE_FORM)
   WebElement naviToFileForm;
 
-  @FindBy(xpath = Locators.FILE_NAME_FIELD)
+  @FindBy(id = Locators.FILE_NAME_FIELD)
   WebElement fileNameField;
 
-  @FindBy(xpath = Locators.FILE_NAME_POPUP)
+  @FindBy(id = Locators.FILE_NAME_POPUP)
   WebElement fileNamePopUp;
 
   /** wait opening of 'Navigate to file' widget */
