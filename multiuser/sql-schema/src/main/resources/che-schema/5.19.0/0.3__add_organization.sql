118a54521c080a508a48c7cbfb58a808b465d36f
Sergii Leshchenko <sleshche@redhat.com>
2017-09-22 15:20:24 +0300

    Fix sql scripts license

--- multiuser/sql-schema/src/main/resources/che-schema/5.19.0/0.3__add_organization.sql
+++ multiuser/sql-schema/src/main/resources/che-schema/5.19.0/0.3__add_organization.sql
@@ -1,16 +1,12 @@
 --
---  [2012] - [2017] Codenvy, S.A.
---  All Rights Reserved.
+-- Copyright (c) 2012-2017 Red Hat, Inc.
+-- All rights reserved. This program and the accompanying materials
+-- are made available under the terms of the Eclipse Public License v1.0
+-- which accompanies this distribution, and is available at
+-- http://www.eclipse.org/legal/epl-v10.html
 --
--- NOTICE:  All information contained herein is, and remains
--- the property of Codenvy S.A. and its suppliers,
--- if any.  The intellectual and technical concepts contained
--- herein are proprietary to Codenvy S.A.
--- and its suppliers and may be covered by U.S. and Foreign Patents,
--- patents in process, and are protected by trade secret or copyright law.
--- Dissemination of this information or reproduction of this material
--- is strictly forbidden unless prior written permission is obtained
--- from Codenvy S.A..
+-- Contributors:
+--   Red Hat, Inc. - initial API and implementation
 --
 
 -- Organization ----------------------------------------------------------------
@@ -64,7 +60,7 @@ CREATE TABLE che_organization_distributed_resources (
     PRIMARY KEY (organization_id)
 );
 -- constraints
-ALTER TABLE che_organization_distributed_resources ADD CONSTRAINT che_fk_organization_distributed_resources_organization_id FOREIGN KEY (organization_id) REFERENCES che_organization (id);
+ALTER TABLE che_organization_distributed_resources ADD CONSTRAINT che_fk_org_distributed_res_organization_id FOREIGN KEY (organization_id) REFERENCES che_organization (id);
 --------------------------------------------------------------------------------
 
 
@@ -76,6 +72,6 @@ CREATE TABLE che_organization_distributed_resources_resource (
     PRIMARY KEY (organization_distributed_resources_id, resource_id)
 );
 -- constraints
-ALTER TABLE che_organization_distributed_resources_resource ADD CONSTRAINT che_fk_organization_distributed_resources_resource_resource_id FOREIGN KEY (resource_id) REFERENCES che_resource (id);
-ALTER TABLE che_organization_distributed_resources_resource ADD CONSTRAINT che_fk_organization_distributed_resources_resource_organization_distributed_resources_id FOREIGN KEY (organization_distributed_resources_id) REFERENCES che_organization_distributed_resources (organization_id);
+ALTER TABLE che_organization_distributed_resources_resource ADD CONSTRAINT che_fk_org_distributed_res_resource_resource_id FOREIGN KEY (resource_id) REFERENCES che_resource (id);
+ALTER TABLE che_organization_distributed_resources_resource ADD CONSTRAINT che_fk_org_distributed_res_resource_org_distributed_res_id FOREIGN KEY (organization_distributed_resources_id) REFERENCES che_organization_distributed_resources (organization_id);
 --------------------------------------------------------------------------------
