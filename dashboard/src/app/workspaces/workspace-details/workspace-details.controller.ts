c5871b5d7a2bdd3430d58893db6a3e74f9e164df
Oleksii Orel <oorel@redhat.com>
2017-09-26 20:29:03 +0300

    CHE-6423 fix page crashed
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- dashboard/src/app/workspaces/workspace-details/workspace-details.controller.ts
+++ dashboard/src/app/workspaces/workspace-details/workspace-details.controller.ts
@@ -20,7 +20,7 @@ export  interface IInitData {
   workspaceDetails: che.IWorkspace;
 }
 
-const TAB: Array<string> = ['Overview', 'Projects', 'Machines', 'Agents', 'Servers', 'Env_Variables', 'Config'];
+const TAB: Array<string> = ['Overview', 'Projects', 'Machines', 'Agents', 'Servers', 'Env_Variables', 'Config', 'SSH'];
 
 const STOPPING = WorkspaceStatus[WorkspaceStatus.STOPPING];
 const STOPPED = WorkspaceStatus[WorkspaceStatus.STOPPED];
