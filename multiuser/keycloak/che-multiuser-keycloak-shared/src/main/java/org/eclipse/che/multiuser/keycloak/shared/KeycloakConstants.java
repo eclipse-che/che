de52d5f04bee7b42fdd8b2be60349ab137e50a09
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-09-27 11:29:54 +0300

    Code cleanup

--- multiuser/keycloak/che-multiuser-keycloak-shared/src/main/java/org/eclipse/che/multiuser/keycloak/shared/KeycloakConstants.java
+++ multiuser/keycloak/che-multiuser-keycloak-shared/src/main/java/org/eclipse/che/multiuser/keycloak/shared/KeycloakConstants.java
@@ -14,21 +14,11 @@ package org.eclipse.che.multiuser.keycloak.shared;
 public class KeycloakConstants {
 
   private static final String KEYCLOAK_SETTING_PREFIX = "che.keycloak.";
-  private static final String PRIVATE_PREFIX = "private.";
   private static final String KEYCLOAK_SETTINGS_ENDPOINT_PATH = "/keycloak/settings";
 
   public static final String AUTH_SERVER_URL_SETTING = KEYCLOAK_SETTING_PREFIX + "auth_server_url";
   public static final String REALM_SETTING = KEYCLOAK_SETTING_PREFIX + "realm";
   public static final String CLIENT_ID_SETTING = KEYCLOAK_SETTING_PREFIX + "client_id";
-  public static final String REWRITE_RULE_SETTING =
-      KEYCLOAK_SETTING_PREFIX + "redirect_rewrite_rules";
-
-  public static final String PRIVATE_REALM_SETTING =
-      KEYCLOAK_SETTING_PREFIX + PRIVATE_PREFIX + "realm";
-  public static final String PRIVATE_CLIENT_ID_SETTING =
-      KEYCLOAK_SETTING_PREFIX + PRIVATE_PREFIX + "client_id";
-  public static final String PRIVATE_CLIENT_SECRET_SETTING =
-      KEYCLOAK_SETTING_PREFIX + PRIVATE_PREFIX + "client_secret";
 
   public static final String OSO_ENDPOINT_SETTING = KEYCLOAK_SETTING_PREFIX + "oso.endpoint";
   public static final String PROFILE_ENDPOINT_SETTING =
