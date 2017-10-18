264b455742484b1cbad2270771ef6df04a17eb90
Thomas Mäder <tmader@redhat.com>
2017-10-18 11:49:31 +0200

    Use LSP editor even if no capabilities are found.
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/editor/LanguageServerReconcileStrategy.java
+++ plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/editor/LanguageServerReconcileStrategy.java
@@ -48,11 +48,14 @@ public class LanguageServerReconcileStrategy implements ReconcilingStrategy {
 
     Either<TextDocumentSyncKind, TextDocumentSyncOptions> sync =
         serverCapabilities.getTextDocumentSync();
-    TextDocumentSyncKind documentSync;
-    if (sync.isLeft()) {
-      documentSync = sync.getLeft();
-    } else {
-      documentSync = sync.getRight().getChange();
+    TextDocumentSyncKind documentSync = null;
+    // sync may be null
+    if (sync != null) {
+      if (sync.isLeft()) {
+        documentSync = sync.getLeft();
+      } else {
+        documentSync = sync.getRight().getChange();
+      }
     }
 
     synchronize = synchronizeFactory.getSynchronize(documentSync);
