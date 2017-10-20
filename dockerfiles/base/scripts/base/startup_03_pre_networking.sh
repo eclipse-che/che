185508a122de37ac6d95a1f4c2d14769652bb388
Eugene Ivantsov <eivantsov@redhat.com>
2017-10-20 10:47:45 +0300

    Edit image list depending on a) single port policy b) single vs multi user assembly

--- dockerfiles/base/scripts/base/startup_03_pre_networking.sh
+++ dockerfiles/base/scripts/base/startup_03_pre_networking.sh
@@ -89,13 +89,14 @@ get_image_manifest() {
   BOOTSTRAP_IMAGE_LIST=$(cat ${SCRIPTS_BASE_CONTAINER_SOURCE_DIR}/images/images-bootstrap)
   if [ -z "${CHE_SINGLE_PORT:-}" ]; then
     IMAGE_LIST=$(cat /version/$1/images | sed '/IMAGE_TRAEFIK/d')
-        if [ -z "${CHE_MULTIUSER:-}" ]; then
-            IMAGE_LIST=$(echo "${IMAGE_LIST}" | sed '/IMAGE_KEYCLOAK/d; /IMAGE_POSTGRES/d; /IMAGE_CHE_MULTI_*/d')
-        else
-            IMAGE_LIST=$(echo "${IMAGE_LIST}" | sed '/IMAGE_CHE/d')
-        fi
-    else IMAGE_LIST=$(cat /version/$1/images)
+  else
+    IMAGE_LIST=$(cat /version/$1/images)
   fi
+  if [ -z "${CHE_MULTIUSER:-}" ]; then
+     IMAGE_LIST=$(echo "${IMAGE_LIST}" | sed '/IMAGE_KEYCLOAK/d; /IMAGE_POSTGRES/d; /IMAGE_CHE_MULTI_*/d')
+  else
+     IMAGE_LIST=$(echo "${IMAGE_LIST}" | sed '/IMAGE_CHE/d')
+   fi
   UTILITY_IMAGE_LIST=$(cat ${SCRIPTS_BASE_CONTAINER_SOURCE_DIR}/images/images-utilities)
 
   # set variables
