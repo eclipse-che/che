a07efc78fadc9c21762ed91ccd1c1189ea8f3232
Mykola Morhun <mmorhun@redhat.com>
2017-10-24 10:45:32 +0300

    CHE-6608: Use new node presentation model.

--- plugins/plugin-git/che-plugin-git-ext-git/src/main/java/org/eclipse/che/ide/ext/git/client/panel/RepositoryNode.java
+++ plugins/plugin-git/che-plugin-git-ext-git/src/main/java/org/eclipse/che/ide/ext/git/client/panel/RepositoryNode.java
@@ -19,8 +19,9 @@ import org.eclipse.che.api.promises.client.Promise;
 import org.eclipse.che.ide.api.data.tree.AbstractTreeNode;
 import org.eclipse.che.ide.api.data.tree.Node;
 import org.eclipse.che.ide.ext.git.client.GitResources;
-import org.eclipse.che.ide.ui.smartTree.presentation.HasPresentation;
-import org.eclipse.che.ide.ui.smartTree.presentation.NodePresentation;
+import org.eclipse.che.ide.ui.smartTree.presentation.HasNewPresentation;
+import org.eclipse.che.ide.ui.smartTree.presentation.NewNodePresentation;
+import org.eclipse.che.ide.ui.smartTree.presentation.StyleConfigurator;
 import org.eclipse.che.ide.util.dom.Elements;
 
 /**
@@ -28,12 +29,12 @@ import org.eclipse.che.ide.util.dom.Elements;
  *
  * @author Mykola Morhun
  */
-public class RepositoryNode extends AbstractTreeNode implements HasPresentation {
+public class RepositoryNode extends AbstractTreeNode implements HasNewPresentation {
   private static final String CHANGES_PREFIX = "\u2213"; // minus-plus sign '∓'
 
   private final GitResources gitResources;
 
-  private NodePresentation nodePresentation;
+  private NewNodePresentation.Builder nodePresentationBuilder;
 
   private final String repository;
   private int changes;
@@ -51,6 +52,8 @@ public class RepositoryNode extends AbstractTreeNode implements HasPresentation 
     this.changes = changes;
 
     this.gitResources = gitResources;
+
+    createBaseNodePresentation();
   }
 
   public String getRepository() {
@@ -65,34 +68,32 @@ public class RepositoryNode extends AbstractTreeNode implements HasPresentation 
     this.changes = changes;
   }
 
-  @Override
-  public void updatePresentation(NodePresentation presentation) {
-    presentation.setPresentableText(repository);
-    presentation.setPresentableTextCss("margin-left: 2px;");
-    presentation.setPresentableIcon(gitResources.gitLogo());
+  private void createBaseNodePresentation() {
+    StyleConfigurator.Builder styleBuilder = new StyleConfigurator.Builder();
+    styleBuilder.withProperty("marginLeft", "2px");
+    StyleConfigurator repositoryNodeStyle = styleBuilder.build();
 
+    nodePresentationBuilder = new NewNodePresentation.Builder();
+    nodePresentationBuilder
+        .withNodeText(repository)
+        .withNodeTextStyle(repositoryNodeStyle)
+        .withIcon(gitResources.gitLogo());
+  }
+
+  @Override
+  public NewNodePresentation getPresentation() {
+    // Immutable part of node presentation is cached in nodePresentationBuilder field.
+    // Every time here should be set variable parts of the node builder.
     if (changes > 0) {
       SpanElement spanElement =
           Elements.createSpanElement(gitResources.gitPanelCss().repositoryChangesLabel());
       spanElement.setInnerText(CHANGES_PREFIX + changes);
-      presentation.setUserElement((Element) spanElement);
+      nodePresentationBuilder.withUserElement((Element) spanElement);
     } else {
-      presentation.setUserElement(null); // remove label from node
-    }
-  }
-
-  @Override
-  public NodePresentation getPresentation(boolean update) {
-    if (nodePresentation == null) {
-      nodePresentation = new NodePresentation();
-      updatePresentation(nodePresentation);
-      return nodePresentation;
+      nodePresentationBuilder.withUserElement(null); // remove label from node
     }
 
-    if (update) {
-      updatePresentation(nodePresentation);
-    }
-    return nodePresentation;
+    return nodePresentationBuilder.build();
   }
 
   @Override
