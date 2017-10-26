9e7386e726011bf52b64389c492d274bd68fea5f
Anton Korneta <akorneta@redhat.com>
2017-10-26 18:36:12 +0300

    Add different strategy for che wokrsapce PVCs

---
+++ infrastructures/openshift/src/main/java/org/eclipse/che/workspace/infrastructure/openshift/project/CommonPVCStrategy.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2012-2017 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.workspace.infrastructure.openshift.project;
+
+import static org.eclipse.che.api.workspace.server.WsAgentMachineFinderUtil.getWsAgentServerMachine;
+import static org.eclipse.che.workspace.infrastructure.openshift.project.OpenShiftObjectUtil.newPVC;
+import static org.eclipse.che.workspace.infrastructure.openshift.project.OpenShiftObjectUtil.newVolume;
+import static org.eclipse.che.workspace.infrastructure.openshift.project.OpenShiftObjectUtil.newVolumeMount;
+
+import com.google.inject.Inject;
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodSpec;
+import javax.inject.Named;
+import org.eclipse.che.api.workspace.server.spi.InfrastructureException;
+import org.eclipse.che.api.workspace.server.spi.InternalEnvironment;
+import org.eclipse.che.workspace.infrastructure.openshift.environment.OpenShiftEnvironment;
+
+/**
+ * Provides common PVC for each workspace in one OpenShift project.
+ *
+ * <p>Note that subpaths are used for resolving of backed up data path collisions. Subpaths
+ * evaluated as following: '{workspaceId}/{workspace data folder}'. Workspace data folder it's a
+ * configured path where workspace projects, logs or any other data located. The number of
+ * workspaces that can simultaneously store backups in one PV is limited only by the storage
+ * capacity. The number of workspaces that can be running simultaneously depends on access mode
+ * configuration and Che configuration limits.
+ *
+ * @author Anton Korneta
+ */
+public class CommonPVCStrategy implements WorkspacePVCStrategy {
+
+  public static final String COMMON_STRATEGY = "common";
+
+  private final String pvcName;
+  private final String pvcQuantity;
+  private final String pvcAccessMode;
+  private final String projectsPath;
+
+  @Inject
+  public CommonPVCStrategy(
+      @Named("che.infra.openshift.pvc.name") String pvcName,
+      @Named("che.infra.openshift.pvc.quantity") String pvcQuantity,
+      @Named("che.infra.openshift.pvc.access_mode") String pvcAccessMode,
+      @Named("che.workspace.projects.storage") String projectFolderPath) {
+    this.pvcName = pvcName;
+    this.pvcQuantity = pvcQuantity;
+    this.pvcAccessMode = pvcAccessMode;
+    this.projectsPath = projectFolderPath;
+  }
+
+  @Override
+  public void prepare(InternalEnvironment env, OpenShiftEnvironment osEnv, String workspaceId)
+      throws InfrastructureException {
+    final PersistentVolumeClaim pvc = osEnv.getPersistentVolumeClaims().get(pvcName);
+    if (pvc != null) {
+      return;
+    }
+    final String machineWithSources =
+        getWsAgentServerMachine(env)
+            .orElseThrow(() -> new InfrastructureException("Machine with ws-agent not found"));
+    osEnv.getPersistentVolumeClaims().put(pvcName, newPVC(pvcName, pvcAccessMode, pvcQuantity));
+    for (Pod pod : osEnv.getPods().values()) {
+      final PodSpec podSpec = pod.getSpec();
+      for (Container container : podSpec.getContainers()) {
+        final String machine = pod.getMetadata().getName() + '/' + container.getName();
+        if (machine.equals(machineWithSources)) {
+          final String subPath =
+              workspaceId + (projectsPath.startsWith("/") ? projectsPath : "/" + projectsPath);
+          container.getVolumeMounts().add(newVolumeMount(pvcName, projectsPath, subPath));
+          podSpec.getVolumes().add(newVolume(pvcName, pvcName));
+          return;
+        }
+      }
+    }
+  }
+
+  @Override
+  public void cleanup(String workspaceId) {
+    // TODO implement https://github.com/eclipse/che/issues/6767
+  }
+}
---
+++ infrastructures/openshift/src/main/java/org/eclipse/che/workspace/infrastructure/openshift/project/WorkspacePVCStrategy.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2012-2017 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.workspace.infrastructure.openshift.project;
+
+import org.eclipse.che.api.workspace.server.spi.InfrastructureException;
+import org.eclipse.che.api.workspace.server.spi.InternalEnvironment;
+import org.eclipse.che.workspace.infrastructure.openshift.environment.OpenShiftEnvironment;
+
+/**
+ * Defines a basic set of operations for workspace PVC strategies.
+ *
+ * @author Anton Korneta
+ */
+public interface WorkspacePVCStrategy {
+
+  /**
+   * Prepares PVC for backup of workspace data on a specific machine and in strategy specific way.
+   *
+   * @param env internal environment is used to get machines information
+   * @param osEnv OpenShift environment that changes as a result of preparation
+   * @param workspaceId the workspace identifier for which PVC will be prepared
+   * @throws InfrastructureException when any error while preparation occurs
+   */
+  void prepare(InternalEnvironment env, OpenShiftEnvironment osEnv, String workspaceId)
+      throws InfrastructureException;
+
+  /**
+   * Cleanups workspace backed up data in a strategy specific way.
+   *
+   * @param workspaceId the workspace identifier for which cleanup will be performed
+   * @throws InfrastructureException when any error while cleanup occurs
+   */
+  void cleanup(String workspaceId) throws InfrastructureException;
+}
