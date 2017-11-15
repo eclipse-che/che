38c714971c6be2424b589b6a19fda7b0c2b4d436
Artem Zatsarynnyi <azatsary@redhat.com>
2017-11-15 15:37:49 +0200

    Add tests
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/workspace/WsAgentServerUtil.java
+++ ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/workspace/WsAgentServerUtil.java
@@ -14,10 +14,11 @@ import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.eclipse.che.api.workspace.shared.Constants.SERVER_WS_AGENT_HTTP_REFERENCE;
 import static org.eclipse.che.api.workspace.shared.Constants.SERVER_WS_AGENT_WEBSOCKET_REFERENCE;
 
-import com.google.inject.Provider;
+import com.google.common.annotations.VisibleForTesting;
 import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import org.eclipse.che.api.workspace.shared.Constants;
 import org.eclipse.che.ide.QueryParameters;
 import org.eclipse.che.ide.api.app.AppContext;
 import org.eclipse.che.ide.api.workspace.model.MachineImpl;
@@ -35,13 +36,12 @@ public class WsAgentServerUtil {
    */
   public static final String WSAGENT_SERVER_REF_PREFIX_PARAM = "wsagent-ref-prefix";
 
-  private final Provider<AppContext> appContextProvider;
+  private final AppContext appContext;
   private final QueryParameters queryParameters;
 
   @Inject
-  public WsAgentServerUtil(
-      Provider<AppContext> appContextProvider, QueryParameters queryParameters) {
-    this.appContextProvider = appContextProvider;
+  public WsAgentServerUtil(AppContext appContext, QueryParameters queryParameters) {
+    this.appContext = appContext;
     this.queryParameters = queryParameters;
   }
 
@@ -64,26 +64,31 @@ public class WsAgentServerUtil {
   }
 
   /**
-   * Returns {@code Optional} with the "wsagent/http" server.
+   * Returns {@code Optional} with the {@link Constants#SERVER_WS_AGENT_HTTP_REFERENCE wsagent/http}
+   * server.
    *
-   * @return {@code Optional} with the "wsagent/http" server if the current workspace has a runtime
-   *     and there is a machine with such server, otherwise an empty {@code Optional}
+   * @return {@code Optional} with the {@link Constants#SERVER_WS_AGENT_HTTP_REFERENCE wsagent/http}
+   *     server if the current workspace has a runtime and there is a machine with such server,
+   *     otherwise an empty {@code Optional}
    */
   public Optional<ServerImpl> getWsAgentHttpServer() {
     return getServerByRef(getWsAgentHttpServerReference());
   }
 
   /**
-   * Returns {@code Optional} with the "wsagent/ws" server.
+   * Returns {@code Optional} with the {@link Constants#SERVER_WS_AGENT_WEBSOCKET_REFERENCE
+   * wsagent/ws} server.
    *
-   * @return {@code Optional} with the "wsagent/ws" server if the current workspace has a runtime
-   *     and there is a machine with such server, otherwise an empty {@code Optional}
+   * @return {@code Optional} with the {@link Constants#SERVER_WS_AGENT_WEBSOCKET_REFERENCE
+   *     wsagent/ws} server if the current workspace has a runtime and there is a machine with such
+   *     server, otherwise an empty {@code Optional}
    */
   public Optional<ServerImpl> getWsAgentWebSocketServer() {
     return getServerByRef(getWsAgentWebSocketServerReference());
   }
 
-  private Optional<ServerImpl> getServerByRef(String ref) {
+  @VisibleForTesting
+  Optional<ServerImpl> getServerByRef(String ref) {
     Optional<RuntimeImpl> runtimeOpt = getWorkspaceRuntime();
 
     if (runtimeOpt.isPresent()) {
@@ -100,10 +105,12 @@ public class WsAgentServerUtil {
   }
 
   /**
-   * Checks whether the provided {@link MachineImpl} contains the "wsagent/http" server.
+   * Checks whether the provided {@link MachineImpl} contains the {@link
+   * Constants#SERVER_WS_AGENT_HTTP_REFERENCE wsagent/http} server.
    *
    * @param machine {@link MachineImpl} to check
-   * @return {@code true} if the given machine contains the "wsagent/http" server, otherwise {@code
+   * @return {@code true} if the given machine contains the {@link
+   *     Constants#SERVER_WS_AGENT_HTTP_REFERENCE wsagent/http} server server, otherwise {@code
    *     false}
    */
   public boolean containsWsAgentHttpServer(MachineImpl machine) {
@@ -111,7 +118,8 @@ public class WsAgentServerUtil {
   }
 
   /**
-   * Returns a reference of the "wsagent/http" server.
+   * Returns a reference of the {@link Constants#SERVER_WS_AGENT_HTTP_REFERENCE wsagent/http}
+   * server.
    *
    * <p><strong>Note</strong>, the returned server reference may be prepended with the prefix passed
    * through the URL's query parameter.
@@ -121,15 +129,14 @@ public class WsAgentServerUtil {
   public String getWsAgentHttpServerReference() {
     String refPrefix = queryParameters.getByName(WSAGENT_SERVER_REF_PREFIX_PARAM);
 
-    if (isNullOrEmpty(refPrefix)) {
-      return SERVER_WS_AGENT_HTTP_REFERENCE;
-    }
-
-    return refPrefix + SERVER_WS_AGENT_HTTP_REFERENCE;
+    return isNullOrEmpty(refPrefix)
+        ? SERVER_WS_AGENT_HTTP_REFERENCE
+        : refPrefix + SERVER_WS_AGENT_HTTP_REFERENCE;
   }
 
   /**
-   * Returns a reference of the "wsagent/ws" server.
+   * Returns a reference of the {@link Constants#SERVER_WS_AGENT_WEBSOCKET_REFERENCE wsagent/ws}
+   * server.
    *
    * <p><strong>Note</strong>, the returned server reference may be prepended with the prefix passed
    * through the URL's query parameter.
@@ -139,15 +146,13 @@ public class WsAgentServerUtil {
   public String getWsAgentWebSocketServerReference() {
     String refPrefix = queryParameters.getByName(WSAGENT_SERVER_REF_PREFIX_PARAM);
 
-    if (isNullOrEmpty(refPrefix)) {
-      return SERVER_WS_AGENT_WEBSOCKET_REFERENCE;
-    }
-
-    return refPrefix + SERVER_WS_AGENT_WEBSOCKET_REFERENCE;
+    return isNullOrEmpty(refPrefix)
+        ? SERVER_WS_AGENT_WEBSOCKET_REFERENCE
+        : refPrefix + SERVER_WS_AGENT_WEBSOCKET_REFERENCE;
   }
 
-  private Optional<RuntimeImpl> getWorkspaceRuntime() {
-    AppContext appContext = appContextProvider.get();
+  @VisibleForTesting
+  Optional<RuntimeImpl> getWorkspaceRuntime() {
     WorkspaceImpl workspace = appContext.getWorkspace();
 
     return Optional.ofNullable(workspace.getRuntime());
