18ebb2216cd134b9c707f3bd86a1155b7b65edc2
Roman Iuvshyn <riuvshyn@redhat.com>
2017-11-15 17:22:07 +0200

    fixup

--- dockerfiles/init/modules/openshift/files/scripts/ocp.sh
+++ dockerfiles/init/modules/openshift/files/scripts/ocp.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Copyright (c) 2012-2017 Red Hat, Inc
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
 
 set -e
 
@@ -7,13 +13,9 @@ init() {
 if [[ "$OSTYPE" == "darwin"* ]]; then
     DEFAULT_OC_PUBLIC_HOSTNAME="192.168.65.2"
     DEFAULT_OC_PUBLIC_IP="192.168.65.2"
-    [ -z "$CHE_OAUTH_GITHUB_CLIENTID" ] && export CHE_OAUTH_GITHUB_CLIENTID=d210c15f1a0c026a808c;
-    [ -z "$CHE_OAUTH_GITHUB_CLIENTSECRET" ] && export CHE_OAUTH_GITHUB_CLIENTSECRET=cc2589e6eed89ca460c8dbd13b9342cfddbe8098;
 else
     DEFAULT_OC_PUBLIC_HOSTNAME="127.0.0.1"
     DEFAULT_OC_PUBLIC_IP="127.0.0.1"
-    [ -z "$CHE_OAUTH_GITHUB_CLIENTID" ] && export CHE_OAUTH_GITHUB_CLIENTID=23638e66bf0d53bdc777;
-    [ -z "$CHE_OAUTH_GITHUB_CLIENTSECRET" ] && export CHE_OAUTH_GITHUB_CLIENTSECRET=e20edd408a4becda5f6ccb83abb4b1e7a432fe46;
 fi
 export OC_PUBLIC_HOSTNAME=${OC_PUBLIC_HOSTNAME:-${DEFAULT_OC_PUBLIC_HOSTNAME}}
 export OC_PUBLIC_IP=${OC_PUBLIC_IP:-${DEFAULT_OC_PUBLIC_IP}}
@@ -30,8 +32,7 @@ export OPENSHIFT_PASSWORD=${OPENSHIFT_PASSWORD:-${DEFAULT_OPENSHIFT_PASSWORD}}
 DEFAULT_OPENSHIFT_NAMESPACE_URL="eclipse-che.${OC_PUBLIC_IP}.nip.io"
 export OPENSHIFT_NAMESPACE_URL=${OPENSHIFT_NAMESPACE_URL:-${DEFAULT_OPENSHIFT_NAMESPACE_URL}}
 
-DEFAULT_OPENSHIFT_FLAVOR="ocp"
-export OPENSHIFT_FLAVOR=${OPENSHIFT_FLAVOR:-${DEFAULT_OPENSHIFT_FLAVOR}}
+export OPENSHIFT_FLAVOR="ocp"
 
 DEFAULT_OPENSHIFT_ENDPOINT="https://${OC_PUBLIC_HOSTNAME}:8443"
 export OPENSHIFT_ENDPOINT=${OPENSHIFT_ENDPOINT:-${DEFAULT_OPENSHIFT_ENDPOINT}}
@@ -51,7 +52,7 @@ export CHE_IMAGE_REPO=${CHE_IMAGE_REPO:-${DEFAULT_CHE_IMAGE_REPO}}
 DEFAULT_IMAGE_INIT="eclipse/che-init"
 export IMAGE_INIT=${IMAGE_INIT:-${DEFAULT_IMAGE_INIT}}:${CHE_IMAGE_TAG}
 
-DEFAULT_CONFIG_DIR="/tmp/config"
+DEFAULT_CONFIG_DIR="/tmp/che-config"
 export CONFIG_DIR=${CONFIG_DIR:-${DEFAULT_CONFIG_DIR}}
 }
 
@@ -89,7 +90,7 @@ get_tools() {
 }
 
 ocp_is_booted() {
-    # for now we check only openshift registry because this is enough
+    # we have to wait before docker registry will be started as it is staring as last container and it should be running before we perform che deploy.
     ocp_registry_container_id=$(docker ps -a  | grep openshift/origin-docker-registry | cut -d ' ' -f1)
     if [ ! -z "$ocp_registry_container_id" ];then
         ocp_registry_container_status=$(docker inspect "$ocp_registry_container_id" | $JQ_BINARY .[0] | $JQ_BINARY -r '.State.Status')
@@ -127,8 +128,8 @@ deploy_che_to_ocp() {
     if [ $DEFAULT_IMAGE_PULL_POLICY == "Always" ]; then
         docker pull "$IMAGE_INIT"
     fi
-    docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${CONFIG_DIR}":/data -e IMAGE_INIT="$IMAGE_INIT" -e CHE_MULTIUSER="$CHE_MULTIUSER" eclipse/che-cli:nightly destroy --quiet --skip:pull --skip:nightly
-    docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${CONFIG_DIR}":/data -e IMAGE_INIT="$IMAGE_INIT" -e CHE_MULTIUSER="$CHE_MULTIUSER" eclipse/che-cli:nightly config --skip:pull --skip:nightly
+    docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${CONFIG_DIR}":/data -e IMAGE_INIT="$IMAGE_INIT" -e CHE_MULTIUSER="$CHE_MULTIUSER" eclipse/che-cli:${CHE_IMAGE_TAG} destroy --quiet --skip:pull --skip:nightly
+    docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${CONFIG_DIR}":/data -e IMAGE_INIT="$IMAGE_INIT" -e CHE_MULTIUSER="$CHE_MULTIUSER" eclipse/che-cli:${CHE_IMAGE_TAG} config --skip:pull --skip:nightly
     cd "${CONFIG_DIR}/instance/config/openshift/scripts/"
     bash deploy_che.sh
     wait_until_server_is_booted
@@ -154,82 +155,6 @@ wait_until_server_is_booted() {
   done
 }
 
-check_workspace_status() {
-  STATUS_URL="http://che-${OPENSHIFT_NAMESPACE_URL}/api/workspace/${ws_id}"
-  WS_STATUS=$(curl -s "${STATUS_URL}" | $JQ_BINARY -r '.status')
-  if [[ "${WS_STATUS}" == *"$1"* ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-wait_workspace_status() {
-  STATUS=$1
-  WS_BOOT_TIMEOUT=300
-  echo "[TEST] wait che workspace status is ${STATUS}..."
-  ELAPSED=0
-  until check_workspace_status "${STATUS}" || [ ${ELAPSED} -eq "${WS_BOOT_TIMEOUT}" ]; do
-    sleep 2
-    ELAPSED=$((ELAPSED+1))
-  done
-}
-
-run_test() {
-    #TODO FIX for multi user need to handle auth
-    echo "[TEST] run CHE workspace test"
-    ws_name="ocp-test-$(date +%s)"
-
-    # create workspace
-    ws_create=$(curl -s "http://che-${OPENSHIFT_NAMESPACE_URL}/api/workspace?namespace=che&attribute=stackId:java-centos" \
-    -H 'Content-Type: application/json;charset=UTF-8' \
-    -H 'Accept: application/json, text/plain, */*' \
-    --data-binary '{"commands":[{"commandLine":"mvn clean install -f ${current.project.path}","name":"build","type":"mvn","attributes":{"goal":"Build","previewUrl":""}}],"projects":[{"tags":["maven","spring","java"],"commands":[{"commandLine":"mvn -f ${current.project.path} clean install \ncp ${current.project.path}/target/*.war $TOMCAT_HOME/webapps/ROOT.war","name":"build","type":"mvn","attributes":{"previewUrl":"","goal":"Build"}},{"commandLine":"$TOMCAT_HOME/bin/catalina.sh run 2>&1","name":"run tomcat","type":"custom","attributes":{"previewUrl":"http://${server.port.8080}","goal":"Run"}},{"commandLine":"$TOMCAT_HOME/bin/catalina.sh stop","name":"stop tomcat","type":"custom","attributes":{"previewUrl":"","goal":"Run"}},{"commandLine":"mvn -f ${current.project.path} clean install \ncp ${current.project.path}/target/*.war $TOMCAT_HOME/webapps/ROOT.war \n$TOMCAT_HOME/bin/catalina.sh run 2>&1","name":"build and run","type":"mvn","attributes":{"previewUrl":"http://${server.port.8080}","goal":"Run"}},{"commandLine":"mvn -f ${current.project.path} clean install \ncp ${current.project.path}/target/*.war $TOMCAT_HOME/webapps/ROOT.war \n$TOMCAT_HOME/bin/catalina.sh jpda run 2>&1","name":"debug","type":"mvn","attributes":{"previewUrl":"http://${server.port.8080}","goal":"Debug"}}],"projects":[],"links":[],"mixins":[],"problems":[],"category":"Samples","projectType":"maven","source":{"location":"https://github.com/che-samples/web-java-spring.git","type":"git","parameters":{}},"description":"A basic example using Spring servlets. The app returns values entered into a submit form.","displayName":"web-java-spring","options":{},"name":"web-java-spring","path":"/web-java-spring","attributes":{"language":["java"]},"type":"maven"}],"defaultEnv":"default","environments":{"default":{"recipe":{"location":"rhche/centos_jdk8","type":"dockerimage"},"machines":{"dev-machine":{"agents":["org.eclipse.che.terminal","org.eclipse.che.ws-agent","com.redhat.bayesian.lsp"],"servers":{},"attributes":{"memoryLimitBytes":"2147483648"}}}}},"name":"'${ws_name}'","links":[]}' \
-    --compressed )
-    [[ "$ws_create" == *"created"* ]] || exit 1
-    [[ "$ws_create" == *"STOPPED"* ]] || exit 1
-    ws_id=$(echo "${ws_create}" | $JQ_BINARY -r '.id')
-    [[ "$ws_id" == *"workspace"* ]] || exit 1
-    echo "[TEST] workspace '$ws_name' created succesfully"
-
-    # start workspace
-    curl -s "http://che-${OPENSHIFT_NAMESPACE_URL}/api/workspace/${ws_id}/runtime?environment=default" \
-    -H 'Content-Type: application/json;charset=UTF-8' \
-    -H 'Accept: application/json, text/plain, */*' \
-    -H 'Connection: keep-alive' \
-    --data-binary '{}' \
-    --compressed
-   wait_workspace_status "RUNNING"
-   echo "[TEST] workspace '$ws_name'started succesfully"
-   #TODO maybe add more checks that state is good
-
-   # stop workspace
-   ws_stop=$(curl -s "http://che-${OPENSHIFT_NAMESPACE_URL}/api/workspace/${ws_id}/runtime?create-snapshot=false" -X DELETE \
-   -H 'Accept: application/json, text/plain, */*' \
-   -H 'Connection: keep-alive' \
-   --compressed \
-   -o /dev/null \
-   --write-out '%{http_code}')
-   [[ "$ws_stop" = "204" ]] || exit 1
-   wait_workspace_status "STOPPED"
-   echo "[TEST] workspace '$ws_name' stopped succesfully"
-
-   # remove workspace
-   ws_remove=$(curl -s "http://che-${OPENSHIFT_NAMESPACE_URL}/api/workspace/${ws_id}" -X DELETE \
-   -H 'Accept: application/json, text/plain, */*' \
-   -H 'Connection: keep-alive' \
-   --compressed \
-   -o /dev/null \
-   --write-out '%{http_code}')
-   [[ "$ws_remove" = "204" ]] || exit 1
-   check_ws_removed=$(curl -s "http://che-${OPENSHIFT_NAMESPACE_URL}/api/workspace" \
-   -H 'Accept: application/json, text/plain, */*' \
-   -H 'Connection: keep-alive' \
-   --compressed)
-   [[ "$check_ws_removed" != *"${ws_id}"* ]] || exit 1
-   echo "[TEST] workspace '$ws_name' removed succesfully"
-}
-
 destroy_ocp() {
     $OC_BINARY login -u system:admin
     $OC_BINARY delete pvc --all
@@ -243,7 +168,6 @@ parse_args() {
     --destroy - destroy ocp cluster \\n
     --deploy-che - deploy che to ocp \\n
     --multiuser - deploy che in multiuser mode \\n
-    --test -  run simple test which will create > start > stop > remove CHE workspace\\n
     =================================== \\n
     ENV vars \\n
     CHE_IMAGE_TAG - set CHE images tag, default: nightly \\n
@@ -277,10 +201,6 @@ parse_args() {
            ;;
            --deploy-che)
                deploy_che_to_ocp
-               shift
-           ;;
-           --test)
-               run_test
                shift
            ;;
         esac
