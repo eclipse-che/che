551005ba0a9d0a8efffec388b9857ce11c85047e
Aleksandr Shmaraiev <oshmarai@redhat.com>
2017-11-17 17:03:19 +0200

    Add changes to the 'PlainJavaProjectConfigureClasspathTest'

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/plainjava/PlainJavaProjectConfigureClasspathTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/plainjava/PlainJavaProjectConfigureClasspathTest.java
@@ -45,14 +45,15 @@ public class PlainJavaProjectConfigureClasspathTest {
 
   private static final List<String> listJar =
       Arrays.asList(
-          "rt.jar - /opt/jdk1.8.0_45/jre/lib", "cldrdata.jar - /opt/jdk1.8.0_45/jre/lib/ext",
-          "dnsns.jar - /opt/jdk1.8.0_45/jre/lib/ext", "jfxrt.jar - /opt/jdk1.8.0_45/jre/lib/ext",
-          "localedata.jar - /opt/jdk1.8.0_45/jre/lib/ext",
-              "nashorn.jar - /opt/jdk1.8.0_45/jre/lib/ext",
-          "sunec.jar - /opt/jdk1.8.0_45/jre/lib/ext",
-              "sunjce_provider.jar - /opt/jdk1.8.0_45/jre/lib/ext",
-          "sunpkcs11.jar - /opt/jdk1.8.0_45/jre/lib/ext ",
-              "zipfs.jar - /opt/jdk1.8.0_45/jre/lib/ext");
+          "rt.jar",
+          "cldrdata.jar",
+          "dnsns.jar",
+          "localedata.jar",
+          "nashorn.jar",
+          "sunec.jar",
+          "sunjce_provider.jar",
+          "sunpkcs11.jar",
+          "zipfs.jar");
 
   @Inject private TestWorkspace ws;
   @Inject private Ide ide;
