b96a90c0b18ae839bef64efd5f97ea05da08354f
Max Shaposhnik <mshaposh@redhat.com>
2017-11-20 14:48:09 +0200

    Merge client

--- ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/projectimport/wizard/ProjectImporter.java
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/projectimport/wizard/ProjectImporter.java
@@ -25,26 +25,27 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.Map;
+import org.eclipse.che.api.auth.shared.dto.OAuthToken;
 import org.eclipse.che.api.core.model.workspace.config.SourceStorage;
 import org.eclipse.che.api.promises.client.Function;
 import org.eclipse.che.api.promises.client.FunctionException;
-import org.eclipse.che.api.promises.client.Operation;
-import org.eclipse.che.api.promises.client.OperationException;
 import org.eclipse.che.api.promises.client.Promise;
 import org.eclipse.che.api.promises.client.PromiseError;
 import org.eclipse.che.api.promises.client.callback.AsyncPromiseHelper.RequestCall;
 import org.eclipse.che.ide.CoreLocalizationConstant;
 import org.eclipse.che.ide.api.app.AppContext;
+import org.eclipse.che.ide.api.auth.OAuthServiceClient;
 import org.eclipse.che.ide.api.oauth.OAuth2Authenticator;
 import org.eclipse.che.ide.api.oauth.OAuth2AuthenticatorRegistry;
 import org.eclipse.che.ide.api.oauth.OAuth2AuthenticatorUrlProvider;
 import org.eclipse.che.ide.api.project.MutableProjectConfig;
 import org.eclipse.che.ide.api.resources.Project;
 import org.eclipse.che.ide.api.wizard.Wizard.CompleteCallback;
+import org.eclipse.che.ide.api.workspace.model.SourceStorageImpl;
 import org.eclipse.che.ide.projectimport.AbstractImporter;
 import org.eclipse.che.ide.resource.Path;
+import org.eclipse.che.ide.rest.AsyncRequestCallback;
 import org.eclipse.che.ide.ui.dialogs.askcredentials.AskCredentialsDialog;
-import org.eclipse.che.ide.ui.dialogs.askcredentials.Credentials;
 import org.eclipse.che.ide.util.ExceptionUtils;
 import org.eclipse.che.security.oauth.OAuthStatus;
 
@@ -59,6 +60,7 @@ public class ProjectImporter extends AbstractImporter {
   private final ProjectResolver projectResolver;
   private final AskCredentialsDialog credentialsDialog;
   private final OAuth2AuthenticatorRegistry oAuth2AuthenticatorRegistry;
+  private final OAuthServiceClient oAuthServiceClient;
 
   @Inject
   public ProjectImporter(
@@ -67,12 +69,14 @@ public class ProjectImporter extends AbstractImporter {
       AppContext appContext,
       ProjectResolver projectResolver,
       AskCredentialsDialog credentialsDialog,
-      OAuth2AuthenticatorRegistry oAuth2AuthenticatorRegistry) {
+      OAuth2AuthenticatorRegistry oAuth2AuthenticatorRegistry,
+      OAuthServiceClient oAuthServiceClient) {
     super(appContext, subscriberFactory);
     this.localizationConstant = localizationConstant;
     this.projectResolver = projectResolver;
     this.credentialsDialog = credentialsDialog;
     this.oAuth2AuthenticatorRegistry = oAuth2AuthenticatorRegistry;
+    this.oAuthServiceClient = oAuthServiceClient;
   }
 
   public void importProject(final CompleteCallback callback, MutableProjectConfig projectConfig) {
@@ -88,21 +92,15 @@ public class ProjectImporter extends AbstractImporter {
 
     startImport(path, projectConfig.getSource())
         .then(
-            new Operation<Project>() {
-              @Override
-              public void apply(Project arg) throws OperationException {
-                if (callback != null) {
-                  callback.onCompleted();
-                }
+            arg -> {
+              if (callback != null) {
+                callback.onCompleted();
               }
             })
         .catchError(
-            new Operation<PromiseError>() {
-              @Override
-              public void apply(PromiseError arg) throws OperationException {
-                if (callback != null) {
-                  callback.onFailure(arg.getCause());
-                }
+            arg -> {
+              if (callback != null) {
+                callback.onFailure(arg.getCause());
               }
             });
   }
@@ -126,13 +124,9 @@ public class ProjectImporter extends AbstractImporter {
         .withBody(importConfig)
         .send()
         .thenPromise(
-            new Function<Project, Promise<Project>>() {
-              @Override
-              public Promise<Project> apply(Project project) throws FunctionException {
-                subscriber.onSuccess();
-
-                return projectResolver.resolve(project);
-              }
+            project -> {
+              subscriber.onSuccess();
+              return projectResolver.resolve(project);
             })
         .catchErrorPromise(
             new Function<PromiseError, Promise<Project>>() {
@@ -175,27 +169,18 @@ public class ProjectImporter extends AbstractImporter {
             credentialsDialog
                 .askCredentials()
                 .then(
-                    new Operation<Credentials>() {
-                      @Override
-                      public void apply(Credentials credentials) throws OperationException {
-                        sourceStorage.getParameters().put("username", credentials.getUsername());
-                        sourceStorage.getParameters().put("password", credentials.getPassword());
-                        doImport(path, sourceStorage)
-                            .then(
-                                new Operation<Project>() {
-                                  @Override
-                                  public void apply(Project project) throws OperationException {
-                                    callback.onSuccess(project);
-                                  }
-                                })
-                            .catchError(
-                                new Operation<PromiseError>() {
-                                  @Override
-                                  public void apply(PromiseError error) throws OperationException {
-                                    callback.onFailure(error.getCause());
-                                  }
-                                });
-                      }
+                    credentials -> {
+                      sourceStorage.getParameters().put("username", credentials.getUsername());
+                      sourceStorage.getParameters().put("password", credentials.getPassword());
+                      doImport(path, sourceStorage)
+                          .then(
+                              project -> {
+                                callback.onSuccess(project);
+                              })
+                          .catchError(
+                              error -> {
+                                callback.onFailure(error.getCause());
+                              });
                     });
           }
         });
@@ -230,21 +215,35 @@ public class ProjectImporter extends AbstractImporter {
                   @Override
                   public void onSuccess(OAuthStatus result) {
                     if (!result.equals(OAuthStatus.NOT_PERFORMED)) {
-                      doImport(path, sourceStorage)
-                          .then(
-                              new Operation<Project>() {
-                                @Override
-                                public void apply(Project project) throws OperationException {
-                                  callback.onSuccess(project);
-                                }
-                              })
-                          .catchError(
-                              new Operation<PromiseError>() {
-                                @Override
-                                public void apply(PromiseError error) throws OperationException {
-                                  callback.onFailure(error.getCause());
-                                }
-                              });
+                      oAuthServiceClient.getToken(
+                          providerName,
+                          new AsyncRequestCallback<OAuthToken>() {
+                            @Override
+                            protected void onSuccess(OAuthToken result) {
+                              final SourceStorageImpl copySourceStorage =
+                                  new SourceStorageImpl(
+                                      sourceStorage.getType(),
+                                      sourceStorage.getLocation(),
+                                      sourceStorage.getParameters());
+                              copySourceStorage.getParameters().put("username", "token");
+                              copySourceStorage.getParameters().put("password", result.getToken());
+
+                              doImport(path, copySourceStorage)
+                                  .then(
+                                      project -> {
+                                        callback.onSuccess(project);
+                                      })
+                                  .catchError(
+                                      error -> {
+                                        callback.onFailure(error.getCause());
+                                      });
+                            }
+
+                            @Override
+                            protected void onFailure(Throwable exception) {
+                              callback.onFailure(new Exception(exception.getMessage()));
+                            }
+                          });
                     } else {
                       subscriber.onFailure("Authentication cancelled");
                       callback.onFailure(new IllegalStateException("Authentication cancelled"));
