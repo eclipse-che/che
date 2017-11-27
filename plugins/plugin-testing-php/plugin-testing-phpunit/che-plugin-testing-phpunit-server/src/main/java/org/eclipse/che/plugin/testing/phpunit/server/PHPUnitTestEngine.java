f2551cf7854a583cdef78724b73f60bb87c99f11
Valeriy Svydenko <vsvydenko@codenvy.com>
2017-11-27 11:42:15 +0200

    use jdt.ls extension in java test-plugin (#7429)
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- plugins/plugin-testing-php/plugin-testing-phpunit/che-plugin-testing-phpunit-server/src/main/java/org/eclipse/che/plugin/testing/phpunit/server/PHPUnitTestEngine.java
+++ plugins/plugin-testing-php/plugin-testing-phpunit/che-plugin-testing-phpunit-server/src/main/java/org/eclipse/che/plugin/testing/phpunit/server/PHPUnitTestEngine.java
@@ -46,7 +46,7 @@ public class PHPUnitTestEngine {
     this.projectsRoot = projectsRoot.toPath().normalize().toAbsolutePath();
   }
 
-  public ProcessHandler executeTests(TestExecutionContext context) {
+  public ProcessHandler executeTests(TestExecutionContext context) throws IOException {
     String projectPath = context.getProjectPath();
     String testTargetRelativePath = context.getFilePath();
     String projectAbsolutePath = ResourcesPlugin.getPathToWorkspace() + projectPath;
@@ -75,13 +75,7 @@ public class PHPUnitTestEngine {
             .directory(testTargetWorkingDirectory)
             .command(cmdRunTests.toShellCommand());
     pb.environment().put("ZEND_PHPUNIT_PORT", String.valueOf(PRINTER_PORT));
-    try {
-      return new ProcessHandler(pb.start());
-    } catch (IOException e) {
-      LOG.error("Can't run PHPUnit", e);
-    }
-
-    return null;
+    return new ProcessHandler(pb.start());
   }
 
   private File getPrinterFile() {
