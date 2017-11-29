417986f58202089be0dd98935c4b88e692de757f
julien <julien.sarik@misys.com>
2017-11-29 18:37:38 +0100

    handle invisible resource

--- plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/editor/PublishDiagnosticsProcessor.java
+++ plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/editor/PublishDiagnosticsProcessor.java
@@ -12,6 +12,7 @@ package org.eclipse.che.plugin.languageserver.ide.editor;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.util.Collection;
 import java.util.Optional;
 import org.eclipse.che.api.languageserver.shared.model.ExtendedPublishDiagnosticsParams;
 import org.eclipse.che.ide.api.editor.EditorAgent;
@@ -22,6 +23,7 @@ import org.eclipse.che.ide.api.editor.texteditor.TextEditor;
 import org.eclipse.che.ide.part.explorer.project.ProjectExplorerPresenter;
 import org.eclipse.che.ide.resource.Path;
 import org.eclipse.che.ide.resources.tree.ResourceNode;
+import org.eclipse.che.ide.ui.smartTree.NodeDescriptor;
 import org.eclipse.che.ide.ui.smartTree.Tree;
 import org.eclipse.lsp4j.Diagnostic;
 
@@ -41,16 +43,7 @@ public class PublishDiagnosticsProcessor {
 
   public void processDiagnostics(ExtendedPublishDiagnosticsParams diagnosticsMessage) {
     final Path path = Path.valueOf(diagnosticsMessage.getParams().getUri());
-    final Optional<ResourceNode> resource =
-        tree.getNodeStorage()
-            .getAll()
-            .stream()
-            .filter(node -> node instanceof ResourceNode)
-            .map(node -> ((ResourceNode) node))
-            .filter(node -> node.getData().isFile())
-            .filter(node -> path.equals(node.getData().asFile().getLocation()))
-            .findAny();
-    resource.ifPresent(this::setHasError);
+    findVisibleResource(path).ifPresent(this::setHasError);
 
     EditorPartPresenter openedEditor = editorAgent.getOpenedEditor(path);
     // TODO add markers
@@ -74,6 +67,46 @@ public class PublishDiagnosticsProcessor {
         }
       }
     }
+  }
+
+  /**
+   * Find the given resource in the Project Explorer or its first visible parent.
+   *
+   * @param path the path of the resource to find in the Project Explorer.
+   * @return an optional containing the {@link ResourceNode} with the given path if it is visible.
+   *     If the desired resource is not visible, try to return its first visible parent. If no
+   *     matching {@link ResourceNode} can be find, returns an empty {@link Optional}.
+   */
+  private Optional<ResourceNode> findVisibleResource(Path path) {
+    if (path.isRoot()) {
+      if (tree.getNodeStorage().getStoredNodes().isEmpty()) {
+        return Optional.empty();
+      }
+      return Optional.of(
+          (ResourceNode) tree.getNodeStorage().getStoredNodes().iterator().next().getNode());
+    }
+    return findVisibleResource(path, tree.getNodeStorage().getStoredNodes(), path.uptoSegment(1));
+  }
+
+  private static Optional<ResourceNode> findVisibleResource(
+      Path completePath, Collection<NodeDescriptor> nodes, Path partialPath) {
+    for (final NodeDescriptor child : nodes) {
+      if (!(child.getNode() instanceof ResourceNode)) {
+        continue;
+      }
+      final ResourceNode resourceNode = (ResourceNode) child.getNode();
+      if (!partialPath.equals(resourceNode.getData().getLocation())) {
+        continue;
+      }
+      if (child.getChildren().isEmpty()) {
+        return Optional.of(resourceNode);
+      }
+      return findVisibleResource(
+          completePath,
+          child.getChildren(),
+          completePath.uptoSegment(partialPath.segmentCount() + 1));
+    }
+    return Optional.empty();
   }
 
   private void setHasError(ResourceNode node) {
