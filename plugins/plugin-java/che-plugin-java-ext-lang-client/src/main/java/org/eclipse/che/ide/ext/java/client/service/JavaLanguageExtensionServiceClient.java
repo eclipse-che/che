060e3088dcd72e1fed77e83c9e29a841cb77849a
Valeriy Svydenko <vsvydenko@codenvy.com>
2017-12-07 15:31:28 +0200

    Getting external libraries via jdt.ls extension (#7679)
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/service/JavaLanguageExtensionServiceClient.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/service/JavaLanguageExtensionServiceClient.java
@@ -10,9 +10,17 @@
  */
 package org.eclipse.che.ide.ext.java.client.service;
 
+import static org.eclipse.che.api.promises.client.js.JsPromiseError.create;
 import static org.eclipse.che.ide.api.jsonrpc.Constants.WS_AGENT_JSON_RPC_ENDPOINT_ID;
+import static org.eclipse.che.ide.ext.java.shared.Constants.CLASS_PATH_TREE;
 import static org.eclipse.che.ide.ext.java.shared.Constants.EFFECTIVE_POM_REQUEST_TIMEOUT;
-import static org.eclipse.che.ide.ext.java.shared.Constants.FILE_STRUCTURE_REQUEST_TIMEOUT;
+import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_LIBRARIES;
+import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_LIBRARIES_CHILDREN;
+import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_LIBRARY_CHILDREN;
+import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_LIBRARY_ENTRY;
+import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_NODE_CONTENT;
+import static org.eclipse.che.ide.ext.java.shared.Constants.FILE_STRUCTURE;
+import static org.eclipse.che.ide.ext.java.shared.Constants.REQUEST_TIMEOUT;
 
 import com.google.gwt.jsonp.client.TimeoutException;
 import com.google.inject.Inject;
@@ -20,10 +28,14 @@ import com.google.inject.Singleton;
 import java.util.List;
 import org.eclipse.che.api.core.jsonrpc.commons.RequestTransmitter;
 import org.eclipse.che.api.promises.client.Promise;
-import org.eclipse.che.api.promises.client.js.JsPromiseError;
 import org.eclipse.che.api.promises.client.js.Promises;
+import org.eclipse.che.api.promises.client.js.RejectFunction;
+import org.eclipse.che.ide.ext.java.shared.dto.classpath.ClasspathEntryDto;
 import org.eclipse.che.jdt.ls.extension.api.dto.ExtendedSymbolInformation;
+import org.eclipse.che.jdt.ls.extension.api.dto.ExternalLibrariesParameters;
 import org.eclipse.che.jdt.ls.extension.api.dto.FileStructureCommandParameters;
+import org.eclipse.che.jdt.ls.extension.api.dto.Jar;
+import org.eclipse.che.jdt.ls.extension.api.dto.JarEntry;
 import org.eclipse.che.plugin.languageserver.ide.service.ServiceUtil;
 
 @Singleton
@@ -42,12 +54,11 @@ public class JavaLanguageExtensionServiceClient {
           requestTransmitter
               .newRequest()
               .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
-              .methodName("java/file-structure")
+              .methodName(FILE_STRUCTURE)
               .paramsAsDto(params)
-              .sendAndReceiveResultAsListOfDto(
-                  ExtendedSymbolInformation.class, FILE_STRUCTURE_REQUEST_TIMEOUT)
+              .sendAndReceiveResultAsListOfDto(ExtendedSymbolInformation.class, REQUEST_TIMEOUT)
               .onSuccess(resolve::apply)
-              .onTimeout(() -> reject.apply(JsPromiseError.create(new TimeoutException("Timeout"))))
+              .onTimeout(() -> onTimeout(reject))
               .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error)));
         });
   }
@@ -71,8 +82,134 @@ public class JavaLanguageExtensionServiceClient {
                 .onTimeout(
                     () ->
                         reject.apply(
-                            JsPromiseError.create(
-                                new TimeoutException("Timeout while getting effective pom."))))
+                            create(new TimeoutException("Timeout while getting effective pom."))))
                 .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
+  }
+
+  /**
+   * Gets external libraries of the project.
+   *
+   * @param params external libraries parameters {@link ExternalLibrariesParameters}
+   * @return list of jars
+   */
+  public Promise<List<Jar>> externalLibraries(ExternalLibrariesParameters params) {
+    return Promises.create(
+        (resolve, reject) ->
+            requestTransmitter
+                .newRequest()
+                .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
+                .methodName(EXTERNAL_LIBRARIES)
+                .paramsAsDto(params)
+                .sendAndReceiveResultAsListOfDto(Jar.class, REQUEST_TIMEOUT)
+                .onSuccess(resolve::apply)
+                .onTimeout(() -> onTimeout(reject))
+                .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
+  }
+
+  /**
+   * Gets classpath structure.
+   *
+   * @param projectPath path to the project
+   * @return classpath structure
+   */
+  public Promise<List<ClasspathEntryDto>> classpathTree(String projectPath) {
+    return Promises.create(
+        (resolve, reject) ->
+            requestTransmitter
+                .newRequest()
+                .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
+                .methodName(CLASS_PATH_TREE)
+                .paramsAsString(projectPath)
+                .sendAndReceiveResultAsListOfDto(ClasspathEntryDto.class, REQUEST_TIMEOUT)
+                .onSuccess(resolve::apply)
+                .onTimeout(() -> onTimeout(reject))
+                .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
+  }
+
+  /**
+   * Gets children of libraries.
+   *
+   * @param params external libraries parameters {@link ExternalLibrariesParameters}
+   * @return list of jars' entries.
+   */
+  public Promise<List<JarEntry>> externalLibrariesChildren(ExternalLibrariesParameters params) {
+    return Promises.create(
+        (resolve, reject) ->
+            requestTransmitter
+                .newRequest()
+                .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
+                .methodName(EXTERNAL_LIBRARIES_CHILDREN)
+                .paramsAsDto(params)
+                .sendAndReceiveResultAsListOfDto(JarEntry.class, REQUEST_TIMEOUT)
+                .onSuccess(resolve::apply)
+                .onTimeout(() -> onTimeout(reject))
+                .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
+  }
+
+  /**
+   * Gets children of the library.
+   *
+   * @param params external libraries parameters {@link ExternalLibrariesParameters}
+   * @return list of entries
+   */
+  public Promise<List<JarEntry>> libraryChildren(ExternalLibrariesParameters params) {
+    return Promises.create(
+        (resolve, reject) ->
+            requestTransmitter
+                .newRequest()
+                .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
+                .methodName(EXTERNAL_LIBRARY_CHILDREN)
+                .paramsAsDto(params)
+                .sendAndReceiveResultAsListOfDto(JarEntry.class, REQUEST_TIMEOUT)
+                .onSuccess(resolve::apply)
+                .onTimeout(() -> onTimeout(reject))
+                .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
+  }
+
+  /**
+   * Gets entry.
+   *
+   * @param params external libraries parameters {@link ExternalLibrariesParameters}
+   * @return entry {@link JarEntry}
+   */
+  public Promise<JarEntry> libraryEntry(ExternalLibrariesParameters params) {
+    return Promises.create(
+        (resolve, reject) ->
+            requestTransmitter
+                .newRequest()
+                .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
+                .methodName(EXTERNAL_LIBRARY_ENTRY)
+                .paramsAsDto(params)
+                .sendAndReceiveResultAsDto(JarEntry.class, REQUEST_TIMEOUT)
+                .onSuccess(resolve::apply)
+                .onTimeout(() -> onTimeout(reject))
+                .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
+  }
+
+  /**
+   * Gets content of the file from the library by file path.
+   *
+   * @param params external libraries parameters {@link ExternalLibrariesParameters}
+   * @return content of the file
+   */
+  public Promise<String> libraryNodeContentByPath(ExternalLibrariesParameters params) {
+    return Promises.create(
+        (resolve, reject) ->
+            requestTransmitter
+                .newRequest()
+                .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
+                .methodName(EXTERNAL_NODE_CONTENT)
+                .paramsAsDto(params)
+                .sendAndReceiveResultAsString(REQUEST_TIMEOUT)
+                .onSuccess(resolve::apply)
+                .onTimeout(() -> onTimeout(reject))
+                .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
+  }
+
+  private void onTimeout(RejectFunction reject) {
+    reject.apply(
+        create(
+            new TimeoutException(
+                "Looks like the language server is taking to long to respond, please try again in sometime.")));
   }
 }
