1b70a3951e24b4a7dfe6bb6da82129c6186422b9
Anatoliy Bazko <abazko@redhat.com>
2017-12-08 15:57:39 +0200

    CHE-6914: Move Java Debugger Location Conversion to jdt.ls extension (#7658)
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

---
+++ plugins/plugin-java-debugger/che-plugin-java-debugger-server/src/test/resources/testclasses/EvaluateExpressionTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2012-2017 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse;
+
+public class EvaluateExpressionTest {
+  public static void main(String[] args) throws Exception {
+    int i = 1;
+    do1(i);
+  }
+
+  private static void do1(int i) {
+    i = 2;
+    System.out.println("Set breakpoint here.");
+  }
+}
---
+++ plugins/plugin-java-debugger/che-plugin-java-debugger-server/src/test/resources/testclasses/ThreadDumpTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2012-2017 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ThreadDumpTest {
+
+  public static void main(String[] args) throws Exception {
+    CountDownLatch latch = new CountDownLatch(1);
+
+    Thread thread = new SomeThread(latch);
+    thread.setDaemon(true);
+    thread.start();
+
+    latch.await();
+
+    System.out.println("Hello, world!");
+
+    for (; ; ) {}
+  }
+
+  public static class SomeThread extends Thread {
+    private final CountDownLatch latch;
+
+    public SomeThread(CountDownLatch latch) {
+      super("SomeThread");
+      this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+      latch.countDown();
+
+      for (; ; ) {}
+    }
+  }
+}
