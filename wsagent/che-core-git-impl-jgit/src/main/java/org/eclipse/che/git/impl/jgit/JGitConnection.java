6b4aaa51e09e9329fdec39d2a34986a0dcc3d404
Igor Vinokur <ivinokur@codenvy.com>
2017-12-14 11:30:53 +0200

    CHE-7220: Fix Git add to index when deleted files are present

--- wsagent/che-core-git-impl-jgit/src/main/java/org/eclipse/che/git/impl/jgit/JGitConnection.java
+++ wsagent/che-core-git-impl-jgit/src/main/java/org/eclipse/che/git/impl/jgit/JGitConnection.java
@@ -342,27 +342,25 @@ class JGitConnection implements GitConnection {
     try {
       addCommand.call();
 
-      addDeletedFilesToIndex(filePatterns);
+      addDeletedFilesToIndexIfNeeded(filePatterns);
     } catch (GitAPIException exception) {
       throw new GitException(exception.getMessage(), exception);
     }
   }
 
   /** To add deleted files in index it is required to perform git rm on them */
-  private void addDeletedFilesToIndex(List<String> filePatterns) throws GitAPIException {
+  private void addDeletedFilesToIndexIfNeeded(List<String> filePatterns) throws GitAPIException {
     Set<String> deletedFiles = getGit().status().call().getMissing();
+    if (deletedFiles.isEmpty()) {
+      return;
+    }
+    if (!filePatterns.isEmpty() && !filePatterns.contains(".")) {
+      deletedFiles =
+          deletedFiles.stream().filter(filePatterns::contains).collect(Collectors.toSet());
+    }
     if (!deletedFiles.isEmpty()) {
       RmCommand rmCommand = getGit().rm();
-      if (filePatterns.contains(".")) {
-        deletedFiles.forEach(rmCommand::addFilepattern);
-      } else {
-        filePatterns.forEach(
-            filePattern ->
-                deletedFiles
-                    .stream()
-                    .filter(deletedFile -> deletedFile.startsWith(filePattern))
-                    .forEach(rmCommand::addFilepattern));
-      }
+      deletedFiles.forEach(rmCommand::addFilepattern);
       rmCommand.call();
     }
   }
