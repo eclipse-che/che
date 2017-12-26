28b62cd9b96de2b39efca76fa4de5835b36f6b2f
Mykola Morhun <mmorhun@redhat.com>
2017-12-26 09:24:56 +0200

    CHE-6910: Use JDT-LS to update maven projects (#7972)

--- plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/service/JavaLanguageExtensionServiceClient.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/service/JavaLanguageExtensionServiceClient.java
@@ -13,6 +13,7 @@ package org.eclipse.che.ide.ext.java.client.service;
 import static org.eclipse.che.api.promises.client.js.JsPromiseError.create;
 import static org.eclipse.che.ide.api.jsonrpc.Constants.WS_AGENT_JSON_RPC_ENDPOINT_ID;
 import static org.eclipse.che.ide.ext.java.shared.Constants.CLASS_PATH_TREE;
+import static org.eclipse.che.ide.ext.java.shared.Constants.EFFECTIVE_POM;
 import static org.eclipse.che.ide.ext.java.shared.Constants.EFFECTIVE_POM_REQUEST_TIMEOUT;
 import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_LIBRARIES;
 import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_LIBRARIES_CHILDREN;
@@ -20,6 +21,8 @@ import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_LIBRARY_CHI
 import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_LIBRARY_ENTRY;
 import static org.eclipse.che.ide.ext.java.shared.Constants.EXTERNAL_NODE_CONTENT;
 import static org.eclipse.che.ide.ext.java.shared.Constants.FILE_STRUCTURE;
+import static org.eclipse.che.ide.ext.java.shared.Constants.REIMPORT_MAVEN_PROJECTS;
+import static org.eclipse.che.ide.ext.java.shared.Constants.REIMPORT_MAVEN_PROJECTS_REQUEST_TIMEOUT;
 import static org.eclipse.che.ide.ext.java.shared.Constants.REQUEST_TIMEOUT;
 
 import com.google.gwt.jsonp.client.TimeoutException;
@@ -36,6 +39,7 @@ import org.eclipse.che.jdt.ls.extension.api.dto.ExternalLibrariesParameters;
 import org.eclipse.che.jdt.ls.extension.api.dto.FileStructureCommandParameters;
 import org.eclipse.che.jdt.ls.extension.api.dto.Jar;
 import org.eclipse.che.jdt.ls.extension.api.dto.JarEntry;
+import org.eclipse.che.jdt.ls.extension.api.dto.ReImportMavenProjectsCommandParameters;
 import org.eclipse.che.plugin.languageserver.ide.service.ServiceUtil;
 
 @Singleton
@@ -75,7 +79,7 @@ public class JavaLanguageExtensionServiceClient {
             requestTransmitter
                 .newRequest()
                 .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
-                .methodName("java/effective-pom")
+                .methodName(EFFECTIVE_POM)
                 .paramsAsString(pathToProject)
                 .sendAndReceiveResultAsString(EFFECTIVE_POM_REQUEST_TIMEOUT)
                 .onSuccess(resolve::apply)
@@ -83,6 +87,27 @@ public class JavaLanguageExtensionServiceClient {
                     () ->
                         reject.apply(
                             create(new TimeoutException("Timeout while getting effective pom."))))
+                .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
+  }
+
+  /**
+   * Updates specified maven projects.
+   *
+   * @param params contains list of paths to projects which should be reimported
+   * @return list of paths to updated projects
+   */
+  public Promise<List<String>> reImportMavenProjects(
+      ReImportMavenProjectsCommandParameters params) {
+    return Promises.create(
+        (resolve, reject) ->
+            requestTransmitter
+                .newRequest()
+                .endpointId(WS_AGENT_JSON_RPC_ENDPOINT_ID)
+                .methodName(REIMPORT_MAVEN_PROJECTS)
+                .paramsAsDto(params)
+                .sendAndReceiveResultAsListOfString(REIMPORT_MAVEN_PROJECTS_REQUEST_TIMEOUT)
+                .onSuccess(resolve::apply)
+                .onTimeout(() -> reject.apply(create("Failed to update maven project.")))
                 .onFailure(error -> reject.apply(ServiceUtil.getPromiseError(error))));
   }
 
