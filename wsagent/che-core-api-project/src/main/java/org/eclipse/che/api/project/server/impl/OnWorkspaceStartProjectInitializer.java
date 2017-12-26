65fdb9b0ef2249160e41da189dc349bc8448a357
Anatoliy Bazko <abazko@redhat.com>
2017-12-26 11:05:28 +0200

    CHE-7836: Update Che JDT-LS workspace on project creation/deletion (#7977)
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- wsagent/che-core-api-project/src/main/java/org/eclipse/che/api/project/server/impl/OnWorkspaceStartProjectInitializer.java
+++ wsagent/che-core-api-project/src/main/java/org/eclipse/che/api/project/server/impl/OnWorkspaceStartProjectInitializer.java
@@ -25,10 +25,12 @@ import org.eclipse.che.api.core.ForbiddenException;
 import org.eclipse.che.api.core.NotFoundException;
 import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.core.model.workspace.config.ProjectConfig;
+import org.eclipse.che.api.core.notification.EventService;
 import org.eclipse.che.api.fs.server.FsManager;
 import org.eclipse.che.api.project.server.handlers.ProjectInitHandler;
 import org.eclipse.che.api.project.shared.RegisteredProject;
 import org.eclipse.che.api.search.server.excludes.HiddenItemPathMatcher;
+import org.eclipse.che.api.project.server.notification.ProjectInitializedEvent;
 
 @Singleton
 public class OnWorkspaceStartProjectInitializer {
@@ -37,6 +39,7 @@ public class OnWorkspaceStartProjectInitializer {
   private final WorkspaceProjectSynchronizer projectSynchronizer;
   private final ProjectConfigRegistry projectConfigRegistry;
   private final ProjectHandlerRegistry projectHandlerRegistry;
+  private final EventService eventService;
   private final HiddenItemPathMatcher hiddenItemPathMatcher;
 
   @Inject
@@ -45,12 +48,15 @@ public class OnWorkspaceStartProjectInitializer {
       WorkspaceProjectSynchronizer projectSynchronizer,
       ProjectConfigRegistry projectConfigRegistry,
       ProjectHandlerRegistry projectHandlerRegistry,
-      HiddenItemPathMatcher hiddenItemPathMatcher) {
+      HiddenItemPathMatcher hiddenItemPathMatcher,
+      ProjectHandlerRegistry projectHandlerRegistry,
+      EventService eventService) {
     this.fsManager = fsManager;
     this.projectSynchronizer = projectSynchronizer;
     this.projectConfigRegistry = projectConfigRegistry;
     this.projectHandlerRegistry = projectHandlerRegistry;
     this.hiddenItemPathMatcher = hiddenItemPathMatcher;
+    this.eventService = eventService;
   }
 
   @PostConstruct
@@ -94,6 +100,8 @@ public class OnWorkspaceStartProjectInitializer {
           hOptional.get().onProjectInitialized(project.getBaseFolder());
         }
       }
+
+      eventService.publish(new ProjectInitializedEvent(project.getBaseFolder()));
     }
   }
 }
