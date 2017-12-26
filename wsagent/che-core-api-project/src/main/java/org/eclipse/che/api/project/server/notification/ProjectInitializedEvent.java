892ae32ea698b1e1fd6fb7c0281d63c6e686087c
Anatoliy Bazko <abazko@redhat.com>
2017-12-26 11:05:28 +0200

    CHE-7836: Update Che JDT-LS workspace on project creation/deletion (#7977)

---
+++ wsagent/che-core-api-project/src/main/java/org/eclipse/che/api/project/server/notification/ProjectInitializedEvent.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2012-2017 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.project.server.notification;
+
+import org.eclipse.che.api.core.notification.EventOrigin;
+
+/**
+ * Publish when project initialized.
+ *
+ * @author Anatolii Bazko
+ */
+@EventOrigin("project")
+public class ProjectInitializedEvent {
+
+  private String projectPath;
+
+  public ProjectInitializedEvent(String projectPath) {
+    this.projectPath = projectPath;
+  }
+
+  public String getProjectPath() {
+    return projectPath;
+  }
+
+  public void setProjectPath(String projectPath) {
+    this.projectPath = projectPath;
+  }
+
+  @Override
+  public String toString() {
+    return "ProjectInitializedEvent{" + "projectPath='" + projectPath + '\'' + '}';
+  }
+}
