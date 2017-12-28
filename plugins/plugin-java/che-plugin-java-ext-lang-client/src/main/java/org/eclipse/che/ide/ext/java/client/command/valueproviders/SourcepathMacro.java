662de17efce4ff0a0ba0e5480c829fad563e8550
Valeriy Svydenko <vsvydenko@codenvy.com>
2017-12-28 11:55:20 +0200

    Add support for "Simple Java" projects for jdt.ls (#8065)

--- plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/command/valueproviders/SourcepathMacro.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/command/valueproviders/SourcepathMacro.java
@@ -11,13 +11,11 @@
  */
 package org.eclipse.che.ide.ext.java.client.command.valueproviders;
 
-import com.google.common.base.Optional;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.List;
 import java.util.Set;
 import org.eclipse.che.api.promises.client.Function;
-import org.eclipse.che.api.promises.client.FunctionException;
 import org.eclipse.che.api.promises.client.Promise;
 import org.eclipse.che.api.promises.client.PromiseProvider;
 import org.eclipse.che.ide.api.app.AppContext;
@@ -28,7 +26,7 @@ import org.eclipse.che.ide.ext.java.client.JavaLocalizationConstant;
 import org.eclipse.che.ide.ext.java.client.command.ClasspathContainer;
 import org.eclipse.che.ide.ext.java.client.project.classpath.ClasspathResolver;
 import org.eclipse.che.ide.ext.java.client.util.JavaUtil;
-import org.eclipse.che.ide.ext.java.shared.dto.classpath.ClasspathEntryDto;
+import org.eclipse.che.jdt.ls.extension.api.dto.ClasspathEntry;
 
 /**
  * Provides project's sourcepath value.
@@ -79,33 +77,31 @@ public class SourcepathMacro implements Macro {
     }
 
     final Resource resource = resources[0];
-    final Optional<Project> project = resource.getRelatedProject();
+    final Project project = resource.getProject();
 
-    if (!JavaUtil.isJavaProject(project.get())) {
+    if (!JavaUtil.isJavaProject(project)) {
       return promises.resolve("");
     }
 
-    final String projectPath = project.get().getLocation().toString();
+    final String projectPath = project.getLocation().toString();
 
     return classpathContainer
         .getClasspathEntries(projectPath)
         .then(
-            new Function<List<ClasspathEntryDto>, String>() {
-              @Override
-              public String apply(List<ClasspathEntryDto> arg) throws FunctionException {
-                classpathResolver.resolveClasspathEntries(arg);
-                Set<String> sources = classpathResolver.getSources();
-                StringBuilder classpath = new StringBuilder();
-                for (String source : sources) {
-                  classpath.append(source.substring(projectPath.length() + 1)).append(':');
-                }
+            (Function<List<ClasspathEntry>, String>)
+                arg -> {
+                  classpathResolver.resolveClasspathEntries(arg);
+                  Set<String> sources = classpathResolver.getSources();
+                  StringBuilder classpath = new StringBuilder();
+                  for (String source : sources) {
+                    classpath.append(source.substring(projectPath.length() + 1)).append(':');
+                  }
 
-                if (classpath.toString().isEmpty()) {
-                  classpath.append(appContext.getProjectsRoot().toString()).append(projectPath);
-                }
+                  if (classpath.toString().isEmpty()) {
+                    classpath.append(appContext.getProjectsRoot().toString()).append(projectPath);
+                  }
 
-                return classpath.toString();
-              }
-            });
+                  return classpath.toString();
+                });
   }
 }
--- plugins/plugin-java/che-plugin-java-plain/che-plugin-java-plain-server/src/main/java/org/eclipse/che/plugin/java/plain/server/projecttype/PlainJavaInitHandler.java
+++ plugins/plugin-java/che-plugin-java-plain/che-plugin-java-plain-server/src/main/java/org/eclipse/che/plugin/java/plain/server/projecttype/PlainJavaInitHandler.java
@@ -11,80 +11,27 @@
  */
 package org.eclipse.che.plugin.java.plain.server.projecttype;
 
-import static org.eclipse.che.api.fs.server.WsPathUtils.absolutize;
 import static org.eclipse.che.ide.ext.java.shared.Constants.JAVAC;
-import static org.eclipse.che.plugin.java.plain.shared.PlainJavaProjectConstants.LIBRARY_FOLDER;
 
-import com.google.inject.Inject;
-import com.google.inject.Provider;
-import java.util.Arrays;
-import java.util.List;
+import org.eclipse.che.api.core.ConflictException;
+import org.eclipse.che.api.core.ForbiddenException;
+import org.eclipse.che.api.core.NotFoundException;
 import org.eclipse.che.api.core.ServerException;
-import org.eclipse.che.api.core.model.workspace.config.ProjectConfig;
-import org.eclipse.che.api.project.server.ProjectManager;
-import org.eclipse.che.ide.ext.java.shared.Constants;
-import org.eclipse.che.plugin.java.server.projecttype.AbstractJavaInitHandler;
-import org.eclipse.jdt.core.IClasspathEntry;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.che.api.project.server.handlers.ProjectInitHandler;
 
 /**
- * Init handler for simple java project. Initialize classpath with JRE classpath entry container and
- * 'src' source classpath entry.
+ * Init handler for simple java project.
  *
  * @author Evgen Vidolob
  * @author Valeriy Svydenko
  */
-public class PlainJavaInitHandler extends AbstractJavaInitHandler {
-
-  private static final Logger LOG = LoggerFactory.getLogger(PlainJavaInitHandler.class);
-  private final ClasspathBuilder classpathBuilder;
-  private final Provider<ProjectManager> projectRegistryProvider;
-
-  @Inject
-  public PlainJavaInitHandler(
-      ClasspathBuilder classpathBuilder, Provider<ProjectManager> projectRegistryProvider) {
-    this.classpathBuilder = classpathBuilder;
-    this.projectRegistryProvider = projectRegistryProvider;
-  }
-
-  @Override
-  protected void initializeClasspath(IJavaProject javaProject) throws ServerException {
-    IClasspathEntry[] projectClasspath;
-    try {
-      projectClasspath = javaProject.getRawClasspath();
-    } catch (JavaModelException e) {
-      LOG.warn(
-          "Can't get classpath for: " + javaProject.getProject().getFullPath().toOSString(), e);
-      throw new ServerException(e);
-    }
-
-    // default classpath
-    IClasspathEntry[] defaultClasspath =
-        new IClasspathEntry[] {JavaCore.newSourceEntry(javaProject.getPath())};
-    if (!Arrays.equals(defaultClasspath, projectClasspath)) {
-      // classpath is already initialized
-      return;
-    }
-
-    String wsPath = absolutize(javaProject.getPath().toOSString());
-    ProjectConfig project =
-        projectRegistryProvider
-            .get()
-            .get(wsPath)
-            .orElseThrow(() -> new ServerException("Can't find a project: " + wsPath));
-
-    List<String> sourceFolders = project.getAttributes().get(Constants.SOURCE_FOLDER);
-    List<String> library = project.getAttributes().get(LIBRARY_FOLDER);
-
-    classpathBuilder.generateClasspath(javaProject, sourceFolders, library);
-  }
-
+public class PlainJavaInitHandler implements ProjectInitHandler {
   @Override
   public String getProjectType() {
     return JAVAC;
   }
+
+  @Override
+  public void onProjectInitialized(String projectFolder)
+      throws ServerException, ForbiddenException, ConflictException, NotFoundException {}
 }
