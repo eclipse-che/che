79ecd84c62d48189f19f9d3f8245269bf5f6ca6a
Valeriy Svydenko <vsvydenko@codenvy.com>
2017-12-28 11:55:20 +0200

    Add support for "Simple Java" projects for jdt.ls (#8065)

--- plugins/plugin-java/che-plugin-java-plain/che-plugin-java-plain-server/src/main/java/org/eclipse/che/plugin/java/plain/server/generator/PlainJavaProjectGenerator.java
+++ plugins/plugin-java/che-plugin-java-plain/che-plugin-java-plain-server/src/main/java/org/eclipse/che/plugin/java/plain/server/generator/PlainJavaProjectGenerator.java
@@ -12,9 +12,9 @@ package org.eclipse.che.plugin.java.plain.server.generator;
 
 import static java.util.Collections.singletonList;
 import static org.eclipse.che.api.fs.server.WsPathUtils.resolve;
+import static org.eclipse.che.api.languageserver.service.LanguageServiceUtils.prefixURI;
 import static org.eclipse.che.ide.ext.java.shared.Constants.JAVAC;
 import static org.eclipse.che.ide.ext.java.shared.Constants.SOURCE_FOLDER;
-import static org.eclipse.che.plugin.java.plain.shared.PlainJavaProjectConstants.DEFAULT_LIBRARY_FOLDER_VALUE;
 import static org.eclipse.che.plugin.java.plain.shared.PlainJavaProjectConstants.DEFAULT_OUTPUT_FOLDER_VALUE;
 import static org.eclipse.che.plugin.java.plain.shared.PlainJavaProjectConstants.DEFAULT_SOURCE_FOLDER_VALUE;
 
@@ -28,11 +28,7 @@ import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.fs.server.FsManager;
 import org.eclipse.che.api.project.server.handlers.CreateProjectHandler;
 import org.eclipse.che.api.project.server.type.AttributeValue;
-import org.eclipse.che.plugin.java.plain.server.projecttype.ClasspathBuilder;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.che.plugin.java.languageserver.JavaLanguageServerExtensionService;
 
 /**
  * Generates new project which contains file with default content.
@@ -43,12 +39,13 @@ public class PlainJavaProjectGenerator implements CreateProjectHandler {
 
   private static final String FILE_NAME = "Main.java";
 
-  private final ClasspathBuilder classpathBuilder;
+  private JavaLanguageServerExtensionService service;
   private final FsManager fsManager;
 
   @Inject
-  protected PlainJavaProjectGenerator(ClasspathBuilder classpathBuilder, FsManager fsManager) {
-    this.classpathBuilder = classpathBuilder;
+  protected PlainJavaProjectGenerator(
+      JavaLanguageServerExtensionService service, FsManager fsManager) {
+    this.service = service;
     this.fsManager = fsManager;
   }
 
@@ -77,11 +74,7 @@ public class PlainJavaProjectGenerator implements CreateProjectHandler {
         mainJavaWsPath,
         getClass().getClassLoader().getResourceAsStream("files/main_class_content"));
 
-    IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectWsPath);
-    IJavaProject javaProject = JavaCore.create(project);
-
-    classpathBuilder.generateClasspath(
-        javaProject, sourceFolders, singletonList(DEFAULT_LIBRARY_FOLDER_VALUE));
+    service.createSimpleProject(prefixURI(projectWsPath), sourceFolders.get(0));
   }
 
   @Override
