9253b09fede2032666a8b5b5d9148d6502dd360a
SkorikSergey <sskorik@codenvy.com>
2018-01-10 16:31:20 +0200

    ServerRuntimeInfoTest test moved from consoles package to miscellaneous

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/miscellaneous/ServerRuntimeInfoTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/miscellaneous/ServerRuntimeInfoTest.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-package org.eclipse.che.selenium.consoles;
+package org.eclipse.che.selenium.miscellaneous;
 
 import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.LOADER_TIMEOUT_SEC;
 
