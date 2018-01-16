8717c366efe94e42d735a335e18b2d37b250c106
James Drummond <james@devcomb.com>
2018-01-16 16:44:32 +0000

    Signed-off-by: James Drummond <james@devcomb.com>

--- dockerfiles/init/modules/openshift/files/scripts/ocp.sh
+++ dockerfiles/init/modules/openshift/files/scripts/ocp.sh
@@ -158,19 +158,19 @@ run_ocp() {
 }
 
 deploy_che_to_ocp() {
+    #Repull init image only if IMAGE_PULL_POLICY is set to Always
+    if [ $IMAGE_PULL_POLICY == "Always" ]; then
+        docker pull "$IMAGE_INIT"
+    fi
     #Only generate scripts and config files if CHE_GENERATE_SCRIPTS=true
     if [ $CHE_GENERATE_SCRIPTS == true ]; then
       echo "OCP generating temporary scripts and configuration files at ${CONFIG_DIR}/instance/config/openshift/scripts/ ."
-      #Repull init image only if IMAGE_PULL_POLICY is set to Always
-      if [ $IMAGE_PULL_POLICY == "Always" ]; then
-         docker pull "$IMAGE_INIT"
-      fi
       #wipeout config folder
       docker run -v "${CONFIG_DIR}":/to_remove alpine sh -c "rm -rf /to_remove/" || true
       docker run -t --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${CONFIG_DIR}":/data -e IMAGE_INIT="$IMAGE_INIT" -e CHE_MULTIUSER="$CHE_MULTIUSER" eclipse/che-cli:${CHE_IMAGE_TAG} config --skip:pull --skip:nightly
       cd "${CONFIG_DIR}/instance/config/openshift/scripts/"
     else
-      echo "OCP using existing scripts in current folder."
+      echo "OCP using existing scripts and configuration files in current folder."
     fi
     if [[ ! -f "deploy_che.sh" ]]; then
       CURRENT_PWD=$(pwd)
