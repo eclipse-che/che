484620c432e3f0ad0c1d11a829fe55b37e2cab41
Thomas Mäder <tsmaeder@users.noreply.github.com>
2018-01-22 10:05:58 +0100

    6682 Use jdt.ls for Usages
    Use jdt.ls for usages
    
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- plugins/plugin-java/che-plugin-java-ext-lang-server/src/main/java/org/eclipse/che/plugin/java/server/rest/JdtExceptionMapper.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-server/src/main/java/org/eclipse/che/plugin/java/server/rest/JdtExceptionMapper.java
@@ -18,7 +18,6 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 import org.eclipse.che.dto.server.DtoFactory;
 import org.eclipse.che.plugin.java.server.JdtException;
-import org.eclipse.che.plugin.java.server.search.SearchException;
 
 /**
  * Exception mapper for all JDT exceptions
@@ -30,15 +29,9 @@ import org.eclipse.che.plugin.java.server.search.SearchException;
 public class JdtExceptionMapper implements ExceptionMapper<JdtException> {
   @Override
   public Response toResponse(JdtException exception) {
-    if (exception instanceof SearchException) {
-      return Response.status(Response.Status.BAD_REQUEST)
-          .entity(DtoFactory.getInstance().toJson(exception.getServiceError()))
-          .type(MediaType.APPLICATION_JSON)
-          .build();
-    } else
-      return Response.serverError()
-          .entity(DtoFactory.getInstance().toJson(exception.getServiceError()))
-          .type(MediaType.APPLICATION_JSON)
-          .build();
+    return Response.serverError()
+        .entity(DtoFactory.getInstance().toJson(exception.getServiceError()))
+        .type(MediaType.APPLICATION_JSON)
+        .build();
   }
 }
