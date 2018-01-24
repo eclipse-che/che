b2c58894a28f29b073693f307c1ed23e5bcf641a
Anatoliy Bazko <abazko@redhat.com>
2018-01-24 10:35:12 +0200

    CHE-6733: Implement User Preferences for jdt.ls (#8346)

--- plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/settings/compiler/ErrorsWarningsPreferenceManager.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/settings/compiler/ErrorsWarningsPreferenceManager.java
@@ -12,35 +12,44 @@
 package org.eclipse.che.ide.ext.java.client.settings.compiler;
 
 import com.google.inject.Inject;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.inject.Singleton;
 import org.eclipse.che.api.promises.client.Function;
-import org.eclipse.che.api.promises.client.FunctionException;
-import org.eclipse.che.api.promises.client.Operation;
-import org.eclipse.che.api.promises.client.OperationException;
 import org.eclipse.che.api.promises.client.Promise;
-import org.eclipse.che.api.promises.client.js.Promises;
+import org.eclipse.che.api.promises.client.PromiseProvider;
 import org.eclipse.che.commons.annotation.Nullable;
 import org.eclipse.che.ide.api.preferences.PreferencesManager;
-import org.eclipse.che.ide.ext.java.client.settings.service.SettingsServiceClient;
+import org.eclipse.che.ide.dto.DtoFactory;
+import org.eclipse.che.ide.ext.java.client.service.JavaLanguageExtensionServiceClient;
+import org.eclipse.che.jdt.ls.extension.api.dto.JavaCoreOptions;
 
 /**
  * The implementation of {@link PreferencesManager} for managing Java compiler properties
  *
  * @author Alexander Andrienko
+ * @author Anatolii Bazko
  */
 @Singleton
 public class ErrorsWarningsPreferenceManager implements PreferencesManager {
 
-  private final SettingsServiceClient service;
+  private final JavaLanguageExtensionServiceClient service;
+  private final PromiseProvider promiseProvider;
   private final Map<String, String> changedPreferences;
+  private final DtoFactory dtoFactory;
 
   private Map<String, String> persistedPreferences;
 
   @Inject
-  protected ErrorsWarningsPreferenceManager(SettingsServiceClient service) {
+  protected ErrorsWarningsPreferenceManager(
+      JavaLanguageExtensionServiceClient service,
+      PromiseProvider promiseProvider,
+      DtoFactory dtoFactory) {
     this.service = service;
+    this.promiseProvider = promiseProvider;
+    this.dtoFactory = dtoFactory;
 
     this.persistedPreferences = new HashMap<>();
     this.changedPreferences = new HashMap<>();
@@ -63,33 +72,37 @@ public class ErrorsWarningsPreferenceManager implements PreferencesManager {
   @Override
   public Promise<Void> flushPreferences() {
     if (changedPreferences.isEmpty()) {
-      return Promises.resolve(null);
+      return promiseProvider.resolve(null);
     }
 
+    JavaCoreOptions javaCoreOptions = dtoFactory.createDto(JavaCoreOptions.class);
+    javaCoreOptions.setOptions(changedPreferences);
+
     return service
-        .applyCompileParameters(changedPreferences)
+        .updateJavaCoreOptions(javaCoreOptions)
         .then(
-            new Operation<Void>() {
-              @Override
-              public void apply(Void aVoid) throws OperationException {
-                persistedPreferences.putAll(changedPreferences);
-                changedPreferences.clear();
-              }
-            });
+            (Function<Boolean, Void>)
+                result -> {
+                  persistedPreferences.putAll(changedPreferences);
+                  changedPreferences.clear();
+                  return null;
+                });
   }
 
   @Override
   public Promise<Map<String, String>> loadPreferences() {
+    List<String> options = new ArrayList<>();
+    for (ErrorWarningsOptions option : ErrorWarningsOptions.values()) {
+      options.add(option.toString());
+    }
+
     return service
-        .getCompileParameters()
+        .getJavaCoreOptions(options)
         .then(
-            new Function<Map<String, String>, Map<String, String>>() {
-              @Override
-              public Map<String, String> apply(Map<String, String> properties)
-                  throws FunctionException {
-                persistedPreferences.putAll(properties);
-                return properties;
-              }
-            });
+            (Function<JavaCoreOptions, Map<String, String>>)
+                javaCoreOptions -> {
+                  persistedPreferences.putAll(javaCoreOptions.getOptions());
+                  return javaCoreOptions.getOptions();
+                });
   }
 }
