e71afd719a465d3a8ff7cdd3a94ea94213ba448b
Vitaliy Guliy <vguliy@codenvy.com>
2018-01-25 12:00:15 +0200

    Fix style

--- ide/che-core-ide-ui/src/main/java/org/eclipse/che/ide/ui/smartTree/Tree.java
+++ ide/che-core-ide-ui/src/main/java/org/eclipse/che/ide/ui/smartTree/Tree.java
@@ -1000,7 +1000,7 @@ public class Tree extends FocusWidget
 
   /** Enable searching @see {@link SpeedSearch#SpeedSearch(Tree, String, NodeConverter, boolean)} */
   public void enableSpeedSearch(boolean filterElements) {
-    speedSearch = new SpeedSearch(this, treeStyles.styles().searchMatch(), null, filterElements);
+    speedSearch = new SpeedSearch(this, treeStyles.treeStylesCss().searchMatch(), null, filterElements);
   }
 
   /** Close searching pop-up enabled by {@link #enableSpeedSearch(boolean)}. */
