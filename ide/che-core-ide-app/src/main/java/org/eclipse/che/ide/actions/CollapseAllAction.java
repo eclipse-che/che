34d894998c807a58cbca93f9e81cb9e6e102c5af
Vitaliy Guliy <vguliy@codenvy.com>
2018-01-29 17:47:59 +0200

    Improved context menus (#8477)
    * Improved editor and project explorer context menus
    
    * Improved editor and project explorer context menus
    
    * Improved editor and project explorer context menus

--- ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/actions/CollapseAllAction.java
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/actions/CollapseAllAction.java
@@ -12,9 +12,13 @@ package org.eclipse.che.ide.actions;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.google.web.bindery.event.shared.EventBus;
 import org.eclipse.che.ide.CoreLocalizationConstant;
 import org.eclipse.che.ide.api.action.ActionEvent;
 import org.eclipse.che.ide.api.action.BaseAction;
+import org.eclipse.che.ide.api.parts.ActivePartChangedEvent;
+import org.eclipse.che.ide.api.parts.ActivePartChangedHandler;
+import org.eclipse.che.ide.api.parts.PartPresenter;
 import org.eclipse.che.ide.part.explorer.project.ProjectExplorerPresenter;
 
 /**
@@ -23,20 +27,36 @@ import org.eclipse.che.ide.part.explorer.project.ProjectExplorerPresenter;
  * @author Vlad Zhukovskiy
  */
 @Singleton
-public class CollapseAllAction extends BaseAction {
+public class CollapseAllAction extends BaseAction implements ActivePartChangedHandler {
+
   private ProjectExplorerPresenter projectExplorer;
+  private PartPresenter activePart;
 
   @Inject
   public CollapseAllAction(
-      ProjectExplorerPresenter projectExplorer, CoreLocalizationConstant localizationConstant) {
+      ProjectExplorerPresenter projectExplorer,
+      CoreLocalizationConstant localizationConstant,
+      EventBus eventBus) {
     super(
         localizationConstant.collapseAllActionTitle(),
         localizationConstant.collapseAllActionDescription());
     this.projectExplorer = projectExplorer;
+
+    eventBus.addHandler(ActivePartChangedEvent.TYPE, this);
+  }
+
+  @Override
+  public void onActivePartChanged(ActivePartChangedEvent event) {
+    activePart = event.getActivePart();
   }
 
   @Override
   public void update(ActionEvent e) {
+    if (!(activePart instanceof ProjectExplorerPresenter)) {
+      e.getPresentation().setEnabledAndVisible(false);
+      return;
+    }
+
     e.getPresentation().setEnabledAndVisible(true);
   }
 
