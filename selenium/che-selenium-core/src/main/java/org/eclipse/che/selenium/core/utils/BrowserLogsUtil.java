fa60e127b5fde710d967368990bfbe67540e903b
Ihor Okhrimenko <iokhrime@redhat.com>
2018-01-30 17:55:31 +0200

    add ability to choose the log type

--- selenium/che-selenium-core/src/main/java/org/eclipse/che/selenium/core/utils/BrowserLogsUtil.java
+++ selenium/che-selenium-core/src/main/java/org/eclipse/che/selenium/core/utils/BrowserLogsUtil.java
@@ -11,13 +11,21 @@
 package org.eclipse.che.selenium.core.utils;
 
 import static java.lang.String.format;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+import static org.openqa.selenium.logging.LogType.CLIENT;
+import static org.openqa.selenium.logging.LogType.DRIVER;
+import static org.openqa.selenium.logging.LogType.PERFORMANCE;
+import static org.openqa.selenium.logging.LogType.PROFILER;
+import static org.openqa.selenium.logging.LogType.SERVER;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.util.Collections;
 import java.util.List;
 import org.eclipse.che.selenium.core.SeleniumWebDriver;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.Logs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,12 +40,64 @@ public class BrowserLogsUtil {
   }
 
   /**
+   * read logs of provided type
+   *
+   * @param logType
+   * @see LogType
+   * @return provided type of browser logs
+   */
+  public List<LogEntry> getLogs(String logType) {
+    Logs logs = seleniumWebDriver.manage().logs();
+    List<LogEntry> result = Collections.emptyList();
+
+    switch (logType) {
+      case BROWSER:
+        {
+          result = logs.get(BROWSER).getAll();
+          break;
+        }
+
+      case CLIENT:
+        {
+          result = logs.get(CLIENT).getAll();
+          break;
+        }
+
+      case DRIVER:
+        {
+          result = logs.get(DRIVER).getAll();
+          break;
+        }
+
+      case PERFORMANCE:
+        {
+          result = logs.get(PERFORMANCE).getAll();
+          break;
+        }
+
+      case PROFILER:
+        {
+          result = logs.get(PROFILER).getAll();
+          break;
+        }
+
+      case SERVER:
+        {
+          result = logs.get(SERVER).getAll();
+          break;
+        }
+    }
+
+    return result;
+  }
+
+  /**
    * read logs from browser console
    *
    * @return log messages from browser console
    */
   public List<LogEntry> getBrowserLogs() {
-    return seleniumWebDriver.manage().logs().get(LogType.BROWSER).getAll();
+    return getLogs(BROWSER);
   }
 
   /**
