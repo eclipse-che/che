ce05ba8beb7838fb064906fd27234287664c120a
Hanno Kolvenbach <kolvenbach@silexica.com>
2018-02-06 09:16:46 +0100

    updated language server script
    Signed-off-by: Hanno Kolvenbach <kolvenbach@silexica.com>

--- agents/ls-clang/src/main/resources/installers/1.0.0/org.eclipse.che.ls.clang.script.sh
+++ agents/ls-clang/src/main/resources/installers/1.0.0/org.eclipse.che.ls.clang.script.sh
@@ -9,14 +9,24 @@
 #   Red Hat, Inc. - initial API and implementation
 #
 
+is_current_user_root() {
+    test "$(id -u)" = 0
+}
+
+is_current_user_sudoer() {
+    sudo -n true > /dev/null 2>&1
+}
+
+set_sudo_command() {
+    if is_current_user_sudoer && ! is_current_user_root; then SUDO="sudo -E"; else unset SUDO; fi
+}
+
+set_sudo_command
 unset PACKAGES
-unset SUDO
 command -v tar >/dev/null 2>&1 || { PACKAGES=${PACKAGES}" tar"; }
 command -v curl >/dev/null 2>&1 || { PACKAGES=${PACKAGES}" curl"; }
 command -v wget >/dev/null 2>&1 || { PACKAGES=${PACKAGES}" wget"; }
-test "$(id -u)" = 0 || SUDO="sudo -E"
 
-AGENT_BINARIES_URI=https://codenvy.com/update/repository/public/download/org.eclipse.che.ls.json.binaries
 CHE_DIR=$HOME/che
 LS_DIR=${CHE_DIR}/ls-clangd
 LS_LAUNCHER=${LS_DIR}/launch.sh
