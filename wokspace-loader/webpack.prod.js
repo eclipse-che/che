beab08d00540aca8343b89c04e6c757ee608a138
Yevhen Vydolob <yvydolob@redhat.com>
2018-02-07 15:17:04 +0200

    #8556 add copyright headers
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- wokspace-loader/webpack.prod.js
+++ wokspace-loader/webpack.prod.js
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
