aabcb1253adb71851eb9b077a25588ef2bd20c9e
Yevhen Vydolob <yvydolob@redhat.com>
2018-02-08 15:58:31 +0200

    #8556 create war packaging, add loader war to the main assembly, use apsolute path for resources in loader index.html file, extract css resources into separate file
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- wokspace-loader/src/rest.ts
+++ wokspace-loader/src/rest.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2018 Red Hat, Inc.
+ * Copyright (c) 2018-2018 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,6 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-
 export function sendGet<T>(url: string): Promise<T> {
     return new Promise((resolve, reject) => {
         let request = new XMLHttpRequest();
