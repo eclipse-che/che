cf7ed194cd0035f1ec47f0685bf85fd2527ec937
Max Shaposhnik <mshaposh@redhat.com>
2018-02-09 12:30:08 +0200

    Fix class

--- infrastructures/docker/infrastructure/src/main/java/org/eclipse/che/workspace/infrastructure/docker/ParallelDockerImagesResolver.java
+++ infrastructures/docker/infrastructure/src/main/java/org/eclipse/che/workspace/infrastructure/docker/ParallelDockerImagesResolver.java
@@ -55,7 +55,9 @@ import org.eclipse.che.workspace.infrastructure.docker.logs.MachineLoggersFactor
 import org.eclipse.che.workspace.infrastructure.docker.model.DockerContainerConfig;
 import org.slf4j.Logger;
 
-/** @author Max Shaposhnik (mshaposh@redhat.com) */
+/**
+ * @author Max Shaposhnik (mshaposh@redhat.com)
+ */
 public class ParallelDockerImagesResolver {
 
   private static final Logger LOG = getLogger(ParallelDockerImagesResolver.class);
@@ -90,10 +92,9 @@ public class ParallelDockerImagesResolver {
             .build();
     ExecutorService executor = Executors.newFixedThreadPool(6, factory); // why 6 ?
     Map<String, String> imagesMap = new ConcurrentHashMap<>(containers.size());
-    Resolver r = new Resolver(dockerCredentials, dockerConnector);
 
     CompletableFuture<Void> firstFailed = new CompletableFuture<>();
-    List<CompletableFuture<String>> futures =
+    List<CompletableFuture<String>> taskFutures =
         containers
             .entrySet()
             .stream()
@@ -104,12 +105,12 @@ public class ParallelDockerImagesResolver {
                           String imageName = null;
                           try {
                             imageName =
-                                r.prepareImage(
+                                prepareImage(
                                     e.getKey(),
                                     e.getValue(),
                                     machineLoggersFactory.newProgressMonitor(e.getKey(), identity));
-                          } catch (InternalInfrastructureException | SourceNotFoundException e1) {
-                            firstFailed.completeExceptionally(e1);
+                          } catch (InternalInfrastructureException | SourceNotFoundException ex) {
+                            firstFailed.completeExceptionally(ex);
                           }
                           return imagesMap.put(e.getKey(), imageName);
                         },
@@ -117,7 +118,7 @@ public class ParallelDockerImagesResolver {
             .collect(toList());
 
     CompletableFuture all =
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
+        CompletableFuture.allOf(taskFutures.toArray(new CompletableFuture[taskFutures.size()]));
     try {
       CompletableFuture.anyOf(all, firstFailed).get();
     } catch (ExecutionException e) {
@@ -132,160 +133,147 @@ public class ParallelDockerImagesResolver {
     return imagesMap;
   }
 
-  class Resolver {
+  private String prepareImage(
+      String machineName, DockerContainerConfig container, ProgressMonitor progressMonitor)
+      throws SourceNotFoundException, InternalInfrastructureException {
 
-    private final UserSpecificDockerRegistryCredentialsProvider dockerCredentials;
-    private final DockerConnector dockerConnector;
+    String imageName = "eclipse-che/" + container.getContainerName();
+    if ((container.getBuild() == null
+        || (container.getBuild().getContext() == null
+        && container.getBuild().getDockerfileContent() == null))
+        && container.getImage() == null) {
 
-    public Resolver(
-        UserSpecificDockerRegistryCredentialsProvider dockerCredentials,
-        DockerConnector dockerConnector) {
-      this.dockerCredentials = dockerCredentials;
-      this.dockerConnector = dockerConnector;
+      throw new InternalInfrastructureException(
+          format("Che container '%s' doesn't have neither build nor image fields", machineName));
     }
 
-    private String prepareImage(
-        String machineName, DockerContainerConfig container, ProgressMonitor progressMonitor)
-        throws SourceNotFoundException, InternalInfrastructureException {
+    if (container.getBuild() != null
+        && (container.getBuild().getContext() != null
+        || container.getBuild().getDockerfileContent() != null)) {
+      buildImage(container, imageName, doForcePullImage, progressMonitor);
+    } else {
+      pullImage(container, imageName, progressMonitor);
+    }
 
-      String imageName = "eclipse-che/" + container.getContainerName();
-      if ((container.getBuild() == null
-              || (container.getBuild().getContext() == null
-                  && container.getBuild().getDockerfileContent() == null))
-          && container.getImage() == null) {
+    return imageName;
+  }
 
-        throw new InternalInfrastructureException(
-            format("Che container '%s' doesn't have neither build nor image fields", machineName));
-      }
+  /**
+   * Builds Docker image for container creation.
+   *
+   * @param containerConfig configuration of container
+   * @param machineImageName name of image that should be applied to built image
+   * @param doForcePullOnBuild whether re-pulling of base image should be performed when it exists
+   * locally
+   * @param progressMonitor consumer of build logs
+   * @throws InternalInfrastructureException when any error occurs
+   */
+  protected void buildImage(
+      DockerContainerConfig containerConfig,
+      String machineImageName,
+      boolean doForcePullOnBuild,
+      ProgressMonitor progressMonitor)
+      throws InternalInfrastructureException {
 
-      if (container.getBuild() != null
-          && (container.getBuild().getContext() != null
-              || container.getBuild().getDockerfileContent() != null)) {
-        buildImage(container, imageName, doForcePullImage, progressMonitor);
+    File workDir = null;
+    try {
+      BuildImageParams buildImageParams;
+      if (containerConfig.getBuild() != null
+          && containerConfig.getBuild().getDockerfileContent() != null) {
+
+        workDir = Files.createTempDirectory(null).toFile();
+        final File dockerfileFile = new File(workDir, "Dockerfile");
+        try (FileWriter output = new FileWriter(dockerfileFile)) {
+          output.append(containerConfig.getBuild().getDockerfileContent());
+        }
+
+        buildImageParams = BuildImageParams.create(dockerfileFile);
       } else {
-        pullImage(container, imageName, progressMonitor);
+        buildImageParams =
+            BuildImageParams.create(containerConfig.getBuild().getContext())
+                .withDockerfile(containerConfig.getBuild().getDockerfilePath());
       }
+      buildImageParams
+          .withForceRemoveIntermediateContainers(true)
+          .withRepository(machineImageName)
+          .withAuthConfigs(dockerCredentials.getCredentials())
+          .withDoForcePull(doForcePullOnBuild)
+          .withMemoryLimit(containerConfig.getMemLimit())
+          .withMemorySwapLimit(-1)
+          .withBuildArgs(containerConfig.getBuild().getArgs());
 
-      return imageName;
-    }
-
-    /**
-     * Builds Docker image for container creation.
-     *
-     * @param containerConfig configuration of container
-     * @param machineImageName name of image that should be applied to built image
-     * @param doForcePullOnBuild whether re-pulling of base image should be performed when it exists
-     *     locally
-     * @param progressMonitor consumer of build logs
-     * @throws InternalInfrastructureException when any error occurs
-     */
-    protected void buildImage(
-        DockerContainerConfig containerConfig,
-        String machineImageName,
-        boolean doForcePullOnBuild,
-        ProgressMonitor progressMonitor)
-        throws InternalInfrastructureException {
-
-      File workDir = null;
-      try {
-        BuildImageParams buildImageParams;
-        if (containerConfig.getBuild() != null
-            && containerConfig.getBuild().getDockerfileContent() != null) {
-
-          workDir = Files.createTempDirectory(null).toFile();
-          final File dockerfileFile = new File(workDir, "Dockerfile");
-          try (FileWriter output = new FileWriter(dockerfileFile)) {
-            output.append(containerConfig.getBuild().getDockerfileContent());
-          }
-
-          buildImageParams = BuildImageParams.create(dockerfileFile);
-        } else {
-          buildImageParams =
-              BuildImageParams.create(containerConfig.getBuild().getContext())
-                  .withDockerfile(containerConfig.getBuild().getDockerfilePath());
-        }
-        buildImageParams
-            .withForceRemoveIntermediateContainers(true)
-            .withRepository(machineImageName)
-            .withAuthConfigs(dockerCredentials.getCredentials())
-            .withDoForcePull(doForcePullOnBuild)
-            .withMemoryLimit(containerConfig.getMemLimit())
-            .withMemorySwapLimit(-1)
-            .withBuildArgs(containerConfig.getBuild().getArgs());
-
-        dockerConnector.buildImage(buildImageParams, progressMonitor);
-      } catch (IOException e) {
-        throw new InternalInfrastructureException(e.getLocalizedMessage(), e);
-      } finally {
-        if (workDir != null) {
-          FileCleaner.addFile(workDir);
-        }
+      dockerConnector.buildImage(buildImageParams, progressMonitor);
+    } catch (IOException e) {
+      throw new InternalInfrastructureException(e.getLocalizedMessage(), e);
+    } finally {
+      if (workDir != null) {
+        FileCleaner.addFile(workDir);
       }
     }
+  }
 
-    /**
-     * Pulls docker image for container creation.
-     *
-     * @param container container that provides description of image that should be pulled
-     * @param machineImageName name of the image that should be assigned on pull
-     * @param progressMonitor consumer of output
-     * @throws SourceNotFoundException if image for pulling not found in registry
-     * @throws InternalInfrastructureException if any other error occurs
-     */
-    protected void pullImage(
-        DockerContainerConfig container, String machineImageName, ProgressMonitor progressMonitor)
-        throws InternalInfrastructureException, SourceNotFoundException {
-      final DockerImageIdentifier dockerImageIdentifier;
-      try {
-        dockerImageIdentifier = DockerImageIdentifierParser.parse(container.getImage());
-      } catch (DockerFileException e) {
-        throw new InternalInfrastructureException(
-            "Try to build a docker machine source with an invalid location/content. It is not in the expected format",
-            e);
-      }
-      if (dockerImageIdentifier.getRepository() == null) {
-        throw new InternalInfrastructureException(
-            format(
-                "Machine creation failed. Machine source is invalid. No repository is defined. Found '%s'.",
-                dockerImageIdentifier.getRepository()));
-      }
-      try {
-        boolean isImageExistLocally =
-            isDockerImageExistLocally(dockerImageIdentifier.getRepository());
-        if (doForcePullImage || !isImageExistLocally) {
-          PullParams pullParams =
-              PullParams.create(dockerImageIdentifier.getRepository())
-                  .withTag(MoreObjects.firstNonNull(dockerImageIdentifier.getTag(), LATEST_TAG))
-                  .withRegistry(dockerImageIdentifier.getRegistry())
-                  .withAuthConfigs(dockerCredentials.getCredentials());
-          dockerConnector.pull(pullParams, progressMonitor);
-        }
-
-        String fullNameOfPulledImage = container.getImage();
-        try {
-          // tag image with generated name to allow sysadmin recognize it
-          dockerConnector.tag(TagParams.create(fullNameOfPulledImage, machineImageName));
-        } catch (ImageNotFoundException nfEx) {
-          throw new SourceNotFoundException(nfEx.getLocalizedMessage(), nfEx);
-        }
-      } catch (IOException e) {
-        throw new InternalInfrastructureException(
-            "Can't create machine from image. Cause: " + e.getLocalizedMessage(), e);
-      }
+  /**
+   * Pulls docker image for container creation.
+   *
+   * @param container container that provides description of image that should be pulled
+   * @param machineImageName name of the image that should be assigned on pull
+   * @param progressMonitor consumer of output
+   * @throws SourceNotFoundException if image for pulling not found in registry
+   * @throws InternalInfrastructureException if any other error occurs
+   */
+  protected void pullImage(
+      DockerContainerConfig container, String machineImageName, ProgressMonitor progressMonitor)
+      throws InternalInfrastructureException, SourceNotFoundException {
+    final DockerImageIdentifier dockerImageIdentifier;
+    try {
+      dockerImageIdentifier = DockerImageIdentifierParser.parse(container.getImage());
+    } catch (DockerFileException e) {
+      throw new InternalInfrastructureException(
+          "Try to build a docker machine source with an invalid location/content. It is not in the expected format",
+          e);
     }
-
-    @VisibleForTesting
-    boolean isDockerImageExistLocally(String imageName) {
-      try {
-        return !dockerConnector
-            .listImages(
-                ListImagesParams.create()
-                    .withFilters(new Filters().withFilter("reference", imageName)))
-            .isEmpty();
-      } catch (IOException e) {
-        LOG.warn("Failed to check image {} availability. Cause: {}", imageName, e.getMessage(), e);
-        return false; // consider that image doesn't exist locally
+    if (dockerImageIdentifier.getRepository() == null) {
+      throw new InternalInfrastructureException(
+          format(
+              "Machine creation failed. Machine source is invalid. No repository is defined. Found '%s'.",
+              dockerImageIdentifier.getRepository()));
+    }
+    try {
+      boolean isImageExistLocally =
+          isDockerImageExistLocally(dockerImageIdentifier.getRepository());
+      if (doForcePullImage || !isImageExistLocally) {
+        PullParams pullParams =
+            PullParams.create(dockerImageIdentifier.getRepository())
+                .withTag(MoreObjects.firstNonNull(dockerImageIdentifier.getTag(), LATEST_TAG))
+                .withRegistry(dockerImageIdentifier.getRegistry())
+                .withAuthConfigs(dockerCredentials.getCredentials());
+        dockerConnector.pull(pullParams, progressMonitor);
       }
+
+      String fullNameOfPulledImage = container.getImage();
+      try {
+        // tag image with generated name to allow sysadmin recognize it
+        dockerConnector.tag(TagParams.create(fullNameOfPulledImage, machineImageName));
+      } catch (ImageNotFoundException nfEx) {
+        throw new SourceNotFoundException(nfEx.getLocalizedMessage(), nfEx);
+      }
+    } catch (IOException e) {
+      throw new InternalInfrastructureException(
+          "Can't create machine from image. Cause: " + e.getLocalizedMessage(), e);
+    }
+  }
+
+  @VisibleForTesting
+  boolean isDockerImageExistLocally(String imageName) {
+    try {
+      return !dockerConnector
+          .listImages(
+              ListImagesParams.create()
+                  .withFilters(new Filters().withFilter("reference", imageName)))
+          .isEmpty();
+    } catch (IOException e) {
+      LOG.warn("Failed to check image {} availability. Cause: {}", imageName, e.getMessage(), e);
+      return false; // consider that image doesn't exist locally
     }
   }
 }
