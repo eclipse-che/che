f9e398bbdbd34b851c3720033d09ff95cde7f0d6
Igor Vinokur <ivinokur@redhat.com>
2018-02-13 08:46:53 +0000

    CHE-8414: Fix selenium test according to new behaviour of JDT LS (#8743)
    Signed-off-by: Igor Vinokur <ivinokur@redhat.com>

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/opendeclaration/Eclipse0122Test.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/opendeclaration/Eclipse0122Test.java
@@ -19,6 +19,7 @@ import org.eclipse.che.selenium.core.client.TestProjectServiceClient;
 import org.eclipse.che.selenium.core.project.ProjectTemplates;
 import org.eclipse.che.selenium.core.workspace.TestWorkspace;
 import org.eclipse.che.selenium.pageobject.CodenvyEditor;
+import org.eclipse.che.selenium.pageobject.Consoles;
 import org.eclipse.che.selenium.pageobject.Ide;
 import org.eclipse.che.selenium.pageobject.ProjectExplorer;
 import org.openqa.selenium.Keys;
@@ -40,6 +41,7 @@ public class Eclipse0122Test {
   @Inject private ProjectExplorer projectExplorer;
   @Inject private CodenvyEditor editor;
   @Inject private TestProjectServiceClient testProjectServiceClient;
+  @Inject private Consoles consoles;
 
   @BeforeClass
   public void prepare() throws Exception {
@@ -47,6 +49,7 @@ public class Eclipse0122Test {
     testProjectServiceClient.importProject(
         ws.getId(), Paths.get(resource.toURI()), PROJECT_NAME, ProjectTemplates.MAVEN_SPRING);
     ide.open(ws);
+    consoles.waitJDTLSProjectResolveFinishedMessage(PROJECT_NAME);
   }
 
   @Test
