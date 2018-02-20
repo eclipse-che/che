a31edb7cbde0db3f5bd9926f0aa6624e46db4a5e
Valeriy Svydenko <vsvydenko@codenvy.com>
2018-02-20 21:15:13 +0200

    Replace old java formatter to jdt.ls formatter (#8829)
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- plugins/plugin-java/che-plugin-java-ext-lang-server/src/main/java/org/eclipse/che/plugin/java/server/rest/JavaFormatterService.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-server/src/main/java/org/eclipse/che/plugin/java/server/rest/JavaFormatterService.java
@@ -18,82 +18,31 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import java.io.File;
-import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import org.eclipse.che.api.core.ConflictException;
 import org.eclipse.che.api.core.NotFoundException;
 import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.fs.server.FsManager;
-import org.eclipse.che.api.fs.server.PathTransformer;
-import org.eclipse.che.ide.ext.java.shared.dto.Change;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.internal.core.JavaModel;
-import org.eclipse.jdt.internal.core.JavaModelManager;
-import org.eclipse.jdt.internal.corext.format.Formatter;
-import org.eclipse.jface.text.BadLocationException;
 
 /**
- * Java formatter service. Have a functionality to format java code and update formatter
- * configuration for the project or for whole workspace.
+ * Java formatter service. Updates formatter's configuration for the project or for whole workspace.
  */
 @Path("java/formatter/")
 public class JavaFormatterService {
 
-  private static final String CHE_FOLDER = ".che";
-  private static final String CHE_FORMATTER_XML = "che-formatter.xml";
-  private static final JavaModel model = JavaModelManager.getJavaModelManager().getJavaModel();
+  public static final String CHE_FOLDER = ".che";
+  public static final String CHE_FORMATTER_XML = "che-formatter.xml";
 
   private final FsManager fsManager;
-  private final PathTransformer pathTransformer;
-  private Formatter formatter;
 
   @Inject
-  public JavaFormatterService(
-      FsManager fsManager, PathTransformer pathTransformer, Formatter formatter) {
+  public JavaFormatterService(FsManager fsManager) {
     this.fsManager = fsManager;
-    this.pathTransformer = pathTransformer;
-    this.formatter = formatter;
-  }
-
-  @POST
-  @Path("/format")
-  @Consumes(MediaType.TEXT_PLAIN)
-  @Produces({MediaType.APPLICATION_JSON})
-  @ApiOperation(value = "Creates edits that describe how to format the given string")
-  @ApiResponses({
-    @ApiResponse(code = 200, message = "The response contains all changes after formating"),
-    @ApiResponse(code = 500, message = "Internal server error occurred")
-  })
-  public List<Change> getFormatChanges(
-      @ApiParam(value = "Path to the root project") @QueryParam("projectpath") String projectPath,
-      @ApiParam(value = "The given offset to start recording the edits (inclusive)")
-          @QueryParam("offset")
-          int offset,
-      @ApiParam(value = "The given length to stop recording the edits (exclusive)")
-          @QueryParam("length")
-          int length,
-      @ApiParam(value = "The content to format. Java code formatting is supported only")
-          String content)
-      throws BadLocationException, IllegalArgumentException {
-    IJavaProject javaProject = model.getJavaProject(projectPath);
-    String formatterPath = CHE_FOLDER + '/' + CHE_FORMATTER_XML;
-    File file = null;
-    IFile iFile = javaProject.getProject().getFile(formatterPath);
-    if (iFile != null) {
-      file = iFile.getLocation().toFile();
-    }
-    if (file == null || !file.exists()) {
-      file = getFormatterFromRootFolder(formatterPath);
-    }
-    return formatter.getFormatChanges(file, content, offset, length);
   }
 
   @POST
@@ -160,10 +109,5 @@ public class JavaFormatterService {
     } catch (ConflictException | NotFoundException e) {
       throw new ServerException(e);
     }
-  }
-
-  private File getFormatterFromRootFolder(String formatterPath) {
-
-    return fsManager.toIoFile(absolutize(formatterPath));
   }
 }
