9518e9ee30facf7267468ebe3be60a8db6a2ccb1
Eugene Ivantsov <eivantsov@redhat.com>
2018-02-20 14:27:55 +0200

    Make Keycloak util image configurable

--- dockerfiles/init/modules/openshift/files/scripts/multi-user/configure_keycloak.sh
+++ dockerfiles/init/modules/openshift/files/scripts/multi-user/configure_keycloak.sh
@@ -19,11 +19,14 @@ fi
 
 CHE_KEYCLOAK_ADMIN_REQUIRE_UPDATE_PASSWORD=${DEFAULT_CHE_KEYCLOAK_ADMIN_REQUIRE_UPDATE_PASSWORD:-${CHE_KEYCLOAK_ADMIN_REQUIRE_UPDATE_PASSWORD}}
 
+IMAGE_KEYCLOAK_UTIL=${IMAGE_KEYCLOAK_UTIL:-"eclipse/che-keycloak-util:nightly"}
+
 echo "[CHE] Configuring Keycloak realm, client and user..."
 
 cat "${COMMAND_DIR}"/keycloak/config/keycloak-config-pod-deployment.yaml | sed "s/\${CHE_HOST}/${CHE_HOST}/" | \
                                                            sed "s/\${KC_HOST}/${KC_HOST}/" | \
                                                            sed "s/\${HTTP_PROTOCOL}/${HTTP_PROTOCOL}/" | \
+                                                           sed "s#\${IMAGE_KEYCLOAK_UTIL}#${IMAGE_KEYCLOAK_UTIL}#" | \
                                                            sed "s/\${CHE_KEYCLOAK_ADMIN_REQUIRE_UPDATE_PASSWORD}/${CHE_KEYCLOAK_ADMIN_REQUIRE_UPDATE_PASSWORD}/" | \
                                                            oc apply -f -
 
