055ebed623e024761bdba92c9dc3bd2307e8be62
Anatoliy Bazko <abazko@redhat.com>
2018-02-20 08:40:15 +0200

    CHE-8510: Recognize multi-module projects (#8661)

---
+++ plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/ProjectsSynchronizer.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.plugin.java.languageserver;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import org.eclipse.che.api.core.BadRequestException;
+import org.eclipse.che.api.core.ConflictException;
+import org.eclipse.che.api.core.ForbiddenException;
+import org.eclipse.che.api.core.NotFoundException;
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.core.notification.EventService;
+import org.eclipse.che.api.project.server.ProjectManager;
+import org.eclipse.che.api.project.server.impl.RegisteredProject;
+import org.eclipse.che.api.project.server.notification.ProjectUpdatedEvent;
+import org.eclipse.che.api.workspace.server.model.impl.ProjectConfigImpl;
+import org.eclipse.che.api.workspace.server.model.impl.SourceStorageImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Synchronizes che and eclipse projects.
+ *
+ * @author Anatolii Bazko
+ */
+@Singleton
+public class ProjectsSynchronizer {
+  private static final Logger LOG = LoggerFactory.getLogger(ProjectsSynchronizer.class);
+
+  private final JavaLanguageServerExtensionService service;
+  private final EventService eventService;
+  private final ProjectManager projectManager;
+
+  @Inject
+  public ProjectsSynchronizer(
+      JavaLanguageServerExtensionService service,
+      EventService eventService,
+      ProjectManager projectManager) {
+    this.service = service;
+    this.eventService = eventService;
+    this.projectManager = projectManager;
+  }
+
+  /**
+   * Retrieves maven projects from the JDT.LS and creates missed ones.
+   *
+   * @param rootPath the root pass to synchronize projects. It can be either a project path or any
+   *     path inside the project
+   */
+  public void synchronize(String rootPath) {
+    Set<String> mavenProjects = new HashSet<>(service.getMavenProjects(rootPath));
+
+    for (String mavenProjectPath : mavenProjects) {
+      Optional<RegisteredProject> project = projectManager.get(mavenProjectPath);
+      if (!project.isPresent()) {
+        doCreateProject(mavenProjectPath);
+      }
+    }
+  }
+
+  private void doCreateProject(String projectPath) {
+    ProjectConfigImpl projectConfig = new ProjectConfigImpl();
+    projectConfig.setSource(new SourceStorageImpl());
+    projectConfig.setType("maven");
+    projectConfig.setPath(projectPath);
+    projectConfig.setName(projectPath.substring(projectPath.lastIndexOf("/") + 1));
+    try {
+      projectManager.update(projectConfig);
+      eventService.publish(new ProjectUpdatedEvent(projectPath));
+    } catch (ConflictException
+        | ForbiddenException
+        | NotFoundException
+        | BadRequestException
+        | ServerException e) {
+      LOG.error(String.format("Failed to update project '%s'", projectConfig.getPath()));
+    }
+  }
+}
--- plugins/plugin-maven/che-plugin-maven-server/src/main/java/org/eclipse/che/plugin/maven/server/inject/MavenModule.java
+++ plugins/plugin-maven/che-plugin-maven-server/src/main/java/org/eclipse/che/plugin/maven/server/inject/MavenModule.java
@@ -25,12 +25,10 @@ import org.eclipse.che.api.project.server.type.ValueProviderFactory;
 import org.eclipse.che.inject.DynaModule;
 import org.eclipse.che.maven.server.MavenTerminal;
 import org.eclipse.che.plugin.maven.generator.archetype.MavenArchetypeJsonRpcMessenger;
-import org.eclipse.che.plugin.maven.server.PomModificationDetector;
 import org.eclipse.che.plugin.maven.server.core.MavenJsonRpcCommunication;
 import org.eclipse.che.plugin.maven.server.core.MavenProgressNotifier;
 import org.eclipse.che.plugin.maven.server.core.MavenServerNotifier;
 import org.eclipse.che.plugin.maven.server.core.MavenTerminalImpl;
-import org.eclipse.che.plugin.maven.server.core.project.PomChangeListener;
 import org.eclipse.che.plugin.maven.server.projecttype.MavenProjectType;
 import org.eclipse.che.plugin.maven.server.projecttype.MavenTargetExcludeMatcher;
 import org.eclipse.che.plugin.maven.server.projecttype.MavenValueProviderFactory;
@@ -39,7 +37,6 @@ import org.eclipse.che.plugin.maven.server.projecttype.handler.GeneratorStrategy
 import org.eclipse.che.plugin.maven.server.projecttype.handler.MavenProjectGenerator;
 import org.eclipse.che.plugin.maven.server.projecttype.handler.MavenProjectInitHandler;
 import org.eclipse.che.plugin.maven.server.projecttype.handler.SimpleGeneratorStrategy;
-import org.eclipse.che.plugin.maven.server.rest.MavenServerService;
 
 /** @author Artem Zatsarynnyi */
 @DynaModule
@@ -78,12 +75,9 @@ public class MavenModule extends AbstractModule {
     bind(MavenTerminal.class).to(MavenTerminalImpl.class).in(Singleton.class);
     bind(MavenProgressNotifier.class).to(MavenServerNotifier.class).in(Singleton.class);
 
-    bind(MavenServerService.class);
     bind(MavenJsonRpcCommunication.class);
     bind(MavenArchetypeJsonRpcMessenger.class);
 
-    bind(PomChangeListener.class).asEagerSingleton();
-    bind(PomModificationDetector.class).asEagerSingleton();
     //    Multibinder.newSetBinder(binder(), LanguageServerLauncher.class)
     //        .addBinding()
     //        .to(MavenLanguageServerLauncher.class)
