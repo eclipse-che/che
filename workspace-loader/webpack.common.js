0d4565a78a66c36df0662242f93faa0c47db9445
Vitaliy Guliy <vguliy@codenvy.com>
2018-02-20 10:37:07 +0200

    Remove .tsx

--- workspace-loader/webpack.common.js
+++ workspace-loader/webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: ['.ts', '.js']
     },
     output: {
         filename: 'bundle.js',
