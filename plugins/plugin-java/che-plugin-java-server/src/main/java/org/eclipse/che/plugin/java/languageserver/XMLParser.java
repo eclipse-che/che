c2c54f193470c73168b056e50d56a8153e52b266
Valeriy Svydenko <vsvydenko@codenvy.com>
2018-02-20 21:15:13 +0200

    Replace old java formatter to jdt.ls formatter (#8829)
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/XMLParser.java
+++ plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/XMLParser.java
@@ -1,13 +1,14 @@
-/**
- * ***************************************************************************** Copyright (c)
- * 2012-2015 Red Hat, Inc. All rights reserved. This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * <p>Contributors: Red Hat, Inc. - initial API and implementation
- * *****************************************************************************
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
  */
-package org.eclipse.jdt.internal.corext.format;
+package org.eclipse.che.plugin.java.languageserver;
 
 import java.util.HashMap;
 import java.util.Map;
