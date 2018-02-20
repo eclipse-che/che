e94c85568ee9965a7a2f0590f541a4b26ed1701c
Valeriy Svydenko <vsvydenko@codenvy.com>
2018-02-20 21:15:13 +0200

    Replace old java formatter to jdt.ls formatter (#8829)
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaTextDocumentServiceWraper.java
+++ plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaTextDocumentServiceWraper.java
@@ -10,13 +10,39 @@
  */
 package org.eclipse.che.plugin.java.languageserver;
 
+import static org.eclipse.che.api.fs.server.WsPathUtils.SEPARATOR;
+import static org.eclipse.che.api.languageserver.service.LanguageServiceUtils.PROJECTS;
+import static org.eclipse.che.api.languageserver.service.LanguageServiceUtils.extractProjectPath;
+import static org.eclipse.che.plugin.java.server.rest.JavaFormatterService.CHE_FOLDER;
+import static org.eclipse.che.plugin.java.server.rest.JavaFormatterService.CHE_FORMATTER_XML;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.eclipse.che.api.fs.server.WsPathUtils;
+import org.eclipse.che.api.languageserver.exception.LanguageServerException;
 import org.eclipse.lsp4j.CodeActionParams;
 import org.eclipse.lsp4j.Command;
+import org.eclipse.lsp4j.DocumentFormattingParams;
+import org.eclipse.lsp4j.FormattingOptions;
+import org.eclipse.lsp4j.TextEdit;
 import org.eclipse.lsp4j.services.TextDocumentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
 public class JavaTextDocumentServiceWraper {
+  private static final Logger LOG = LoggerFactory.getLogger(JavaTextDocumentServiceWraper.class);
+
   private TextDocumentService wrapped;
 
   public JavaTextDocumentServiceWraper(TextDocumentService wrapped) {
@@ -35,5 +61,52 @@ public class JavaTextDocumentServiceWraper {
               });
           return commands;
         });
+  }
+
+  public CompletableFuture<List<? extends TextEdit>> formatting(DocumentFormattingParams params)
+      throws LanguageServerException {
+
+    String fileUri = params.getTextDocument().getUri();
+    FormattingOptions options = params.getOptions();
+    if (options == null) {
+      options = new FormattingOptions();
+      params.setOptions(options);
+    }
+
+    updateFormatterOptions(params, fileUri);
+
+    return wrapped.formatting(params);
+  }
+
+  private void updateFormatterOptions(DocumentFormattingParams params, String fileUri)
+      throws LanguageServerException {
+    String projectPath = WsPathUtils.absolutize(extractProjectPath(fileUri));
+    String formatterPathSuffix = CHE_FOLDER + SEPARATOR + CHE_FORMATTER_XML;
+    Path projectFormatterPath = Paths.get(projectPath, formatterPathSuffix);
+    Path wsFormatterPath = Paths.get(PROJECTS, formatterPathSuffix);
+    if (Files.exists(projectFormatterPath)) {
+      updateFormatterOptions(params, getFormatSettingsFromFile(projectFormatterPath.toFile()));
+    } else if (Files.exists(wsFormatterPath)) {
+      updateFormatterOptions(params, getFormatSettingsFromFile(wsFormatterPath.toFile()));
+    }
+  }
+
+  private void updateFormatterOptions(
+      DocumentFormattingParams params, Map<String, String> options) {
+    for (String key : options.keySet()) {
+      params.getOptions().putString(key, options.get(key));
+    }
+  }
+
+  private static Map<String, String> getFormatSettingsFromFile(File file) {
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+    XMLParser parserXML = new XMLParser();
+    try (FileInputStream fis = new FileInputStream(file)) {
+      SAXParser parser = factory.newSAXParser();
+      parser.parse(fis, parserXML);
+    } catch (ParserConfigurationException | SAXException | IOException e) {
+      LOG.error("It is not possible to parse file " + file.getName(), e);
+    }
+    return parserXML.getSettings();
   }
 }
