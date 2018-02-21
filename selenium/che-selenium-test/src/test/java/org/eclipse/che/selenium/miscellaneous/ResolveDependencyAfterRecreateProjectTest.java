1ca039f641b837867d35844a6512cdea9418ce05
SkorikSergey <sskorik@codenvy.com>
2018-02-21 11:52:29 +0200

    changed variable

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/miscellaneous/ResolveDependencyAfterRecreateProjectTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/miscellaneous/ResolveDependencyAfterRecreateProjectTest.java
@@ -40,7 +40,7 @@ import org.testng.annotations.Test;
 public class ResolveDependencyAfterRecreateProjectTest {
   private static final String PROJECT_NAME1 = generate("project1", 4);
   private static final String PROJECT_NAME2 = generate("project2", 4);
-  private static final String PATH_FOR_EXPAND = "/src/main/java/org.eclipse.che.examples";
+  private static final String PATH_TO_EXPAND = "/src/main/java/org.eclipse.che.examples";
   private static final String PATH_TO_FILE =
       "/src/main/java/org/eclipse/che/examples/GreetingController.java";
 
@@ -67,7 +67,7 @@ public class ResolveDependencyAfterRecreateProjectTest {
     projectExplorer.waitItem(PROJECT_NAME1);
     mavenPluginStatusBar.waitClosingInfoPanel();
     notificationsPopupPanel.waitProgressPopupPanelClose();
-    projectExplorer.expandPathInProjectExplorer(PROJECT_NAME1 + PATH_FOR_EXPAND);
+    projectExplorer.expandPathInProjectExplorer(PROJECT_NAME1 + PATH_TO_EXPAND);
     projectExplorer.openItemByPath(PROJECT_NAME1 + PATH_TO_FILE);
     editor.waitActive();
     editor.waitAllMarkersDisappear(ERROR_MARKER);
@@ -77,7 +77,7 @@ public class ResolveDependencyAfterRecreateProjectTest {
 
     projectExplorer.waitItem(PROJECT_NAME2);
     projectExplorer.selectVisibleItem(PROJECT_NAME2);
-    projectExplorer.expandPathInProjectExplorer(PROJECT_NAME2 + PATH_FOR_EXPAND);
+    projectExplorer.expandPathInProjectExplorer(PROJECT_NAME2 + PATH_TO_EXPAND);
 
     try {
       projectExplorer.openItemByPath(PROJECT_NAME2 + PATH_TO_FILE);
