8d28c0c5106d1e33959ccc6ed0d037b2c0bd988a
Hanno Kolvenbach <hannokolvenbach@gmx.de>
2018-03-14 18:17:16 +0100

    fixed test

--- infrastructures/docker/infrastructure/src/test/java/org/eclipse/che/workspace/infrastructure/docker/local/LocalCheDockerEnvironmentProvisionerTest.java
+++ infrastructures/docker/infrastructure/src/test/java/org/eclipse/che/workspace/infrastructure/docker/local/LocalCheDockerEnvironmentProvisionerTest.java
@@ -60,7 +60,6 @@ public class LocalCheDockerEnvironmentProvisionerTest {
     provisioner =
         new LocalCheDockerEnvironmentProvisioner(
             true,
-            "http",
             settingsProvisioners,
             projectsVolumeProvisioner,
             installerConfigProvisioner,
