22262e8f2fc73c7c6d269669a044efe257f65c7a
Oleksandr Andriienko <oandriie@redhat.com>
2018-03-15 12:23:27 +0200

    Cache node modules before rebuild Theia with selected plugins.  (#9096)
    * Cash node modules before rebuild Theia with selected plugins. Should use to speed build.
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    * Fix up.
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    * Remove debug logs.
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    * Fix up variable name.
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    * Fix up.
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- dockerfiles/theia/src/main.js
+++ dockerfiles/theia/src/main.js
@@ -14,7 +14,8 @@ const cp = require("child_process");
 const theiaRoot = '/home/theia';
 const theiaPath = theiaRoot + '/package.json';
 
-const defaultConfig = require('/home/default/theia/package.json');
+const defaultTheiaPath = `/home/default/theia`;
+const defaultConfig = require(`${defaultTheiaPath}/package.json`);
 
 process.chdir(theiaRoot);
 
@@ -46,8 +47,7 @@ else {
         fs.writeFileSync(theiaPath, JSON.stringify(theiaConfig));
         handlePromise(callYarn().then(callBuild).then(callRun));
     } else {
-        const defaultTheia = `/home/default/theia`;
-        cp.execSync(`cp -r ${defaultTheia}/\* ${theiaRoot}`);
+        cp.execSync(`rsync -rv ${defaultTheiaPath}/ ${theiaRoot} --exclude 'node_modules' --exclude 'yarn.lock'`);
         handlePromise(callRun());
     }
 
