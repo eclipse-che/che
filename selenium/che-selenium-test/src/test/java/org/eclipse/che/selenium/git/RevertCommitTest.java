0fa8cfb4c2c497a5bcf3fda53eb7a8a5a6e1dd9a
Aleksandr Shmaraiev <oshmarai@redhat.com>
2018-03-16 16:45:27 +0200

    Improve and rework the 'RevertCommitTest' to move in the multi thread test suite (#9030)
    * Replace import the test project from github on the import by API from resources
    * Add extra steps to the 'RevertCommitTest' to check changes in the project tree and the editor
    * Move the 'RevertCommitTest' from the 'CheOneThreadTestsSuite.xml' to the 'CheSuite.xml'
    * Add the message related to known issue
    * Correct the methods in the 'GitRevertCommit' using the 'SeleniumWebDriverHelper'

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/git/RevertCommitTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/git/RevertCommitTest.java
@@ -10,32 +10,43 @@
  */
 package org.eclipse.che.selenium.git;
 
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Git.COMMIT;
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Git.GIT;
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Git.INITIALIZE_REPOSITORY;
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Git.REVERT_COMMIT;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import java.net.URL;
+import java.nio.file.Paths;
 import org.eclipse.che.commons.lang.NameGenerator;
 import org.eclipse.che.selenium.core.TestGroup;
-import org.eclipse.che.selenium.core.client.TestGitHubKeyUploader;
+import org.eclipse.che.selenium.core.client.TestProjectServiceClient;
 import org.eclipse.che.selenium.core.client.TestUserPreferencesServiceClient;
+import org.eclipse.che.selenium.core.constant.TestGitConstants;
+import org.eclipse.che.selenium.core.project.ProjectTemplates;
 import org.eclipse.che.selenium.core.user.TestUser;
 import org.eclipse.che.selenium.core.workspace.TestWorkspace;
+import org.eclipse.che.selenium.pageobject.AskDialog;
+import org.eclipse.che.selenium.pageobject.CodenvyEditor;
 import org.eclipse.che.selenium.pageobject.Ide;
 import org.eclipse.che.selenium.pageobject.Menu;
 import org.eclipse.che.selenium.pageobject.ProjectExplorer;
-import org.eclipse.che.selenium.pageobject.Wizard;
+import org.eclipse.che.selenium.pageobject.git.Git;
 import org.eclipse.che.selenium.pageobject.git.GitRevertCommit;
 import org.eclipse.che.selenium.pageobject.git.GitStatusBar;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-/** @author Anatolii Bazko */
+/**
+ * @author Anatolii Bazko
+ * @author Aleksandr Shmaraiev
+ */
 @Test(groups = TestGroup.GITHUB)
 public class RevertCommitTest {
-  private static final String PROJECT_NAME = NameGenerator.generate("project", 2);
+  private static final String PROJECT_NAME = NameGenerator.generate("GitRevertProject-", 4);
 
   @Inject private TestWorkspace ws;
   @Inject private Ide ide;
@@ -49,45 +60,111 @@ public class RevertCommitTest {
   @Named("github.password")
   private String gitHubPassword;
 
+  @Inject private TestProjectServiceClient testProjectServiceClient;
   @Inject private ProjectExplorer projectExplorer;
+  @Inject private AskDialog askDialog;
   @Inject private Menu menu;
-  @Inject private org.eclipse.che.selenium.pageobject.git.Git git;
-  @Inject private TestGitHubKeyUploader testGitHubKeyUploader;
-  @Inject private TestUserPreferencesServiceClient testUserPreferencesServiceClient;
+  @Inject private Git git;
+  @Inject private CodenvyEditor editor;
   @Inject private GitRevertCommit gitRevertCommit;
+  @Inject private TestUserPreferencesServiceClient testUserPreferencesServiceClient;
   @Inject private GitStatusBar gitStatusBar;
 
   @BeforeClass
   public void prepare() throws Exception {
-    testGitHubKeyUploader.updateGithubKey();
+    URL resource = getClass().getResource("/projects/git-pull-test");
     testUserPreferencesServiceClient.addGitCommitter(gitHubUsername, productUser.getEmail());
+    testProjectServiceClient.importProject(
+        ws.getId(), Paths.get(resource.toURI()), PROJECT_NAME, ProjectTemplates.MAVEN_SPRING);
     ide.open(ws);
-
-    git.importJavaApp(
-        "git@github.com:" + gitHubUsername + "/testRepo-1.git",
-        PROJECT_NAME,
-        Wizard.TypeProject.MAVEN);
-
-    projectExplorer.waitProjectExplorer();
-    projectExplorer.waitItem(PROJECT_NAME);
-    projectExplorer.waitAndSelectItem(PROJECT_NAME);
   }
 
   @Test
-  public void shouldRevertCommit() {
+  public void shouldRevertCommit() throws Exception {
+    // preconditions
+    String newFile = "newFile.xml";
+    String htmlFile = "file.html";
+    String changeContent = "<! change content>";
+    String initRepoMessage = "init";
+    String createFileMessage = "create newFile.xml";
+    String updateFileMessage = "update file.html";
+    String pathToNewFile = String.format("%s/%s", PROJECT_NAME, newFile);
+    String pathToHtmlFile = String.format("%s/%s", PROJECT_NAME, htmlFile);
+
+    // perform git initialize repository
+    projectExplorer.waitProjectExplorer();
+
+    gitInitRepo();
+    commitFiles(initRepoMessage);
+
+    // create new file and perform commit
+    testProjectServiceClient.createFileInProject(ws.getId(), PROJECT_NAME, newFile, changeContent);
+    projectExplorer.quickExpandWithJavaScript();
+    projectExplorer.waitItem(pathToNewFile);
+
+    commitFiles(createFileMessage);
+
+    // perform git revert and check author and comment
+    projectExplorer.waitAndSelectItem(PROJECT_NAME);
+
+    performGitRevert();
+
+    // check that 'newFile.xml' is disappear from the project tree
+    projectExplorer.waitDisappearItemByPath(pathToNewFile);
+
+    // update the 'file.html' and commit change
+    testProjectServiceClient.updateFile(ws.getId(), pathToHtmlFile, changeContent);
+    projectExplorer.waitAndSelectItem(PROJECT_NAME);
+
+    commitFiles(updateFileMessage);
+
+    // perform revert and check that 'change content' is not present in the editor
+    performGitRevert();
+
+    projectExplorer.openItemByPath(pathToHtmlFile);
+    editor.waitActive();
+    editor.waitTextNotPresentIntoEditor(changeContent);
+  }
+
+  private void gitInitRepo() {
+    menu.runCommand(GIT, INITIALIZE_REPOSITORY);
+    askDialog.waitFormToOpen();
+    askDialog.clickOkBtn();
+    askDialog.waitFormToClose();
+    git.waitGitStatusBarWithMess(TestGitConstants.GIT_INITIALIZED_SUCCESS);
+  }
+
+  private void performGitRevert() {
     menu.runCommand(GIT, REVERT_COMMIT);
 
     String revision = gitRevertCommit.getTopCommitRevision();
     String comment = gitRevertCommit.getTopCommitComment();
 
+    gitRevertCommit.waitRevertPanelOpened();
     gitRevertCommit.selectRevision(revision);
     gitRevertCommit.clickRevertButton();
+    gitRevertCommit.waitRevertPanelClosed();
 
     gitStatusBar.waitMessageInGitTab("Reverted commits: - " + revision);
 
     menu.runCommand(GIT, REVERT_COMMIT);
 
-    assertEquals(gitRevertCommit.getTopCommitAuthor(), gitHubUsername);
+    // TODO delete the message after resolve issue
+    assertEquals(
+        gitRevertCommit.getTopCommitAuthor(),
+        gitHubUsername,
+        "Known issue https://github.com/eclipse/che/issues/9066");
+
     assertTrue(gitRevertCommit.getTopCommitComment().contains("Revert \"" + comment + "\""));
+
+    gitRevertCommit.clickCancelButton();
+  }
+
+  private void commitFiles(String commiitMess) {
+    projectExplorer.waitAndSelectItem(PROJECT_NAME);
+    menu.runCommand(GIT, COMMIT);
+
+    git.waitAndRunCommit(commiitMess);
+    git.waitGitStatusBarWithMess(TestGitConstants.COMMIT_MESSAGE_SUCCESS);
   }
 }
