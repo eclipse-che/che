6e6a088dd9efeeb1a22fc43f11ffc0a6b5441131
SkorikSergey <sskorik@codenvy.com>
2018-03-16 12:16:15 +0200

    created ShareWorkspaceTest

---
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/dashboard/organization/ShareWorkspacesTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.selenium.dashboard.organization;
+
+import static org.eclipse.che.commons.lang.NameGenerator.generate;
+import static org.eclipse.che.selenium.core.constant.TestStacksConstants.JAVA;
+
+import com.google.inject.Inject;
+import org.eclipse.che.selenium.core.TestGroup;
+import org.eclipse.che.selenium.core.organization.InjectTestOrganization;
+import org.eclipse.che.selenium.core.organization.TestOrganization;
+import org.eclipse.che.selenium.core.user.AdminTestUser;
+import org.eclipse.che.selenium.core.user.TestUser;
+import org.eclipse.che.selenium.pageobject.dashboard.CheMultiuserAdminDashboard;
+import org.eclipse.che.selenium.pageobject.dashboard.NavigationBar;
+import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace;
+import org.eclipse.che.selenium.pageobject.dashboard.organization.OrganizationListPage;
+import org.eclipse.che.selenium.pageobject.dashboard.organization.OrganizationPage;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails.TabNames;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceShare;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Test(groups = {TestGroup.MULTIUSER})
+public class ShareWorkspacesTest {
+
+  private static final String WORKSPACE_NAME = generate("workspace", 4);
+
+  @InjectTestOrganization(prefix = "org")
+  private TestOrganization org;
+
+  @Inject private OrganizationListPage organizationListPage;
+  @Inject private CheMultiuserAdminDashboard dashboard;
+  @Inject private WorkspaceDetails workspaceDetails;
+  @Inject private OrganizationPage organizationPage;
+  @Inject private NavigationBar navigationBar;
+  @Inject private AdminTestUser adminTestUser;
+  @Inject private NewWorkspace newWorkspace;
+  @Inject private Workspaces workspaces;
+  @Inject private TestUser testUser;
+  @Inject private WorkspaceShare workspaceShare;
+
+  @BeforeClass
+  public void setUp() throws Exception {
+    org.addAdmin(adminTestUser.getId());
+    org.addMember(testUser.getId());
+
+    dashboard.open(adminTestUser.getName(), adminTestUser.getPassword());
+    createWorkspace(org.getName(), WORKSPACE_NAME);
+  }
+
+  @Test
+  public void checkShareWorkspaceTab() {
+    navigationBar.waitNavigationBar();
+
+    dashboard.selectWorkspacesItemOnDashboard();
+    workspaces.selectWorkspaceItemName(WORKSPACE_NAME);
+    workspaceDetails.waitToolbarTitleName(WORKSPACE_NAME);
+    workspaceDetails.selectTabInWorkspaceMenu(TabNames.SHARE);
+  }
+
+  @Test
+  public void shareWorkspaceWithMember() {}
+
+  private void createWorkspace(String organizationName, String workspaceName) {
+    dashboard.selectWorkspacesItemOnDashboard();
+    dashboard.waitToolbarTitleName("Workspaces");
+
+    workspaces.clickOnAddWorkspaceBtn();
+    newWorkspace.waitToolbar();
+    newWorkspace.openOrganizationsList();
+    newWorkspace.selectOrganizationFromList(organizationName);
+    newWorkspace.selectStack(JAVA.getId());
+    newWorkspace.typeWorkspaceName(workspaceName);
+    newWorkspace.clickOnCreateButtonAndEditWorkspace();
+    workspaceDetails.waitToolbarTitleName(workspaceName);
+  }
+}
