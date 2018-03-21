2994836b6637d40d56f48017b273229d5aa1c3e0
Oleksandr Garagatyi <ogaragat@redhat.com>
2018-03-21 15:50:29 +0200

    Simplify filter and map it in Guice module instead.
    Signed-off-by: Oleksandr Garagatyi <ogaragat@redhat.com>

--- assembly/assembly-ide-war/src/main/java/org/eclipse/che/ApiAccessRejectionFilter.java
+++ assembly/assembly-ide-war/src/main/java/org/eclipse/che/ApiAccessRejectionFilter.java
@@ -11,7 +11,6 @@
 package org.eclipse.che;
 
 import java.io.IOException;
-import java.util.regex.Pattern;
 import javax.inject.Singleton;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -19,12 +18,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * Returns internal server error when request is supposed to be handled by API war. Situation occurs
- * when API war failed to start and all requests to it handled by ROOT war.
+ * when API war failed to start and all requests to it are handled by ROOT war.
  *
  * @author Alexander Garagatyi
  */
@@ -33,20 +31,13 @@ public class ApiAccessRejectionFilter implements Filter {
   public static final String ERROR_MESSAGE = "Internal server occurs. API is not accessible";
 
   private static final byte[] ERROR_MESSAGE_IN_BYTES = ERROR_MESSAGE.getBytes();
-  private static final Pattern API_PATTERN = Pattern.compile("^/api(/.*)?$");
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    HttpServletRequest req = (HttpServletRequest) request;
     HttpServletResponse resp = (HttpServletResponse) response;
-
-    if (API_PATTERN.matcher(req.getRequestURI()).matches()) {
-      resp.setStatus(500);
-      resp.getOutputStream().write(ERROR_MESSAGE_IN_BYTES);
-      return;
-    }
-    chain.doFilter(request, response);
+    resp.setStatus(500);
+    resp.getOutputStream().write(ERROR_MESSAGE_IN_BYTES);
   }
 
   @Override
--- assembly/assembly-ide-war/src/test/java/org/eclipse/che/ApiAccessRejectionFilterTest.java
+++ assembly/assembly-ide-war/src/test/java/org/eclipse/che/ApiAccessRejectionFilterTest.java
@@ -10,23 +10,18 @@
  */
 package org.eclipse.che;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.testng.MockitoTestNGListener;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -43,11 +38,9 @@ public class ApiAccessRejectionFilterTest {
 
   @InjectMocks private ApiAccessRejectionFilter filter;
 
-  @Test(dataProvider = "apiPathProvider")
-  public void shouldReturnErrorOnRequestsToApi(String path) throws Exception {
+  @Test
+  public void shouldReturnError() throws Exception {
     // given
-    when(request.getMethod()).thenReturn("GET");
-    when(request.getRequestURI()).thenReturn(path);
     when(response.getOutputStream()).thenReturn(outputStream);
 
     // when
@@ -57,34 +50,5 @@ public class ApiAccessRejectionFilterTest {
     verifyZeroInteractions(chain);
     verify(response).setStatus(500);
     verify(outputStream).write(eq(ApiAccessRejectionFilter.ERROR_MESSAGE.getBytes()));
-  }
-
-  @DataProvider(name = "apiPathProvider")
-  public static Object[][] apiPathProvider() {
-    return new Object[][] {
-      {"/api"}, {"/api/"}, {"/api/workspace"}, {"/api/workspace/"}, {"/api/system/state"},
-    };
-  }
-
-  @Test(dataProvider = "nonApiPathProvider")
-  public void shouldSkipNonApiRequests(String path) throws Exception {
-    // given
-    when(request.getMethod()).thenReturn("GET");
-    when(request.getRequestURI()).thenReturn(path);
-
-    // when
-    filter.doFilter(request, response, chain);
-
-    // then
-    verify(chain).doFilter(any(ServletRequest.class), any(ServletResponse.class));
-    verifyZeroInteractions(response);
-    verifyNoMoreInteractions(chain);
-  }
-
-  @DataProvider(name = "nonApiPathProvider")
-  public Object[][] nonApiPathProvider() {
-    return new Object[][] {
-      {"/ws-id/"}, {"/wsname"}, {"/dashboard"}, {"/dashboard/"}, {"/"}, {""},
-    };
   }
 }
