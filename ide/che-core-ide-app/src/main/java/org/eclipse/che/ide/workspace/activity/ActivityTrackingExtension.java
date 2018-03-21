3da09252eb43d65c77ce5a282dceb36c694e0fb7
Max Shaposhnik <mshaposh@redhat.com>
2018-03-21 17:19:17 +0200

    Mode IDE part of activity

--- ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/workspace/activity/ActivityTrackingExtension.java
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/workspace/activity/ActivityTrackingExtension.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-package org.eclipse.che.workspace.activity.ide;
+package org.eclipse.che.ide.workspace.activity;
 
 import static com.google.gwt.core.client.ScriptInjector.TOP_WINDOW;
 
