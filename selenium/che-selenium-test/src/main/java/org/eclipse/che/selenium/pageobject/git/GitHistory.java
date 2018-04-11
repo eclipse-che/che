67403c05c108de0e48032105d1b2cb49e6bca1fd
Aleksandr Shmaraiev <oshmarai@redhat.com>
2018-04-11 13:17:34 +0300

    Selenium: Rework the 'CheckoutReferenceTest' to run in the multi-thread mode
    * Rework the 'CheckoutReferenceTest' to run in the multi-threaded mode
    * Rework methods in the 'TestGitHubRepository'
    * Rework methods in the 'GitHistory' using the 'SeleniumWebDriverHelper'
    * Move the 'CheckoutReferenceTest' to the 'CheSuite.xml'
    * Use the 'kohsuke Github' library

--- selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/git/GitHistory.java
+++ selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/git/GitHistory.java
@@ -11,31 +11,31 @@
 package org.eclipse.che.selenium.pageobject.git;
 
 import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.ELEMENT_TIMEOUT_SEC;
-import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.LOAD_PAGE_TIMEOUT_SEC;
 import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.REDRAW_UI_ELEMENTS_TIMEOUT_SEC;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.eclipse.che.selenium.core.SeleniumWebDriver;
 import org.eclipse.che.selenium.pageobject.Loader;
+import org.eclipse.che.selenium.pageobject.SeleniumWebDriverHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /** @author Musienko Maxim */
 @Singleton
 public class GitHistory {
-  private final SeleniumWebDriver seleniumWebDriver;
   private final Loader loader;
+  private final SeleniumWebDriverHelper seleniumWebDriverHelper;
 
   @Inject
-  public GitHistory(SeleniumWebDriver seleniumWebDriver, Loader loader) {
-    this.seleniumWebDriver = seleniumWebDriver;
+  public GitHistory(
+      SeleniumWebDriver seleniumWebDriver,
+      Loader loader,
+      SeleniumWebDriverHelper seleniumWebDriverHelper) {
     this.loader = loader;
+    this.seleniumWebDriverHelper = seleniumWebDriverHelper;
     PageFactory.initElements(seleniumWebDriver, this);
   }
 
@@ -60,32 +60,18 @@ public class GitHistory {
 
   /** wait appear history form */
   public void waitHistoryForm() {
-    new WebDriverWait(seleniumWebDriver, ELEMENT_TIMEOUT_SEC)
-        .until(ExpectedConditions.visibilityOf(historyForm));
+    seleniumWebDriverHelper.waitVisibility(historyForm, ELEMENT_TIMEOUT_SEC);
   }
 
   /** wait history form is closed */
   public void waitHistoryFormToClose() {
-    new WebDriverWait(seleniumWebDriver, LOAD_PAGE_TIMEOUT_SEC)
-        .until(ExpectedConditions.invisibilityOfElementLocated(By.id(Locators.HISTORY_FORM_ID)));
+    seleniumWebDriverHelper.waitInvisibility(By.id(Locators.HISTORY_FORM_ID));
   }
 
   /** click on the 'Compare' button in the history form */
   public void clickCompareButton() {
-    new WebDriverWait(seleniumWebDriver, REDRAW_UI_ELEMENTS_TIMEOUT_SEC)
-        .until(ExpectedConditions.elementToBeClickable(By.id(Locators.COMPARE_BUTTON_HISTORY)))
-        .click();
-  }
-
-  /**
-   * get current value from the the editor form
-   *
-   * @return value of message from the editor form
-   */
-  public String getFullDescription() {
-    new WebDriverWait(seleniumWebDriver, LOAD_PAGE_TIMEOUT_SEC)
-        .until(ExpectedConditions.visibilityOf(historyEditor));
-    return historyEditor.getAttribute("value");
+    seleniumWebDriverHelper.waitAndClick(
+        By.id(Locators.COMPARE_BUTTON_HISTORY), REDRAW_UI_ELEMENTS_TIMEOUT_SEC);
   }
 
   /**
@@ -94,25 +80,19 @@ public class GitHistory {
    * @param rowIndex is a specified number
    */
   public void selectHistoryRowByIndex(int rowIndex) {
-    new WebDriverWait(seleniumWebDriver, LOAD_PAGE_TIMEOUT_SEC)
-        .until(
-            ExpectedConditions.visibilityOfElementLocated(
-                By.xpath(String.format(Locators.HISTORY_ROW_NUMBER, Integer.toString(rowIndex)))))
-        .click();
-    loader.waitOnClosed();
+    seleniumWebDriverHelper.waitAndClick(
+        By.xpath(String.format(Locators.HISTORY_ROW_NUMBER, Integer.toString(rowIndex))));
   }
 
   /** wait appear text in history list */
   public void waitTextInHistoryList(final String expectedText) {
-    new WebDriverWait(seleniumWebDriver, REDRAW_UI_ELEMENTS_TIMEOUT_SEC)
-        .until(
-            (ExpectedCondition<Boolean>) webDriver -> historyForm.getText().contains(expectedText));
+    seleniumWebDriverHelper.waitTextPresence(
+        historyForm, expectedText, REDRAW_UI_ELEMENTS_TIMEOUT_SEC);
   }
 
   /** wait appearance history editor */
   public void waitHistoryEditor() {
-    new WebDriverWait(seleniumWebDriver, LOAD_PAGE_TIMEOUT_SEC)
-        .until(ExpectedConditions.visibilityOf(historyEditor));
+    seleniumWebDriverHelper.waitVisibility(historyEditor);
   }
 
   /**
@@ -121,39 +101,40 @@ public class GitHistory {
    * @param expectedContent
    */
   public void waitContentIntoHistoryEditor(final String expectedContent) {
-    new WebDriverWait(seleniumWebDriver, LOAD_PAGE_TIMEOUT_SEC)
-        .until(
-            (ExpectedCondition<Boolean>)
-                webDriver -> getFullDescription().contains(expectedContent));
+    seleniumWebDriverHelper.waitValuePresence(historyEditor, expectedContent);
   }
 
   public void waitCommitInHistoryForm(String text) {
-    new WebDriverWait(seleniumWebDriver, LOAD_PAGE_TIMEOUT_SEC)
-        .until(
-            ExpectedConditions.visibilityOfElementLocated(
-                By.xpath(String.format(Locators.COMMIT_INTO_HISTORY_FORM, text))));
+    seleniumWebDriverHelper.waitVisibility(
+        By.xpath(String.format(Locators.COMMIT_INTO_HISTORY_FORM, text)));
   }
 
   public void clickCommitInHistoryForm(String text) {
-    new WebDriverWait(seleniumWebDriver, LOAD_PAGE_TIMEOUT_SEC)
-        .until(
-            ExpectedConditions.visibilityOfElementLocated(
-                By.xpath(String.format(Locators.COMMIT_INTO_HISTORY_FORM, text))))
-        .click();
+    seleniumWebDriverHelper.waitAndClick(
+        By.xpath(String.format(Locators.COMMIT_INTO_HISTORY_FORM, text)));
   }
 
   public void waitCommitInHistoryFormNotPresent(String text) {
-    new WebDriverWait(seleniumWebDriver, LOAD_PAGE_TIMEOUT_SEC)
-        .until(
-            ExpectedConditions.invisibilityOfElementLocated(
-                By.xpath(String.format(Locators.COMMIT_INTO_HISTORY_FORM, text))));
+    seleniumWebDriverHelper.waitInvisibility(
+        By.xpath(String.format(Locators.COMMIT_INTO_HISTORY_FORM, text)));
   }
 
   /** click on close button history */
   public void clickOnCloseButtonHistory() {
-    new WebDriverWait(seleniumWebDriver, REDRAW_UI_ELEMENTS_TIMEOUT_SEC)
-        .until(ExpectedConditions.elementToBeClickable(closeButtonHistory))
-        .click();
+    seleniumWebDriverHelper.waitAndClick(closeButtonHistory, REDRAW_UI_ELEMENTS_TIMEOUT_SEC);
     waitHistoryFormToClose();
+  }
+
+  public String getTopCommitRevision() {
+    loader.waitOnClosed();
+    int numberOfTopRevisionCell = 0;
+    return getGitHistoryTopContent().split("\n")[numberOfTopRevisionCell];
+  }
+
+  /** Returns all cells from top row divided by "\n" */
+  private String getGitHistoryTopContent() {
+    String rowTopIndex = Integer.toString(0);
+    return seleniumWebDriverHelper.waitVisibilityAndGetText(
+        By.xpath(String.format(Locators.HISTORY_ROW_NUMBER, rowTopIndex)));
   }
 }
