9f24db82253f6134cf6f7594ad64e0a45254b06f
RomanNikitenko <rnikiten@redhat.com>
2018-04-11 13:53:01 +0300

    Change way of storing IDE state data (#9196)
    * CHE-7551. Change way of storing IDE state data
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

---
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/statepersistance/AppStateTracker.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.ide.statepersistance;
+
+import static elemental.events.Event.BLUR;
+import static org.eclipse.che.ide.util.dom.Elements.getWindow;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+import elemental.events.EventRemover;
+import elemental.json.JsonObject;
+import org.eclipse.che.ide.DelayedTask;
+import org.eclipse.che.ide.api.WindowActionEvent;
+import org.eclipse.che.ide.api.WindowActionHandler;
+import org.eclipse.che.ide.api.app.AppContext;
+import org.eclipse.che.ide.api.parts.PartStackStateChangedEvent;
+import org.eclipse.che.ide.api.workspace.WorkspaceReadyEvent;
+import org.eclipse.che.ide.api.workspace.event.WorkspaceStoppingEvent;
+
+/**
+ * Contains handlers to track app state and persist/restore IDE state across sessions.
+ *
+ * @author Roman Nikitenko
+ */
+@Singleton
+public class AppStateTracker
+    implements WindowActionHandler,
+        PartStackStateChangedEvent.Handler,
+        WorkspaceStoppingEvent.Handler {
+
+  private EventBus eventBus;
+  private AppContext appContext;
+  private AppStateSyncWriter appStateSyncWriter;
+  private AppStateManager appStateManager;
+  private AppStateBackwardCompatibility appStateBackwardCompatibility;
+
+  private EventRemover blurEventRemover;
+  private HandlerRegistration windowEventsRemover;
+  private HandlerRegistration partStackStateEventRemover;
+  private HandlerRegistration workspaceStoppingEventRemover;
+
+  private final DelayedTask restoreStateTask =
+      new DelayedTask() {
+        @Override
+        public void onExecute() {
+          restoreState();
+        }
+      };
+
+  private final DelayedTask persistStateTask =
+      new DelayedTask() {
+        @Override
+        public void onExecute() {
+          appStateManager.persistState();
+        }
+      };
+
+  @Inject
+  public AppStateTracker(
+      EventBus eventBus,
+      AppContext appContext,
+      AppStateSyncWriter appStateSyncWriter,
+      AppStateManager appStateManager,
+      AppStateBackwardCompatibility appStateBackwardCompatibility) {
+    this.eventBus = eventBus;
+    this.appContext = appContext;
+    this.appStateSyncWriter = appStateSyncWriter;
+    this.appStateManager = appStateManager;
+    this.appStateBackwardCompatibility = appStateBackwardCompatibility;
+
+    eventBus.addHandler(WorkspaceReadyEvent.getType(), this::onWorkspaceReady);
+  }
+
+  private void onWorkspaceReady(WorkspaceReadyEvent event) {
+    // delay is required because we need to wait some time while different components initialized
+    restoreStateTask.delay(1_000);
+  }
+
+  @Override
+  public void onWorkspaceStopping(WorkspaceStoppingEvent event) {
+    cleanUpHandlers();
+  }
+
+  @Override
+  public void onWindowClosing(WindowActionEvent event) {
+    if (appContext.getWorkspace() == null) {
+      return;
+    }
+
+    JsonObject appState = appStateManager.collectAppStateData();
+    if (appState.keys().length > 0) {
+      appStateSyncWriter.saveState(appState);
+    }
+  }
+
+  @Override
+  public void onPartStackStateChanged(PartStackStateChangedEvent event) {
+    persistStateTask.delay(500);
+  }
+
+  private void restoreState() {
+    JsonObject appStateFromPreferences = appStateBackwardCompatibility.getAppState();
+    if (appStateFromPreferences != null) {
+      appStateManager
+          .restoreState(appStateFromPreferences)
+          .then(
+              arg -> {
+                addHandlers();
+              });
+
+      appStateBackwardCompatibility.removeAppState();
+    } else {
+      appStateManager
+          .readState()
+          .then(
+              arg -> {
+                appStateManager
+                    .restoreState()
+                    .then(
+                        aVoid -> {
+                          addHandlers();
+                        });
+              });
+    }
+  }
+
+  private void addHandlers() {
+    cleanUpHandlers();
+
+    partStackStateEventRemover = eventBus.addHandler(PartStackStateChangedEvent.TYPE, this);
+    blurEventRemover = getWindow().addEventListener(BLUR, evt -> appStateManager.persistState());
+    windowEventsRemover = eventBus.addHandler(WindowActionEvent.TYPE, this);
+    workspaceStoppingEventRemover = eventBus.addHandler(WorkspaceStoppingEvent.TYPE, this);
+  }
+
+  private void cleanUpHandlers() {
+    if (partStackStateEventRemover != null) {
+      partStackStateEventRemover.removeHandler();
+    }
+
+    if (blurEventRemover != null) {
+      blurEventRemover.remove();
+    }
+
+    if (windowEventsRemover != null) {
+      windowEventsRemover.removeHandler();
+    }
+
+    if (workspaceStoppingEventRemover != null) {
+      workspaceStoppingEventRemover.removeHandler();
+    }
+  }
+
+  @Override
+  public void onWindowClosed(WindowActionEvent event) {}
+}
---
+++ wsagent/che-wsagent-core/src/main/java/org/eclipse/che/wsagent/server/appstate/AppStateService.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.wsagent.server.appstate;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import com.google.inject.Inject;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import org.eclipse.che.api.core.BadRequestException;
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.core.ValidationException;
+import org.eclipse.che.api.core.rest.annotations.Required;
+
+/**
+ * Service allows to get or persist serialized IDE state by user identifier.
+ *
+ * @author Roman Nikitenko
+ */
+@Path("app/state")
+public class AppStateService {
+  private AppStateManager appStateManager;
+
+  @Inject
+  public AppStateService(AppStateManager appStateManager) {
+    this.appStateManager = appStateManager;
+  }
+
+  @GET
+  @Consumes(APPLICATION_JSON)
+  @Produces(APPLICATION_JSON)
+  @ApiOperation(
+    value = "Load saved serialized IDE state of current workspace by user identifier",
+    notes =
+        "It is expected that saved IDE state object is valid, so any validations are not performed. "
+            + "Empty string will be returned when IDE state is not found."
+  )
+  @ApiResponses({
+    @ApiResponse(code = 200, message = "OK"),
+    @ApiResponse(code = 400, message = "User ID should be defined"),
+    @ApiResponse(code = 500, message = "Server error")
+  })
+  public String loadAppState(
+      @ApiParam(value = "User identifier") @Required @QueryParam("userId") String userId)
+      throws ServerException, BadRequestException {
+    try {
+      return appStateManager.loadAppState(userId);
+    } catch (ValidationException e) {
+      throw new BadRequestException(e.getMessage());
+    }
+  }
+
+  @POST
+  @Path("update")
+  @Consumes(APPLICATION_JSON)
+  @ApiOperation(
+    value = "Save serialized IDE state of current workspace for given user",
+    notes =
+        "It is expected that incoming IDE state object is valid, so any validations are not performed."
+  )
+  @ApiResponses({
+    @ApiResponse(code = 200, message = "OK"),
+    @ApiResponse(code = 400, message = "User ID should be defined"),
+    @ApiResponse(code = 500, message = "Server error")
+  })
+  public void saveState(
+      @ApiParam(value = "User identifier") @Required @QueryParam("userId") String userId,
+      @ApiParam(value = "Serialized IDE state") String json)
+      throws ServerException, BadRequestException {
+    try {
+      appStateManager.saveState(userId, json);
+    } catch (ValidationException e) {
+      throw new BadRequestException(e.getMessage());
+    }
+  }
+}
