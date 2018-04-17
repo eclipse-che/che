c980af9826cc0610a251bdf210eb08db5b431647
Eugene Ivantsov <eivantsov@redhat.com>
2018-04-17 07:52:06 +0300

    Remove use of credentials

--- deploy/openshift/ocp.sh
+++ deploy/openshift/ocp.sh
@@ -254,7 +254,7 @@ deploy_che_to_ocp() {
       fi
       $OC_BINARY new-app -f ${BASE_DIR}/templates/multi/postgres-template.yaml
       wait_for_postgres
-      $OC_BINARY new-app -f ${BASE_DIR}/templates/multi/keycloak-template.yaml -p ROUTING_SUFFIX=${OC_PUBLIC_IP}.${DNS_PROVIDER} -p IMAGE_KEYCLOAK=eivantsov/keycloak ${KEYCLOAK_PARAM}
+      $OC_BINARY new-app -f ${BASE_DIR}/templates/multi/keycloak-template.yaml -p ROUTING_SUFFIX=${OC_PUBLIC_IP}.${DNS_PROVIDER} ${KEYCLOAK_PARAM}
       wait_for_keycloak
       export CHE_MULTIUSER_PARAM="-p CHE_MULTIUSER=true"
     fi
@@ -263,7 +263,7 @@ deploy_che_to_ocp() {
         ENV="-e ${CHE_VAR_ARRAY}"
       fi
 
-    $OC_BINARY new-app -f ${BASE_DIR}/templates/che-server-template.yaml -p ROUTING_SUFFIX=${OC_PUBLIC_IP}.${DNS_PROVIDER} -p IMAGE_CHE=${CHE_IMAGE_REPO} -p CHE_VERSION=${CHE_IMAGE_TAG} ${CHE_MULTIUSER_PARAM} -p CHE_INFRA_OPENSHIFT_PROJECT='' -p OPENSHIFT_USERNAME=${OPENSHIFT_USERNAME} -p OPENSHIFT_PASSWORD=${OPENSHIFT_PASSWORD} ${ENV}
+    $OC_BINARY new-app -f ${BASE_DIR}/templates/che-server-template.yaml -p ROUTING_SUFFIX=${OC_PUBLIC_IP}.${DNS_PROVIDER} -p IMAGE_CHE=${CHE_IMAGE_REPO} -p CHE_VERSION=${CHE_IMAGE_TAG} ${CHE_MULTIUSER_PARAM} ${ENV}
     $OC_BINARY set volume dc/che --add -m /data --name=che-data-volume --claim-name=che-data-volume
     echo "Waiting for Che to boot..."
     wait_for_che
