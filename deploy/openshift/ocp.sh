aea61c2f8d26d34025a9dd7290fc1f54d16188ff
Eugene Ivantsov <eivantsov@redhat.com>
2018-04-19 09:19:12 +0300

    Remove testing echo

--- deploy/openshift/ocp.sh
+++ deploy/openshift/ocp.sh
@@ -262,7 +262,6 @@ deploy_che_to_ocp() {
       if [ ${#CHE_VAR_ARRAY[@]} > 0 ]; then
         ENV="-e ${CHE_VAR_ARRAY}"
       fi
-    echo "### $OC_BINARY new-app -f ${BASE_DIR}/templates/che-server-template.yaml -p ROUTING_SUFFIX=${OC_PUBLIC_IP}.${DNS_PROVIDER} -p IMAGE_CHE=${CHE_IMAGE_REPO} -p CHE_VERSION=${CHE_IMAGE_TAG} ${CHE_MULTIUSER_PARAM} ${ENV} ###"
     $OC_BINARY new-app -f ${BASE_DIR}/templates/che-server-template.yaml -p ROUTING_SUFFIX=${OC_PUBLIC_IP}.${DNS_PROVIDER} -p IMAGE_CHE=${CHE_IMAGE_REPO} -p CHE_VERSION=${CHE_IMAGE_TAG} ${CHE_MULTIUSER_PARAM} ${ENV}
     $OC_BINARY set volume dc/che --add -m /data --name=che-data-volume --claim-name=che-data-volume
     echo "Waiting for Che to boot..."
