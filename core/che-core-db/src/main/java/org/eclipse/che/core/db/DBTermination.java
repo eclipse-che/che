6f7addfe9011ac4c30dfc76cf730cb933d5149bb
Anton Korneta <korneta.a.o@gmail.com>
2018-04-27 11:55:45 +0300

    Add trick for closing JGroups EclipseLinkCommandChannel

--- core/che-core-db/src/main/java/org/eclipse/che/core/db/DBTermination.java
+++ core/che-core-db/src/main/java/org/eclipse/che/core/db/DBTermination.java
@@ -14,10 +14,15 @@ import static org.eclipse.che.api.system.shared.SystemStatus.READY_TO_SHUTDOWN;
 
 import com.google.inject.Inject;
 import com.google.inject.persist.PersistService;
+import java.lang.reflect.Field;
 import javax.inject.Singleton;
+import javax.persistence.EntityManagerFactory;
 import org.eclipse.che.api.core.notification.EventService;
 import org.eclipse.che.api.core.notification.EventSubscriber;
 import org.eclipse.che.api.system.shared.dto.SystemStatusChangedEventDto;
+import org.eclipse.persistence.internal.sessions.AbstractSession;
+import org.eclipse.persistence.internal.sessions.coordination.jgroups.JGroupsRemoteConnection;
+import org.eclipse.persistence.sessions.coordination.TransportManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,7 +37,8 @@ public class DBTermination {
   private static final Logger LOG = LoggerFactory.getLogger(DBTermination.class);
 
   @Inject
-  public DBTermination(EventService eventService, PersistService persistService) {
+  public DBTermination(
+      EventService eventService, PersistService persistService, EntityManagerFactory emFactory) {
     eventService.subscribe(
         new EventSubscriber<SystemStatusChangedEventDto>() {
           @Override
@@ -40,6 +46,7 @@ public class DBTermination {
             if (READY_TO_SHUTDOWN.equals(event.getStatus())) {
               try {
                 LOG.info("Stopping persistence service.");
+                fixJChannelClosing(emFactory);
                 persistService.stop();
               } catch (RuntimeException ex) {
                 LOG.error("Failed to stop persistent service. Cause: " + ex.getMessage());
@@ -48,5 +55,30 @@ public class DBTermination {
           }
         },
         SystemStatusChangedEventDto.class);
+  }
+
+  /**
+   * This method is hack that changes value of {@link JGroupsRemoteConnection#isLocal} to false.
+   * This is needed to close the JGroups EclipseLinkCommandChannel and as result gracefully stop of
+   * the system.<br>
+   * For more details see {@link JGroupsRemoteConnection#closeInternal()}
+   *
+   * <p>TODO insert eclipse-link bug here
+   */
+  private void fixJChannelClosing(EntityManagerFactory emFactory) {
+    try {
+      final AbstractSession session = emFactory.unwrap(AbstractSession.class);
+      final TransportManager transportManager = session.getCommandManager().getTransportManager();
+      final JGroupsRemoteConnection conn =
+          (JGroupsRemoteConnection) transportManager.getConnectionToLocalHost();
+      final Field isLocal = conn.getClass().getDeclaredField("isLocal");
+      isLocal.setAccessible(true);
+      isLocal.set(conn, false);
+    } catch (IllegalAccessException | NoSuchFieldException ex) {
+      LOG.error(
+          "Failed to change JGroupsRemoteConnection#isLocal this may prevent the graceful stop of "
+              + "the system because EclipseLinkCommandChannel won't be closed. Cause: "
+              + ex.getMessage());
+    }
   }
 }
