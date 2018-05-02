1381477961addec3d245959db4c4174c71e0a5ec
Max Shaposhnyk <mshaposh@redhat.com>
2018-05-02 14:28:47 +0300

    fixup! fixup! Fixed removal of remote subscriptions in manager

--- core/che-core-api-core/src/main/java/org/eclipse/che/api/core/notification/RemoteSubscriptionStorage.java
+++ core/che-core-api-core/src/main/java/org/eclipse/che/api/core/notification/RemoteSubscriptionStorage.java
@@ -20,9 +20,8 @@ import java.util.Set;
 public interface RemoteSubscriptionStorage {
 
   /**
-   * Returns all active subscriptions for the given method. It is recommended to use for read-only
-   * operations because it is not guaranteed that all storage implementations will reflect changes
-   * of this set.
+   * Returns all active subscriptions for the given method. It is recommended for implementations to
+   * return copy of the stored set, so this method should not be used for modifying operations.
    *
    * @param method Method name
    * @return active subscriptions to this method
