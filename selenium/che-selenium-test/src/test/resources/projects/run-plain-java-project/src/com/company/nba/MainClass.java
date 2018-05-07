0639a2dc3b37af36819878b7c82a55fd33cdc450
Aleksandr Shmaraiev <oshmarai@redhat.com>
2018-05-07 09:52:13 +0300

    Selenium: Rework the 'RunPlainJavaProjectTest' to use the 'kohsuke Github' library (#9602)
    * Rework the 'RunPlainJavaProjectTest' to use the 'kohsuke Github' library
    * Add the 'run-plain-java-project' to the resources
    * Delete the 'KeepDirectotryGitImportTest' from the 'SmokeTestsSuite'
    * Delete the 'PushChangeNotUpdatedRepoTest' from the project as a duplicate test

---
+++ selenium/che-selenium-test/src/test/resources/projects/run-plain-java-project/src/com/company/nba/MainClass.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package com.company.nba;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+	// write your code here
+	String string = "I love this game!";
+	System.out.println(string);
+	
+	
+    }
+}
