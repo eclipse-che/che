c9955c354c729f8380988059bf811a355deca3c8
Josh Pinkney <Joshpinkney@gmail.com>
2018-05-09 09:00:52 -0400

    Fixed quick documentation (#9508)
    Signed-off-by: jpinkney <josh.pinkney@mail.utoronto.ca>

--- plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/hover/HoverProvider.java
+++ plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/hover/HoverProvider.java
@@ -14,7 +14,11 @@ package org.eclipse.che.plugin.languageserver.ide.hover;
 import com.google.common.base.Joiner;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import elemental.dom.Element;
+import elemental.events.Event;
+import elemental.html.Window;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.eclipse.che.api.promises.client.Promise;
 import org.eclipse.che.api.promises.client.js.JsPromise;
@@ -26,11 +30,17 @@ import org.eclipse.che.ide.editor.orion.client.OrionHoverHandler;
 import org.eclipse.che.ide.editor.orion.client.jso.OrionHoverContextOverlay;
 import org.eclipse.che.ide.editor.orion.client.jso.OrionHoverOverlay;
 import org.eclipse.che.ide.util.StringUtils;
+import org.eclipse.che.ide.util.browser.BrowserUtils;
+import org.eclipse.che.ide.util.dom.Elements;
 import org.eclipse.che.plugin.languageserver.ide.editor.LanguageServerEditorConfiguration;
 import org.eclipse.che.plugin.languageserver.ide.service.TextDocumentServiceClient;
 import org.eclipse.che.plugin.languageserver.ide.util.DtoBuildHelper;
+import org.eclipse.che.plugin.languageserver.ide.util.OpenFileInEditorHelper;
 import org.eclipse.lsp4j.Hover;
+import org.eclipse.lsp4j.Location;
 import org.eclipse.lsp4j.MarkedString;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.TextDocumentPositionParams;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 
@@ -45,13 +55,22 @@ public class HoverProvider implements OrionHoverHandler {
   private final EditorAgent editorAgent;
   private final TextDocumentServiceClient client;
   private final DtoBuildHelper helper;
+  private final OpenFileInEditorHelper openFileInEditorHelper;
+  private final DtoBuildHelper dtoBuildHelper;
+  private final String[] openableSchemes = new String[] {"file:/", "jdt:/"};
 
   @Inject
   public HoverProvider(
-      EditorAgent editorAgent, TextDocumentServiceClient client, DtoBuildHelper helper) {
+      EditorAgent editorAgent,
+      TextDocumentServiceClient client,
+      DtoBuildHelper helper,
+      OpenFileInEditorHelper openFileInEditorHelper,
+      DtoBuildHelper dtoBuildHelper) {
     this.editorAgent = editorAgent;
     this.client = client;
     this.helper = helper;
+    this.openFileInEditorHelper = openFileInEditorHelper;
+    this.dtoBuildHelper = dtoBuildHelper;
   }
 
   @Override
@@ -91,6 +110,9 @@ public class HoverProvider implements OrionHoverHandler {
 
               return hover;
             });
+
+    createHoverLinkListener(activeEditor);
+
     return (JsPromise<OrionHoverOverlay>) then;
   }
 
@@ -105,5 +127,91 @@ public class HoverProvider implements OrionHoverHandler {
       }
     }
     return Joiner.on("\n\n").join(contents);
+  }
+
+  private void createHoverLinkListener(EditorPartPresenter activeEditor) {
+    final Window window = Elements.getWindow();
+    window.addEventListener(
+        Event.MOUSEDOWN,
+        evt -> {
+          Element anchorEle = (Element) evt.getTarget();
+
+          // Register the onClick and open only if the element is in a tooltip.
+          if (anchorEle.getOffsetParent() != null
+              && anchorEle.getOffsetParent().getClassList() != null
+              && anchorEle.getOffsetParent().getClassList().contains("textViewTooltipOnHover")
+              && anchorEle.hasAttribute("href")) {
+            String hrefContent = anchorEle.getAttribute("href");
+            Location uriLocation = getLocationFromURI(hrefContent);
+            anchorEle.setOnclick(
+                anchorEleClick -> {
+                  anchorEleClick.preventDefault();
+                  anchorEleClick.stopPropagation();
+                  if (Arrays.stream(openableSchemes)
+                          .filter(scheme -> hrefContent.startsWith(scheme))
+                          .count()
+                      > 0) {
+                    openFileInEditorHelper.openLocation(uriLocation);
+                  } else {
+                    BrowserUtils.openInNewTab(hrefContent);
+                  }
+                  ((TextEditor) activeEditor).getEditorWidget().hideTooltip();
+                });
+          }
+        });
+  }
+
+  /**
+   * Create a workable URI for opening files in the workspace
+   *
+   * @param uri The URI to fix
+   * @return A URI that can be opened by the editor
+   */
+  private String createWorkableURI(String uri) {
+    String fixedURI = uri;
+    if (uri.indexOf("#") != -1) {
+      fixedURI = fixedURI.substring(0, uri.lastIndexOf("#"));
+    }
+
+    // Because of the javadoc2markdown library in jdt.ls file:///projects gets stripped to
+    // file:/projects when converting the markdown to html
+    if (uri.startsWith("file:/projects")) {
+      fixedURI = fixedURI.replace("file:/projects", "");
+    }
+
+    return fixedURI;
+  }
+
+  /**
+   * Gets the location from a URI that is in the form of URI#LineNumber
+   *
+   * @param uri The URI to get the location from
+   * @return Location object from the URI
+   */
+  private Location getLocationFromURI(String uri) {
+    Location uriLoc = new Location();
+    uriLoc.setUri(createWorkableURI(uri));
+
+    Position uriPos = new Position();
+
+    int lineIndex = uri.lastIndexOf('#');
+
+    // We have line information from the URI
+    if (lineIndex != -1) {
+      String lastMatch = uri.substring(lineIndex + 1);
+      Integer lineNumber = Integer.parseInt(lastMatch);
+
+      if (lineNumber > 0) {
+        uriPos.setLine(lineNumber - 1);
+      } else {
+        uriPos.setLine(lineNumber);
+      }
+    } else {
+      uriPos.setLine(0);
+    }
+
+    uriLoc.setRange(new Range(uriPos, uriPos));
+
+    return uriLoc;
   }
 }
