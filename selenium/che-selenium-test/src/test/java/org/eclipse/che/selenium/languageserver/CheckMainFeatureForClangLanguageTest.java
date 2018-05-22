4e923cde988342194ef945a60ec3e0af43eda16a
sskoryk <sskoryk@redhat.com>
2018-05-22 17:18:04 +0300

    add methods for checking LS initialization and autocomplete

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/languageserver/CheckMainFeatureForClangLanguageTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/languageserver/CheckMainFeatureForClangLanguageTest.java
@@ -10,8 +10,18 @@
  */
 package org.eclipse.che.selenium.languageserver;
 
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.New.FILE;
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.New.NEW;
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.PROJECT;
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Workspace.CREATE_PROJECT;
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Workspace.WORKSPACE;
+import static org.eclipse.che.selenium.pageobject.CodenvyEditor.MarkerLocator.ERROR;
+import static org.eclipse.che.selenium.pageobject.Wizard.SamplesName.CONSOLE_CPP_SIMPLE;
+
 import com.google.inject.Inject;
+import org.eclipse.che.selenium.core.workspace.InjectTestWorkspace;
 import org.eclipse.che.selenium.core.workspace.TestWorkspace;
+import org.eclipse.che.selenium.core.workspace.WorkspaceTemplate;
 import org.eclipse.che.selenium.pageobject.AskForValueDialog;
 import org.eclipse.che.selenium.pageobject.CodenvyEditor;
 import org.eclipse.che.selenium.pageobject.Consoles;
@@ -21,12 +31,20 @@ import org.eclipse.che.selenium.pageobject.ProjectExplorer;
 import org.eclipse.che.selenium.pageobject.Wizard;
 import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
 import org.eclipse.che.selenium.pageobject.intelligent.CommandsPalette;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /** @author Skoryk Serhii */
 public class CheckMainFeatureForClangLanguageTest {
+  private static final String PROJECT_NAME = "console-cpp-simple";
+  private static final String CPP_FILE_NAME = "hello.cc";
+  private static final String H_MODULE_FILE_NAME = "iseven.h";
+  private static final String OTHER_FILE_NAME = "iseven.cpp";
+  private static final String LS_INIT_MESSAGE =
+      "Finished language servers initialization, file path '/console-cpp-simple/hello.cc";
 
+  @InjectTestWorkspace(template = WorkspaceTemplate.ECLIPSE_CPP_GCC)
   private TestWorkspace workspace;
 
   @Inject private Ide ide;
@@ -46,5 +64,64 @@ public class CheckMainFeatureForClangLanguageTest {
 
   @Test
   public void checkLanguageServerInitialized() {
+    ide.waitOpenedWorkspaceIsReadyToUse();
+
+    menu.runCommand(WORKSPACE, CREATE_PROJECT);
+    wizard.selectProjectAndCreate(CONSOLE_CPP_SIMPLE, PROJECT_NAME);
+
+    projectExplorer.waitItem(PROJECT_NAME);
+    projectExplorer.openItemByPath(PROJECT_NAME);
+    projectExplorer.openItemByPath(PROJECT_NAME + "/hello.cc");
+    editor.waitTabIsPresent("hello.cc");
+
+    consoles.selectProcessByTabName("dev-machine");
+    consoles.waitExpectedTextIntoConsole(LS_INIT_MESSAGE);
+  }
+
+  @Test(priority = 1)
+  public void checkErrorMessages() {
+    editor.selectTabByName(CPP_FILE_NAME);
+    editor.goToCursorPositionVisible(1, 1);
+    editor.waitMarkerInvisibility(ERROR, 1);
+    editor.typeTextIntoEditor("c");
+    editor.waitMarkerInPosition(ERROR, 1);
+    editor.typeTextIntoEditor(Keys.DELETE.toString());
+    editor.waitAllMarkersInvisibility(ERROR);
+  }
+
+  @Test(priority = 1)
+  public void checkAutocompleteFeature() {
+    editor.selectTabByName(CPP_FILE_NAME);
+    editor.goToPosition(7, 1);
+
+    // check contents of autocomplete container
+    editor.deleteCurrentLineAndInsertNew();
+    editor.typeTextIntoEditor("std::cou");
+    editor.waitMarkerInPosition(ERROR, 7);
+    editor.launchAutocompleteAndWaitContainer();
+
+    System.out.println(editor.getAllVisibleTextFromAutocomplete());
+
+    editor.waitTextIntoAutocompleteContainer("cout ostream");
+    editor.waitTextIntoAutocompleteContainer("wcout wostream");
+    editor.enterAutocompleteProposal("ostream");
+    editor.waitTextIntoEditor("  std::cout");
+    editor.typeTextIntoEditor("<<\"Hello World!\";");
+    editor.waitAllMarkersInvisibility(ERROR);
+
+    commandsPalette.openCommandPalette();
+    commandsPalette.startCommandByDoubleClick(PROJECT_NAME + ":build and run");
+    consoles.waitTabNameProcessIsPresent(PROJECT_NAME + ":build and run");
+    consoles.waitExpectedTextIntoConsole("Hello World!");
+  }
+
+  private void createFile(String fileName) {
+    projectExplorer.waitAndSelectItem(PROJECT_NAME);
+    menu.runCommand(PROJECT, NEW, FILE);
+    askForValueDialog.waitFormToOpen();
+    askForValueDialog.typeAndWaitText(fileName);
+    askForValueDialog.clickOkBtn();
+    askForValueDialog.waitFormToClose();
+    editor.waitTabIsPresent(fileName);
   }
 }
