963234af5ddfca779eea76680440a689864e1d32
Oleksii Kurinnyi <okurinny@redhat.com>
2018-05-23 14:35:48 +0300

    CHE-8931: fix blank screen on project details page (#9771)
    * check WebSocket connection
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>
    
    * check workspace and workspace agent for they to be existing
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- dashboard/src/components/api/json-rpc/che-json-rpc-api.factory.ts
+++ dashboard/src/components/api/json-rpc/che-json-rpc-api.factory.ts
@@ -21,13 +21,14 @@ import {CheJsonRpcWsagentApi} from './che-json-rpc-wsagent-api';
  */
 export class CheJsonRpcApi {
 
-  static $inject = ['$q', '$websocket', '$log', '$timeout'];
+  static $inject = ['$q', '$websocket', '$log', '$timeout', '$interval'];
 
   private $q: ng.IQService;
   private $websocket: any;
   private $log: ng.ILogService;
   private jsonRpcApiConnection: Map<string, CheJsonRpcMasterApi>;
   private $timeout: ng.ITimeoutService;
+  private $interval: ng.IIntervalService;
 
   /**
    * Default constructor that is using resource
@@ -35,23 +36,25 @@ export class CheJsonRpcApi {
   constructor($q: ng.IQService,
               $websocket: any,
               $log: ng.ILogService,
-              $timeout: ng.ITimeoutService) {
+              $timeout: ng.ITimeoutService,
+              $interval: ng.IIntervalService) {
     this.$q = $q;
     this.$websocket = $websocket;
     this.$log = $log;
     this.$timeout = $timeout;
+    this.$interval = $interval;
     this.jsonRpcApiConnection = new Map<string, CheJsonRpcMasterApi>();
   }
 
   getJsonRpcMasterApi(entrypoint: string): CheJsonRpcMasterApi {
-   if (this.jsonRpcApiConnection.has(entrypoint)) {
-     return this.jsonRpcApiConnection.get(entrypoint);
-   } else {
-     let websocketClient = new WebsocketClient(this.$websocket, this.$q);
-     let cheJsonRpcMasterApi: CheJsonRpcMasterApi = new CheJsonRpcMasterApi(websocketClient, entrypoint, this.$log, this.$timeout);
-     this.jsonRpcApiConnection.set(entrypoint, cheJsonRpcMasterApi);
-     return cheJsonRpcMasterApi;
-   }
+    if (this.jsonRpcApiConnection.has(entrypoint)) {
+      return this.jsonRpcApiConnection.get(entrypoint);
+    } else {
+      const websocketClient = new WebsocketClient(this.$websocket, this.$q);
+      const cheJsonRpcMasterApi: CheJsonRpcMasterApi = new CheJsonRpcMasterApi(websocketClient, entrypoint, this.$log, this.$timeout, this.$interval, this.$q);
+      this.jsonRpcApiConnection.set(entrypoint, cheJsonRpcMasterApi);
+      return cheJsonRpcMasterApi;
+    }
   }
 
   getJsonRpcWsagentApi(entrypoint: string): CheJsonRpcWsagentApi {
