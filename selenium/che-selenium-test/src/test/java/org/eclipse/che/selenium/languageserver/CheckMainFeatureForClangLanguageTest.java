77659b15ce87f39d615d08c7f96f54a93a5067d5
sskoryk <sskoryk@redhat.com>
2018-05-24 17:28:17 +0300

    add test method for checking FindDefinition feature

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/languageserver/CheckMainFeatureForClangLanguageTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/languageserver/CheckMainFeatureForClangLanguageTest.java
@@ -10,6 +10,8 @@
  */
 package org.eclipse.che.selenium.languageserver;
 
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Assistant.ASSISTANT;
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Assistant.FIND_DEFINITION;
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.New.FILE;
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.New.NEW;
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.PROJECT;
@@ -17,8 +19,10 @@ import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.W
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Workspace.WORKSPACE;
 import static org.eclipse.che.selenium.pageobject.CodenvyEditor.MarkerLocator.ERROR;
 import static org.eclipse.che.selenium.pageobject.Wizard.SamplesName.CONSOLE_CPP_SIMPLE;
+import static org.openqa.selenium.Keys.F4;
 
 import com.google.inject.Inject;
+import org.eclipse.che.selenium.core.client.TestProjectServiceClient;
 import org.eclipse.che.selenium.core.workspace.InjectTestWorkspace;
 import org.eclipse.che.selenium.core.workspace.TestWorkspace;
 import org.eclipse.che.selenium.core.workspace.WorkspaceTemplate;
@@ -38,11 +42,45 @@ import org.testng.annotations.Test;
 /** @author Skoryk Serhii */
 public class CheckMainFeatureForClangLanguageTest {
   private static final String PROJECT_NAME = "console-cpp-simple";
-  private static final String CPP_FILE_NAME = "hello.cc";
-  private static final String H_MODULE_FILE_NAME = "iseven.h";
-  private static final String OTHER_FILE_NAME = "iseven.cpp";
+  private static final String HELLO_CC = "hello.cc";
+  private static final String ISEVEN_H = "iseven.h";
+  private static final String ISEVEN_CPP = "iseven.cpp";
+  private static final String HELLO_CPP = "hello.cpp";
+
   private static final String LS_INIT_MESSAGE =
       "Finished language servers initialization, file path '/console-cpp-simple/hello.cc";
+  private static final String HELLO_CC_CONTENT =
+      "// Simple Hello World\n"
+          + " \n"
+          + "#include <iostream>\n"
+          + " \n"
+          + "int main()\n"
+          + "{\n"
+          + "  std::cout << \"Hello World!\" << std::endl;\n"
+          + "  return 0;\n"
+          + "}";
+  private static final String ISEVEN_H_CONTENT =
+      "#ifndef VARIABLE\n"
+          + "#define VARIABLE\n"
+          + "\n"
+          + "int isEven(int arg);\n"
+          + "\n"
+          + "#endif";
+  private static final String ISEVEN_CPP_CONTENT =
+      "int isEven(int x) {\n" + "    return x % 2 == 0;\n" + "}";
+  private static final String HELLO_CPP_CONTENT =
+      "#include <iostream>\n"
+          + "#include \"iseven.h\"\n"
+          + "\n"
+          + "void test(int);\n"
+          + "\n"
+          + "int main()\n"
+          + "{\n"
+          + "  int x = 4;\n"
+          + "  std::cout << \"Hello World!\" << std::endl;\n"
+          + "  std::cout << isEven(x) << std::endl;\n"
+          + "  return 0;\n"
+          + "}";
 
   @InjectTestWorkspace(template = WorkspaceTemplate.ECLIPSE_CPP_GCC)
   private TestWorkspace workspace;
@@ -56,6 +94,7 @@ public class CheckMainFeatureForClangLanguageTest {
   @Inject private CommandsPalette commandsPalette;
   @Inject private ProjectExplorer projectExplorer;
   @Inject private AskForValueDialog askForValueDialog;
+  @Inject private TestProjectServiceClient testProjectServiceClient;
 
   @BeforeClass
   public void setUp() throws Exception {
@@ -69,7 +108,7 @@ public class CheckMainFeatureForClangLanguageTest {
     menu.runCommand(WORKSPACE, CREATE_PROJECT);
     wizard.selectProjectAndCreate(CONSOLE_CPP_SIMPLE, PROJECT_NAME);
 
-    projectExplorer.waitItem(PROJECT_NAME);
+    projectExplorer.waitAndSelectItem(PROJECT_NAME);
     projectExplorer.openItemByPath(PROJECT_NAME);
     projectExplorer.openItemByPath(PROJECT_NAME + "/hello.cc");
     editor.waitTabIsPresent("hello.cc");
@@ -79,40 +118,76 @@ public class CheckMainFeatureForClangLanguageTest {
   }
 
   @Test(priority = 1)
-  public void checkErrorMessages() {
-    editor.selectTabByName(CPP_FILE_NAME);
-    editor.goToCursorPositionVisible(1, 1);
-    editor.waitMarkerInvisibility(ERROR, 1);
+  public void checkMarkers() throws Exception {
+    editor.selectTabByName(HELLO_CC);
+    testProjectServiceClient.updateFile(
+        workspace.getId(), PROJECT_NAME + "/" + HELLO_CC, HELLO_CC_CONTENT);
+
+    editor.goToCursorPositionVisible(5, 1);
+    editor.waitMarkerInvisibility(ERROR, 5);
     editor.typeTextIntoEditor("c");
-    editor.waitMarkerInPosition(ERROR, 1);
+    editor.waitMarkerInPosition(ERROR, 5);
     editor.typeTextIntoEditor(Keys.DELETE.toString());
     editor.waitAllMarkersInvisibility(ERROR);
   }
 
   @Test(priority = 1)
-  public void checkAutocompleteFeature() {
-    editor.selectTabByName(CPP_FILE_NAME);
-    editor.goToPosition(7, 1);
+  public void checkAutocompleteFeature() throws Exception {
+    editor.selectTabByName(HELLO_CC);
+    testProjectServiceClient.updateFile(
+        workspace.getId(), PROJECT_NAME + "/" + HELLO_CC, HELLO_CC_CONTENT);
 
     // check contents of autocomplete container
+    editor.goToPosition(7, 1);
     editor.deleteCurrentLineAndInsertNew();
     editor.typeTextIntoEditor("std::cou");
-    editor.waitMarkerInPosition(ERROR, 7);
+    // editor.waitMarkerInPosition(ERROR, 7);
     editor.launchAutocompleteAndWaitContainer();
-
-    System.out.println(editor.getAllVisibleTextFromAutocomplete());
 
     editor.waitTextIntoAutocompleteContainer("cout ostream");
     editor.waitTextIntoAutocompleteContainer("wcout wostream");
+    editor.enterAutocompleteProposal("cou");
+    // TODO fix proposal selection from Autocompete
     editor.enterAutocompleteProposal("ostream");
     editor.waitTextIntoEditor("  std::cout");
     editor.typeTextIntoEditor("<<\"Hello World!\";");
+    editor.typeTextIntoEditor(Keys.DELETE.toString() + Keys.DELETE.toString());
     editor.waitAllMarkersInvisibility(ERROR);
 
     commandsPalette.openCommandPalette();
     commandsPalette.startCommandByDoubleClick(PROJECT_NAME + ":build and run");
     consoles.waitTabNameProcessIsPresent(PROJECT_NAME + ":build and run");
     consoles.waitExpectedTextIntoConsole("Hello World!");
+  }
+
+  @Test(priority = 1)
+  public void checkFindDefinitionFeature() throws Exception {
+    prepareFile(ISEVEN_H, ISEVEN_H_CONTENT);
+    editor.closeFileByNameWithSaving(ISEVEN_H);
+    prepareFile(ISEVEN_CPP, ISEVEN_CPP_CONTENT);
+    editor.closeFileByNameWithSaving(ISEVEN_CPP);
+    prepareFile(HELLO_CPP, HELLO_CPP_CONTENT);
+
+    editor.selectTabByName(HELLO_CPP);
+    editor.waitActive();
+
+    // check Find Definition feature from Assistant menu
+    editor.goToPosition(10, 17);
+    menu.runCommand(ASSISTANT, FIND_DEFINITION);
+    editor.waitTabIsPresent(ISEVEN_H);
+    editor.clickOnCloseFileIcon(ISEVEN_H);
+
+    // check Find Definition feature by pressing F4
+    editor.goToPosition(10, 17);
+    editor.typeTextIntoEditor(F4.toString());
+    editor.waitTabIsPresent(ISEVEN_H);
+  }
+
+  private void prepareFile(String fileName, String text) throws Exception {
+    createFile(fileName);
+    editor.selectTabByName(fileName);
+    editor.deleteAllContent();
+    testProjectServiceClient.updateFile(workspace.getId(), PROJECT_NAME + "/" + fileName, text);
   }
 
   private void createFile(String fileName) {
