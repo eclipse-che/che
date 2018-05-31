d80629d3288b2e249f05a02e3d25d760d2c8446e
Thomas Mäder <tmader@redhat.com>
2018-05-31 16:14:02 +0200

    Make jdt.ls branch work with refactored LS launchers
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- wsagent/che-core-api-languageserver/src/test/java/org/eclipse/che/api/languageserver/SnippetTest.java
+++ wsagent/che-core-api-languageserver/src/test/java/org/eclipse/che/api/languageserver/SnippetTest.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-package org.eclipse.che.api.languageserver.service;
+package org.eclipse.che.api.languageserver;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
