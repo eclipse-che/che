2c43be2e15fde40aa18ca6aed466efa544ed12b4
musienko maksym <mmusiien@redhat.com>
2018-06-18 18:56:09 +0300

    put the system specific methods into separate class

---
+++ selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/core/client/CheTestSystemClient.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.selenium.core.client;
+
+import static org.eclipse.che.selenium.core.client.CheTestSystemClient.WsMasterStatus.*;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import org.eclipse.che.commons.json.JsonHelper;
+import org.eclipse.che.selenium.core.provider.CheTestApiEndpointUrlProvider;
+import org.eclipse.che.selenium.core.requestfactory.CheTestAdminHttpJsonRequestFactory;
+import org.eclipse.che.selenium.core.utils.WaitUtils;
+
+/** @author Musienko Maxim */
+@Singleton
+public class CheTestSystemClient {
+
+  @Inject CheTestApiEndpointUrlProvider cheTestApiEndpointUrlProvider;
+
+  @Inject CheTestAdminHttpJsonRequestFactory testUserHttpJsonRequestFactory;
+
+  public enum WsMasterStatus {
+    RUNNING,
+    READY_TO_SHUTDOWN,
+    PREPARING_TO_SHUTDOWN,
+    SUSPENDED;
+  }
+
+  /**
+   * Prepare ws master to stopping. It means sending sopping request as admin and checking that next
+   * statuses (PREPARING_TO_SHUTDOWN, READY_TO_SHUTDOWN) passed as well
+   *
+   * @throws Exception
+   */
+  public void prepareToStopping() throws Exception {
+    int timeLimitForReadyToShutdownStatus = 30;
+    int delayBetweenRequestes = 1;
+
+    String restUrlForSuspendingWorkspaces =
+        cheTestApiEndpointUrlProvider.get().toString() + "system/stop";
+
+    testUserHttpJsonRequestFactory
+        .fromUrl(restUrlForSuspendingWorkspaces)
+        .usePostMethod()
+        .request();
+
+    waitWorkspaceMasterStatus(
+        timeLimitForReadyToShutdownStatus, delayBetweenRequestes, PREPARING_TO_SHUTDOWN);
+
+    waitWorkspaceMasterStatus(
+        timeLimitForReadyToShutdownStatus, delayBetweenRequestes, READY_TO_SHUTDOWN);
+  }
+
+  public String getCurrentState() throws Exception {
+    String restUrlForGettingSuspendingStatus =
+        cheTestApiEndpointUrlProvider.get().toString() + "system/state";
+
+    // get current response code - if system is suspended,  we will get IO exception from the
+    // server. This mean that we have suspended status
+    try {
+      testUserHttpJsonRequestFactory
+          .fromUrl(restUrlForGettingSuspendingStatus)
+          .useGetMethod()
+          .request()
+          .getResponseCode();
+    } catch (IOException ex) {
+      return SUSPENDED.toString();
+    }
+
+    return JsonHelper.parseJson(
+            testUserHttpJsonRequestFactory
+                .fromUrl(restUrlForGettingSuspendingStatus)
+                .useGetMethod()
+                .request()
+                .asString())
+        .getElement("status")
+        .getStringValue();
+  }
+
+  public void waitWorkspaceMasterStatus(
+      int maxReadStatusAttempts, int delayBetweenRequestsInSec, WsMasterStatus expectedStatus)
+      throws Exception {
+    int readStatusAttempts = maxReadStatusAttempts;
+    // if the limit is not exceeded - do request and check status of the system
+    while (readStatusAttempts-- > 0) {
+      if (getCurrentState().equals(expectedStatus.toString())) {
+        break;
+      }
+
+      // delay if expected status has been not achieved and decrement limit
+      WaitUtils.sleepQuietly(delayBetweenRequestsInSec);
+
+      // if the limit has exceeded and we have not achieved expected status - something went wrong
+      if (maxReadStatusAttempts <= 0) {
+        throw new IOException(
+            String.format(
+                "Workspace Master hasn't achieved status '%s' in '%' seconds.",
+                maxReadStatusAttempts, maxReadStatusAttempts * maxReadStatusAttempts));
+      }
+    }
+  }
+}
--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/hotupdate/RecreateUpdateStrategyTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/hotupdate/RecreateUpdateStrategyTest.java
@@ -11,24 +11,17 @@
 package org.eclipse.che.selenium.hotupdate;
 
 import static java.lang.Integer.*;
+import static org.eclipse.che.selenium.core.client.CheTestSystemClient.WsMasterStatus.RUNNING;
 import static org.eclipse.che.selenium.hotupdate.RecreateUpdateStrategyTest.WsMasterStatus.*;
 import static org.testng.Assert.*;
 
 import com.google.inject.Inject;
 import java.io.IOException;
-import org.eclipse.che.api.core.BadRequestException;
-import org.eclipse.che.api.core.ConflictException;
-import org.eclipse.che.api.core.ForbiddenException;
-import org.eclipse.che.api.core.NotFoundException;
-import org.eclipse.che.api.core.ServerException;
-import org.eclipse.che.api.core.UnauthorizedException;
-import org.eclipse.che.commons.json.JsonHelper;
 import org.eclipse.che.selenium.core.SeleniumWebDriver;
 import org.eclipse.che.selenium.core.TestGroup;
+import org.eclipse.che.selenium.core.client.CheTestSystemClient;
 import org.eclipse.che.selenium.core.executor.OpenShiftCliCommandExecutor;
-import org.eclipse.che.selenium.core.provider.CheTestApiEndpointUrlProvider;
 import org.eclipse.che.selenium.core.requestfactory.CheTestAdminHttpJsonRequestFactory;
-import org.eclipse.che.selenium.core.utils.WaitUtils;
 import org.eclipse.che.selenium.core.utils.process.ProcessAgent;
 import org.eclipse.che.selenium.core.workspace.TestWorkspace;
 import org.eclipse.che.selenium.pageobject.CheTerminal;
@@ -44,7 +37,7 @@ public class RecreateUpdateStrategyTest {
       "get dc | grep che | awk '{print $2}'";
 
   @Inject CheTestAdminHttpJsonRequestFactory testUserHttpJsonRequestFactory;
-  @Inject CheTestApiEndpointUrlProvider cheTestApiEndpointUrlProvider;
+  @Inject CheTestSystemClient cheTestSystemClient;
   @Inject ProjectExplorer projectExplorer;
   @Inject OpenShiftCliCommandExecutor openShiftCliCommandExecutor;
   @Inject private ProcessAgent processAgent;
@@ -56,16 +49,8 @@ public class RecreateUpdateStrategyTest {
 
   private int cheDeploymentBeforeRollout;
 
-  protected enum WsMasterStatus {
-    RUNNING,
-    READY_TO_SHUTDOWN,
-    PREPARING_TO_SHUTDOWN,
-    SUSPENDED;
-  }
-
   @BeforeClass
   public void setUp() throws IOException {
-
     cheDeploymentBeforeRollout =
         parseInt(openShiftCliCommandExecutor.execute(COMMAND_FOR_GETTING_CURRENT_DEPLOYMENT_CHE));
   }
@@ -73,108 +58,35 @@ public class RecreateUpdateStrategyTest {
   @Test
   public void checkRecreateUpdateStrategy() throws Exception {
     String ocClientRolloutCommand = "rollout latest che";
-    String restUrlForSuspendingWorkspaces =
-        cheTestApiEndpointUrlProvider.get().toString() + "system/stop";
+
     int timeLimitInSecForRecreatingUpdate = 600;
     int delayBetweenRequests = 6;
 
     // open a user workspace and send request for preparing to shutdown
     ide.open(workspace);
 
-    prepareToShutdownRequest(restUrlForSuspendingWorkspaces);
+    cheTestSystemClient.prepareToStopping();
 
-    // make sure, that system is prepared to  shutdown and than ready to shutdown
-    checkExpectedStatusesAndWorkspaceAfterShutDowning();
+    // reopen the workspace and make sure that this one is not available after suspending system
+    ide.open(workspace);
+    projectExplorer.waitProjectExplorerDisappearance(delayBetweenRequests);
+    terminal.waitTerminalIsNotPresent(delayBetweenRequests);
 
     // performs rollout
     openShiftCliCommandExecutor.execute(ocClientRolloutCommand);
-    waitWorkspaceMasterStatus(timeLimitInSecForRecreatingUpdate, delayBetweenRequests, RUNNING);
+    cheTestSystemClient.waitWorkspaceMasterStatus(
+        timeLimitInSecForRecreatingUpdate, delayBetweenRequests, RUNNING);
 
     // get current version of deployment after rollout
     int cheDeploymentAfterRollout =
         parseInt(openShiftCliCommandExecutor.execute(COMMAND_FOR_GETTING_CURRENT_DEPLOYMENT_CHE));
-    // After rollout updating - deployment should be increased on 1. So we previews version +1
-    // should be equal current
+
+    // After rollout updating - deployment should be increased on 1. Previews version +1 should be
+    // equal current
     assertEquals(cheDeploymentAfterRollout, cheDeploymentBeforeRollout + 1);
 
     // make sure that CHE ide is available after updating again
     ide.open(workspace);
     ide.waitOpenedWorkspaceIsReadyToUse();
-  }
-
-  private void prepareToShutdownRequest(String restUrlForSuspendingWorkspaces)
-      throws IOException, ServerException, UnauthorizedException, ForbiddenException,
-          NotFoundException, ConflictException, BadRequestException {
-    testUserHttpJsonRequestFactory
-        .fromUrl(restUrlForSuspendingWorkspaces)
-        .usePostMethod()
-        .request();
-  }
-
-  private void checkExpectedStatusesAndWorkspaceAfterShutDowning() throws Exception {
-    int timeLimitForReadyToShutdownStatus = 30;
-    int delayBetweenRequestes = 1;
-
-    waitWorkspaceMasterStatus(
-        timeLimitForReadyToShutdownStatus,
-        delayBetweenRequestes,
-        WsMasterStatus.PREPARING_TO_SHUTDOWN);
-
-    waitWorkspaceMasterStatus(
-        timeLimitForReadyToShutdownStatus, delayBetweenRequestes, WsMasterStatus.READY_TO_SHUTDOWN);
-
-    // reopen the workspace and make sure that this one is not available after suspending system
-    ide.open(workspace);
-    projectExplorer.waitProjectExplorerDisappearance(timeLimitForReadyToShutdownStatus);
-    terminal.waitTerminalIsNotPresent(timeLimitForReadyToShutdownStatus);
-  }
-
-  private void waitWorkspaceMasterStatus(
-      int maxReadStatusAttempts, int delayBetweenRequestsInSec, WsMasterStatus expectedStatus)
-      throws Exception {
-    int readStatusAttempts = maxReadStatusAttempts;
-    // if the limit is not exceeded - do request and check status of the system
-    while (readStatusAttempts-- > 0) {
-      if (getCurrentRollingStatus().equals(expectedStatus.toString())) {
-        break;
-      }
-
-      // delay if expected status has been not achieved and decrement limit
-      WaitUtils.sleepQuietly(delayBetweenRequestsInSec);
-
-      // if the limit has exceeded and we have not achieved expected status - something went wrong
-      if (maxReadStatusAttempts <= 0) {
-        throw new IOException(
-            String.format(
-                "Workspace Master hasn't achieved status '%s' in '%' seconds.",
-                maxReadStatusAttempts, maxReadStatusAttempts * maxReadStatusAttempts));
-      }
-    }
-  }
-
-  private String getCurrentRollingStatus() throws Exception {
-    String restUrlForGettingSuspendingStatus =
-        cheTestApiEndpointUrlProvider.get().toString() + "system/state";
-
-    // get current response code - if system is suspended,  we will get IO exception from the
-    // server. This mean that we have suspended status
-    try {
-      testUserHttpJsonRequestFactory
-          .fromUrl(restUrlForGettingSuspendingStatus)
-          .useGetMethod()
-          .request()
-          .getResponseCode();
-    } catch (IOException ex) {
-      return WsMasterStatus.SUSPENDED.toString();
-    }
-
-    return JsonHelper.parseJson(
-            testUserHttpJsonRequestFactory
-                .fromUrl(restUrlForGettingSuspendingStatus)
-                .useGetMethod()
-                .request()
-                .asString())
-        .getElement("status")
-        .getStringValue();
   }
 }
