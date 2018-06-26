f77d5e7b3c9d07225d1d8abc9ef3060a792db1a4
Ihor Okhrimenko <iokhrime@redhat.com>
2018-06-26 16:22:47 +0300

    Do changes according to review

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/hotupdate/rolling/RollingUpdateStrategyWithWorkspacesStartStopTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/hotupdate/rolling/RollingUpdateStrategyWithWorkspacesStartStopTest.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-package org.eclipse.che.selenium.hotupdate;
+package org.eclipse.che.selenium.hotupdate.rolling;
 
 import static org.testng.Assert.assertEquals;
 
