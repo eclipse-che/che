806331c766e33511e1835a80cd76ae18e27605b7
sskoryk <sskoryk@redhat.com>
2018-07-11 10:19:45 +0300

    add template for NodeJs stack with JSON language server enabled

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/languageserver/JsonFileEditingTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/languageserver/JsonFileEditingTest.java
@@ -11,59 +11,44 @@
 package org.eclipse.che.selenium.languageserver;
 
 import static java.lang.String.format;
-import static org.eclipse.che.commons.lang.NameGenerator.generate;
-import static org.eclipse.che.selenium.core.workspace.WorkspaceTemplate.PYTHON;
-import static org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Stack.JAVA;
+import static org.eclipse.che.selenium.core.workspace.WorkspaceTemplate.NODEJS_WITH_JSON_LS;
 
 import com.google.inject.Inject;
-import org.eclipse.che.selenium.core.client.TestProjectServiceClient;
 import org.eclipse.che.selenium.core.workspace.InjectTestWorkspace;
 import org.eclipse.che.selenium.core.workspace.TestWorkspace;
 import org.eclipse.che.selenium.pageobject.CodenvyEditor;
 import org.eclipse.che.selenium.pageobject.Consoles;
 import org.eclipse.che.selenium.pageobject.Ide;
-import org.eclipse.che.selenium.pageobject.Menu;
 import org.eclipse.che.selenium.pageobject.ProjectExplorer;
-import org.eclipse.che.selenium.pageobject.dashboard.CreateWorkspaceHelper;
-import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
+import org.eclipse.che.selenium.pageobject.Wizard;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /** @author Skoryk Serhii */
 public class JsonFileEditingTest {
 
-  private static final String WORKSPACE_NAME = generate("workspace", 4);
   private static final String PROJECT_NAME = "nodejs-hello-world";
   private static final String JSON_FILE_NAME = "package.json";
   private static final String PATH_TO_JSON_FILE = PROJECT_NAME + "/" + JSON_FILE_NAME;
   private static final String LS_INIT_MESSAGE =
       format("Finished language servers initialization, file path '/%s'", PATH_TO_JSON_FILE);
 
-  @InjectTestWorkspace(template = PYTHON)
+  @InjectTestWorkspace(template = NODEJS_WITH_JSON_LS)
   private TestWorkspace workspace;
 
   @Inject private Ide ide;
-  @Inject private Menu menu;
   @Inject private Consoles consoles;
-  @Inject private Dashboard dashboard;
   @Inject private CodenvyEditor editor;
+  @Inject private Wizard wizard;
   @Inject private ProjectExplorer projectExplorer;
-  @Inject private CreateWorkspaceHelper createWorkspaceHelper;
-  @Inject private TestProjectServiceClient testProjectServiceClient;
 
   @BeforeClass
   public void setUp() throws Exception {
-    dashboard.open();
+    ide.open(workspace);
   }
 
   @Test
   public void checkLanguageServerInitialized() {
-    createWorkspaceHelper.createWorkspaceFromStackWithProject(JAVA, WORKSPACE_NAME, PROJECT_NAME);
-
-    ide.switchToIdeAndWaitWorkspaceIsReadyToUse();
-
-    projectExplorer.waitProjectInitialization(PROJECT_NAME);
-
     projectExplorer.waitAndSelectItem(PROJECT_NAME);
     projectExplorer.openItemByPath(PROJECT_NAME);
     projectExplorer.openItemByPath(PATH_TO_JSON_FILE);
