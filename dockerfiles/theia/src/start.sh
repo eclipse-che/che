39ed730235e340f303aa987cdeb76b1ab2b09cc9
Oleksandr Garagatyi <ogaragat@redhat.com>
2018-07-23 15:54:15 +0200

    Parse THEIA_PORT environment variable
    Parse THEIA_PORT environment variable in Theia start.sh script
    to prevent failure of Theia start on k8s when there is a k8s
    service "theia" and k8s injects THEIA_PORT environment variable
    with a value such as tcp://19.19.191.19:3000
    Signed-off-by: Oleksandr Garagatyi <ogaragat@redhat.com>
    
    Signed-off-by: Oleksandr Garagatyi <ogaragat@redhat.com>

--- dockerfiles/theia/src/start.sh
+++ dockerfiles/theia/src/start.sh
@@ -10,6 +10,19 @@
 
 if [ -z "$THEIA_PORT" ]; then
     export THEIA_PORT=3000
+else
+    # Parse THEIA_PORT env var in case it has weird value, such as tcp://10.108.137.206:3000
+    theia_port_number_regexp='^[0-9]+$'
+    if ! [[ "$THEIA_PORT" =~ $theia_port_number_regexp ]]; then
+        # THEIA_PORT contains something other than a number
+        theia_port_tcp_uri_regexp='^tcp://[0-9a-zA-Z.]+:[0-9]+$'
+        if [[ "$THEIA_PORT" =~ $theia_port_tcp_uri_regexp ]]; then
+            # Remove tcp://....: prefix
+            THEIA_PORT=${THEIA_PORT##*:}
+        else
+            echo  1>&2 "Environment variable THEIA_PORT contains unexpected value:$THEIA_PORT"
+        fi
+    fi
 fi
 
 yarn theia start /projects --hostname=0.0.0.0 --port=${THEIA_PORT}
