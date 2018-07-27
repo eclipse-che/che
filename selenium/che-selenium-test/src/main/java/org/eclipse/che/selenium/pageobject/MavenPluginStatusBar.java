77b39fce33a635a3efb92b481d7cbd2fd1566b40
Aleksandr Shmaraiev <oshmarai@redhat.com>
2018-07-27 14:49:20 +0000

    Selenium: Create test 'ImportAndValidateEclipseCheProjectTest' (#10558)
    * Create the selenium test 'ImportAndValidateEclipseCheProjectTest'
    * Correct the methods in the 'InformationDialog' using the 'SeleniumWebDriverHelper'
    * Correct the methods in the 'MavenPluginStatusBar'  using the 'SeleniumWebDriverHelper'

--- selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/MavenPluginStatusBar.java
+++ selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/MavenPluginStatusBar.java
@@ -10,60 +10,74 @@
  */
 package org.eclipse.che.selenium.pageobject;
 
+import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.ELEMENT_TIMEOUT_SEC;
 import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.EXPECTED_MESS_IN_CONSOLE_SEC;
-import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.REDRAW_UI_ELEMENTS_TIMEOUT_SEC;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.eclipse.che.selenium.core.SeleniumWebDriver;
+import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
+import org.eclipse.che.selenium.core.webdriver.WebDriverWaitFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /** @author Musienko Maxim */
 @Singleton
 public class MavenPluginStatusBar {
   private final SeleniumWebDriver seleniumWebDriver;
+  private final SeleniumWebDriverHelper seleniumWebDriverHelper;
+  private final WebDriverWaitFactory webDriverWaitFactory;
 
   @Inject
-  public MavenPluginStatusBar(SeleniumWebDriver seleniumWebDriver) {
+  public MavenPluginStatusBar(
+      SeleniumWebDriver seleniumWebDriver,
+      SeleniumWebDriverHelper seleniumWebDriverHelper,
+      WebDriverWaitFactory webDriverWaitFactory) {
     this.seleniumWebDriver = seleniumWebDriver;
+    this.seleniumWebDriverHelper = seleniumWebDriverHelper;
+    this.webDriverWaitFactory = webDriverWaitFactory;
     PageFactory.initElements(seleniumWebDriver, this);
   }
 
   private interface Locators {
     String MAVEN_PLUGIN_STATUS_BAR_INFO_XPATH =
         "//div[@id='gwt-debug-statusPanel']//div[@id='gwt-debug-loaderView-iconPanel']/following-sibling::div";
+    String MAVEN_RESOLVE_DEPENDENCIES_FORM = "//table[@title='Resolving dependencies']";
   }
 
   @FindBy(xpath = Locators.MAVEN_PLUGIN_STATUS_BAR_INFO_XPATH)
   WebElement infoPanel;
 
+  @FindBy(xpath = Locators.MAVEN_RESOLVE_DEPENDENCIES_FORM)
+  WebElement resolveDependenciesForm;
+
   /**
    * wait expected text on information panel of maven plugin
    *
-   * @param expectedText
+   * @param expectedText text which should be presented
    */
   public void waitExpectedTextInInfoPanel(String expectedText) {
-    new WebDriverWait(seleniumWebDriver, REDRAW_UI_ELEMENTS_TIMEOUT_SEC)
-        .until(ExpectedConditions.textToBePresentInElement(infoPanel, expectedText));
+    seleniumWebDriverHelper.waitTextContains(infoPanel, expectedText);
+  }
+
+  /**
+   * wait expected text on information panel of maven plugin
+   *
+   * @param expectedText text which should be presented
+   * @param expectedTimeout timeout defined by user
+   */
+  public void waitExpectedTextInInfoPanel(String expectedText, int expectedTimeout) {
+    seleniumWebDriverHelper.waitTextContains(infoPanel, expectedText, expectedTimeout);
   }
 
   /** wait some text into information panel of maven plugin */
-  public void waitInfPanelIsNotEmpty() {
-    new WebDriverWait(seleniumWebDriver, REDRAW_UI_ELEMENTS_TIMEOUT_SEC)
-        .until(
-            new ExpectedCondition<Boolean>() {
-              @Override
-              public Boolean apply(WebDriver input) {
-                return !infoPanel.getText().isEmpty();
-              }
-            });
+  public void waitInfoPanelIsNotEmpty() {
+    webDriverWaitFactory
+        .get(ELEMENT_TIMEOUT_SEC)
+        .until((ExpectedCondition<Boolean>) driver -> !getVisibleTextFromInfoPanel().isEmpty());
   }
 
   /**
@@ -72,17 +86,13 @@ public class MavenPluginStatusBar {
    * @return visible text from the widget
    */
   public String getVisibleTextFromInfoPanel() {
-    return new WebDriverWait(seleniumWebDriver, REDRAW_UI_ELEMENTS_TIMEOUT_SEC)
-        .until(ExpectedConditions.visibilityOf(infoPanel))
-        .getText();
+    return seleniumWebDriverHelper.waitVisibilityAndGetText(infoPanel);
   }
 
   /** wait closing of the info panel Maven plugin widget */
   public void waitClosingInfoPanel() {
-    new WebDriverWait(seleniumWebDriver, EXPECTED_MESS_IN_CONSOLE_SEC)
-        .until(
-            ExpectedConditions.invisibilityOfElementLocated(
-                By.xpath(Locators.MAVEN_PLUGIN_STATUS_BAR_INFO_XPATH)));
+    seleniumWebDriverHelper.waitInvisibility(
+        By.xpath(Locators.MAVEN_PLUGIN_STATUS_BAR_INFO_XPATH), EXPECTED_MESS_IN_CONSOLE_SEC);
   }
 
   /**
@@ -91,9 +101,32 @@ public class MavenPluginStatusBar {
    * @param expectedTimeout timeout defined by user
    */
   public void waitClosingInfoPanel(final int expectedTimeout) {
-    new WebDriverWait(seleniumWebDriver, expectedTimeout)
-        .until(
-            ExpectedConditions.invisibilityOfElementLocated(
-                By.xpath(Locators.MAVEN_PLUGIN_STATUS_BAR_INFO_XPATH)));
+    seleniumWebDriverHelper.waitInvisibility(
+        By.xpath(Locators.MAVEN_PLUGIN_STATUS_BAR_INFO_XPATH), expectedTimeout);
+  }
+
+  /** click on the info panel maven plagin */
+  public void clickOnInfoPanel() {
+    seleniumWebDriverHelper.waitAndClick(infoPanel);
+  }
+
+  /** wait open the resolve dependencies form */
+  public void waitResolveDependenciesFormToOpen() {
+    seleniumWebDriverHelper.waitVisibility(resolveDependenciesForm);
+  }
+
+  /** wait close the resolve dependencies form */
+  public void waitResolveDependenciesFormToClose() {
+    seleniumWebDriverHelper.waitInvisibility(resolveDependenciesForm);
+  }
+
+  /**
+   * close the resolve dependencies form
+   *
+   * @param text is the text for sending
+   */
+  public void closeResolveDependenciesFormByKeys(String text) {
+    seleniumWebDriverHelper.sendKeys(text);
+    waitResolveDependenciesFormToClose();
   }
 }
---
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/stack/ImportAndValidateEclipseCheProjectTest.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.selenium.stack;
+
+import static org.eclipse.che.commons.lang.NameGenerator.generate;
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Workspace.IMPORT_PROJECT;
+import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Workspace.WORKSPACE;
+import static org.eclipse.che.selenium.pageobject.CodenvyEditor.MarkerLocator.ERROR;
+import static org.eclipse.che.selenium.pageobject.Wizard.TypeProject.MAVEN;
+import static org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Stack.ECLIPSE_CHE;
+import static org.openqa.selenium.Keys.DELETE;
+import static org.openqa.selenium.Keys.ESCAPE;
+import static org.testng.Assert.fail;
+
+import com.google.inject.Inject;
+import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
+import org.eclipse.che.selenium.core.user.DefaultTestUser;
+import org.eclipse.che.selenium.pageobject.CodenvyEditor;
+import org.eclipse.che.selenium.pageobject.Ide;
+import org.eclipse.che.selenium.pageobject.ImportProjectFromLocation;
+import org.eclipse.che.selenium.pageobject.InformationDialog;
+import org.eclipse.che.selenium.pageobject.Loader;
+import org.eclipse.che.selenium.pageobject.MavenPluginStatusBar;
+import org.eclipse.che.selenium.pageobject.Menu;
+import org.eclipse.che.selenium.pageobject.ProjectExplorer;
+import org.eclipse.che.selenium.pageobject.Wizard;
+import org.eclipse.che.selenium.pageobject.dashboard.CreateWorkspaceHelper;
+import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/** @author Aleksandr Shmaraev */
+public class ImportAndValidateEclipseCheProjectTest {
+
+  private static final String WORKSPACE_NAME = generate("EclipseCheWs", 4);
+  private static final String PROJECT_NAME = "eclipse-che";
+  private static final String ECLIPSE_CHE_PROJECT_URL = "https://github.com/eclipse/che.git";
+  private static final String PATH_TO_JAVA_FILE =
+      PROJECT_NAME
+          + "/selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/CodenvyEditor.java";
+  private static final String PATH_TO_POM_FILE = PROJECT_NAME + "/dashboard/pom.xml";
+  private static final String PATH_TO_TS_FILE = PROJECT_NAME + "/dashboard/src/app/index.module.ts";
+
+  @Inject private Ide ide;
+  @Inject private Menu menu;
+  @Inject private ImportProjectFromLocation importProject;
+  @Inject private Loader loader;
+  @Inject private ProjectExplorer projectExplorer;
+  @Inject private Wizard projectWizard;
+  @Inject private MavenPluginStatusBar mavenPluginStatusBar;
+  @Inject private InformationDialog informationDialog;
+  @Inject private CodenvyEditor editor;
+  @Inject private Dashboard dashboard;
+  @Inject private CreateWorkspaceHelper createWorkspaceHelper;
+  @Inject private TestWorkspaceServiceClient workspaceServiceClient;
+  @Inject private DefaultTestUser defaultTestUser;
+
+  @BeforeClass
+  public void prepare() {
+    dashboard.open();
+    createWorkspaceHelper.createWorkspaceFromStackWithoutProject(ECLIPSE_CHE, WORKSPACE_NAME);
+
+    ide.switchToIdeAndWaitWorkspaceIsReadyToUse();
+    ide.waitOpenedWorkspaceIsReadyToUse();
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    workspaceServiceClient.delete(WORKSPACE_NAME, defaultTestUser.getName());
+  }
+
+  @Test
+  public void checkImportAndResolveDependenciesEclipceCheProject() {
+    final int timeoutToOpenInfoPanel = 120;
+    final int timeoutToClosingInfoPanel = 2900;
+
+    // import the eclipse-che project
+    projectExplorer.waitProjectExplorer();
+    menu.runCommand(WORKSPACE, IMPORT_PROJECT);
+    importProject.waitAndTypeImporterAsGitInfo(ECLIPSE_CHE_PROJECT_URL, PROJECT_NAME);
+
+    // config the project
+    projectWizard.waitCreateProjectWizardForm();
+    projectWizard.selectTypeProject(MAVEN);
+    projectWizard.clickSaveButton();
+    loader.waitOnClosed();
+
+    // TODO it is the workaround, delete it after resolve the issue
+    // TODO https://github.com/eclipse/che/issues/10515
+    closeErrorDialog();
+
+    projectWizard.waitCreateProjectWizardFormIsClosed();
+    loader.waitOnClosed();
+
+    // expand the project
+    projectExplorer.waitItem(PROJECT_NAME);
+    projectExplorer.openItemByPath(PROJECT_NAME);
+    loader.waitOnClosed();
+
+    // then open files
+    // open a java file
+    quickRevealToItemWithJavaScriptAndOpenFile(PATH_TO_JAVA_FILE);
+    editor.waitActive();
+
+    // open a xml file
+    quickRevealToItemWithJavaScriptAndOpenFile(PATH_TO_POM_FILE);
+    editor.waitActive();
+
+    // open a ts file
+    quickRevealToItemWithJavaScriptAndOpenFile(PATH_TO_TS_FILE);
+    editor.waitActive();
+
+    // open the resolving dependencies form
+    loader.waitOnClosed();
+    mavenPluginStatusBar.waitExpectedTextInInfoPanel("Resolving project:", timeoutToOpenInfoPanel);
+    mavenPluginStatusBar.clickOnInfoPanel();
+
+    // should close the resolve dependencies form by Esc
+    mavenPluginStatusBar.waitResolveDependenciesFormToOpen();
+    mavenPluginStatusBar.closeResolveDependenciesFormByKeys(ESCAPE.toString());
+
+    // then open it again
+    mavenPluginStatusBar.clickOnInfoPanel();
+    mavenPluginStatusBar.waitResolveDependenciesFormToOpen();
+
+    // wait while dependencies are resolved
+    mavenPluginStatusBar.waitClosingInfoPanel(timeoutToClosingInfoPanel);
+    mavenPluginStatusBar.waitResolveDependenciesFormToClose();
+
+    // wait the project and the files
+    projectExplorer.waitItem(PROJECT_NAME);
+    editor.waitTabIsPresent("CodenvyEditor");
+    editor.waitTabIsPresent("che-dashboard-war");
+    editor.waitTabIsPresent("index.module.ts");
+  }
+
+  @Test(priority = 1)
+  public void checkErrorMarkersInEditor() {
+    // check an error marker in the pom.xml file
+    projectExplorer.openItemByPath(PATH_TO_POM_FILE);
+    editor.waitActive();
+    editor.waitAllMarkersInvisibility(ERROR);
+    editor.typeTextIntoEditor("q");
+    editor.goToPosition(1, 1);
+    editor.waitMarkerInPosition(ERROR, 1);
+    editor.typeTextIntoEditor(DELETE.toString());
+    editor.waitMarkerInvisibility(ERROR, 1);
+
+    // check error marker in the java file
+    projectExplorer.openItemByPath(PATH_TO_JAVA_FILE);
+    editor.waitActive();
+    editor.waitAllMarkersInvisibility(ERROR);
+    editor.setCursorToLine(12);
+    editor.typeTextIntoEditor("q");
+    editor.goToPosition(12, 1);
+    editor.waitMarkerInPosition(ERROR, 12);
+    editor.typeTextIntoEditor(DELETE.toString());
+    editor.waitMarkerInvisibility(ERROR, 12);
+
+    // check error marker in the ts file
+    projectExplorer.openItemByPath(PATH_TO_TS_FILE);
+    editor.waitActive();
+    editor.typeTextIntoEditor("q");
+    editor.waitMarkerInPosition(ERROR, 1);
+  }
+
+  private void closeErrorDialog() {
+    // if the error dialog is appeared more 5 times, the test will be failed
+    int counterErrorDialog = 0;
+
+    while (informationDialog.isFormOpened()) {
+      counterErrorDialog++;
+
+      if (counterErrorDialog > 5) {
+
+        fail(
+            "The unexpected error information dialog is appeared more than "
+                + counterErrorDialog
+                + " times");
+      }
+
+      informationDialog.waitFormToOpen();
+      informationDialog.clickOkBtn();
+      projectWizard.clickSaveButton();
+      loader.waitOnClosed();
+    }
+  }
+
+  private void quickRevealToItemWithJavaScriptAndOpenFile(String pathToItem) {
+    projectExplorer.quickRevealToItemWithJavaScript(pathToItem);
+    projectExplorer.openItemByPath(pathToItem);
+  }
+}
