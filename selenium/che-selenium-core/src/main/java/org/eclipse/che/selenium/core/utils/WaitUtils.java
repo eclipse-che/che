effec47d3eb8fcfe2cc83f33d5228a02bc245f96
Ihor Okhrimenko <iokhrime@redhat.com>
2018-07-30 12:55:46 +0300

    Initial commit

--- selenium/che-selenium-core/src/main/java/org/eclipse/che/selenium/core/utils/WaitUtils.java
+++ selenium/che-selenium-core/src/main/java/org/eclipse/che/selenium/core/utils/WaitUtils.java
@@ -10,7 +10,10 @@
  */
 package org.eclipse.che.selenium.core.utils;
 
+import static org.eclipse.che.selenium.core.constant.TestTimeoutsConstants.LOAD_PAGE_TIMEOUT_SEC;
+
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.TimeoutException;
 
 /** @author Mykola Morhun */
 public class WaitUtils {
@@ -44,5 +47,30 @@ public class WaitUtils {
       // Considering the above, we must stop this thread here immediately.
       throw new RuntimeException(e);
     }
+  }
+
+  public static void waitSuccessCondition(BooleanCondition condition, int timeout)
+      throws InterruptedException {
+    for (int i = 1; i <= timeout; i++) {
+      if (condition.execution()) {
+        break;
+      }
+
+      if (i == timeout) {
+        throw new TimeoutException("The condition has not being in \"true\" state during timeout");
+      }
+
+      Thread.sleep(1000);
+    }
+  }
+
+  public static void waitSuccessCondition(BooleanCondition condition) throws InterruptedException {
+    final int defaultTimeout = LOAD_PAGE_TIMEOUT_SEC;
+    waitSuccessCondition(condition, defaultTimeout);
+  }
+
+  @FunctionalInterface
+  public interface BooleanCondition {
+    boolean execution();
   }
 }
