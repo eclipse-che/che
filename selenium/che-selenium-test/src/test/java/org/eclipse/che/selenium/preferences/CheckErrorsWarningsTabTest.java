83a2a5c1375e6c121ae2b6cc87723a087e476c66
Valeriy Svydenko <vsvydenk@redhat.com>
2018-07-31 19:10:37 +0200

    Add test project to test Java preferences
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/preferences/CheckErrorsWarningsTabTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/preferences/CheckErrorsWarningsTabTest.java
@@ -17,7 +17,6 @@ import static org.eclipse.che.selenium.pageobject.CodenvyEditor.MarkerLocator.ER
 import static org.eclipse.che.selenium.pageobject.CodenvyEditor.MarkerLocator.WARNING;
 import static org.eclipse.che.selenium.pageobject.CodenvyEditor.MarkerLocator.WARNING_OVERVIEW;
 
-import com.google.common.base.Joiner;
 import com.google.inject.Inject;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -47,8 +46,6 @@ import org.testng.annotations.Test;
 public class CheckErrorsWarningsTabTest {
   private static final String PROJECT_NAME =
       NameGenerator.generate(CheckErrorsWarningsTabTest.class.getSimpleName(), 4);
-  private static final String PATH_TO_CLASS_IN_SPRING_PACKAGE =
-      PROJECT_NAME + "/src/main/java/org/eclipse/qa/examples/AppController.java";
   @Inject private TestWorkspace workspace;
   @Inject private Ide ide;
   @Inject private ProjectExplorer projectExplorer;
@@ -62,7 +59,7 @@ public class CheckErrorsWarningsTabTest {
 
   @BeforeClass
   public void setUp() throws Exception {
-    URL resource = getClass().getResource("/projects/default-spring-project");
+    URL resource = getClass().getResource("/projects/prefs-spring-project");
     testProjectServiceClient.importProject(
         workspace.getId(),
         Paths.get(resource.toURI()),
@@ -75,7 +72,6 @@ public class CheckErrorsWarningsTabTest {
   @Test
   public void errorsWarningTest() throws Exception {
     List<String> expectedErrorsWarningsList = getTextInListFromFile("errors-warnings");
-    String embedCode = getTextInStringFromFile("embed-code");
     projectExplorer.waitItem(PROJECT_NAME);
     notificationsPopupPanel.waitProgressPopupPanelClose();
     projectExplorer.quickExpandWithJavaScript();
@@ -83,10 +79,6 @@ public class CheckErrorsWarningsTabTest {
     editor.waitActive();
     loader.waitOnClosed();
     editor.waitActive();
-    editor.deleteAllContent();
-    editor.typeTextIntoEditorWithoutDelayForSaving(embedCode);
-    loader.waitOnClosed();
-    editor.removeLineAndAllAfterIt(104);
 
     menu.runCommand(TestMenuCommandsConstants.Profile.PROFILE_MENU, PREFERENCES);
     preferences.waitPreferencesForm();
@@ -122,10 +114,8 @@ public class CheckErrorsWarningsTabTest {
     preferences.selectDroppedMenuByName(Preferences.DropDownJavaCompilerMenu.ERRORS_WARNINGS);
     preferences.setAllSettingsInErrorWaitingWidget(valueOfRadioButton);
     preferences.clickOnOkBtn();
-    preferences.closeForm();
     loader.waitOnClosed();
-    projectExplorer.waitItem(PATH_TO_CLASS_IN_SPRING_PACKAGE);
-    projectExplorer.openItemByPath(PATH_TO_CLASS_IN_SPRING_PACKAGE);
+    preferences.closeForm();
     loader.waitOnClosed();
     editor.setCursorToLine(84);
     editor.typeTextIntoEditor(Keys.END.toString());
@@ -135,13 +125,6 @@ public class CheckErrorsWarningsTabTest {
     editor.typeTextIntoEditor(Keys.DELETE.toString());
     editor.typeTextIntoEditor(Keys.DELETE.toString());
     WaitUtils.sleepQuietly(DEFAULT_TIMEOUT);
-  }
-
-  private String getTextInStringFromFile(String path) throws Exception {
-    URL pathInUrl = CheckErrorsWarningsTabTest.class.getResource(path);
-    List<String> textFromFile =
-        Files.readAllLines(Paths.get(pathInUrl.toURI()), Charset.forName("UTF-8"));
-    return Joiner.on('\n').join(textFromFile);
   }
 
   private List<String> getTextInListFromFile(String path) throws Exception {
