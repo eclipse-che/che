1d444e4ce28e4942e759d1850972b961a8291fd3
Max Shaposhnik <mshaposh@redhat.com>
2018-08-02 12:14:58 +0300

    fixup! Fix some proerties & generation order

--- infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/server/secure/jwtproxy/JwtProxyConfigBuilderTest.java
+++ infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/server/secure/jwtproxy/JwtProxyConfigBuilderTest.java
@@ -44,7 +44,6 @@ public class JwtProxyConfigBuilderTest {
 
     // when
     String jwtProxyConfigYaml = jwtProxyConfigBuilder.build();
-    System.out.println(jwtProxyConfigYaml);
     // then
     assertEquals(
         jwtProxyConfigYaml,
