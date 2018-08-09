f236f9b9e0a6ff34f8c8d8f6855e72ee9e17d177
Anatoliy Bazko <abazko@redhat.com>
2018-08-09 08:10:28 +0000

    Update project config when event is fired (#10677)
    * Update project config when event is fired
    
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/service/ExecuteClientCommandProcessor.java
+++ plugins/plugin-languageserver/che-plugin-languageserver-ide/src/main/java/org/eclipse/che/plugin/languageserver/ide/service/ExecuteClientCommandProcessor.java
@@ -13,13 +13,17 @@ package org.eclipse.che.plugin.languageserver.ide.service;
 
 import static org.eclipse.che.jdt.ls.extension.api.Commands.CLIENT_UPDATE_PROJECT;
 import static org.eclipse.che.jdt.ls.extension.api.Commands.CLIENT_UPDATE_PROJECTS_CLASSPATH;
+import static org.eclipse.che.jdt.ls.extension.api.Commands.CLIENT_UPDATE_PROJECT_CONFIG;
 
 import com.google.gwt.json.client.JSONString;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.web.bindery.event.shared.EventBus;
+import java.util.List;
 import org.eclipse.che.ide.api.app.AppContext;
+import org.eclipse.che.ide.project.ProjectServiceClient;
 import org.eclipse.che.ide.project.node.ProjectClasspathChangedEvent;
+import org.eclipse.che.ide.resource.Path;
 import org.eclipse.lsp4j.ExecuteCommandParams;
 
 /**
@@ -32,11 +36,14 @@ import org.eclipse.lsp4j.ExecuteCommandParams;
 public class ExecuteClientCommandProcessor {
   private EventBus eventBus;
   private AppContext appContext;
+  private final ProjectServiceClient projectService;
 
   @Inject
-  public ExecuteClientCommandProcessor(EventBus eventBus, AppContext appContext) {
+  public ExecuteClientCommandProcessor(
+      EventBus eventBus, AppContext appContext, ProjectServiceClient projectService) {
     this.eventBus = eventBus;
     this.appContext = appContext;
+    this.projectService = projectService;
   }
 
   public void execute(ExecuteCommandParams params) {
@@ -48,6 +55,9 @@ public class ExecuteClientCommandProcessor {
         break;
       case CLIENT_UPDATE_PROJECT:
         updateProject(stringValue(params.getArguments()));
+        break;
+      case CLIENT_UPDATE_PROJECT_CONFIG:
+        updateProjectConfig(stringValue(params.getArguments()));
         break;
       default:
         break;
@@ -66,7 +76,31 @@ public class ExecuteClientCommandProcessor {
             });
   }
 
+  private void updateProjectConfig(String project) {
+    appContext
+        .getWorkspaceRoot()
+        .getContainer(project)
+        .then(
+            container -> {
+              projectService
+                  .getProject(Path.valueOf(project))
+                  .then(
+                      projectConfigDto -> {
+                        projectService
+                            .updateProject(projectConfigDto)
+                            .then(
+                                arg -> {
+                                  if (container.isPresent()) {
+                                    container.get().synchronize();
+                                  }
+                                });
+                      });
+            });
+  }
+
   private String stringValue(Object value) {
-    return value instanceof JSONString ? ((JSONString) value).stringValue() : String.valueOf(value);
+    return value instanceof JSONString
+        ? ((JSONString) value).stringValue()
+        : (value instanceof List ? stringValue(((List) value).get(0)) : String.valueOf(value));
   }
 }
