9848aec31918ffea66155a5e56d6511d3120d706
Mykhailo Kuznietsov <mkuznets@redhat.com>
2018-08-16 11:09:04 +0300

    Add more licenses

--- wsagent/che-core-api-languageserver-maven-plugin/src/main/java/org/eclipse/che/api/languageserver/generator/EitherUtil.java
+++ wsagent/che-core-api-languageserver-maven-plugin/src/main/java/org/eclipse/che/api/languageserver/generator/EitherUtil.java
@@ -1,10 +1,10 @@
-/**
- * ***************************************************************************** Copyright (c) 2016
- * TypeFox GmbH (http://www.typefox.io) and others. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * *****************************************************************************
+/*
+ * Copyright (c) 2016 TypeFox GmbH (http://www.typefox.io) and others.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.che.api.languageserver.generator;
 
