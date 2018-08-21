075d4e1c463708ec74acd9bd2f02bbc1ab402a1c
Thomas Mäder <tsmaeder@users.noreply.github.com>
2018-08-21 16:55:56 +0200

    Adapt tests to correct observed behaviour (#10824)
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/CodenvyEditor.java
+++ selenium/che-selenium-test/src/main/java/org/eclipse/che/selenium/pageobject/CodenvyEditor.java
@@ -590,7 +590,9 @@ public class CodenvyEditor {
    * @param text text which should be typed
    */
   public void typeTextIntoEditor(String text) {
+    loader.waitOnClosed();
     seleniumWebDriverHelper.sendKeys(text);
+    loader.waitOnClosed();
   }
 
   /**
@@ -701,6 +703,7 @@ public class CodenvyEditor {
 
   /** Launches code assistant by "ctrl" + "space" keys pressing. */
   public void launchAutocomplete() {
+    loader.waitOnClosed();
     Actions action = actionsFactory.createAction(seleniumWebDriver);
     action.keyDown(CONTROL).perform();
     typeTextIntoEditor(SPACE.toString());
@@ -947,7 +950,7 @@ public class CodenvyEditor {
    */
   public void selectAutocompleteProposal(String item) {
     seleniumWebDriverHelper.waitAndClick(
-        By.xpath(format(AUTOCOMPLETE_CONTAINER + "/li/span[.='%s']", item)));
+        By.xpath(format(AUTOCOMPLETE_CONTAINER + "/li/span[text()='%s']", item)));
   }
 
   /**
@@ -2117,11 +2120,8 @@ public class CodenvyEditor {
         By.xpath(format(Locators.TEXT_TO_MOVE_CURSOR_XPATH, text)));
   }
 
-  public String getProposalDocumentationHTML() {
-    return seleniumWebDriverHelper
-        .waitVisibility(proposalDoc)
-        .findElement(By.tagName("div"))
-        .getAttribute("innerHTML");
+  public void checkProposalDocumentation(String expectedText) {
+    seleniumWebDriverHelper.waitTextContains(proposalDoc, expectedText);
   }
 
   public void launchCommentCodeFeature() {
