3d9904e7850e417d12d4c679f8e6a136601d3dfb
Igor Vinokur <ivinokur@redhat.com>
2018-08-23 15:42:25 +0300

    CHE-10850: Add notification about stop workspcae by idle timeout

--- wsmaster/che-core-api-workspace-shared/src/main/java/org/eclipse/che/api/workspace/shared/Constants.java
+++ wsmaster/che-core-api-workspace-shared/src/main/java/org/eclipse/che/api/workspace/shared/Constants.java
@@ -26,6 +26,7 @@ public final class Constants {
   public static final String LINK_REL_ENVIRONMENT_STATUS_CHANNEL = "environment/statusChannel";
 
   public static final String WORKSPACE_STOPPED_BY = "stopped_by";
+  public static final String WORKSPACE_STOP_REASON = "stop_reason";
 
   public static final String LINK_REL_CREATE_STACK = "create stack";
   public static final String LINK_REL_UPDATE_STACK = "update stack";
