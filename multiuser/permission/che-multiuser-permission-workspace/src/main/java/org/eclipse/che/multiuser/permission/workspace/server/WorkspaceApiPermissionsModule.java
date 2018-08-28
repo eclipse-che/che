f8e0440e87f8d4b31518504310eb5c6ea56f345e
Sergii Leshchenko <sleshche@redhat.com>
2018-08-28 16:19:21 +0300

    CHE-10861 Add permissions check for workspaces related remote subscriptions

--- multiuser/permission/che-multiuser-permission-workspace/src/main/java/org/eclipse/che/multiuser/permission/workspace/server/WorkspaceApiPermissionsModule.java
+++ multiuser/permission/che-multiuser-permission-workspace/src/main/java/org/eclipse/che/multiuser/permission/workspace/server/WorkspaceApiPermissionsModule.java
@@ -25,17 +25,20 @@ import org.eclipse.che.multiuser.permission.workspace.server.filters.PublicPermi
 import org.eclipse.che.multiuser.permission.workspace.server.filters.StackDomainSetPermissionsChecker;
 import org.eclipse.che.multiuser.permission.workspace.server.filters.StackPermissionsFilter;
 import org.eclipse.che.multiuser.permission.workspace.server.filters.WorkspacePermissionsFilter;
+import org.eclipse.che.multiuser.permission.workspace.server.filters.WorkspaceRemoteSubscriptionPermissionFilter;
 import org.eclipse.che.multiuser.permission.workspace.server.stack.MultiuserStackLoader;
 import org.eclipse.che.multiuser.permission.workspace.server.stack.StackCreatorPermissionsProvider;
 import org.eclipse.che.multiuser.permission.workspace.server.stack.StackDomain;
 
 /** @author Sergii Leschenko */
 public class WorkspaceApiPermissionsModule extends AbstractModule {
+
   @Override
   protected void configure() {
     bind(WorkspacePermissionsFilter.class);
     bind(StackPermissionsFilter.class);
     bind(InstallerServicePermissionFilter.class).asEagerSingleton();
+    bind(WorkspaceRemoteSubscriptionPermissionFilter.class).asEagerSingleton();
 
     bind(WorkspaceCreatorPermissionsProvider.class).asEagerSingleton();
     bind(StackCreatorPermissionsProvider.class).asEagerSingleton();
