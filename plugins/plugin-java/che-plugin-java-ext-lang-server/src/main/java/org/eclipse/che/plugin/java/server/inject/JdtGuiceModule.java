a82cab9a71bfcdbe859ea5d852995d9384ee0235
Thomas Mäder <tsmaeder@users.noreply.github.com>
2018-08-28 10:17:34 +0200

    Removed unused eclipse bundles (#10907)
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- plugins/plugin-java/che-plugin-java-ext-lang-server/src/main/java/org/eclipse/che/plugin/java/server/inject/JdtGuiceModule.java
+++ plugins/plugin-java/che-plugin-java-ext-lang-server/src/main/java/org/eclipse/che/plugin/java/server/inject/JdtGuiceModule.java
@@ -12,13 +12,8 @@
 package org.eclipse.che.plugin.java.server.inject;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.google.inject.name.Named;
-import java.nio.file.Paths;
 import org.eclipse.che.inject.DynaModule;
 import org.eclipse.che.plugin.java.server.rest.JavaFormatterService;
-import org.eclipse.core.resources.ResourcesPlugin;
 
 /** @author Evgen Vidolob */
 @DynaModule
@@ -27,20 +22,5 @@ public class JdtGuiceModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(JavaFormatterService.class);
-    bind(ResourcesPlugin.class).asEagerSingleton();
-  }
-
-  @Provides
-  @Named("che.jdt.settings.dir")
-  @Singleton
-  protected String provideSettings(@Named("che.workspace.metadata") String wsMetadata) {
-    return Paths.get(System.getProperty("user.home"), wsMetadata, "settings").toString();
-  }
-
-  @Provides
-  @Named("che.jdt.workspace.index.dir")
-  @Singleton
-  protected String provideIndex(@Named("che.workspace.metadata") String wsMetadata) {
-    return Paths.get(System.getProperty("user.home"), wsMetadata, "index").toString();
   }
 }
