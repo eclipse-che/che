8f61c409dc774190826b1898f4c288c881768e18
Sergii Kabashniuk <skabashniuk@redhat.com>
2018-08-30 14:28:17 +0300

    fixup! fixup! Rework exception handling

--- infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/server/secure/jwtproxy/JwtProxyProvisioner.java
+++ infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/server/secure/jwtproxy/JwtProxyProvisioner.java
@@ -43,7 +43,6 @@ import java.util.Map;
 import java.util.Set;
 import javax.inject.Inject;
 import javax.inject.Named;
-
 import org.eclipse.che.api.core.ConflictException;
 import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.core.model.workspace.config.MachineConfig;
@@ -214,7 +213,7 @@ public class JwtProxyProvisioner {
       KeyPair keyPair;
       try {
         keyPair = signatureKeyManager.getKeyPair(identity.getWorkspaceId());
-      } catch (ServerException| ConflictException e) {
+      } catch (ServerException | ConflictException e) {
         throw new InternalInfrastructureException(
             "Signature key pair for machine authentication cannot be retrieved. Reason: "
                 + e.getMessage());
