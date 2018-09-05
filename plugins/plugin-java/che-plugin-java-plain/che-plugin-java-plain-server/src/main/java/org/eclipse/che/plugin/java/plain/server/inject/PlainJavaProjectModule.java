b121a593eda424debe00dc09af1b77c7c7156a3f
Victor V. Rubezhny <vrubezhny@redhat.com>
2018-09-05 22:11:48 +0200

    New folders do not register in the jdt.ls workspace #10115 (#10893)
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>

--- plugins/plugin-java/che-plugin-java-plain/che-plugin-java-plain-server/src/main/java/org/eclipse/che/plugin/java/plain/server/inject/PlainJavaProjectModule.java
+++ plugins/plugin-java/che-plugin-java-plain/che-plugin-java-plain-server/src/main/java/org/eclipse/che/plugin/java/plain/server/inject/PlainJavaProjectModule.java
@@ -32,5 +32,6 @@ public class PlainJavaProjectModule extends AbstractModule {
     newSetBinder(binder(), ProjectHandler.class).addBinding().to(PlainJavaInitHandler.class);
 
     bind(ClasspathUpdaterService.class);
+    bind(PlainJavaProjectSourceFolderWatcher.class).asEagerSingleton();
   }
 }
