535c53ede65b84d50658af93988d15021c02516b
Max Shaposhnik <mshaposh@redhat.com>
2018-09-07 14:40:56 +0300

    Add migration to fix signature keys incosistency

---
+++ wsmaster/che-core-sql-schema/src/main/resources/che-schema/6.11.0/1__add_signature_key_constraints.sql
@@ -0,0 +1,41 @@
+--
+-- Copyright (c) 2012-2018 Red Hat, Inc.
+-- This program and the accompanying materials are made
+-- available under the terms of the Eclipse Public License 2.0
+-- which is available at https://www.eclipse.org/legal/epl-2.0/
+--
+-- SPDX-License-Identifier: EPL-2.0
+--
+-- Contributors:
+--   Red Hat, Inc. - initial API and implementation
+--
+
+
+-- remove key pair records which linked to non-existing keys
+DELETE FROM che_sign_key_pair kp
+WHERE NOT EXISTS (
+   SELECT id
+   FROM   che_sign_key k
+   WHERE  k.id = kp.private_key
+   )
+OR NOT EXISTS (
+   SELECT id
+   FROM   che_sign_key k
+   WHERE  k.id = kp.public_key
+   );
+
+-- remove duplicated keys id if any
+DELETE FROM che_sign_key_pair t1
+WHERE (
+      SELECT count(*) FROM che_sign_key_pair t2
+      WHERE t1.private_key  = t2.private_key
+      OR t1.private_key = t2.public_key
+      ) > 1;
+
+
+-- add pair uniqueness constraint
+CREATE UNIQUE INDEX index_sign_public_private_key_id ON che_sign_key_pair (public_key, private_key);
+
+-- add keys table constraints
+ALTER TABLE che_sign_key_pair ADD CONSTRAINT fk_sign_public_key_id FOREIGN KEY (public_key) REFERENCES che_sign_key (id);
+ALTER TABLE che_sign_key_pair ADD CONSTRAINT fk_sign_private_key_id FOREIGN KEY (private_key) REFERENCES che_sign_key (id);
