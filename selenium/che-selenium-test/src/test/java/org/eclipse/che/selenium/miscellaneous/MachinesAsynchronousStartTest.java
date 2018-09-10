6202bf81d34400e0cd744121334e2c6bad59bc79
Ihor Okhrimenko <iokhrime@redhat.com>
2018-09-10 17:24:08 +0300

    Rework test according to new expected behavior

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/miscellaneous/MachinesAsynchronousStartTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/miscellaneous/MachinesAsynchronousStartTest.java
@@ -12,33 +12,48 @@
 package org.eclipse.che.selenium.miscellaneous;
 
 import static java.lang.String.format;
-import static java.util.Arrays.asList;
 import static org.eclipse.che.selenium.core.TestGroup.OPENSHIFT;
-import static org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces.Status.RUNNING;
-import static org.testng.Assert.fail;
+import static org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails.ActionButton.SAVE_BUTTON;
+import static org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails.WorkspaceDetailsTab.MACHINES;
 
 import com.google.inject.Inject;
-import java.util.List;
+import org.eclipse.che.api.core.model.workspace.Workspace;
+import org.eclipse.che.commons.lang.NameGenerator;
 import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
 import org.eclipse.che.selenium.core.executor.OpenShiftCliCommandExecutor;
 import org.eclipse.che.selenium.core.user.DefaultTestUser;
+import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
 import org.eclipse.che.selenium.core.webdriver.WebDriverWaitFactory;
 import org.eclipse.che.selenium.core.workspace.TestWorkspace;
 import org.eclipse.che.selenium.core.workspace.TestWorkspaceProvider;
-import org.eclipse.che.selenium.core.workspace.WorkspaceTemplate;
 import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
+import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.EditMachineForm;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetails;
+import org.eclipse.che.selenium.pageobject.dashboard.workspaces.WorkspaceDetailsMachines;
 import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 @Test(groups = OPENSHIFT)
 public class MachinesAsynchronousStartTest {
-  private static final String GET_POD_NAME_COMMAND_COMMAND_TEMPLATE =
-      "get pod --no-headers=true -l che.workspace_id=%s | awk '{print $1}'";
+  private static final String WORKSPACE_NAME = NameGenerator.generate("test-workspace", 4);
+  private static final String MACHINE_NAME = "dev-machine";
+  private static final String IMAGE_NAME = "eclipse/ubuntu_jdk8";
+  private static final String IMAGE_NAME_SUFFIX = NameGenerator.generate("", 4);
+  private static final String NOT_EXISTED_IMAGE_NAME = IMAGE_NAME + IMAGE_NAME_SUFFIX;
+  private static final String SUCCESS_NOTIFICATION_TEST = "Workspace updated.";
   private static final String GET_POD_RELATED_EVENTS_COMMAND_TEMPLATE =
-      "get events --no-headers=true --field-selector involvedObject.name=%s | awk '{print $7 \" \" $8}'";
+      // "get event --no-headers=true | grep %s | awk '{print $7 \" \" $8}'";
+      "get event --no-headers=true | grep %s ";
+  private static final String EXPECTED_ERROR_NOTIFICATION_TEXT =
+      format(
+          "Unrecoverable event occurred: 'Failed', 'Failed to pull image \"%s\": "
+              + "rpc error: code = Unknown desc = Error response from daemon: pull "
+              + "access denied for %s, repository does not exist or may require 'docker login''",
+          NOT_EXISTED_IMAGE_NAME, NOT_EXISTED_IMAGE_NAME);
 
   @Inject private Dashboard dashboard;
   @Inject private Workspaces workspaces;
@@ -47,6 +62,11 @@ public class MachinesAsynchronousStartTest {
   @Inject private DefaultTestUser defaultTestUser;
   @Inject private OpenShiftCliCommandExecutor openShiftCliCommandExecutor;
   @Inject private WebDriverWaitFactory webDriverWaitFactory;
+  @Inject private NewWorkspace newWorkspace;
+  @Inject private WorkspaceDetails workspaceDetails;
+  @Inject private WorkspaceDetailsMachines workspaceDetailsMachines;
+  @Inject private EditMachineForm editMachineForm;
+  @Inject private SeleniumWebDriverHelper seleniumWebDriverHelper;
 
   private TestWorkspace brokenWorkspace;
 
@@ -55,59 +75,55 @@ public class MachinesAsynchronousStartTest {
     brokenWorkspace.delete();
   }
 
-  @Test
-  public void checkWorkspaces() throws Exception {
-    // prepare
+  @BeforeClass
+  public void setUp() {
     dashboard.open();
-
-    // create and start broken workspace
-    brokenWorkspace = createBrokenWorkspace();
-    startBrokenWorkspaceAndWaitRunningStatus();
-
-    // check that broken workspace is displayed with "Running" status
     dashboard.waitDashboardToolbarTitle();
     dashboard.selectWorkspacesItemOnDashboard();
+    workspaces.clickOnAddWorkspaceBtn();
+    newWorkspace.waitPageLoad();
+    newWorkspace.typeWorkspaceName(WORKSPACE_NAME);
+    newWorkspace.clickOnCreateButtonAndEditWorkspace();
+    workspaceDetails.waitToolbarTitleName(WORKSPACE_NAME);
+    workspaceDetails.selectTabInWorkspaceMenu(MACHINES);
+    workspaceDetailsMachines.waitMachineListItem(MACHINE_NAME);
+    workspaceDetailsMachines.clickOnEditButton(MACHINE_NAME);
+    editMachineForm.waitForm();
+    editMachineForm.clickOnRecipeForm();
+    editMachineForm.waitRecipeCursorVisibility();
+    seleniumWebDriverHelper.sendKeys(IMAGE_NAME_SUFFIX);
+    editMachineForm.waitRecipeText(NOT_EXISTED_IMAGE_NAME);
+    editMachineForm.waitSaveButtonEnabling();
+    editMachineForm.clickOnSaveButton();
+    editMachineForm.waitFormInvisibility();
+    workspaceDetailsMachines.waitImageNameInMachineListItem(MACHINE_NAME, NOT_EXISTED_IMAGE_NAME);
+    workspaceDetails.waitAllEnabled(SAVE_BUTTON);
+    workspaceDetails.clickOnSaveChangesBtn();
+    workspaceDetailsMachines.waitNotificationMessage(SUCCESS_NOTIFICATION_TEST);
+    dashboard.selectWorkspacesItemOnDashboard();
     workspaces.waitPageLoading();
-    waitWorkspaceRunningStatusOnDashboard();
+    workspaces.waitWorkspaceIsPresent(WORKSPACE_NAME);
+  }
+
+  @Test
+  public void checkWorkspace() {
+    // check behavior of the broken workspace
+    workspaces.clickOnWorkspaceStopStartButton(WORKSPACE_NAME);
+    workspaces.waitErrorNotificationContainsText(EXPECTED_ERROR_NOTIFICATION_TEXT);
 
     // check openshift events log
     waitEvent("Failed");
     waitEvent("BackOff");
   }
 
-  private void startBrokenWorkspaceAndWaitRunningStatus() throws Exception {
-    try {
-      testWorkspaceServiceClient.start(
-          brokenWorkspace.getName(), brokenWorkspace.getId(), defaultTestUser);
-    } catch (Exception ex) {
-      // remove try-catch block after issue has been resolved
-      fail("Known issue https://github.com/eclipse/che/issues/10295", ex);
-    }
-  }
-
-  private void waitWorkspaceRunningStatusOnDashboard() throws Exception {
-    try {
-      workspaces.waitWorkspaceStatus(brokenWorkspace.getName(), RUNNING);
-    } catch (TimeoutException ex) {
-      // remove try-catch block after issue has been resolved
-      fail("Known issue https://github.com/eclipse/che/issues/10295", ex);
-    }
-  }
-
-  private TestWorkspace createBrokenWorkspace() throws Exception {
-    return testWorkspaceProvider.createWorkspace(
-        defaultTestUser, 2, WorkspaceTemplate.BROKEN, true);
-  }
-
-  private String getPodName() throws Exception {
-    String command = format(GET_POD_NAME_COMMAND_COMMAND_TEMPLATE, brokenWorkspace.getId());
+  private String getPodRelatedEvents() throws Exception {
+    final Workspace brokenWorkspace =
+        testWorkspaceServiceClient.getByName(WORKSPACE_NAME, defaultTestUser.getName());
+    final String command = format(GET_POD_RELATED_EVENTS_COMMAND_TEMPLATE, brokenWorkspace.getId());
+    // final String events =
     return openShiftCliCommandExecutor.execute(command);
-  }
 
-  private List<String> getPodRelatedEvents() throws Exception {
-    String command = format(GET_POD_RELATED_EVENTS_COMMAND_TEMPLATE, getPodName());
-    String events = openShiftCliCommandExecutor.execute(command);
-    return asList(events.split("[\\ \\n]"));
+    // return asList(events.split("[\\ \\n]"));
   }
 
   private boolean eventIsPresent(String event) {
