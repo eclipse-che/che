979fbb1fbcaa94a19e8ef58b8127b7b931d648d8
Sergii Leshchenko <sleshche@redhat.com>
2018-09-10 17:02:41 +0300

    Add authorization checks for all factory service's methods
    Signed-off-by: Sergii Leshchenko <sleshche@redhat.com>

--- multiuser/permission/che-multiuser-permission-factory/src/main/java/org/eclipse/che/multiuser/permission/factory/FactoryPermissionsFilter.java
+++ multiuser/permission/che-multiuser-permission-factory/src/main/java/org/eclipse/che/multiuser/permission/factory/FactoryPermissionsFilter.java
@@ -11,8 +11,14 @@
  */
 package org.eclipse.che.multiuser.permission.factory;
 
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 import org.eclipse.che.api.core.ApiException;
+import org.eclipse.che.api.core.ForbiddenException;
+import org.eclipse.che.api.core.NotFoundException;
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.core.model.factory.Factory;
+import org.eclipse.che.api.factory.server.FactoryManager;
 import org.eclipse.che.commons.env.EnvironmentContext;
 import org.eclipse.che.commons.subject.Subject;
 import org.eclipse.che.everrest.CheMethodInvokerFilter;
@@ -21,13 +27,21 @@ import org.everrest.core.Filter;
 import org.everrest.core.resource.GenericResourceMethod;
 
 /**
- * Restricts access to methods of FactoryService by user's permissions
+ * Restricts access to methods of FactoryService by user's permissions.
  *
  * @author Anton Korneta
+ * @author Sergii Leshchenko
  */
 @Filter
 @Path("/factory/{path:.*}")
 public class FactoryPermissionsFilter extends CheMethodInvokerFilter {
+
+  private final FactoryManager factoryManager;
+
+  @Inject
+  public FactoryPermissionsFilter(FactoryManager factoryManager) {
+    this.factoryManager = factoryManager;
+  }
 
   @Override
   protected void filter(GenericResourceMethod genericResourceMethod, Object[] arguments)
@@ -35,20 +49,42 @@ public class FactoryPermissionsFilter extends CheMethodInvokerFilter {
     final String methodName = genericResourceMethod.getMethod().getName();
 
     final Subject currentSubject = EnvironmentContext.getCurrent().getSubject();
-    String action;
-    String workspaceId;
 
     switch (methodName) {
       case "getFactoryJson":
         {
-          workspaceId = ((String) arguments[0]);
-          action = WorkspaceDomain.READ;
-          break;
+          String workspaceId = ((String) arguments[0]);
+
+          currentSubject.checkPermission(
+              WorkspaceDomain.DOMAIN_ID, workspaceId, WorkspaceDomain.READ);
+          return;
         }
-      default:
-        // public methods
+      case "removeFactory":
+        checkSubjectIsCreator((String) arguments[0], currentSubject, "remove");
         return;
+      case "updateFactory":
+        checkSubjectIsCreator((String) arguments[0], currentSubject, "update");
+        return;
+
+      case "getFactory":
+      case "saveFactory":
+      case "getFactoryByAttribute":
+      case "resolveFactory":
+        // public methods
+        // do nothing
+        return;
+
+      default:
+        throw new ForbiddenException("The user does not have permission to perform this operation");
     }
-    currentSubject.checkPermission(WorkspaceDomain.DOMAIN_ID, workspaceId, action);
+  }
+
+  private void checkSubjectIsCreator(String factoryId, Subject currentSubject, String action)
+      throws NotFoundException, ServerException, ForbiddenException {
+    Factory factory = factoryManager.getById(factoryId);
+    String creatorId = factory.getCreator().getUserId();
+    if (!creatorId.equals(currentSubject.getUserId())) {
+      throw new ForbiddenException("It is not allowed to " + action + " foreign factory");
+    }
   }
 }
