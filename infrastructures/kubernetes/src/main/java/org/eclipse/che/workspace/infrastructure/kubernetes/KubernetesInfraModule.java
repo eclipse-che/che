24ccece9dfa21426cdcb14bad214f030cc9c3614
Oleksandr Garagatyi <ogaragat@redhat.com>
2018-09-11 15:48:47 +0300

    Rework plugin brokering to reuse PVC strategies code (#11119)
    CHE-10879 Rework plugin broker code to reuse PVC strategies code.
    Moreover, it applies other OS/K8s provisioners with proxy,
    name uniqueness, private docker image registry, pod termination
    support.
    Signed-off-by: Oleksandr Garagatyi <ogaragat@redhat.com>

--- infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/KubernetesInfraModule.java
+++ infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/KubernetesInfraModule.java
@@ -56,6 +56,10 @@ import org.eclipse.che.workspace.infrastructure.kubernetes.server.secure.Default
 import org.eclipse.che.workspace.infrastructure.kubernetes.server.secure.SecureServerExposerFactory;
 import org.eclipse.che.workspace.infrastructure.kubernetes.server.secure.SecureServerExposerFactoryProvider;
 import org.eclipse.che.workspace.infrastructure.kubernetes.wsplugins.KubernetesPluginsToolingApplier;
+import org.eclipse.che.workspace.infrastructure.kubernetes.wsplugins.PluginBrokerManager;
+import org.eclipse.che.workspace.infrastructure.kubernetes.wsplugins.SidecarToolingProvisioner;
+import org.eclipse.che.workspace.infrastructure.kubernetes.wsplugins.brokerphases.BrokerEnvironmentFactory;
+import org.eclipse.che.workspace.infrastructure.kubernetes.wsplugins.brokerphases.KubernetesBrokerEnvironmentFactory;
 import org.eclipse.che.workspace.infrastructure.kubernetes.wsplugins.events.BrokerService;
 
 /** @author Sergii Leshchenko */
@@ -149,5 +153,14 @@ public class KubernetesInfraModule extends AbstractModule {
         .to(new TypeLiteral<DefaultSecureServersFactory<KubernetesEnvironment>>() {});
 
     bind(BrokerService.class);
+
+    bind(new TypeLiteral<BrokerEnvironmentFactory<KubernetesEnvironment>>() {})
+        .to(KubernetesBrokerEnvironmentFactory.class);
+
+    bind(PluginBrokerManager.class)
+        .to(new TypeLiteral<PluginBrokerManager<KubernetesEnvironment>>() {});
+
+    bind(SidecarToolingProvisioner.class)
+        .to(new TypeLiteral<SidecarToolingProvisioner<KubernetesEnvironment>>() {});
   }
 }
