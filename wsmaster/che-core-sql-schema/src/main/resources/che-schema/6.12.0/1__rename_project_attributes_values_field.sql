86906cd68adfd713b4804b7848026655c4284fae
Max Shaposhnik <mshaposh@redhat.com>
2018-09-17 12:27:50 +0300

    fixup! Rename project attributes values

--- wsmaster/che-core-sql-schema/src/main/resources/che-schema/6.12.0/1__rename_project_attributes_values_field.sql
+++ wsmaster/che-core-sql-schema/src/main/resources/che-schema/6.12.0/1__rename_project_attributes_values_field.sql
@@ -10,4 +10,4 @@
 --   Red Hat, Inc. - initial API and implementation
 --
 
-ALTER TABLE projectattribute_values RENAME COLUMN values TO projectattribute_values;
+ALTER TABLE projectattribute_values RENAME COLUMN values TO attribute_values;
