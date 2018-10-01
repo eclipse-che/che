8476d5f560df4a17f9e0f73b315da1e5269d2c79
Thomas Mäder <tmader@redhat.com>
2018-10-01 19:40:01 +0200

    Update goto symbol result to observed correct behaviour
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/languageserver/TypeScriptEditingTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/languageserver/TypeScriptEditingTest.java
@@ -121,7 +121,7 @@ public class TypeScriptEditingTest {
     menu.runCommand(ASSISTANT, FIND_PROJECT_SYMBOL);
     assistantFindPanel.typeToInputField("testPrint");
     assistantFindPanel.clickOnActionNodeWithTextContains("testPrint");
-    editor.waitCursorPosition(26, 6);
+    editor.waitCursorPosition(23, 5);
   }
 
   @Test(priority = 2, alwaysRun = true)
