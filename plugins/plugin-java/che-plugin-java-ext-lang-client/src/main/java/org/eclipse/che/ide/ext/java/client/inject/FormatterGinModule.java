714d399a81bbde06dbb59b58ee1151494ae502f9
Valeriy Svydenko <vsvydenko@codenvy.com>
2018-10-09 17:24:44 +0300

    Add binding for Java Code Formatter (#11491)
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

---
+++ plugins/plugin-java/che-plugin-java-ext-lang-client/src/main/java/org/eclipse/che/ide/ext/java/client/inject/FormatterGinModule.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.ide.ext.java.client.inject;
+
+import com.google.gwt.inject.client.AbstractGinModule;
+import com.google.gwt.inject.client.multibindings.GinMultibinder;
+import javax.inject.Singleton;
+import org.eclipse.che.ide.api.preferences.PreferencePagePresenter;
+import org.eclipse.che.ide.ext.java.client.formatter.preferences.FormatterPreferencePagePresenter;
+import org.eclipse.che.ide.ext.java.client.formatter.preferences.FormatterPreferencePageView;
+import org.eclipse.che.ide.ext.java.client.formatter.preferences.FormatterPreferencePageViewImpl;
+
+public class FormatterGinModule extends AbstractGinModule {
+  @Override
+  protected void configure() {
+    bind(FormatterPreferencePageView.class)
+        .to(FormatterPreferencePageViewImpl.class)
+        .in(Singleton.class);
+    final GinMultibinder<PreferencePagePresenter> prefBinder =
+        GinMultibinder.newSetBinder(binder(), PreferencePagePresenter.class);
+    prefBinder.addBinding().to(FormatterPreferencePagePresenter.class);
+  }
+}
