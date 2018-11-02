c67c12e699ad9e655f8c32cf6cafe5a4f8d88bab
Aleksandr Shmaraiev <oshmarai@redhat.com>
2018-11-02 14:17:47 +0200

    Selenium: Add the 'try-catch' to the 'GitCompareTest' according to the known issue #11769 (#11770)
    * Add the 'try-catch' to the 'GitCompareTest' according to the known issue #11769
    * Add the 'try-catch' to the 'GitCompareTest' according to known issue
    * Rework the methods 'setCursorToLine()' in the page objects 'GitCompare' and 'CodenvyEditor'

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/git/GitCompareTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/git/GitCompareTest.java
@@ -23,6 +23,7 @@ import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.G
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.New.JAVA_CLASS;
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.New.NEW;
 import static org.eclipse.che.selenium.core.constant.TestMenuCommandsConstants.Project.PROJECT;
+import static org.testng.Assert.fail;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
@@ -48,6 +49,7 @@ import org.eclipse.che.selenium.pageobject.WarningDialog;
 import org.eclipse.che.selenium.pageobject.Wizard;
 import org.eclipse.che.selenium.pageobject.git.Git;
 import org.eclipse.che.selenium.pageobject.git.GitCompare;
+import org.openqa.selenium.TimeoutException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -228,9 +230,23 @@ public class GitCompareTest {
     git.setFocusOnLeftGitCompareEditor();
     git.setCursorToLine(2, LEFT_COMPARE_ST);
     git.typeTextIntoGitCompareEditor("//change content from compare editor");
-    git.waitExpTextIntoCompareLeftEditor("//change content from compare editor");
+
+    try {
+      git.waitExpTextIntoCompareLeftEditor("//change content from compare editor");
+    } catch (TimeoutException ex) {
+      // remove try-catch block after issue has been resolved
+      fail("Known random failure https://github.com/eclipse/che/issues/11791");
+    }
+
     git.clickOnGitCompareCloseButton();
-    askDialog.confirmAndWaitClosed();
+
+    try {
+      askDialog.confirmAndWaitClosed();
+    } catch (TimeoutException ex) {
+      // remove try-catch block after issue has been resolved
+      fail("Known random failure https://github.com/eclipse/che/issues/11769");
+    }
+
     git.waitGitCompareFormIsClosed();
     git.waitGitCompareRevisionFormIsOpen();
     git.clickOnCloseRevisionButton();
