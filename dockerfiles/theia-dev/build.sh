ab959d49a63b695ab467eef491957b614e1d61b1
Florent Benoit <fbenoit@redhat.com>
2018-11-30 14:25:02 +0100

    Add eclipse/theia-dev image will be used by developers and by theia image (as builder image)
    Change-Id: Ie78cb2d616ef6e5a32ce7c34a25b3adab1f28948
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ dockerfiles/theia-dev/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) 2018 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
+base_dir=$(cd "$(dirname "$0")"; pwd)
+. "${base_dir}"/../build.include
+
+init --name:theia-dev "$@"
+build
+if ! skip_tests; then
+  bash "${base_dir}"/e2e/build.sh "$@"
+fi
