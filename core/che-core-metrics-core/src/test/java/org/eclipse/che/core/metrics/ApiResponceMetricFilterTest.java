34122b691d8d2bbae17096abdd1147f6c9c12f3a
Mykhailo Kuznietsov <mkuznets@redhat.com>
2019-01-03 11:08:05 +0200

    fixup! fixup! fixup! fixup! Add filter to track 5xx errors for Prometheus

--- core/che-core-metrics-core/src/test/java/org/eclipse/che/core/metrics/ApiResponceMetricFilterTest.java
+++ core/che-core-metrics-core/src/test/java/org/eclipse/che/core/metrics/ApiResponceMetricFilterTest.java
@@ -12,21 +12,22 @@
 package org.eclipse.che.core.metrics;
 
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.RedirectConfig.redirectConfig;
+import static com.jayway.restassured.config.RestAssuredConfig.newConfig;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.everrest.assured.JettyHttpServer.ADMIN_USER_NAME;
 import static org.everrest.assured.JettyHttpServer.ADMIN_USER_PASSWORD;
 import static org.everrest.assured.JettyHttpServer.SECURE_PATH;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 
 import com.jayway.restassured.response.Response;
+import java.net.URI;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import org.everrest.assured.EverrestJetty;
-import org.everrest.core.Filter;
-import org.everrest.core.GenericContainerRequest;
-import org.everrest.core.RequestFilter;
-import org.everrest.core.impl.EnvironmentContext;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.testng.MockitoTestNGListener;
@@ -42,41 +43,138 @@ public class ApiResponceMetricFilterTest {
 
   @InjectMocks private ApiResponceMetricFilter filter;
 
-  private DummyService dummyService = new DummyService();
+  private DummyService dummyService;
 
   @BeforeMethod
   public void setUp() {
     filter = new ApiResponceMetricFilter();
     filter.apiResponseCounter = apiResponseCounter;
+    dummyService = new DummyService();
+  }
+
+  @Test(dataProvider = "successes")
+  public void shouldIncrementCounterOnSuccessResponses(int status) {
+    dummyService.setStatusToReturn(status);
+
+    given()
+        .auth()
+        .basic(ADMIN_USER_NAME, ADMIN_USER_PASSWORD)
+        .when()
+        .get(SECURE_PATH +  "/service/success")
+        .then()
+        .statusCode(status);
+
+    verify(apiResponseCounter).incrementSuccessResponseCounter();
+    verify(apiResponseCounter, never()).incrementRedirectResonseCounter();
+    verify(apiResponseCounter, never()).incrementClientErrorResponseCounter();
+    verify(apiResponseCounter, never()).incrementServerErrorResponceCounter();
+  }
+
+  @Test(dataProvider = "redirects")
+  public void shouldIncrementCounterOnRedirectResponses(int status) {
+    dummyService.setStatusToReturn(status);
+
+    given()
+        .config(newConfig().redirect(redirectConfig().followRedirects(false)))
+        .auth()
+        .basic(ADMIN_USER_NAME, ADMIN_USER_PASSWORD)
+        .when()
+        .get(SECURE_PATH +  "/service/redirect")
+        .then()
+        .statusCode(status);
+
+    verify(apiResponseCounter).incrementRedirectResonseCounter();
+    verify(apiResponseCounter, never()).incrementSuccessResponseCounter();
+    verify(apiResponseCounter, never()).incrementClientErrorResponseCounter();
+    verify(apiResponseCounter, never()).incrementServerErrorResponceCounter();
+  }
+
+  @Test(dataProvider = "clientErrors")
+  public void shouldIncrementCounterOnClientErrorResponses(int status) {
+    dummyService.setStatusToReturn(status);
+
+    given()
+        .auth()
+        .basic(ADMIN_USER_NAME, ADMIN_USER_PASSWORD)
+        .when()
+        .get(SECURE_PATH +  "/service/clienterror")
+        .then()
+        .statusCode(status);
+
+    verify(apiResponseCounter).incrementClientErrorResponseCounter();
+    verify(apiResponseCounter, never()).incrementSuccessResponseCounter();
+    verify(apiResponseCounter, never()).incrementRedirectResonseCounter();
+    verify(apiResponseCounter, never()).incrementServerErrorResponceCounter();
   }
 
   @Test(dataProvider = "serverErrors")
-  public void shouldIncrementCounterOnServerErrorResponces(int status) {
-    final Response response =
-        given()
+  public void shouldIncrementCounterOnServerErrorResponses(int status) {
+    dummyService.setStatusToReturn(status);
+
+    given()
             .auth()
             .basic(ADMIN_USER_NAME, ADMIN_USER_PASSWORD)
             .when()
-            .put(SECURE_PATH +  "/service/test");
+            .get(SECURE_PATH +  "/service/servererror")
+            .then()
+            .statusCode(status);
 
     verify(apiResponseCounter).incrementServerErrorResponceCounter();
+    verify(apiResponseCounter, never()).incrementSuccessResponseCounter();
+    verify(apiResponseCounter, never()).incrementRedirectResonseCounter();
+    verify(apiResponseCounter, never()).incrementClientErrorResponseCounter();
+
   }
 
   @Path("/service")
   public static class DummyService {
 
-    @GET
-    @Path("/test")
-    public void testMethod() {
-      System.out.println("222     ");
+    int statusToReturn;
+
+    public void setStatusToReturn(int statusToReturn) {
+      this.statusToReturn = statusToReturn;
     }
 
     @GET
-    @Path("/error")
-    @Produces(APPLICATION_JSON)
-    public void serverErrorMethod() {
-      throw new RuntimeException("test");
+    @Path("/success")
+    public javax.ws.rs.core.Response success() {
+      return javax.ws.rs.core.Response.status(statusToReturn).build();
     }
+
+    @GET
+    @Path("/redirect")
+    public javax.ws.rs.core.Response redirect() {
+      return javax.ws.rs.core.Response.temporaryRedirect(URI.create("localhost")).status(statusToReturn).build();
+    }
+
+
+    @GET
+    @Path("/clienterror")
+    public javax.ws.rs.core.Response clientError() {
+      return javax.ws.rs.core.Response.status(statusToReturn).build();
+    }
+
+
+    @GET
+    @Path("/servererror")
+    public javax.ws.rs.core.Response serverErrorMethod() {
+      return javax.ws.rs.core.Response.status(statusToReturn).build();
+    }
+  }
+
+  @DataProvider(name = "successes")
+  public Object[][] success() {
+    return new Object[][] {{200}, {201}, {202}, {203}, {204}};
+  }
+
+  @DataProvider(name = "redirects")
+  public Object[][] redirects() {
+    return new Object[][] {{300}, {301}, {302}, {303}, {304}};
+  }
+
+  @DataProvider(name = "clientErrors")
+  public Object[][] clientErrors() {
+    return new Object[][] {{400}, {401}, {402}, {403}, {404}, {405}};
   }
 
   @DataProvider(name = "serverErrors")
