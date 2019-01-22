c8b7ac10c5bfa63aedc1982538add8ed9bce303c
David Festal <dfestal@redhat.com>
2019-01-22 15:00:18 +0100

    Theia cdn support (#12358)
    * CDN / long-term-caching support for the `che-theia` IDE.
    * Allow disabling the CDN support at runtime (WIP)
    * Allow specifying the `THEIA_GENERATOR_PACKAGE` as a build argument: this makes testing with a custom theia generator much easier.
    * Add CDN related build actions:
        - Duplicate `cdn.json` content to a label
        - Push CDN-supported artifacts to the Akamai CDN if an Akamai NetStorage Auth configuration is provided as an environment variable.
    * Add CDN-related informations in the Readme
    * Allow specifying the CDN prefixes as environment variables
    
    Signed-off-by: David Festal <dfestal@redhat.com>

--- dockerfiles/theia/build.sh
+++ dockerfiles/theia/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2018 Red Hat, Inc.
+# Copyright (c) 2018-2019 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -11,7 +11,27 @@ base_dir=$(cd "$(dirname "$0")"; pwd)
 . "${base_dir}"/../build.include
 
 init --name:theia "$@"
+
+if [ "${CDN_PREFIX:-}" != "" ]; then
+  BUILD_ARGS+="--build-arg CDN_PREFIX=${CDN_PREFIX} "
+fi
+
+if [ "${MONACO_CDN_PREFIX:-}" != "" ]; then
+  BUILD_ARGS+="--build-arg MONACO_CDN_PREFIX=${MONACO_CDN_PREFIX} "
+fi
+
 build
 if ! skip_tests; then
   bash "${base_dir}"/e2e/build.sh "$@"
+fi
+
+echo "Extracting artifacts for the CDN"
+mkdir -p "${base_dir}/theia_artifacts"
+"${base_dir}"/extract-for-cdn.sh "$IMAGE_NAME" "${base_dir}/theia_artifacts"
+LABEL_CONTENT=$(cat "${base_dir}"/theia_artifacts/cdn.json || true 2>/dev/null)
+if [ -n "${LABEL_CONTENT}" ]; then
+  BUILD_ARGS+="--label che-plugin.cdn.artifacts=$(echo ${LABEL_CONTENT} | sed 's/ //g') "
+  echo "Rebuilding with CDN label..."
+  build
+  "${base_dir}"/push-cdn-files-to-akamai.sh
 fi
