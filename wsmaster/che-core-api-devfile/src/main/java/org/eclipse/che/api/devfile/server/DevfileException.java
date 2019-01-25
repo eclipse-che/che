2460de22b13a9113acecb8bc41b24a7a4faa1aad
Max Shaposhnik <mshaposh@redhat.com>
2019-01-25 17:19:13 +0200

    Add missing class
    Signed-off-by: Max Shaposhnik <mshaposh@redhat.com>

---
+++ wsmaster/che-core-api-devfile/src/main/java/org/eclipse/che/api/devfile/server/DevfileException.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.devfile.server;
+
+/** Describes general devfile exception. */
+public class DevfileException extends Exception {
+
+  public DevfileException(String message, Exception cause) {
+    super(message, cause);
+  }
+
+  public DevfileException(String message) {
+    super(message);
+  }
+}
