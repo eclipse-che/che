766d5e75cabcc6c55dcf15ad3c150073a9b77844
Max Shaposhnik <mshaposh@redhat.com>
2019-03-06 16:45:44 +0200

    Fix docs site checkout
    Signed-off-by: Max Shaposhnik <mshaposh@redhat.com>

--- wsmaster/che-core-api-devfile/src/main/resources/build_docs.sh
+++ wsmaster/che-core-api-devfile/src/main/resources/build_docs.sh
@@ -73,7 +73,6 @@ deploy() {
      DOCS_REPOSITORY_URL=https://${GH_TOKEN}@github.com/${DEPLOY_ORGANIZATION}/devfile.git
    fi
 
-   echo git clone ${BRANCH_ARG} ${DOCS_REPOSITORY_URL}
    rm -rf devfile && git clone ${BRANCH_ARG} ${DOCS_REPOSITORY_URL}
    cp -f docs/* ./devfile/docs
    cd devfile
