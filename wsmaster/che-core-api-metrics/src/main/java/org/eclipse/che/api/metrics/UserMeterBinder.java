0dab321a45d363ffec84cb2f73d09240df24f59b
Mykhailo Kuznietsov <mkuznets@redhat.com>
2019-03-28 13:35:01 +0200

    fixup! fixup! Add metrics for number of users
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>

--- wsmaster/che-core-api-metrics/src/main/java/org/eclipse/che/api/metrics/UserMeterBinder.java
+++ wsmaster/che-core-api-metrics/src/main/java/org/eclipse/che/api/metrics/UserMeterBinder.java
@@ -15,12 +15,20 @@ import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.user.server.UserManager;
 
+/** Provide metric for number of users in Che database */
+@Singleton
 public class UserMeterBinder implements MeterBinder {
 
-  @Inject UserManager userManager;
+  private final UserManager userManager;
+
+  @Inject
+  public UserMeterBinder(UserManager userManager) {
+    this.userManager = userManager;
+  }
 
   @Override
   public void bindTo(MeterRegistry registry) {
