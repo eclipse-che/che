Diffblue-benchmarks/Eclipse-che

Add model objects for Devfile
Signed-off-by: Sergii Leshchenko <sleshche@redhat.com>

core/che-core-api-model/src/main/java/org/eclipse/che/api/core/model/workspace/devfile/Component.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.core.model.workspace.devfile;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Component {
+
+  String getName();
+
+  String getType();
+
+  // editor/plugin
+  String getId();
+
+  // openshift/kubernetes
+  String getLocal();
+
+  List<? extends Entrypoint> getEntrypoints();
+
+  String getLocalContent();
+
+  Map<String, String> getSelector();
+
+  // dockerimage
+
+  String getImage();
+
+  String getMemoryLimit();
+
+  boolean getMountSources();
+
+  List<String> getCommand();
+
+  List<String> getArgs();
+
+  List<? extends Volume> getVolumes();
+
+  List<? extends Env> getEnv();
+
+  List<? extends Endpoint> getEndpoints();
+}

wsmaster/che-core-api-workspace-shared/src/main/java/org/eclipse/che/api/workspace/shared/dto/devfile/ComponentDto.java
@@ -1,0 +1,131 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.workspace.shared.dto.devfile;
+
+import java.util.List;
+import java.util.Map;
+import org.eclipse.che.api.core.model.workspace.devfile.Component;
+
+/** @author Sergii Leshchenko */
+public interface ComponentDto extends Component {
+
+  @Override
+  String getName();
+
+  void setName(String name);
+
+  ComponentDto withName(String name);
+
+  @Override
+  String getType();
+
+  void setType(String type);
+
+  ComponentDto withType(String type);
+
+  // editor/plugin
+
+  @Override
+  String getId();
+
+  void setId(String id);
+
+  ComponentDto withId(String id);
+
+  // k8s/OS
+
+  @Override
+  String getLocal();
+
+  void setLocal(String local);
+
+  ComponentDto withLocal(String local);
+
+  @Override
+  String getLocalContent();
+
+  void setLocalContent(String localContent);
+
+  ComponentDto withLocalContent(String localContent);
+
+  @Override
+  List<EntrypointDto> getEntrypoints();
+
+  void setEntrypoints(List<EntrypointDto> entrypoints);
+
+  ComponentDto withEntrypoints(List<EntrypointDto> entrypoints);
+
+  @Override
+  Map<String, String> getSelector();
+
+  void setSelector(Map<String, String> selector);
+
+  ComponentDto withSelector(Map<String, String> selector);
+
+  // dockerimage
+
+  @Override
+  String getImage();
+
+  void setImage(String image);
+
+  ComponentDto withImage(String image);
+
+  @Override
+  String getMemoryLimit();
+
+  void setMemoryLimit(String memoryLimit);
+
+  ComponentDto withMemoryLimit(String memoryLimit);
+
+  @Override
+  boolean getMountSources();
+
+  void setMountSources(boolean mountSources);
+
+  ComponentDto withMountSources(boolean mountSources);
+
+  @Override
+  List<String> getCommand();
+
+  void setCommand(List<String> command);
+
+  ComponentDto withCommand(List<String> command);
+
+  @Override
+  List<String> getArgs();
+
+  void setArgs(List<String> args);
+
+  ComponentDto withArgs(List<String> args);
+
+  @Override
+  List<VolumeDto> getVolumes();
+
+  void setVolumes(List<VolumeDto> volumes);
+
+  ComponentDto withVolumes(List<VolumeDto> volumes);
+
+  @Override
+  List<EnvDto> getEnv();
+
+  void setEnv(List<EnvDto> env);
+
+  ComponentDto withEnv(List<EnvDto> env);
+
+  @Override
+  List<EndpointDto> getEndpoints();
+
+  void setEndpoints(List<EndpointDto> endpoints);
+
+  ComponentDto withEndpoints(List<EndpointDto> endpoints);
+}

wsmaster/che-core-api-workspace-shared/src/main/java/org/eclipse/che/api/workspace/shared/dto/devfile/DevfileDto.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.workspace.shared.dto.devfile;
+
+import java.util.List;
+import java.util.Map;
+import org.eclipse.che.api.core.model.workspace.devfile.Devfile;
+
+/** @author Sergii Leshchenko */
+public interface DevfileDto extends Devfile {
+  @Override
+  String getSpecVersion();
+
+  void setSpecVersion(String specVersion);
+
+  DevfileDto withSpecVersion(String specVersion);
+
+  @Override
+  String getName();
+
+  void setName(String name);
+
+  DevfileDto withName(String name);
+
+  @Override
+  List<ProjectDto> getProjects();
+
+  void setProjects(List<ProjectDto> projects);
+
+  DevfileDto withProjects(List<ProjectDto> projects);
+
+  @Override
+  List<ComponentDto> getComponents();
+
+  void setComponents(List<ComponentDto> components);
+
+  DevfileDto withComponents(List<ComponentDto> components);
+
+  @Override
+  List<CommandDto> getCommands();
+
+  void setCommands(List<CommandDto> commands);
+
+  DevfileDto withCommands(List<CommandDto> commands);
+
+  @Override
+  Map<String, String> getAttributes();
+
+  void setAttributes(Map<String, String> attributes);
+
+  DevfileDto withAttributes(Map<String, String> attributes);
+}

eclipse-che/che

core/che-core-api-model/src/main/java/org/eclipse/che/api/core/model/workspace/devfile/Component.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.core.model.workspace.devfile;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Component {
+
+  String getName();
+
+  String getType();
+
+  // editor/plugin
+  String getId();
+
+  // openshift/kubernetes
+  String getReference();
+
+  String getReferenceContent();
+
+  List<? extends Entrypoint> getEntrypoints();
+
+  Map<String, String> getSelector();
+
+  // dockerimage
+
+  String getImage();
+
+  String getMemoryLimit();
+
+  boolean getMountSources();
+
+  List<String> getCommand();
+
+  List<String> getArgs();
+
+  List<? extends Volume> getVolumes();
+
+  List<? extends Env> getEnv();
+
+  List<? extends Endpoint> getEndpoints();
+}

wsmaster/che-core-api-workspace-shared/src/main/java/org/eclipse/che/api/workspace/shared/dto/devfile/ComponentDto.java
@@ -1,0 +1,133 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.workspace.shared.dto.devfile;
+
+import java.util.List;
+import java.util.Map;
+import org.eclipse.che.api.core.model.workspace.devfile.Component;
+import org.eclipse.che.dto.shared.DTO;
+
+/** @author Sergii Leshchenko */
+@DTO
+public interface ComponentDto extends Component {
+
+  @Override
+  String getName();
+
+  void setName(String name);
+
+  ComponentDto withName(String name);
+
+  @Override
+  String getType();
+
+  void setType(String type);
+
+  ComponentDto withType(String type);
+
+  // editor/plugin
+
+  @Override
+  String getId();
+
+  void setId(String id);
+
+  ComponentDto withId(String id);
+
+  // k8s/OS
+
+  @Override
+  String getReference();
+
+  void setReference(String reference);
+
+  ComponentDto withReference(String reference);
+
+  @Override
+  String getReferenceContent();
+
+  void setReferenceContent(String referenceContent);
+
+  ComponentDto withReferenceContent(String referenceContent);
+
+  @Override
+  List<EntrypointDto> getEntrypoints();
+
+  void setEntrypoints(List<EntrypointDto> entrypoints);
+
+  ComponentDto withEntrypoints(List<EntrypointDto> entrypoints);
+
+  @Override
+  Map<String, String> getSelector();
+
+  void setSelector(Map<String, String> selector);
+
+  ComponentDto withSelector(Map<String, String> selector);
+
+  // dockerimage
+
+  @Override
+  String getImage();
+
+  void setImage(String image);
+
+  ComponentDto withImage(String image);
+
+  @Override
+  String getMemoryLimit();
+
+  void setMemoryLimit(String memoryLimit);
+
+  ComponentDto withMemoryLimit(String memoryLimit);
+
+  @Override
+  boolean getMountSources();
+
+  void setMountSources(boolean mountSources);
+
+  ComponentDto withMountSources(boolean mountSources);
+
+  @Override
+  List<String> getCommand();
+
+  void setCommand(List<String> command);
+
+  ComponentDto withCommand(List<String> command);
+
+  @Override
+  List<String> getArgs();
+
+  void setArgs(List<String> args);
+
+  ComponentDto withArgs(List<String> args);
+
+  @Override
+  List<DevfileVolumeDto> getVolumes();
+
+  void setVolumes(List<DevfileVolumeDto> volumes);
+
+  ComponentDto withVolumes(List<DevfileVolumeDto> volumes);
+
+  @Override
+  List<EnvDto> getEnv();
+
+  void setEnv(List<EnvDto> env);
+
+  ComponentDto withEnv(List<EnvDto> env);
+
+  @Override
+  List<EndpointDto> getEndpoints();
+
+  void setEndpoints(List<EndpointDto> endpoints);
+
+  ComponentDto withEndpoints(List<EndpointDto> endpoints);
+}

wsmaster/che-core-api-workspace-shared/src/main/java/org/eclipse/che/api/workspace/shared/dto/devfile/DevfileDto.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.workspace.shared.dto.devfile;
+
+import java.util.List;
+import java.util.Map;
+import org.eclipse.che.api.core.model.workspace.devfile.Devfile;
+import org.eclipse.che.dto.shared.DTO;
+
+/** @author Sergii Leshchenko */
+@DTO
+public interface DevfileDto extends Devfile {
+  @Override
+  String getSpecVersion();
+
+  void setSpecVersion(String specVersion);
+
+  DevfileDto withSpecVersion(String specVersion);
+
+  @Override
+  String getName();
+
+  void setName(String name);
+
+  DevfileDto withName(String name);
+
+  @Override
+  List<ProjectDto> getProjects();
+
+  void setProjects(List<ProjectDto> projects);
+
+  DevfileDto withProjects(List<ProjectDto> projects);
+
+  @Override
+  List<ComponentDto> getComponents();
+
+  void setComponents(List<ComponentDto> components);
+
+  DevfileDto withComponents(List<ComponentDto> components);
+
+  @Override
+  List<DevfileCommandDto> getCommands();
+
+  void setCommands(List<DevfileCommandDto> commands);
+
+  DevfileDto withCommands(List<DevfileCommandDto> commands);
+
+  @Override
+  Map<String, String> getAttributes();
+
+  void setAttributes(Map<String, String> attributes);
+
+  DevfileDto withAttributes(Map<String, String> attributes);
+}