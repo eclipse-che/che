046552f4e5efb9890d6910261e8388d4bff35530
Sergii Leshchenko <sleshche@redhat.com>
2019-04-15 11:53:58 +0300

    Make Devfile part of a workspace (#13122)
    It includes only model objects changes. Devfile can not be used as part
    of workspace yet.
    
    Signed-off-by: Sergii Leshchenko <sleshche@redhat.com>

---
+++ wsmaster/che-core-api-workspace-shared/src/main/java/org/eclipse/che/api/workspace/shared/dto/devfile/DevfileDto.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.workspace.shared.dto.devfile;
+
+import java.util.List;
+import java.util.Map;
+import org.eclipse.che.api.core.model.workspace.devfile.Devfile;
+import org.eclipse.che.dto.shared.DTO;
+
+/** @author Sergii Leshchenko */
+@DTO
+public interface DevfileDto extends Devfile {
+  @Override
+  String getSpecVersion();
+
+  void setSpecVersion(String specVersion);
+
+  DevfileDto withSpecVersion(String specVersion);
+
+  @Override
+  String getName();
+
+  void setName(String name);
+
+  DevfileDto withName(String name);
+
+  @Override
+  List<ProjectDto> getProjects();
+
+  void setProjects(List<ProjectDto> projects);
+
+  DevfileDto withProjects(List<ProjectDto> projects);
+
+  @Override
+  List<ComponentDto> getComponents();
+
+  void setComponents(List<ComponentDto> components);
+
+  DevfileDto withComponents(List<ComponentDto> components);
+
+  @Override
+  List<DevfileCommandDto> getCommands();
+
+  void setCommands(List<DevfileCommandDto> commands);
+
+  DevfileDto withCommands(List<DevfileCommandDto> commands);
+
+  @Override
+  Map<String, String> getAttributes();
+
+  void setAttributes(Map<String, String> attributes);
+
+  DevfileDto withAttributes(Map<String, String> attributes);
+}
