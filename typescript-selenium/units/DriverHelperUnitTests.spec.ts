292d32e2215a58e55001ef133db3bd91ed889ad9
Ihor Okhrimenko <iokhrime@redhat.com>
2019-04-22 14:39:40 +0300

    Remove 'mocha.opts' config file
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- typescript-selenium/units/DriverHelperUnitTests.spec.ts
+++ typescript-selenium/units/DriverHelperUnitTests.spec.ts
@@ -8,16 +8,16 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 
-import { e2eContainer } from "./inversify.config";
-import { Driver } from "./driver/Driver";
-import { TYPES, CLASSES } from "./types";
-import { DriverHelper } from "./utils/DriverHelper";
+import { e2eContainer } from "../inversify.config";
+import { Driver } from "../driver/Driver";
+import { TYPES, CLASSES } from "../types";
+import { DriverHelper } from "../utils/DriverHelper";
 import { By, WebElementCondition, Condition } from "selenium-webdriver";
 import { describe, after } from "mocha";
-import { LoginPage } from "./pageobjects/login/LoginPage";
-import { Dashboard } from "./pageobjects/dashboard/Dashboard";
+import { LoginPage } from "../pageobjects/login/LoginPage";
+import { Dashboard } from "../pageobjects/dashboard/Dashboard";
 import { expect, assert } from 'chai'
-import { Workspaces } from "./pageobjects/dashboard/Workspaces";
+import { Workspaces } from "../pageobjects/dashboard/Workspaces";
 
 
 
