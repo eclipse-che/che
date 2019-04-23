508831e70144fedafd4946e08d93f7efe1aeb363
Ihor Okhrimenko <iokhrime@redhat.com>
2019-04-23 14:56:16 +0300

    Intermediate reporter version
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- typescript-selenium/reporter/CheReporter.ts
+++ typescript-selenium/reporter/CheReporter.ts
@@ -7,12 +7,12 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
-import * as mocha from 'mocha'
+import * as mocha from 'mocha';
 import { Driver } from '../driver/Driver';
 import { e2eContainer } from '../inversify.config';
 import { TYPES } from '../types';
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from 'fs';
+import * as path from 'path';
 import { inject, injectable } from 'inversify';
 import { types } from 'util';
 import { ThenableWebDriver } from 'selenium-webdriver';
@@ -20,45 +20,53 @@ import { ThenableWebDriver } from 'selenium-webdriver';
 const driver: Driver = e2eContainer.get(TYPES.Driver);
 
 class CheReporter extends mocha.reporters.Spec {
-  
+
   constructor(runner: mocha.Runner, options: mocha.MochaOptions) {
     super(runner, options);
 
-    runner.on('fail', async function (test) {
-      const fileName: string = `screenshot-${test.title}.png`
+
+
+
+    runner.on('fail', async function (test: mocha.Test) {
+      
+      const reportDirPath: string = './report'
+      const testFullTitle: string = test.fullTitle().replace(/\s/g, '_')
+      const testTitle: string = test.title.replace(/\s/g, '_')
+
+      const testReportDirPath: string = `${reportDirPath}/${testFullTitle}`
+      const fileName: string = `${testReportDirPath}/screenshot-${testTitle}.png`
+
+      await fs.exists(reportDirPath, async isDirExist => {
+        if (!isDirExist) {
+          await fs.mkdir(reportDirPath, err => {
+            if (err) {
+              throw err
+            }
+          })
+        }
+      })
+
+      await fs.exists(testReportDirPath, async isDirExist => {
+        if (!isDirExist) {
+          await fs.mkdir(testReportDirPath, err => {
+            if (err) {
+              throw err
+            }
+          })
+        }
+      })
 
       let screenshot = await driver.get().takeScreenshot().catch(err => {
-          throw err
+        throw err
       });
-  
+
       let stream = fs.createWriteStream(fileName)
-  
+
       stream.write(new Buffer(screenshot, 'base64'))
       stream.end()
-  
-      console.log("====>>>> The ", fileName, " file saved")
-      return
-      })
-    }
+    })
   }
-
-
-//   async saveScreenshot(test: mocha.Test): Promise<void> {
-
-//     const fileName: string = `screenshot-${test.title}.png`
-
-//     let screenshot = await this.driver.takeScreenshot().catch(err => {
-//         throw err
-//     });
-
-//     let stream = fs.createWriteStream(fileName)
-
-//     stream.write(new Buffer(screenshot, 'base64'))
-//     stream.end()
-
-//     console.log("====>>>> The ", fileName, " file saved")
-//     return
-// }
+}
 
 
 module.exports = CheReporter;
