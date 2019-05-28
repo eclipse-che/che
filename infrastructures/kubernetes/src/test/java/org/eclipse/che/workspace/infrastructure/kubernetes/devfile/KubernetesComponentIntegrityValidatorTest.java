a2d24aa820b4e85fe93c60568a9f64480b8eefa1
Lukas Krejci <lkrejci@redhat.com>
2019-05-28 18:32:22 +0200

    Added happy-path tests for the KubernetesComponentIntegrityValidatorTest
    Signed-off-by: Lukas Krejci <lkrejci@redhat.com>

--- infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/devfile/KubernetesComponentIntegrityValidatorTest.java
+++ infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/devfile/KubernetesComponentIntegrityValidatorTest.java
@@ -16,6 +16,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import io.fabric8.kubernetes.api.model.PodBuilder;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -43,6 +44,116 @@ public class KubernetesComponentIntegrityValidatorTest {
     Set<String> k8sComponentTypes = new HashSet<>();
     k8sComponentTypes.add(KUBERNETES_COMPONENT_TYPE);
     validator = new KubernetesComponentValidator(kubernetesRecipeParser, k8sComponentTypes);
+  }
+
+  @Test
+  public void shouldApplySelector() throws Exception {
+    // given
+    when(kubernetesRecipeParser.parse(any(String.class)))
+        .thenReturn(
+            Arrays.asList(
+                new PodBuilder().withNewMetadata().addToLabels("app", "test").endMetadata().build(),
+                new PodBuilder()
+                    .withNewMetadata()
+                    .addToLabels("app", "other")
+                    .endMetadata()
+                    .build()));
+
+    Map<String, String> selector = new HashMap<>();
+    selector.put("app", "test");
+
+    ComponentImpl component = new ComponentImpl();
+    component.setType(KUBERNETES_COMPONENT_TYPE);
+    component.setReference("ref");
+    component.setSelector(selector);
+    component.setReferenceContent("content");
+
+    // when
+    validator.validateComponent(component, __ -> "");
+
+    // then no exception is thrown
+  }
+
+  @Test
+  public void shouldApplyEntrypoint() throws Exception {
+    // given
+    when(kubernetesRecipeParser.parse(any(String.class)))
+        .thenReturn(
+            Arrays.asList(
+                new PodBuilder()
+                    .withNewSpec()
+                    .addNewContainer()
+                    .withName("container_a")
+                    .endContainer()
+                    .endSpec()
+                    .build(),
+                new PodBuilder()
+                    .withNewSpec()
+                    .addNewContainer()
+                    .withName("container_b")
+                    .endContainer()
+                    .endSpec()
+                    .build()));
+
+    ComponentImpl component = new ComponentImpl();
+    component.setType(KUBERNETES_COMPONENT_TYPE);
+    component.setReferenceContent("content");
+    component.setReference("ref");
+
+    EntrypointImpl entrypoint = new EntrypointImpl();
+    entrypoint.setContainerName("container_a");
+    component.setEntrypoints(Collections.singletonList(entrypoint));
+
+    // when
+    validator.validateComponent(component, __ -> "");
+
+    // then no exception is thrown
+  }
+
+  @Test
+  public void shouldValidateContainerMatchingEntrypointInPodMatchingSelector() throws Exception {
+    // given
+    when(kubernetesRecipeParser.parse(any(String.class)))
+        .thenReturn(
+            Arrays.asList(
+                new PodBuilder()
+                    .withNewMetadata()
+                    .addToLabels("app", "test")
+                    .endMetadata()
+                    .withNewSpec()
+                    .addNewContainer()
+                    .withName("container_a")
+                    .endContainer()
+                    .endSpec()
+                    .build(),
+                new PodBuilder()
+                    .withNewMetadata()
+                    .addToLabels("app", "other")
+                    .endMetadata()
+                    .withNewSpec()
+                    .addNewContainer()
+                    .withName("container_a")
+                    .endContainer()
+                    .endSpec()
+                    .build()));
+
+    Map<String, String> selector = new HashMap<>();
+    selector.put("app", "test");
+
+    ComponentImpl component = new ComponentImpl();
+    component.setType(KUBERNETES_COMPONENT_TYPE);
+    component.setReference("ref");
+    component.setSelector(selector);
+    component.setReferenceContent("content");
+
+    EntrypointImpl entrypoint = new EntrypointImpl();
+    entrypoint.setContainerName("container_a");
+    component.setEntrypoints(Collections.singletonList(entrypoint));
+
+    // when
+    validator.validateComponent(component, __ -> "");
+
+    // then no exception is thrown
   }
 
   @Test(
@@ -99,6 +210,56 @@ public class KubernetesComponentIntegrityValidatorTest {
 
     EntrypointImpl entrypoint = new EntrypointImpl();
     entrypoint.setContainerName("not that container");
+    component.setEntrypoints(Collections.singletonList(entrypoint));
+
+    // when
+    validator.validateComponent(component, __ -> "");
+
+    // then exception is thrown
+  }
+
+  @Test(
+      expectedExceptions = DevfileFormatException.class,
+      expectedExceptionsMessageRegExp =
+          "Failed to validate content reference of component 'ref' of type 'kubernetes': Component 'ref' of type 'kubernetes' contains an entry point that doesn't match any container.")
+  public void shouldThrowExceptionOnEntrypointNotMatchingAnyContainerOfPodsMatchingSelector()
+      throws Exception {
+    // given
+    when(kubernetesRecipeParser.parse(any(String.class)))
+        .thenReturn(
+            Arrays.asList(
+                new PodBuilder()
+                    .withNewMetadata()
+                    .addToLabels("app", "test")
+                    .endMetadata()
+                    .withNewSpec()
+                    .addNewContainer()
+                    .withName("container_a")
+                    .endContainer()
+                    .endSpec()
+                    .build(),
+                new PodBuilder()
+                    .withNewMetadata()
+                    .addToLabels("app", "other")
+                    .endMetadata()
+                    .withNewSpec()
+                    .addNewContainer()
+                    .withName("container_b")
+                    .endContainer()
+                    .endSpec()
+                    .build()));
+
+    Map<String, String> selector = new HashMap<>();
+    selector.put("app", "test");
+
+    ComponentImpl component = new ComponentImpl();
+    component.setType(KUBERNETES_COMPONENT_TYPE);
+    component.setReferenceContent("content");
+    component.setReference("ref");
+    component.setSelector(selector);
+
+    EntrypointImpl entrypoint = new EntrypointImpl();
+    entrypoint.setContainerName("container_b");
     component.setEntrypoints(Collections.singletonList(entrypoint));
 
     // when
