59ad85012834239424222649eb940086e3afa7c8
Ihor Okhrimenko <iokhrime@redhat.com>
2019-07-02 10:28:19 +0300

    Changes for debug
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- e2e/pageobjects/ide/QuickOpenContainer.ts
+++ e2e/pageobjects/ide/QuickOpenContainer.ts
@@ -33,6 +33,7 @@ export class QuickOpenContainer {
         const quickContainerItemLocator: By = By.xpath(`//div[@class='quick-open-entry']//span[text()='${itemText}']`);
 
         await this.waitContainer(timeout);
+        await this.driverHelper.wait(2000);
         await this.driverHelper.waitAndClick(quickContainerItemLocator, timeout);
         await this.waitContainerDisappearance();
     }
