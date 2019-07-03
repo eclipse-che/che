90e9427f49162ceeb7d93d94d8cb42e9503f52b7
Denis Kepeschuk <denis.k@tonlabs.io>
2019-07-03 18:37:32 +0500

    Reformatted.

--- plugins/plugin-ton-project/plugin-ton-project-ide/src/main/java/io/tonlabs/ide/inject/TonProjectModule.java
+++ plugins/plugin-ton-project/plugin-ton-project-ide/src/main/java/io/tonlabs/ide/inject/TonProjectModule.java
@@ -13,7 +13,6 @@ package io.tonlabs.ide.inject;
 
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.gwt.inject.client.multibindings.GinMultibinder;
-import io.tonlabs.ide.project.TonCProjectWizardRegistrar;
 import io.tonlabs.ide.project.TonSolProjectWizardRegistrar;
 import org.eclipse.che.ide.api.extension.ExtensionGinModule;
 import org.eclipse.che.ide.api.project.type.wizard.ProjectWizardRegistrar;
@@ -24,7 +23,7 @@ public class TonProjectModule extends AbstractGinModule {
 
   @Override
   protected void configure() {
-    //GinMultibinder.newSetBinder(this.binder(), ProjectWizardRegistrar.class)
+    // GinMultibinder.newSetBinder(this.binder(), ProjectWizardRegistrar.class)
     //    .addBinding()
     //    .to(TonCProjectWizardRegistrar.class);
 
