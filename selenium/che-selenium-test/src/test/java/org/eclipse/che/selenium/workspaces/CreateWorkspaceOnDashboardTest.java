005f4c90525d1b2d1b63102fff003cf4b113c15e
Skorik Sergey <sskoryk@redhat.com>
2019-07-22 15:42:37 +0300

    don't check project cloning in CreateWorkspaceOnDashboardTest. This test only checks workspace creation from Dashboard

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/workspaces/CreateWorkspaceOnDashboardTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/workspaces/CreateWorkspaceOnDashboardTest.java
@@ -16,7 +16,6 @@ import static org.eclipse.che.commons.lang.NameGenerator.generate;
 import com.google.inject.Inject;
 import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
 import org.eclipse.che.selenium.core.user.DefaultTestUser;
-import org.eclipse.che.selenium.core.webdriver.SeleniumWebDriverHelper;
 import org.eclipse.che.selenium.core.workspace.TestWorkspace;
 import org.eclipse.che.selenium.core.workspace.TestWorkspaceProvider;
 import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
@@ -25,8 +24,6 @@ import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Stack;
 import org.eclipse.che.selenium.pageobject.dashboard.workspaces.Workspaces;
 import org.eclipse.che.selenium.pageobject.theia.TheiaIde;
 import org.eclipse.che.selenium.pageobject.theia.TheiaProjectTree;
-import org.eclipse.che.selenium.pageobject.theia.TheiaProposalForm;
-import org.openqa.selenium.Keys;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
@@ -34,12 +31,8 @@ import org.testng.annotations.Test;
 public class CreateWorkspaceOnDashboardTest {
 
   private static final String WS_NAME = generate("workspace", 4);
-  private static final String PROJECT_NAME = "web-java-spring";
-  private static final String WEB_JAVA_SPRING_SAMPLE_URL =
-      "https://github.com/che-samples/web-java-spring.git";
 
   @Inject private TestWorkspaceServiceClient workspaceServiceClient;
-  @Inject private SeleniumWebDriverHelper seleniumWebDriverHelper;
   @Inject private NewWorkspace newWorkspace;
   @Inject private DefaultTestUser defaultTestUser;
   @Inject private Workspaces workspaces;
@@ -47,8 +40,6 @@ public class CreateWorkspaceOnDashboardTest {
   @Inject private TestWorkspaceProvider testWorkspaceProvider;
   @Inject private TheiaIde theiaIde;
   @Inject private TheiaProjectTree theiaProjectTree;
-  @Inject private TheiaProposalForm theiaProposalForm;
-
   // it is used to read workspace logs on test failure
   private TestWorkspace testWorkspace;
 
@@ -89,15 +80,5 @@ public class CreateWorkspaceOnDashboardTest {
     theiaIde.waitNotificationMessageContains("Do you want to exclude");
     theiaIde.clickOnNotificationCloseButton();
     theiaIde.waitNotificationPanelClosed();
-
-    // Import the sample "web-java-spring" project
-    theiaIde.pressKeyCombination(Keys.LEFT_CONTROL, Keys.LEFT_SHIFT, "p");
-    theiaProposalForm.waitForm();
-    theiaProposalForm.enterTextToSearchField(">clo");
-    theiaProposalForm.waitProposal("Git: Clone...");
-    theiaProposalForm.clickOnProposal("Git: Clone...");
-    theiaProposalForm.enterTextToSearchField(WEB_JAVA_SPRING_SAMPLE_URL);
-    seleniumWebDriverHelper.pressEnter();
-    theiaProjectTree.waitItem(PROJECT_NAME);
   }
 }
