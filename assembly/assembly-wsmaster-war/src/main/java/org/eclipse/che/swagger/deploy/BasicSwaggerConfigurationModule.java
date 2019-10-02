da60b11818bbd515478133233d1d5643ae97f573
Sergii Kabashniuk <skabashniuk@redhat.com>
2019-10-02 21:17:27 +0200

    fixup! Setup log4j over slf4j, fixed resource loading
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- assembly/assembly-wsmaster-war/src/main/java/org/eclipse/che/swagger/deploy/BasicSwaggerConfigurationModule.java
+++ assembly/assembly-wsmaster-war/src/main/java/org/eclipse/che/swagger/deploy/BasicSwaggerConfigurationModule.java
@@ -12,11 +12,7 @@
 package org.eclipse.che.swagger.deploy;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.io.Resources;
 import com.google.inject.servlet.ServletModule;
-import java.io.InputStream;
-import java.util.jar.Manifest;
-import org.slf4j.LoggerFactory;
 
 /**
  * Module provide basic swagger configuration
@@ -24,8 +20,6 @@ import org.slf4j.LoggerFactory;
  * @author Sergii Kabashnyuk
  */
 public class BasicSwaggerConfigurationModule extends ServletModule {
-  private static final org.slf4j.Logger LOG =
-      LoggerFactory.getLogger(BasicSwaggerConfigurationModule.class);
 
   @Override
   protected void configureServlets() {
@@ -35,21 +29,8 @@ public class BasicSwaggerConfigurationModule extends ServletModule {
         .with(
             io.swagger.jaxrs.config.DefaultJaxrsConfig.class,
             ImmutableMap.of(
-                "api.version", getCheVersion(),
+                "api.version", "7.x",
                 "swagger.api.title", "Eclipse Che",
                 "swagger.api.basepath", "/api"));
-  }
-
-  private String getCheVersion() {
-    try {;
-      try (InputStream manifestInputStream =
-          Resources.asByteSource(Resources.getResource("/META-INF/MANIFEST.MF")).openStream()) {
-        final Manifest manifest = new Manifest(manifestInputStream);
-        return manifest.getMainAttributes().getValue("Specification-Version");
-      }
-    } catch (Exception e) {
-      LOG.error("Unable to retrieve implementation version from manifest file", e);
-      return "unknown";
-    }
   }
 }
