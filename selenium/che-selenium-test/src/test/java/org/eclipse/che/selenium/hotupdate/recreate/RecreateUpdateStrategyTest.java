86699b7772df5500f57af6316ead46a51aeac7fe
Skorik Sergey <sskoryk@redhat.com>
2019-10-03 15:24:46 +0300

    adapt selenium test from hotupdate.rolling package to work with Che 7 workspaces

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/hotupdate/recreate/RecreateUpdateStrategyTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/hotupdate/recreate/RecreateUpdateStrategyTest.java
@@ -11,54 +11,43 @@
  */
 package org.eclipse.che.selenium.hotupdate.recreate;
 
-import static org.eclipse.che.commons.lang.NameGenerator.generate;
-
 import com.google.inject.Inject;
 import java.io.IOException;
-import java.util.Collections;
 import org.eclipse.che.api.system.shared.SystemStatus;
+import org.eclipse.che.selenium.core.SeleniumWebDriver;
 import org.eclipse.che.selenium.core.TestGroup;
 import org.eclipse.che.selenium.core.client.CheTestSystemClient;
-import org.eclipse.che.selenium.core.client.TestWorkspaceServiceClient;
+import org.eclipse.che.selenium.core.executor.OpenShiftCliCommandExecutor;
 import org.eclipse.che.selenium.core.executor.hotupdate.HotUpdateUtil;
-import org.eclipse.che.selenium.core.user.DefaultTestUser;
-import org.eclipse.che.selenium.pageobject.dashboard.CreateWorkspaceHelper;
-import org.eclipse.che.selenium.pageobject.dashboard.Dashboard;
-import org.eclipse.che.selenium.pageobject.dashboard.NewWorkspace.Devfile;
-import org.eclipse.che.selenium.pageobject.theia.TheiaIde;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
+import org.eclipse.che.selenium.core.requestfactory.CheTestAdminHttpJsonRequestFactory;
+import org.eclipse.che.selenium.core.utils.process.ProcessAgent;
+import org.eclipse.che.selenium.core.workspace.TestWorkspace;
+import org.eclipse.che.selenium.pageobject.CheTerminal;
+import org.eclipse.che.selenium.pageobject.Ide;
+import org.eclipse.che.selenium.pageobject.Menu;
+import org.eclipse.che.selenium.pageobject.ProjectExplorer;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 @Test(groups = {TestGroup.OPENSHIFT, TestGroup.K8S, TestGroup.MULTIUSER})
 public class RecreateUpdateStrategyTest {
-  private static final String WORKSPACE_NAME =
-      generate(RecreateUpdateStrategyTest.class.getSimpleName(), 5);
-
-  @Inject private CheTestSystemClient cheTestSystemClient;
+  @Inject CheTestAdminHttpJsonRequestFactory testUserHttpJsonRequestFactory;
+  @Inject CheTestSystemClient cheTestSystemClient;
+  @Inject ProjectExplorer projectExplorer;
+  @Inject OpenShiftCliCommandExecutor openShiftCliCommandExecutor;
+  @Inject private ProcessAgent processAgent;
+  @Inject private TestWorkspace workspace;
+  @Inject private Ide ide;
+  @Inject private SeleniumWebDriver seleniumWebDriver;
+  @Inject private CheTerminal terminal;
+  @Inject private Menu menu;
   @Inject private HotUpdateUtil hotUpdateUtil;
-  @Inject private CreateWorkspaceHelper createWorkspaceHelper;
-  @Inject private TheiaIde theiaIde;
-  @Inject private TestWorkspaceServiceClient workspaceServiceClient;
-  @Inject private DefaultTestUser defaultTestUser;
-  @Inject private Dashboard dashboard;
-  @Inject private TestWorkspaceServiceClient testWorkspaceServiceClient;
 
   private int cheDeploymentBeforeRollout;
 
   @BeforeClass
   public void setUp() throws IOException {
     cheDeploymentBeforeRollout = hotUpdateUtil.getMasterPodRevision();
-
-    dashboard.open();
-    createWorkspaceHelper.createAndStartWorkspaceFromStack(
-        Devfile.JAVA_MAVEN, WORKSPACE_NAME, Collections.emptyList(), null);
-  }
-
-  @AfterClass
-  public void tearDown() throws Exception {
-    workspaceServiceClient.delete(WORKSPACE_NAME, defaultTestUser.getName());
   }
 
   @Test
@@ -68,17 +57,16 @@ public class RecreateUpdateStrategyTest {
     int requestTimeoutInSec = 6;
 
     // open a user workspace and send request for preparing to shutdown
-    theiaIde.switchToIdeFrame();
-    theiaIde.waitTheiaIde();
-    theiaIde.waitLoaderInvisibility();
-    theiaIde.waitTheiaIdeTopPanel();
+    ide.open(workspace);
 
     cheTestSystemClient.stop();
-    cheTestSystemClient.waitWorkspaceMasterStatus(
-        requestAttempts, requestTimeoutInSec, SystemStatus.READY_TO_SHUTDOWN);
 
     // reopen the workspace and make sure that this one is not available after suspending system
+    ide.open(workspace);
+    projectExplorer.waitProjectExplorerDisappearance(requestTimeoutInSec);
+    terminal.waitTerminalIsNotPresent(requestTimeoutInSec);
 
+    // performs rollout
     hotUpdateUtil.executeMasterPodUpdateCommand();
     cheTestSystemClient.waitWorkspaceMasterStatus(
         requestAttempts, requestTimeoutInSec, SystemStatus.RUNNING);
@@ -86,11 +74,8 @@ public class RecreateUpdateStrategyTest {
     // After rollout updating - deployment should be increased on 1
     hotUpdateUtil.waitMasterPodRevision(cheDeploymentBeforeRollout + 1);
 
-    Assert.assertTrue(testWorkspaceServiceClient.exists(WORKSPACE_NAME, defaultTestUser.getName()));
-
     // make sure that CHE ide is available after updating again
-    theiaIde.waitTheiaIde();
-    theiaIde.waitLoaderInvisibility();
-    theiaIde.waitTheiaIdeTopPanel();
+    ide.open(workspace);
+    ide.waitOpenedWorkspaceIsReadyToUse();
   }
 }
