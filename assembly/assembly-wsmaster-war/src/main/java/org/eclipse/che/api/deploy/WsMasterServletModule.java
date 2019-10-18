64ee025da9418cb755389b23f3f0b941ad3b6247
Sergii Kabashniuk <skabashniuk@redhat.com>
2019-10-18 09:48:31 +0200

    fixup! Disable cors on master. Cleanup cors configuration on ws-agent Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- assembly/assembly-wsmaster-war/src/main/java/org/eclipse/che/api/deploy/WsMasterServletModule.java
+++ assembly/assembly-wsmaster-war/src/main/java/org/eclipse/che/api/deploy/WsMasterServletModule.java
@@ -52,7 +52,7 @@ public class WsMasterServletModule extends ServletModule {
   private boolean isCheCorsEnabled() {
     String cheCorsEnabledEnvVar = System.getenv("CHE_CORS_ENABLED");
     if (cheCorsEnabledEnvVar == null) {
-      // by default CORS should be disable
+      // by default CORS should be disabled
       return false;
     } else {
       return Boolean.valueOf(cheCorsEnabledEnvVar);
