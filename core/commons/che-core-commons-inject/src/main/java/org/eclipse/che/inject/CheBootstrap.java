418e729badc352005e4ab8c9a9c377e9100075f0
Lukas Krejci <lkrejci@redhat.com>
2019-11-26 11:08:04 +0100

    Log all the configuration bindings at INFO so that they appear in the CI logs.
    Signed-off-by: Lukas Krejci <lkrejci@redhat.com>

--- core/commons/che-core-commons-inject/src/main/java/org/eclipse/che/inject/CheBootstrap.java
+++ core/commons/che-core-commons-inject/src/main/java/org/eclipse/che/inject/CheBootstrap.java
@@ -438,24 +438,24 @@ public class CheBootstrap extends EverrestGuiceContextListener {
       String key = prefix == null ? name : (prefix + name);
       Set<String> aliasesForName = bindMap.get(name);
       if (value == null) {
-        LOG.debug("Binding `{}` to `null`", key);
+        LOG.info("Binding `{}` to `null`", key);
         bind(String.class).annotatedWith(Names.named(key)).toProvider(Providers.<String>of(null));
         if (aliasesForName != null) {
           for (String alias : aliasesForName) {
             String bindKey = prefix == null ? alias : prefix + alias;
-            LOG.debug("Binding `{}` to `null`", bindKey);
+            LOG.info("Binding `{}` to `null`", bindKey);
             bind(String.class)
                 .annotatedWith(Names.named(bindKey))
                 .toProvider(Providers.<String>of(null));
           }
         }
       } else {
-        LOG.debug("Binding `{}` to `{}`", key, value);
+        LOG.info("Binding `{}` to `{}`", key, value);
         bindConstant().annotatedWith(Names.named(key)).to(value);
         if (aliasesForName != null) {
           for (String alias : aliasesForName) {
             String bindKey = prefix == null ? alias : prefix + alias;
-            LOG.debug("Binding `{}` to `{}`", bindKey, value);
+            LOG.info("Binding `{}` to `{}`", bindKey, value);
             bindConstant().annotatedWith(Names.named(bindKey)).to(value);
           }
         }
