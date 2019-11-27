5179594fb235be41a21deaefb3f5c679d5b7ebec
Dmytro Nochevnov <dnochevn@redhat.com>
2019-11-27 16:10:06 +0200

    Turn off the tests which check refresh (#15335)
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/workspaces/ProjectStateAfterRefreshTest.java
+++ selenium/che-selenium-test/src/test/java/org/eclipse/che/selenium/workspaces/ProjectStateAfterRefreshTest.java
@@ -12,6 +12,7 @@
 package org.eclipse.che.selenium.workspaces;
 
 import static org.eclipse.che.commons.lang.NameGenerator.generate;
+import static org.eclipse.che.selenium.core.TestGroup.UNDER_REPAIR;
 import static org.eclipse.che.selenium.pageobject.dashboard.ProjectSourcePage.Template.CONSOLE_JAVA_SIMPLE;
 
 import com.google.inject.Inject;
@@ -29,7 +30,11 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-/** @author Andrey chizhikov */
+/**
+ * @author Andrey chizhikov TODO turn on the test after https://github.com/eclipse/che/issues/15317
+ *     resolved
+ */
+@Test(groups = UNDER_REPAIR)
 public class ProjectStateAfterRefreshTest {
   private static final String WORKSPACE_NAME =
       generate(ProjectStateAfterRefreshTest.class.getSimpleName(), 5);
