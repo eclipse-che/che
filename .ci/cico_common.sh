e9c0dca05f036581624695b9fe8a3c7ffdc3690e
Vitalii Parfonov <vparfono@redhat.com>
2019-12-23 11:42:09 +0200

    Testing release
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- .ci/cico_common.sh
+++ .ci/cico_common.sh
@@ -11,11 +11,30 @@
 # update machine, get required deps in place
 # this script assumes its being run on CentOS Linux 7/x86_64
 
+
+function die_with() {
+	echo "$*" >&2
+	exit 1
+}
+
+function getCurrentVersion() {
+    echo $(scl enable rh-maven33 "mvn help:evaluate -Dexpression=project.version -q -DforceStdout")
+}
+
+function getReleaseVersion() {
+    echo "$(echo $1 | cut -d'-' -f1)" #cut SNAPSHOT form the version name
+}
+
+function setReleaseVersionInMavenProject(){
+    scl enable rh-maven33 "mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1"
+}
+
 load_jenkins_vars() {
     set +x
     eval "$(./env-toolkit load -f jenkins-env.json \
                               CHE_BOT_GITHUB_TOKEN \
                               CHE_MAVEN_SETTINGS \
+                              CHE_GITHUB_SSH_KEY \
                               CHE_OSS_SONATYPE_GPG_KEY \
                               CHE_OSS_SONATYPE_PASSPHRASE \
                               QUAY_ECLIPSE_CHE_USERNAME \
@@ -23,15 +42,26 @@ load_jenkins_vars() {
 }
 
 load_mvn_settings_gpg_key() {
-    set -x
-    mkdir $HOME/.m2
     set +x
+    mkdir $HOME/.m2
     #prepare settings.xml for maven and sonatype (central maven repository)
-    echo $CHE_MAVEN_SETTINGS | base64 -d > $HOME/.m2/settings.xml 
+    echo $CHE_MAVEN_SETTINGS | base64 -d > $HOME/.m2/settings.xml
     #load GPG key for sign artifacts
     echo $CHE_OSS_SONATYPE_GPG_KEY | base64 -d > $HOME/.m2/gpg.key
+    #load SSH key for release process
+    echo ${#CHE_OSS_SONATYPE_GPG_KEY}
+    echo $CHE_GITHUB_SSH_KEY | base64 -d > $HOME/.ssh/id_rsa
+    chmod 0400 $HOME/.ssh/id_rsa
+    ssh-keyscan github.com >> ~/.ssh/known_hosts
     set -x
     gpg --import $HOME/.m2/gpg.key
+}
+
+setup_gitconfig() {
+  set +x
+  git config --global github.user che-bot
+  git config --global github.token $CHE_BOT_GITHUB_TOKEN
+
 }
 
 install_deps(){
@@ -60,12 +90,34 @@ build_and_deploy_artifacts() {
     fi
 }
 
+gitHttps2ssh(){
+    #git remote set-url origin git@github.com:$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')
+    #git version 1.8.3 not support get-url sub-command so hardcode url
+    git remote set-url origin git@github.com:eclipse/che-parent
+}
+
+
+setup_gitconfig() {
+  #git config --global github.user che-bot
+  #git config --global github.token $CHE_BOT_GITHUB_TOKEN
+  git config --global user.name "Vitalii Parfonov"
+  git config --global user.email vparfono@redhat.com
+}
+
 releaseProject() {
-    #test 4
-    CUR_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-    TAG = $(echo $CUR_VERSION | cut -d'-' -f1) #cut SNAPSHOT form the version name
-    echo -e "\x1B[92m############### Release: $TAG\x1B[0m"
-    scl enable rh-maven33 "mvn release:prepare release:perform -B -Dresume=false -Dtag=$TAG -DreleaseVersion=$TAG '-Darguments=-DskipTests=true -Dskip-validate-sources -Dgpg.passphrase=$CHE_OSS_SONATYPE_PASSPHRASE -Darchetype.test.skip=true -Dversion.animal-sniffer.enforcer-rule=1.16'"
+    set -x
+    git checkout -f release
+    curVer=$(getCurrentVersion)
+    echo ">>>>>>>> $curVer"
+    tag=$(getReleaseVersion $curVer)
+    echo ">>>>>>>>>> $tag"
+    setReleaseVersionInMavenProject $tag
+    git commit -asm "Release version ${tag}"
+    build_and_deploy_artifacts
+    git tag "${tag}" || die_with "Failed to create tag ${tag}! Release has been deployed, however"
+    git push --tags ||  die_with "Failed to push tags. Please do this manually"
+    publishImagesOnQuayLatest
+    exit 0
 }
 
 
@@ -159,7 +211,7 @@ publishImagesOnQuayLatest() {
     set -e
     set -o pipefail
 
-    TAG = "7.6.0"
+    TAG = $1
     git checkout ${TAG}
     REGISTRY="quay.io"
     ORGANIZATION="eclipse"
