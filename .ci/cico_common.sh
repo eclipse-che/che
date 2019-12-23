2806541eb6d61ca4943d3cdf1f54f48a7cbf57c9
Vitalii Parfonov <vparfono@redhat.com>
2019-12-23 18:13:38 +0200

    Test release
    Signed-off-by: Vitalii Parfonov <vparfono@redhat.com>

--- .ci/cico_common.sh
+++ .ci/cico_common.sh
@@ -93,7 +93,7 @@ build_and_deploy_artifacts() {
 gitHttps2ssh(){
     #git remote set-url origin git@github.com:$(git remote get-url origin | sed 's/https:\/\/github.com\///' | sed 's/git@github.com://')
     #git version 1.8.3 not support get-url sub-command so hardcode url
-    git remote set-url origin git@github.com:eclipse/che-parent
+    git remote set-url origin git@github.com:eclipse/che
 }
 
 
