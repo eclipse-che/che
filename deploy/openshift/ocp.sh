e9eb58f7257e4b4b743556dcaf43a578ae18e88b
musienko maksym <mmusiien@redhat.com>
2020-01-06 15:46:47 +0200

    change oc client version

--- deploy/openshift/ocp.sh
+++ deploy/openshift/ocp.sh
@@ -23,7 +23,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
     DEFAULT_OC_PUBLIC_HOSTNAME="$LOCAL_IP_ADDRESS"
     DEFAULT_OC_PUBLIC_IP="$LOCAL_IP_ADDRESS"
-    DEFAULT_OC_BINARY_DOWNLOAD_URL="https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz"
+    DEFAULT_OC_BINARY_DOWNLOAD_URL="https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz"
 fi
 
 export OC_PUBLIC_HOSTNAME=${OC_PUBLIC_HOSTNAME:-${DEFAULT_OC_PUBLIC_HOSTNAME}}
