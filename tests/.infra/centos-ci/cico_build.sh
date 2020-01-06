00444da5ce631815292220ee7824e59696b2a3e7
musienko maksym <mmusiien@redhat.com>
2020-01-06 12:19:29 +0200

    add parts for starting OKD cluster

--- tests/.infra/centos-ci/cico_build.sh
+++ tests/.infra/centos-ci/cico_build.sh
@@ -1,14 +1,96 @@
-#!/bin/bash
-# Copyright (c) 2017 Red Hat, Inc.
+#!/usr/bin/env bash
+# Copyright (c) 2018 Red Hat, Inc.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 
-# Just a script to get and build eclipse-che locally
-# please send PRs to github.com/kbsingh/build-run-che
+set -e
 
-# update machine, get required deps in place
-# this script assumes its being run on CentOS Linux 7/x86_64
+echo "****** Starting RH-Che PR check $(date) ******"
+total_start_time=$(date +%s)
+export PR_CHECK_BUILD="true"
+export BASEDIR=$(pwd)
 
-echo -e +++++++++++++++++++++++ $(rpm -q centos-release)
+
+
+
+source .ci/functional_tests_utils.sh
+
+echo "Checking credentials:"
+checkAllCreds
+
+echo "Installing dependencies:"
+start=$(date +%s)
+installDependencies
+stop=$(date +%s)
+instal_dep_duration=$(($stop - $start))
+echo "Installing all dependencies lasted $instal_dep_duration seconds."
+
+### DO NOT MERGE!!!
+set -x
+nmcli > nmclioutput
+cat nmclioutput
+
+firewall-cmd --permanent --new-zone dockerc
+firewall-cmd --permanent --zone dockerc --add-source 172.17.0.0/16
+firewall-cmd --permanent --zone dockerc --add-port 8443/tcp
+firewall-cmd --permanent --zone dockerc --add-port 53/udp
+firewall-cmd --permanent --zone dockerc --add-port 8053/udp
+firewall-cmd --reload
+
+
+LOCAL_IP_ADDRESS=$(ip a show | grep -e "scope.*eth0" | grep -v ':' | cut -d/ -f1 | awk 'NR==1{print $2}')
+echo $LOCAL_IP_ADDRESS
+
+oc cluster up --public-hostname="${LOCAL_IP_ADDRESS}" --routing-suffix="${LOCAL_IP_ADDRESS}.nip.io" --loglevel=6
+
+# oc cluster up --loglevel=6
+
+oc login -u system:admin
+oc adm policy add-cluster-role-to-user cluster-admin developer
+oc login -u developer -p pass
+
+bash <(curl -sL  https://www.eclipse.org/che/chectl/) --channel=next
+
+
+echo "====Replace CRD===="
+curl -o org_v1_che_crd.yaml https://raw.githubusercontent.com/eclipse/che-operator/63402ddb5b6ed31c18b397cb477906b4b5cf7c22/deploy/crds/org_v1_che_crd.yaml
+cp org_v1_che_crd.yaml /usr/local/lib/chectl/templates/che-operator/crds/
+
+if chectl server:start -a operator -p openshift --k8spodreadytimeout=360000 --listr-renderer=verbose
+then
+        echo "Started succesfully"
+else
+        echo "==== oc get events ===="
+        oc get events
+        echo "==== oc get all ===="
+        oc get all
+        echo "==== docker ps ===="
+        docker ps
+        echo "==== docker ps -q | xargs -L 1 docker logs ===="
+        docker ps -q | xargs -L 1 docker logs | true
+        oc logs $(oc get pods --selector=component=che -o jsonpath="{.items[].metadata.name}") || true
+        oc logs $(oc get pods --selector=component=keycloak -o jsonpath="{.items[].metadata.name}") || true
+        curl -vL http://keycloak-che.${LOCAL_IP_ADDRESS}.nip.io/auth/realms/che/.well-known/openid-configuration
+        exit 1337
+fi
+
+CHE_ROUTE=$(oc get route che --template='{{ .spec.host }}')
+
+docker run --shm-size=256m -e TS_SELENIUM_BASE_URL="http://$CHE_ROUTE" eclipse/che-e2e:nightly
+
+set +x
+### DO NOT MERGE!!!
+
+# export PROJECT_NAMESPACE=prcheck-${RH_PULL_REQUEST_ID}
+# export DOCKER_IMAGE_TAG="${RH_TAG_DIST_SUFFIX}"-"${RH_PULL_REQUEST_ID}"
+# CHE_VERSION=$(getVersionFromPom)
+# export CHE_VERSION
+
+# echo "Running ${JOB_NAME} PR: #${RH_PULL_REQUEST_ID}, build number #${BUILD_NUMBER} for che-version:${CHE_VERSION}"
+# .ci/cico_build_deploy_test_rhche.sh
+
+# end_time=$(date +%s)
+# whole_check_duration=$(($end_time - $total_start_time))
+# echo "****** PR check ended at $(date) and whole run took $whole_check_duration seconds. ******"
---
+++ tests/.infra/centos-ci/functional_tests_utils.sh
@@ -0,0 +1,191 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2017 Red Hat, Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+
+function installOC() {
+  OC_DIR_NAME=openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit
+  curl -vL  "https://github.com/openshift/origin/releases/download/v3.11.0/${OC_DIR_NAME}.tar.gz" --output ${OC_DIR_NAME}.tar.gz
+  tar -xvf ${OC_DIR_NAME}.tar.gz
+  cp ${OC_DIR_NAME}/oc /usr/local/bin
+}
+
+function installJQ() {
+  installEpelRelease
+  yum install --assumeyes -d1 jq
+}
+
+function installEpelRelease() {
+  if yum repolist | grep epel; then
+    echo "Epel already installed, skipping instalation."
+  else
+    #excluding mirror1.ci.centos.org 
+    echo "exclude=mirror1.ci.centos.org" >> /etc/yum/pluginconf.d/fastestmirror.conf
+    echo "Installing epel..."
+    yum install -d1 --assumeyes epel-release
+    yum update --assumeyes -d1
+  fi
+}
+
+function installYQ() {
+  installEpelRelease
+  yum install --assumeyes -d1 python-pip
+  pip install yq
+}
+
+function installStartDocker() {
+  yum install --assumeyes -d1 yum-utils device-mapper-persistent-data lvm2
+  yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  yum install --assumeyes -d1 docker-ce
+  mkdir -p /etc/docker
+  echo "{ \"insecure-registries\": [\"172.30.0.0/16\"] }" > /etc/docker/daemon.json
+  systemctl start docker
+  docker version
+}
+
+function installMvn() {
+  yum install --assumeyes -d1 centos-release-scl
+  yum install --assumeyes -d1 rh-maven33
+}
+
+function installNodejs() {
+  yum install --assumeyes -d1 rh-nodejs8
+}
+
+function installGit(){
+  yum install --assumeyes -d1 git
+}
+
+function installDependencies() {
+  installEpelRelease
+  installYQ
+  installStartDocker
+  installJQ
+  installOC
+  installGit  
+  # Getting dependencies ready
+  yum install --assumeyes -d1 \
+              patch \
+              pcp \
+              bzip2 \
+              golang \
+              make \
+              java-1.8.0-openjdk \
+              java-1.8.0-openjdk-devel 
+  installMvn
+  installNodejs
+}
+
+function checkAllCreds() {
+  CREDS_NOT_SET="false"
+
+  if [[ -z "${QUAY_USERNAME}" || -z "${QUAY_PASSWORD}" ]]; then
+    echo "Docker registry credentials not set"
+    CREDS_NOT_SET="true"
+  fi
+  
+  if [[ -z "${RH_CHE_AUTOMATION_DEV_CLUSTER_SA_TOKEN}" ]]; then
+    echo "RDU2C credentials not set"
+    CREDS_NOT_SET="true"
+  fi
+
+  if [[ -z "${RH_CHE_AUTOMATION_CHE_PREVIEW_EMAIL}" ]] ||
+     [[ -z "${RH_CHE_AUTOMATION_CHE_PREVIEW_USERNAME}" ]] ||
+     [[ -z "${RH_CHE_AUTOMATION_CHE_PREVIEW_PASSWORD}" ]]; then
+    echo "Prod-preview credentials not set."
+    CREDS_NOT_SET="true"
+  fi
+  
+  if [[ "${CREDS_NOT_SET}" = "true" ]]; then
+    echo "Failed to parse jenkins secure store credentials"
+    exit 2
+  else
+    echo "Credentials set successfully."
+  fi
+}
+
+function archiveArtifacts() {
+  set +e
+  echo "Archiving artifacts from ${DATE} for ${JOB_NAME}/${BUILD_NUMBER}"
+  ls -la ./artifacts.key
+  chmod 600 ./artifacts.key
+  chown $(whoami) ./artifacts.key
+  mkdir -p ./rhche/${JOB_NAME}/${BUILD_NUMBER}/surefire-reports
+  cp ./logs/*.log ./rhche/${JOB_NAME}/${BUILD_NUMBER}/ | true
+  cp -R ./logs/artifacts/screenshots/ ./rhche/${JOB_NAME}/${BUILD_NUMBER}/ | true
+  cp -R ./logs/artifacts/failsafe-reports/ ./rhche/${JOB_NAME}/${BUILD_NUMBER}/ | true
+  cp ./events_report.txt ./rhche/${JOB_NAME}/${BUILD_NUMBER}/ | true
+  rsync --password-file=./artifacts.key -Hva --partial --relative ./rhche/${JOB_NAME}/${BUILD_NUMBER} devtools@artifacts.ci.centos.org::devtools/
+  set -e
+}
+
+function getVersionFromPom() {
+  version=$(scl enable rh-maven33 "mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn org.apache.maven.plugins:maven-help-plugin:evaluate -q -Dexpression=project.parent.version -DforceStdout")
+  echo $version
+}
+
+function getActiveToken() {
+  rm -rf cookie-file loginfile.html
+  if [[ "$USERNAME" == *"preview"* ]] || [[ "$USERNAME" == *"saas"* ]]; then
+    preview="prod-preview."
+  else
+    preview=""
+  fi
+
+  response=$(curl -s -g -X GET --header 'Accept: application/json' "https://api.${preview}openshift.io/api/users?filter[username]=$USERNAME")
+  data=$(echo "$response" | jq .data)
+  if [ "$data" == "[]" ]; then
+    exit 1
+  fi        
+
+  #get html of developers login page
+  curl -sX GET -L -c cookie-file -b cookie-file "https://auth.${preview}openshift.io/api/login?redirect=https://che.openshift.io" > loginfile.html
+
+  #get url for login from form
+  url=$(grep "form id" loginfile.html | grep -o 'http.*.tab_id=.[^\"]*')
+  dataUrl="username=$USERNAME&password=$PASSWORD&login=Log+in"
+  url=${url//\&amp;/\&}
+
+  #send login and follow redirects  
+  set +e
+  url=$(curl -w '%{redirect_url}' -s -X POST -c cookie-file -b cookie-file -d "$dataUrl" "$url")
+  found=$(echo "$url" | grep "token_json")
+
+  while true 
+  do
+    url=$(curl -c cookie-file -b cookie-file -s -o /dev/null -w '%{redirect_url}' "$url")
+    if [[ ${#url} == 0 ]]; then
+      #all redirects were done but token was not found
+      break
+    fi
+    found=$(echo "$url" | grep "token_json")
+    if [[ ${#found} -gt 0 ]]; then
+      #some redirects were done and token was found as a part of url
+      break
+    fi
+  done
+  set -e
+
+  #extract active token
+  token=$(echo "$url" | grep -o "ey.[^%]*" | head -1)
+  if [[ ${#token} -gt 0 ]]; then
+    echo ${token}
+  else
+    exit 1
+  fi
+}
+
+function getVersionFromProdPreview() {
+  token=$(getActiveToken)
+  version=$(curl -s -X OPTIONS --header "Content-Type: application/json" --header "Authorization: Bearer ${token}" https://che.prod-preview.openshift.io/api/ | jq '.buildInfo')
+  echo ${version//\"/}
+}
+
+function getVersionFromProd() {
+  token=$(getActiveToken)
+  version=$(curl -s -X OPTIONS --header "Content-Type: application/json" --header "Authorization: Bearer ${token}" https://che.openshift.io/api/ | jq '.buildInfo')
+  echo ${version//\"/}
+}
