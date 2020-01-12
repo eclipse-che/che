6bf6ed1b243c8221bcc74be22646e73352249592
Aleksandr Shmaraiev <oshmarai@redhat.com>
2020-01-12 18:08:09 +0200

    Test E2E: Modify 'InstallCheByOperatorHub' to use it for update application (#15590)
    * Modify 'InstallCheByOperatorHub' to use it for update application
    * Create e2e 'SubscribeCheByOperatorHub' test
    * Create 'mocha-che-subscribe-operator' file
    * Add new test suite to 'package.json' file
    * Update 'mocha-che-operatorhub' file
    * Remove unused variable from 'TestConstants'
    * Change timeout in 'SubscribeCheByOperatorHub' e2e test

--- tests/e2e/tests/e2e_install_che_operatorhub/InstallCheByOperatorHub.spec.ts
+++ tests/e2e/tests/e2e_install_che_operatorhub/InstallCheByOperatorHub.spec.ts
@@ -9,61 +9,13 @@
  **********************************************************************/
 
 import { e2eContainer } from '../../inversify.config';
-import { IOcpLoginPage } from '../../pageobjects/login/IOcpLoginPage';
-import { CLASSES, TYPES } from '../../inversify.types';
-import { OcpLoginPage } from '../../pageobjects/openshift/OcpLoginPage';
+import { CLASSES } from '../../inversify.types';
 import { OcpWebConsolePage } from '../../pageobjects/openshift/OcpWebConsolePage';
 import { TestConstants } from '../../TestConstants';
 
-const ocpLogin: IOcpLoginPage = e2eContainer.get<IOcpLoginPage>(TYPES.OcpLogin);
-const ocpLoginPage: OcpLoginPage = e2eContainer.get(CLASSES.OcpLoginPage);
 const ocpWebConsole: OcpWebConsolePage = e2eContainer.get(CLASSES.OcpWebConsolePage);
 
 suite('E2E', async () => {
-
-    suite('Go to OCP and wait console OpenShift', async () => {
-        test('Open login page', async () => {
-            await ocpLoginPage.openLoginPageOpenShift(TestConstants.TS_SELENIUM_WEB_CONSOLE_OCP_URL);
-            await ocpLoginPage.waitOpenShiftLoginPage();
-        });
-        test('Log into OCP', async () => {
-            ocpLogin.login();
-        });
-    });
-
-    suite('Subscribe Operator to defined namespace', async () => {
-        test('Go to the OperatorHub main page', async () => {
-            await ocpWebConsole.waitNavpanelOpenShift();
-            await ocpWebConsole.openOperatorHubMainPageByUrl(TestConstants.TS_SELENIUM_WEB_CONSOLE_OCP_URL + '/operatorhub/all-namespaces');
-            await ocpWebConsole.waitOperatorHubMainPage();
-        });
-
-        test('Select Operator from catalog and install it', async () => {
-            await ocpWebConsole.clickOnCatalogOperatorIcon();
-            await ocpWebConsole.clickOnInstallButton();
-        });
-
-        test('Select a namespace and subscribe Operator', async () => {
-            await ocpWebConsole.waitCreateOperatorSubscriptionPage();
-            await ocpWebConsole.selectUpdateChannelOnSubscriptionPage();
-            await ocpWebConsole.clickOnDropdownNamespaceListOnSubscriptionPage();
-            await ocpWebConsole.waitListBoxNamespacesOnSubscriptionPage();
-            await ocpWebConsole.selectDefinedNamespaceOnSubscriptionPage();
-            await ocpWebConsole.clickOnSubscribeButtonOnSubscriptionPage();
-        });
-
-    });
-
-    suite('Wait the operator is represented by CSV', async () => {
-        test('Select the Installed Operators in the nav panel', async () => {
-            await ocpWebConsole.waitNavpanelOpenShift();
-            await ocpWebConsole.selectInstalledOperatorsOnNavPanel();
-        });
-
-        test('Wait installed Operator', async () => {
-            await ocpWebConsole.waitInstalledOperatorLogoName();
-        });
-    });
 
     suite('Create new Che cluster', async () => {
         test('Click on the logo-name Che operator', async () => {
