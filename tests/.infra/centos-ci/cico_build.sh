5a311d51759d51df600702aacc28e5e333538a5d
musienko maksym <mmusiien@redhat.com>
2020-01-14 20:02:40 +0200

    check artifacts att7

--- tests/.infra/centos-ci/cico_build.sh
+++ tests/.infra/centos-ci/cico_build.sh
@@ -6,7 +6,8 @@
 # http://www.eclipse.org/legal/epl-v10.html
 
 function archiveArtifacts1(){
- find / -name artifacts.key 2>&1 | grep -v "Permission denied"
+  echo "FIND artifacts: "
+  find / -name artifacts.key 2>&1 | grep -v "Permission denied"
   set +e
   JOB_NAME=che-nightly
   echo "Archiving artifacts from ${DATE} for ${JOB_NAME}/${BUILD_NUMBER}"
