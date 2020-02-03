45604b90ef68caea3238f2bc8faf9759c6828d60
Dmytro Nochevnov <dnochevn@redhat.com>
2020-02-04 00:04:18 +0200

    Turn on E2E tests which check restore of browser's tabs (#15886)
    * Turn on E2E tests which check restore of browser's tabs
    * Turn off failing test ProjectStateAfterWorkspaceRestartTest
    * Notify kfoniok@redhat.com about nightly tests failure; retry che installation 2 times
    
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- tests/legacy-e2e/che-selenium-test/src/test/java/org/eclipse/che/selenium/workspaces/ProjectStateAfterRenameWorkspaceTest.java
+++ tests/legacy-e2e/che-selenium-test/src/test/java/org/eclipse/che/selenium/workspaces/ProjectStateAfterRenameWorkspaceTest.java
@@ -12,7 +12,6 @@
 package org.eclipse.che.selenium.workspaces;
 
 import static org.eclipse.che.commons.lang.NameGenerator.generate;
-import static org.eclipse.che.selenium.core.TestGroup.UNDER_REPAIR;
 import static org.eclipse.che.selenium.pageobject.dashboard.ProjectSourcePage.Template.CONSOLE_JAVA_SIMPLE;
 
 import com.google.inject.Inject;
@@ -36,7 +35,7 @@ import org.testng.annotations.Test;
  * @author Aleksandr Shmaraev TODO turn on the test after
  *     https://github.com/eclipse/che/issues/15317 resolved
  */
-@Test(groups = UNDER_REPAIR)
+@Test
 public class ProjectStateAfterRenameWorkspaceTest {
   private static final String WORKSPACE_NAME =
       generate(ProjectStateAfterRenameWorkspaceTest.class.getSimpleName(), 5);
