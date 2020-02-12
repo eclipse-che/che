8375eb4c671159e27b021ca6989d742230244c8b
Skorik Sergey <sskoryk@redhat.com>
2020-02-12 15:26:46 +0200

    revert changes in cico_pr_test.sh

--- tests/.infra/centos-ci/cico_pr_test.sh
+++ tests/.infra/centos-ci/cico_pr_test.sh
@@ -19,9 +19,9 @@ function prepareCustomResourceFile() {
   cd /tmp
   wget https://raw.githubusercontent.com/eclipse/che-operator/master/deploy/crds/org_v1_che_cr.yaml -O custom-resource.yaml
   sed -i "s@server:@server:\n    customCheProperties:\n      CHE_LIMITS_USER_WORKSPACES_RUN_COUNT: '-1'@g" /tmp/custom-resource.yaml
-#  sed -i "s/customCheProperties:/customCheProperties:\n      CHE_WORKSPACE_AGENT_DEV_INACTIVE__STOP__TIMEOUT__MS: '300000'/" /tmp/custom-resource.yaml
-#  sed -i "s@cheImage: ''@cheImage: 'quay.io/eclipse/che-server'@g" /tmp/custom-resource.yaml
-#  sed -i "s@cheImageTag: 'nightly'@cheImageTag: '${TAG}'@g" /tmp/custom-resource.yaml
+  sed -i "s/customCheProperties:/customCheProperties:\n      CHE_WORKSPACE_AGENT_DEV_INACTIVE__STOP__TIMEOUT__MS: '300000'/" /tmp/custom-resource.yaml
+  sed -i "s@cheImage: ''@cheImage: 'quay.io/eclipse/che-server'@g" /tmp/custom-resource.yaml
+  sed -i "s@cheImageTag: 'nightly'@cheImageTag: '${TAG}'@g" /tmp/custom-resource.yaml
   cat /tmp/custom-resource.yaml
 }
 
@@ -38,8 +38,8 @@ function pushImageToRegistry() {
 setupEnvs
 installDependencies
 installDockerCompose
-#buidCheServer
-#pushImageToRegistry
+buidCheServer
+pushImageToRegistry
 installKVM
 installAndStartMinishift
 loginToOpenshiftAndSetDevRole
@@ -50,7 +50,7 @@ deployCheIntoCluster  --chenamespace=eclipse-che --che-operator-cr-yaml=/tmp/cus
 seleniumTestsSetup
 createIndentityProvider
 
-bash /root/payload/tests/legacy-e2e/che-selenium-test/selenium-tests.sh --threads=1 --host=${CHE_ROUTE} --port=80 --multiuser --test=org.eclipse.che.selenium.hotupdate.rolling.**
+bash /root/payload/tests/legacy-e2e/che-selenium-test/selenium-tests.sh --threads=3 --host=${CHE_ROUTE} --port=80 --multiuser
 
 saveSeleniumTestResult
 getOpenshiftLogs
