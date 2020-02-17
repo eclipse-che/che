68c0ba8991ea07d979dfac7932c09bec0552bcd5
Max Shaposhnik <mshaposh@redhat.com>
2020-02-17 11:47:34 +0200

    Fixup javadoc
    Co-Authored-By: Sergii Leshchenko <sleshche@redhat.com>

--- infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/limits/ram/ContainerResourceLimitRequestProvisioner.java
+++ infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/limits/ram/ContainerResourceLimitRequestProvisioner.java
@@ -50,7 +50,7 @@ import org.eclipse.che.workspace.infrastructure.kubernetes.util.KubernetesSize;
  *
  * <ul>
  *   <li>che.workspace.default_cpu_limit_cores - defines default machine CPU limit
- *   <li>-che.workspace.default_cpu_request_cores - defines default machine CPU request
+ *   <li>che.workspace.default_cpu_request_cores - defines default machine CPU request
  * </ul>
  *
  * @author Anton Korneta
