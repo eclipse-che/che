eed09a833c036c030a190ce70e38c5131c32ac5c
Aleksandr Shmaraiev <oshmarai@redhat.com>
2020-02-20 11:02:41 +0200

    Add scripts to launch 'che-rc' jobs on the 'centos-ci' (#16039)

---
+++ tests/.infra/centos-ci/rc/cico-rc-multiuser-happy-path-test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Copyright (c) 2020 Red Hat, Inc.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+set -e
+set +x
+
+source tests/.infra/centos-ci/functional_tests_utils.sh
+source tests/.infra/centos-ci/rc/rc_function_util.sh
+
+ # It needs to implement the patch of 'devfile' yaml to appropriate release tags images
+
+setupEnvs
+installKVM
+installDependencies
+setupReleaseVersionAndTag
+installAndStartMinishift
+loginToOpenshiftAndSetDevRole
+prepareCustomResourceFile false
+installCheCtl
+deployCheIntoCluster  --che-operator-cr-yaml=/tmp/custom-resource.yaml
+createTestUserAndObtainUserToken
+createTestWorkspaceAndRunTest  --devfile=https://raw.githubusercontent.com/eclipse/che/cico-rc-test/tests/e2e/files/happy-path/happy-path-workspace.yaml
+getOpenshiftLogs
+archiveArtifacts "rc-multiuser-happy-path-test"
+if [[ "$IS_TESTS_FAILED" == "true" ]]; then exit 1; fi
