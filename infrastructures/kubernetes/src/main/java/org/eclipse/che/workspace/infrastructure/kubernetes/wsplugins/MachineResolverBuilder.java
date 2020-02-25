8f3e650677f4821d038a63d246aa20b9483d37b1
Max Shaposhnik <mshaposh@redhat.com>
2020-02-25 09:49:31 +0200

    Fixup messsage

--- infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/wsplugins/MachineResolverBuilder.java
+++ infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/wsplugins/MachineResolverBuilder.java
@@ -40,7 +40,7 @@ public class MachineResolverBuilder {
         || defaultSidecarCpuRequestAttribute == null
         || containerEndpoints == null
         || projectsRootPathEnvVar == null) {
-      throw new IllegalStateException("Unable to build MachineResolver cause some fields are null");
+      throw new IllegalStateException("Unable to build MachineResolver because some fields are null");
     }
 
     return new MachineResolver(
