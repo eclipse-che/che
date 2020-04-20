14f528dd85772be5eff9f688ec0bae195b5f603a
Michal Vala <sparkoo@users.noreply.github.com>
2020-04-20 21:27:19 +0200

    Gh15126 plugin endpoints (#16669)
    * devfile endpoints for editor and plugin components
    
    Signed-off-by: Michal Vala <mvala@redhat.com>
    
    * fail when devfile endpoint is trying to override plugin's one
    
    Signed-off-by: Michal Vala <mvala@redhat.com>
    
    * improve exception message
    
    Signed-off-by: Michal Vala <mvala@redhat.com>
    
    * Update MachineResolver.java
    
    exception text grammar

--- infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/wsplugins/MachineResolverTest.java
+++ infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/wsplugins/MachineResolverTest.java
@@ -20,14 +20,17 @@ import static org.eclipse.che.api.core.model.workspace.config.MachineConfig.MEMO
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
 
+import com.google.common.collect.ImmutableMap;
 import io.fabric8.kubernetes.api.model.Container;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.eclipse.che.api.core.model.workspace.config.ServerConfig;
 import org.eclipse.che.api.workspace.server.model.impl.ServerConfigImpl;
 import org.eclipse.che.api.workspace.server.model.impl.VolumeImpl;
 import org.eclipse.che.api.workspace.server.model.impl.devfile.ComponentImpl;
+import org.eclipse.che.api.workspace.server.model.impl.devfile.EndpointImpl;
 import org.eclipse.che.api.workspace.server.spi.InfrastructureException;
 import org.eclipse.che.api.workspace.server.spi.environment.InternalMachineConfig;
 import org.eclipse.che.api.workspace.server.wsplugins.model.CheContainer;
@@ -291,6 +294,34 @@ public class MachineResolverTest {
     assertEquals(2, config.getVolumes().size());
     assertEquals("/bar", config.getVolumes().get("foo").getPath());
     assertEquals("/foo/test", config.getVolumes().get("test").getPath());
+  }
+
+  @Test(expectedExceptions = InfrastructureException.class)
+  public void shouldFailWhenEndpointNameAlreadyExist() throws InfrastructureException {
+    // given
+    ChePluginEndpoint pluginEndpoint = new ChePluginEndpoint();
+    pluginEndpoint.setName("test-endpoint");
+    endpoints.add(pluginEndpoint);
+    component.setEndpoints(
+        Collections.singletonList(new EndpointImpl("test-endpoint", 8080, Collections.emptyMap())));
+
+    // when -> then exception
+    resolver.resolve();
+  }
+
+  @Test
+  public void shouldAddEndpointsFromDevfileComponent() throws InfrastructureException {
+    component.setEndpoints(
+        asList(
+            new EndpointImpl("endpoint8080", 8080, Collections.emptyMap()),
+            new EndpointImpl("endpoint9999", 9999, ImmutableMap.of("secure", "true"))));
+
+    InternalMachineConfig config = resolver.resolve();
+
+    assertEquals(config.getServers().size(), 2);
+    assertEquals(config.getServers().get("endpoint8080").getPort(), "8080");
+    assertEquals(config.getServers().get("endpoint9999").getPort(), "9999");
+    assertEquals(config.getServers().get("endpoint9999").getAttributes().get("secure"), "true");
   }
 
   private static String toBytesString(String k8sMemorySize) {
