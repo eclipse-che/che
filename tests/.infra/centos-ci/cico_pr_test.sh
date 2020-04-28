d9408f6220568f004d9603e1e28e614f3e6da950
Skorik Sergey <sskoryk@redhat.com>
2020-04-28 18:17:07 +0300

    restore cico_pr_test script

--- tests/.infra/centos-ci/cico_pr_test.sh
+++ tests/.infra/centos-ci/cico_pr_test.sh
@@ -1,34 +1,67 @@
 #!/usr/bin/env bash
-# Copyright (c) 2018 Red Hat, Inc.
+# Copyright (c) 2020 Red Hat, Inc.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
-set -e
 
-echo "========Starting nigtly test job $(date)========"
+set -ex
 
 source tests/.infra/centos-ci/functional_tests_utils.sh
-source .ci/cico_common.sh
+
+eval "$(./env-toolkit load -f jenkins-env.json -r ^ghprbPullId)"
+
+export PULL_REQUEST_ID="${ghprbPullId}"
+export TAG=PR-${PULL_REQUEST_ID}
 
 function prepareCustomResourceFile() {
   cd /tmp
   wget https://raw.githubusercontent.com/eclipse/che-operator/master/deploy/crds/org_v1_che_cr.yaml -O custom-resource.yaml
+  sed -i "s@server:@server:\n    customCheProperties:\n      CHE_LIMITS_USER_WORKSPACES_RUN_COUNT: '-1'@g" /tmp/custom-resource.yaml
+  sed -i "s/customCheProperties:/customCheProperties:\n      CHE_WORKSPACE_AGENT_DEV_INACTIVE__STOP__TIMEOUT__MS: '300000'/" /tmp/custom-resource.yaml
+  sed -i "s@cheImage: ''@cheImage: 'quay.io/eclipse/che-server'@g" /tmp/custom-resource.yaml
+  sed -i "s@cheImageTag: 'nightly'@cheImageTag: '${TAG}'@g" /tmp/custom-resource.yaml
   sed -i "s@tlsSupport: true@tlsSupport: false@g" /tmp/custom-resource.yaml
   cat /tmp/custom-resource.yaml
 }
 
+function buidCheServer() {
+  mvn clean install -Pintegration
+  bash dockerfiles/che/build.sh --organization:quay.io/eclipse --tag:${TAG} --dockerfile:Dockerfile
+}
+
+function pushImageToRegistry() {
+  docker login -u "${QUAY_ECLIPSE_CHE_USERNAME}" -p "${QUAY_ECLIPSE_CHE_PASSWORD}" "quay.io"
+  docker push "quay.io/eclipse/che-server:${TAG}"
+}
+
 setupEnvs
-installKVM
 installDependencies
-installCheCtl
+installDockerCompose
+buidCheServer
+pushImageToRegistry
+installKVM
 installAndStartMinishift
 loginToOpenshiftAndSetDevRole
 prepareCustomResourceFile
-deployCheIntoCluster --chenamespace=che --che-operator-cr-yaml=/tmp/custom-resource.yaml
-createTestUserAndObtainUserToken
-createTestWorkspaceAndRunTest
+installCheCtl
+
+deployCheIntoCluster  --chenamespace=eclipse-che --che-operator-cr-yaml=/tmp/custom-resource.yaml
+seleniumTestsSetup
+createIndentityProvider
+
+bash /root/payload/tests/legacy-e2e/che-selenium-test/selenium-tests.sh \
+  --threads=3 \
+  --host=${CHE_ROUTE} \
+  --port=80 \
+  --multiuser \
+  --fail-script-on-failed-tests \
+  || IS_TESTS_FAILED=true
+
+
 echo "=========================== THIS IS POST TEST ACTIONS =============================="
+saveSeleniumTestResult
 getOpenshiftLogs
-archiveArtifacts "che-nightly-happy-path"
+archiveArtifacts "che-pullrequests-java-selenium-tests"
+
 if [[ "$IS_TESTS_FAILED" == "true" ]]; then exit 1; fi
