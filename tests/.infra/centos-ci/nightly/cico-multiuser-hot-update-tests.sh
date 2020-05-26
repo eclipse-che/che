4bdfd363bd0d699ff4cb58c823538b5c23b4bd76
Sergey Skorik <sskoryk@redhat.com>
2020-05-26 17:41:42 +0300

    [Selenium] Change workspace limit for hotupdate tests Jenkins job

--- tests/.infra/centos-ci/nightly/cico-multiuser-hot-update-tests.sh
+++ tests/.infra/centos-ci/nightly/cico-multiuser-hot-update-tests.sh
@@ -13,6 +13,7 @@ source tests/.infra/centos-ci/functional_tests_utils.sh
 function prepareCustomResourceFile() {
   cd /tmp
   wget https://raw.githubusercontent.com/eclipse/che-operator/master/deploy/crds/org_v1_che_cr.yaml -O custom-resource.yaml
+  sed -i "s@server:@server:\n    customCheProperties:\n      CHE_LIMITS_USER_WORKSPACES_RUN_COUNT: '-1'@g" /tmp/custom-resource.yaml
   sed -i "s@tlsSupport: true@tlsSupport: false@g" /tmp/custom-resource.yaml
   sed -i "s@identityProviderPassword: ''@identityProviderPassword: 'admin'@g" /tmp/custom-resource.yaml
   cat /tmp/custom-resource.yaml
