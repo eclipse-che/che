6423f439beadf4c8ac0e0e0372cd81cd55b5e78b
kkanova <kkanova@redhat.com>
2020-05-26 12:48:56 +0200

    Move error handling to Ide class, use devfile from devfile-registry.

--- tests/e2e/tests/e2e/FactoryUrl.spec.ts
+++ tests/e2e/tests/e2e/FactoryUrl.spec.ts
@@ -1,5 +1,5 @@
 // /*********************************************************************
-//  * Copyright (c) 2019 Red Hat, Inc.
+//  * Copyright (c) 2020 Red Hat, Inc.
 //  *
 //  * This program and the accompanying materials are made
 //  * available under the terms of the Eclipse Public License 2.0
@@ -12,40 +12,27 @@ import { e2eContainer } from '../../inversify.config';
 import { CLASSES } from '../../inversify.types';
 import { TestConstants } from '../../TestConstants';
 import { DriverHelper } from '../../utils/DriverHelper';
-import { Ide } from '../../pageobjects/ide/Ide';
-import { ProjectTree } from '../../pageobjects/ide/ProjectTree';
-import { error } from 'selenium-webdriver';
-import { Logger } from '../../utils/Logger';
 import { WorkspaceNameHandler } from '../..';
 import * as workspaceHandling from '../../testsLibrary/WorksapceHandlingTests';
+import * as projectAndFileTests from '../../testsLibrary/ProjectAndFileTests';
 
 const driverHelper: DriverHelper = e2eContainer.get(CLASSES.DriverHelper);
-const ide : Ide = e2eContainer.get(CLASSES.Ide);
-const projectTree : ProjectTree = e2eContainer.get(CLASSES.ProjectTree);
 
 // the suite expect user to be logged in
 suite('Workspace creation via factory url', async () => {
 
-    let factoryUrl : string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=https://gist.githubusercontent.com/Katka92/fe747fa93b8275abb2fb9f3803de1f0f/raw/33c811ac1fe03715bb51fdd6f84acc6211ba405e/load-test-devfile.yaml`;
+    let factoryUrl : string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=https://raw.githubusercontent.com/eclipse/che-devfile-registry/master/devfiles/java-maven/devfile.yaml`;
+    const workspaceSampleName: string = 'console-java-simple';
+    const workspaceRootFolderName: string = 'src';
 
-    test('Open factory URL', async () => {
-        driverHelper.navigateToUrl(factoryUrl);
+    suite('Open factory URL', async () => {
+        test(`Navigating to factory URL`, async () => {
+            await driverHelper.navigateToUrl(factoryUrl);
+        });
     });
 
-    test('Wait workspace readyness', async () => {
-        try {
-            await ide.waitAndSwitchToIdeFrame();
-        } catch (err) {
-            if (err instanceof error.StaleElementReferenceError) {
-                Logger.warn('StaleElementException occured during waiting for IDE. Sleeping for 2 secs and retrying.');
-                driverHelper.wait(2000);
-                await ide.waitAndSwitchToIdeFrame();
-            }
-        }
-        await ide.waitPreloaderVisible();
-        await ide.waitPreloaderAbsent();
-        await ide.waitIde();
-        await projectTree.openProjectTreeContainer();
+    suite('Wait workspace readyness', async () => {
+        projectAndFileTests.waitWorkspaceReadiness(workspaceSampleName, workspaceRootFolderName);
     });
 
     suite ('Stopping and deleting the workspace', async () => {
