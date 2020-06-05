9167e98a80a3dda28df000a131e40467fa2004e5
Ihor Okhrimenko <iokhrime@redhat.com>
2020-06-05 11:53:44 +0300

    Update the 'che-util.sh' script
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- .ci/common-qe/che-util.sh
+++ .ci/common-qe/che-util.sh
@@ -16,22 +16,13 @@ SCRIPT_PATH="${BASH_SOURCE[0]}"
 SCRIPT_DIR="$(dirname $SCRIPT_PATH)"
 . $SCRIPT_DIR/common-util.sh
 
-USERNAME=$(readConfigProperty test.username)
-PASSWORD=$(readConfigProperty test.password)
-SUITE=$(readConfigProperty test.suite)
-MULTIUSER=$(readConfigProperty test.multiuser)
-DEFAULT_TIMEOUT=$(readConfigProperty timeout.default)
-WORKSPACE_STATUS_POLLING=$(readConfigProperty timeout.workspace.status.polling)
-LOAD_PAGE_TIMEOUT=$(readConfigProperty timeout.load.page)
-DEVFILE_URL=$(readConfigProperty test.workspace.devfile.url)
-ADDITIONAL_OPTIONS=$(readConfigProperty test.additional.options)
-
-
 function obtainUserToken() {
+    local username=$(readConfigProperty test.username)
+    local password=$(readConfigProperty test.password)
     KEYCLOAK_URL=$(oc get checluster eclipse-che -o jsonpath='{.status.keycloakURL}')
     KEYCLOAK_BASE_URL="${KEYCLOAK_URL}/auth"
     
-    local userAccessToken=$(curl -k -v -X POST $KEYCLOAK_BASE_URL/realms/che/protocol/openid-connect/token -H "Content-Type: application/x-www-form-urlencoded" -d "username=${USERNAME}" -d "password=${PASSWORD}" -d "grant_type=password" -d "client_id=che-public" | jq -r .access_token)
+    local userAccessToken=$(curl -k -v -X POST $KEYCLOAK_BASE_URL/realms/che/protocol/openid-connect/token -H "Content-Type: application/x-www-form-urlencoded" -d "username=${username}" -d "password=${password}" -d "grant_type=password" -d "client_id=che-public" | jq -r .access_token)
     
     echo "$userAccessToken"
 }
@@ -46,13 +37,13 @@ function createServerPatchFile(){
         echo "Patch template has not been provided"
         exit 1
     fi
-
+    
     echo "$1" > /tmp/che-cr-patch.yaml
 }
 
 function startCheServer(){
     createServerPatchFile "$1"
-
+    
     if chectl server:start --listr-renderer=verbose -a operator -p openshift --k8spodreadytimeout=360000 --che-operator-cr-patch-yaml=/tmp/che-cr-patch.yaml; then
         echo "Started succesfully"
         oc get checluster -o yaml
@@ -68,13 +59,22 @@ function startCheServer(){
 }
 
 function createTestWorkspace(){
+    local devfile_url=$(readConfigProperty test.workspace.devfile.url)
     local userAccessToken=$(obtainUserToken)
     
-    chectl workspace:create --start --access-token "$userAccessToken" --devfile="$DEVFILE_URL"
+    chectl workspace:create --start --access-token "$userAccessToken" --devfile="$devfile_url"
 }
 
 function runTest() {
-    CHE_URL=$(oc get checluster eclipse-che -o jsonpath='{.status.cheURL}')
+    local username=$(readConfigProperty test.username)
+    local password=$(readConfigProperty test.password)
+    local suite=$(readConfigProperty test.suite)
+    local multiuser=$(readConfigProperty test.multiuser)
+    local default_timeout=$(readConfigProperty timeout.default)
+    local workspace_status_polling=$(readConfigProperty timeout.workspace.status.polling)
+    local load_page_timeout=$(readConfigProperty timeout.load.page)
+    local additional_options=$(readConfigProperty test.additional.options)
+    local che_url=$(oc get checluster eclipse-che -o jsonpath='{.status.cheURL}')
     
     # ### Create directory for report
     cd /root/payload
@@ -83,17 +83,17 @@ function runTest() {
     
     ### Run tests
     docker run --shm-size=1g --net=host  --ipc=host -v $REPORT_FOLDER:/tmp/e2e/report:Z \
-    -e TS_SELENIUM_BASE_URL="$CHE_URL" \
+    -e TS_SELENIUM_BASE_URL="$che_url" \
     -e TS_SELENIUM_LOG_LEVEL=DEBUG \
-    -e TS_SELENIUM_MULTIUSER="$MULTIUSER" \
-    -e TS_SELENIUM_USERNAME="$USERNAME" \
-    -e TS_SELENIUM_PASSWORD="$PASSWORD" \
-    -e TS_SELENIUM_DEFAULT_TIMEOUT="$DEFAULT_TIMEOUT" \
-    -e TS_SELENIUM_WORKSPACE_STATUS_POLLING="$WORKSPACE_STATUS_POLLING" \
-    -e TS_SELENIUM_LOAD_PAGE_TIMEOUT="$LOAD_PAGE_TIMEOUT" \
-    -e TEST_SUITE="$SUITE" \
+    -e TS_SELENIUM_MULTIUSER="$multiuser" \
+    -e TS_SELENIUM_USERNAME="$username" \
+    -e TS_SELENIUM_PASSWORD="$password" \
+    -e TS_SELENIUM_DEFAULT_TIMEOUT="$default_timeout" \
+    -e TS_SELENIUM_WORKSPACE_STATUS_POLLING="$workspace_status_polling" \
+    -e TS_SELENIUM_LOAD_PAGE_TIMEOUT="$load_page_timeout" \
+    -e TEST_SUITE="$suite" \
     -e NODE_TLS_REJECT_UNAUTHORIZED=0 \
-    $ADDITIONAL_OPTIONS quay.io/eclipse/che-e2e:nightly || IS_TESTS_FAILED=true
+    $additional_options quay.io/eclipse/che-e2e:nightly || IS_TESTS_FAILED=true
     
     export IS_TESTS_FAILED
 }
