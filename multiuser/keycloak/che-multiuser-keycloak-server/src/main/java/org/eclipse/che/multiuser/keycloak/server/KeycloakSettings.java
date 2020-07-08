1ffda237e19d2eace0253f4f41c2fe3fbff82abd
Max Shaposhnik <mshaposh@redhat.com>
2020-07-08 09:38:58 +0300

    Review fixups

--- multiuser/keycloak/che-multiuser-keycloak-server/src/main/java/org/eclipse/che/multiuser/keycloak/server/KeycloakSettings.java
+++ multiuser/keycloak/che-multiuser-keycloak-server/src/main/java/org/eclipse/che/multiuser/keycloak/server/KeycloakSettings.java
@@ -93,10 +93,8 @@ public class KeycloakSettings {
 
     URL url;
     Map<String, Object> openIdConfiguration;
-    try {
-      ProxyAuthenticator.initAuthenticator(wellKnownEndpoint);
-      url = new URL(wellKnownEndpoint);
-      final InputStream inputStream = url.openStream();
+    ProxyAuthenticator.initAuthenticator(wellKnownEndpoint);
+    try (InputStream inputStream = new URL(wellKnownEndpoint).openStream()) {
       final JsonFactory factory = new JsonFactory();
       final JsonParser parser = factory.createParser(inputStream);
       final TypeReference<Map<String, Object>> typeReference =
