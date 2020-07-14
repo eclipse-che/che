68719df1b38399bbc04dc89160580f4296590a42
kkanova <kkanova@redhat.com>
2020-07-14 13:19:07 +0200

    Stabilising LS tests.

--- tests/e2e/testsLibrary/LsTests.ts
+++ tests/e2e/testsLibrary/LsTests.ts
@@ -68,8 +68,20 @@ const ide: Ide = e2eContainer.get(CLASSES.Ide);
 
  export function codeNavigation(openedFile: string, line: number, char: number, codeNavigationClassName: string) {
     test('Codenavigation', async () => {
-        await editor.moveCursorToLineAndChar(openedFile, line, char);
-        await editor.performKeyCombination(openedFile, Key.chord(Key.CONTROL, Key.F12));
-        await editor.waitEditorAvailable(codeNavigationClassName);
+        try {
+            await editor.moveCursorToLineAndChar(openedFile, line, char);
+            await editor.performKeyCombination(openedFile, Key.chord(Key.CONTROL, Key.F12));
+            await editor.waitEditorAvailable(codeNavigationClassName);
+        } catch (err) {
+            if (err instanceof error.TimeoutError) {
+                Logger.warn('Code navigatino didn\'t work. Trying again.');
+                await editor.moveCursorToLineAndChar(openedFile, line, char);
+                await editor.performKeyCombination(openedFile, Key.chord(Key.CONTROL, Key.F12));
+                await editor.waitEditorAvailable(codeNavigationClassName);
+            } else {
+                Logger.error('Code navigation didn\'t work even after retrying.');
+                throw err;
+            }
+        }
     });
  }
