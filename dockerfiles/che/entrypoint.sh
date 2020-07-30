82d572c1b7f2c8cdb09892c2814193c6a64990dd
Nick Boldt <nboldt@redhat.com>
2020-07-30 10:43:36 -0400

    CRW-1093 CRW-1124 fix problem with cacert... (#17528)
    * CRW-1093 CRW-1124 fix problem with cacert permission changes
    
    Change-Id: Ifd09eb30732a31bcc0fcf331700a43eed75589b1
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    * CRW-1124 CRW-1093 add comments explaining WHY we're making these changes; use g+w instead of 0777 on HOME/cacerts file
    
    Change-Id: Iba80fc86f4e54e9e6633858b205a6aa64f067c9e
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- dockerfiles/che/entrypoint.sh
+++ dockerfiles/che/entrypoint.sh
@@ -306,7 +306,8 @@ add_cert_to_truststore() {
   echo "$1" > $SELF_SIGNED_CERT
 
   # make sure that owner has permissions to write and other groups have permissions to read
-  chmod 644 $JAVA_TRUST_STORE
+  # note that this might not work when running as anyuid OpenShift user, which is why we're forcing a true if it fails
+  chmod 644 $JAVA_TRUST_STORE || true
 
   echo yes | keytool -keystore $JAVA_TRUST_STORE -importcert -alias "$2" -file $SELF_SIGNED_CERT -storepass $DEFAULT_JAVA_TRUST_STOREPASS > /dev/null
   # allow only read by all groups
