d0a95af6a19708b5fa407d3c1d1e026ae01714ad
Tibor Dancs <tdancs@redhat.com>
2020-10-20 03:26:11 +0200

    Added Java with SpringBoot devfile test including build task
    Signed-off-by: Tibor Dancs <tdancs@redhat.com>

---
+++ tests/e2e/tests/devfiles/JavaSpringBoot.spec.ts
@@ -0,0 +1,60 @@
+/*********************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+import 'reflect-metadata';
+import { WorkspaceNameHandler} from '../..';
+import * as projectAndFileTests from '../../testsLibrary/ProjectAndFileTests';
+import * as commonLsTests from '../../testsLibrary/LsTests';
+import * as workspaceHandling from '../../testsLibrary/WorksapceHandlingTests';
+import * as codeExecutionTests from '../../testsLibrary/CodeExecutionTests';
+
+const stack : string = 'Java Spring Boot';
+const workspaceSampleName: string = 'java-web-spring';
+const workspaceRootFolderName: string = 'src';
+const fileFolderPath: string = `${workspaceSampleName}/${workspaceRootFolderName}/main/java/org/springframework/samples/petclinic`;
+const tabTitle: string = 'PetClinicApplication.java';
+const codeNavigationClassName: string = 'SpringApplication.class';
+const taskName: string = 'maven build';
+
+suite(`${stack} test`, async () => {
+    suite (`Create ${stack} workspace`, async () => {
+        workspaceHandling.createAndOpenWorkspace(stack);
+        projectAndFileTests.waitWorkspaceReadiness(workspaceSampleName, workspaceRootFolderName);
+    });
+
+    suite('Test opening file', async () => {
+        // opening file that soon should give time for LS to initialize
+        projectAndFileTests.openFile(fileFolderPath, tabTitle);
+    });
+
+    suite('Validation of workspace build and run', async () => {
+        codeExecutionTests.runTask(taskName, 240_000);
+        codeExecutionTests.closeTerminal(taskName);
+    });
+
+    suite('Language server validation', async () => {
+        commonLsTests.autocomplete(tabTitle, 32, 56, 'args : String[]');
+        commonLsTests.errorHighlighting(tabTitle, 'error_text', 30);
+        commonLsTests.codeNavigation(tabTitle, 32, 23, codeNavigationClassName, 30_000); // extended timout to give LS enough time to start
+        commonLsTests.suggestionInvoking(tabTitle, 32, 23, 'run(Class<?>');
+    });
+
+    suite ('Stopping and deleting the workspace', async () => {
+        let workspaceName = 'not defined';
+        suiteSetup( async () => {
+            workspaceName = await WorkspaceNameHandler.getNameFromUrl();
+        });
+        test (`Stop worksapce`, async () => {
+            await workspaceHandling.stopWorkspace(workspaceName);
+        });
+        test (`Remove workspace`, async () => {
+            await workspaceHandling.removeWorkspace(workspaceName);
+        });
+    });
+});
