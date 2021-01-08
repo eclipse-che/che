06d224b6d1c5ddcef454de5a8e8d028c0e380ff9
Max Shaposhnik <mshaposh@redhat.com>
2021-01-08 10:49:39 +0200

    Add javadocs for annotations

--- infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/secret/KubernetesSecretAnnotationNames.java
+++ infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/secret/KubernetesSecretAnnotationNames.java
@@ -16,18 +16,27 @@ package org.eclipse.che.workspace.infrastructure.kubernetes.provision.secret;
  */
 public class KubernetesSecretAnnotationNames {
 
+  /** Common prefix for all annotations */
   public static final String ANNOTATION_PREFIX = "che.eclipse.org";
+
+  /** Indicates the way secret should be mount. Supported values are 'file' and 'env' */
   public static final String ANNOTATION_MOUNT_AS = ANNOTATION_PREFIX + "/" + "mount-as";
 
+  /** Indicates whether given secret should be automatically mount into all workspace containers */
   public static final String ANNOTATION_AUTOMOUNT =
       ANNOTATION_PREFIX + "/" + "automount-workspace-secret";
 
+  /** Indicates whether given secret is a git credential secret. */
   public static final String ANNOTATION_GIT_CREDENTIALS =
       ANNOTATION_PREFIX + "/" + "git-credential";
 
+  /** For 'file' type secrets defines the path where ih should be mount */
   public static final String ANNOTATION_MOUNT_PATH = ANNOTATION_PREFIX + "/" + "mount-path";
 
+  /** For 'env' type secrets defines the environment variable name to mount secret with */
   public static final String ANNOTATION_ENV_NAME = ANNOTATION_PREFIX + "/" + "env-name";
+
+  /** For 'env' type secrets defines the environment variable name template to mount secret with */
   public static final String ANNOTATION_ENV_NAME_TEMPLATE = ANNOTATION_PREFIX + "/%s_" + "env-name";
 
   private KubernetesSecretAnnotationNames() {}
