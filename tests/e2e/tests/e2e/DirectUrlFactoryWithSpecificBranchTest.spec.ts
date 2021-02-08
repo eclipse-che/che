a08409bf91fafe409e1bed13f438db5a55b98eca
Skorik Sergey <sskoryk@redhat.com>
2021-02-08 15:20:25 +0200

    add factories typescript selenium tests

---
+++ tests/e2e/tests/e2e/DirectUrlFactoryWithSpecificBranchTest.spec.ts
@@ -0,0 +1,58 @@
+// /*********************************************************************
+//  * Copyright (c) 2020 Red Hat, Inc.
+//  *
+//  * This program and the accompanying materials are made
+//  * available under the terms of the Eclipse Public License 2.0
+//  * which is available at https://www.eclipse.org/legal/epl-2.0/
+//  *
+//  * SPDX-License-Identifier: EPL-2.0
+//  **********************************************************************/
+
+import { e2eContainer } from '../../inversify.config';
+import { CLASSES } from '../../inversify.types';
+import { TestConstants } from '../../TestConstants';
+import { DriverHelper } from '../../utils/DriverHelper';
+import { WorkspaceNameHandler } from '../..';
+import * as workspaceHandling from '../../testsLibrary/WorksapceHandlingTests';
+import * as projectAndFileTests from '../../testsLibrary/ProjectAndFileTests';
+
+const driverHelper: DriverHelper = e2eContainer.get(CLASSES.DriverHelper);
+
+// the suite expect user to be logged in
+suite('Workspace creation via factory url', async () => {
+
+    let factoryUrl : string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=https://github.com/che-samples/console-java-simple/tree/java1.11`;
+    const workspaceSampleName: string = 'console-java-simple';
+    const workspaceRootFolderName: string = 'src';
+    const fileFolderPath: string = `${workspaceSampleName}/${workspaceRootFolderName}/main/java/org/eclipse/che/examples`;
+    const tabTitle: string = 'HelloWorld.java';
+
+    suite('Open factory URL', async () => {
+        test(`Navigating to factory URL`, async () => {
+            await driverHelper.navigateToUrl(factoryUrl);
+        });
+    });
+
+    suite('Wait workspace readyness', async () => {
+        projectAndFileTests.waitWorkspaceReadiness(workspaceSampleName, workspaceRootFolderName);
+    });
+
+    suite('Check imported project', async () => {
+        projectAndFileTests.openFile(fileFolderPath, tabTitle);
+        projectAndFileTests.checkProjectBranchName('java1.11');
+    });
+
+    suite ('Stopping and deleting the workspace', async () => {
+        let workspaceName = 'not defined';
+        suiteSetup( async () => {
+            workspaceName = await WorkspaceNameHandler.getNameFromUrl();
+        });
+        test (`Stop workspace`, async () => {
+            await workspaceHandling.stopWorkspace(workspaceName);
+        });
+        test (`Remove workspace`, async () => {
+            await workspaceHandling.removeWorkspace(workspaceName);
+        });
+    });
+
+});
--- tests/e2e/testsLibrary/ProjectAndFileTests.ts
+++ tests/e2e/testsLibrary/ProjectAndFileTests.ts
@@ -8,10 +8,13 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 
+import { By } from 'selenium-webdriver';
 import { CLASSES, Ide, ProjectTree, Editor } from '..';
 import { e2eContainer } from '../inversify.config';
+import { DriverHelper } from '../utils/DriverHelper';
 
 const ide: Ide = e2eContainer.get(CLASSES.Ide);
+const driverHelper: DriverHelper = e2eContainer.get(CLASSES.DriverHelper);
 const projectTree: ProjectTree = e2eContainer.get(CLASSES.ProjectTree);
 const editor: Editor = e2eContainer.get(CLASSES.Editor);
 
@@ -37,5 +40,17 @@ export function openFile(filePath: string, fileName: string) {
     test('Expand project and open file in editor', async () => {
         await projectTree.expandPathAndOpenFile(filePath, fileName);
         await editor.selectTab(fileName);
+    });
+}
+
+export function checkFileNotExists(filePath: string) {
+    test('Check that file is not exist in project', async () => {
+        await projectTree.waitItemDisappearance(filePath);
+    });
+}
+
+export function checkProjectBranchName(branchName: string) {
+    test('Check branch name is ${}', async () => {
+        await driverHelper.waitVisibility(By.xpath(`//div[@id='theia-statusBar']/div//span[text()=' ${branchName}']`));
     });
 }
