23507bf403c2fba1b4ea17ee2fdd3e6cdd0e9fc9
Dmytro Nochevnov <dnochevn@redhat.com>
2021-02-11 20:38:32 +0200

    Expand threads view in Happy path tests
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- tests/e2e/pageobjects/ide/DebugView.ts
+++ tests/e2e/pageobjects/ide/DebugView.ts
@@ -52,15 +52,20 @@ export class DebugView {
     async waitForDebuggerToConnect(timeout: number = TimeoutConstants.TS_DEBUGGER_CONNECTION_TIMEOUT) {
         await this.driverHelper.getDriver().wait(async () => {
             Logger.debug(`Waiting for debugger to connect (threads to appear in "Threads" view)`);
-
-            // collapse "Threads" view to work around che/18034 issue
-
-            const threadElements: WebElement[] = await this.driverHelper.getDriver().findElements(By.xpath(`//div[contains(@class, 'theia-debug-thread')]`));
+            const threadElements: WebElement[] = await this.driverHelper.getDriver().findElements(By.xpath(`'theia-debug-thread`));
             if (threadElements.length > 1) {
                 return true;
             }
             return false;
         }, timeout);
+    }
+
+    /**
+     * Click on "Threads" view title.
+     */
+    async clickOnThreadsViewTitle() {
+        const threadsViewTitleLocator: By = By.xpath('//div[@id="debug:view-container:-1--debug:threads:-1"]/*/span[@title="Threads"]');
+        await this.driverHelper.waitAndClick(threadsViewTitleLocator);
     }
 
 }
