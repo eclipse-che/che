8af130bf6c9b0ae35519b6d34e82e7a68a708a62
Max Shaposhnik <mshaposh@redhat.com>
2021-03-04 12:08:14 +0200

    Add gitlab

---
+++ wsmaster/che-core-api-factory-gitlab/src/test/java/org/eclipse/che/api/factory/server/gitlab/GitlabFileContentProviderTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.api.factory.server.gitlab;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+import org.eclipse.che.api.workspace.server.devfile.FileContentProvider;
+import org.eclipse.che.api.workspace.server.devfile.URLFetcher;
+import org.mockito.Mockito;
+import org.testng.annotations.Test;
+
+public class GitlabFileContentProviderTest {
+
+  @Test
+  public void shouldExpandRelativePaths() throws Exception {
+    URLFetcher urlFetcher = Mockito.mock(URLFetcher.class);
+    GitlabUrl gitlabUrl =
+        new GitlabUrl()
+            .withHostName("https://gitlab.net")
+            .withUsername("eclipse")
+            .withProject("che");
+    FileContentProvider fileContentProvider = new GitlabFileContentProvider(gitlabUrl, urlFetcher);
+    fileContentProvider.fetchContent("devfile.yaml");
+    verify(urlFetcher).fetch(eq("https://gitlab.net/eclipse/che/-/raw/master/devfile.yaml"));
+  }
+
+  @Test
+  public void shouldPreserveAbsolutePaths() throws Exception {
+    URLFetcher urlFetcher = Mockito.mock(URLFetcher.class);
+    GitlabUrl githubUrl =
+        new GitlabUrl().withHostName("gitlab.net").withUsername("eclipse").withProject("che");
+    FileContentProvider fileContentProvider = new GitlabFileContentProvider(githubUrl, urlFetcher);
+    String url = "https://gitlab.net/eclipse/che/-/raw/master/devfile.yaml";
+    fileContentProvider.fetchContent(url);
+    verify(urlFetcher).fetch(eq(url));
+  }
+}
