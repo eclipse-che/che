556f214cc4af442b8c709e9be16e133c4baabef8
xbaran4 <pbaran@redhat.com>
2021-03-10 11:55:29 +0100

    remove unecessery funciton wrap
    Signed-off-by: xbaran4 <pbaran@redhat.com>

--- check_properties_description.sh
+++ check_properties_description.sh
@@ -3,23 +3,19 @@
 CHE_PROPERTIES_PATH="assembly/assembly-wsmaster-war/src/main/webapp/WEB-INF/classes/che/che.properties"
 MULTIUSER_PROPERTIES_PATH="assembly/assembly-wsmaster-war/src/main/webapp/WEB-INF/classes/che/multiuser.properties"
 
-check_properties_description() {
-  HAS_DESCRIPTION=false
-
-  cat $CHE_PROPERTIES_PATH $MULTIUSER_PROPERTIES_PATH |
-  while read -r LINE
-  do
-    if [[ $LINE == '#'* ]]; then
-      HAS_DESCRIPTION=true
-    elif [[ -z $LINE ]]; then
-      HAS_DESCRIPTION=false
-    else
-      if [[ $HAS_DESCRIPTION == false ]]; then
-        echo "Property $LINE seems to be missing a description!"
-        exit 1
-      fi
-      HAS_DESCRIPTION=false
+HAS_DESCRIPTION=false
+cat $CHE_PROPERTIES_PATH $MULTIUSER_PROPERTIES_PATH |
+while read -r LINE
+do
+  if [[ $LINE == '#'* ]]; then
+    HAS_DESCRIPTION=true
+  elif [[ -z $LINE ]]; then
+    HAS_DESCRIPTION=false
+  else
+    if [[ $HAS_DESCRIPTION == false ]]; then
+      echo "Property $LINE seems to be missing a description!"
+      exit 1
     fi
-  done
-}
-check_properties_description
+    HAS_DESCRIPTION=false
+  fi
+done
