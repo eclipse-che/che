b5f19ef8c9757c0c14b7ec04b22f49be5d1a1bb7
Ihor Okhrimenko <iokhrime@redhat.com>
2021-03-22 11:15:11 +0200

    Update kubernetes test
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- tests/e2e/tests/plugins/VscodeKubernetesPlugin.spec.ts
+++ tests/e2e/tests/plugins/VscodeKubernetesPlugin.spec.ts
@@ -13,25 +13,9 @@ import * as workspaceHandling from '../../testsLibrary/WorksapceHandlingTests';
 import { DriverHelper } from '../../utils/DriverHelper';
 import { e2eContainer } from '../../inversify.config';
 import { CLASSES } from '../../inversify.types';
-import { Ide } from '../../pageobjects/ide/Ide';
-import { TimeoutConstants } from '../../TimeoutConstants';
-import { TestConstants } from '../../TestConstants';
-import { PreferencesHandler } from '../../utils/PreferencesHandler';
 import { KubernetesPlugin } from '../../pageobjects/ide/plugins/KubernetesPlugin';
-import { ProjectTree } from '../../pageobjects/ide/ProjectTree';
 
-
-const driverHelper: DriverHelper = e2eContainer.get(CLASSES.DriverHelper);
-const ide: Ide = e2eContainer.get(CLASSES.Ide);
-const preferencesHandler: PreferencesHandler = e2eContainer.get(CLASSES.PreferencesHandler);
 const kubernetesPlugin: KubernetesPlugin = e2eContainer.get(CLASSES.KubernetesPlugin);
-const projectTree: ProjectTree = e2eContainer.get(CLASSES.ProjectTree);
-
-const devfileUrl: string = 'https://gist.githubusercontent.com/Ohrimenko1988/94d1a70ff94d4d4bc5f2e4678dc8d538/raw/353a2513ea9e2f61b6cb1e0a88be21efd35b353b/kubernetes-plugin-test.yaml';
-const factoryUrl: string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=${devfileUrl}`;
-const sampleName: string = 'nodejs-web-app';
-const subRootFolder: string = 'app';
-const vsKubernetesConfig = { 'vs-kubernetes.kubeconfig': '/projects/nodejs-web-app/config' };
 
 suite(`The 'VscodeKubernetesPlugin' test`, async () => {
     suite('Check the "Kubernetes" plugin', async () => {
