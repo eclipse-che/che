f581d74e767feee1327b90e651c798f162661e65
Ihor Okhrimenko <iokhrime@redhat.com>
2021-03-30 16:16:46 +0300

    Create E2E test for checking the "Java11" plugin (#19446)
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

---
+++ tests/e2e/tests/plugins/JavaPlugin.spec.ts
@@ -0,0 +1,97 @@
+/*********************************************************************
+ * Copyright (c) 2021 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+import { WorkspaceNameHandler } from '../..';
+import 'reflect-metadata';
+import * as workspaceHandling from '../../testsLibrary/WorksapceHandlingTests';
+import { DriverHelper } from '../../utils/DriverHelper';
+import { e2eContainer } from '../../inversify.config';
+import { CLASSES } from '../../inversify.types';
+import { Ide } from '../../pageobjects/ide/Ide';
+import { TimeoutConstants } from '../../TimeoutConstants';
+import { TestConstants } from '../../TestConstants';
+import { ProjectTree } from '../../pageobjects/ide/ProjectTree';
+import * as projectAndFileTests from '../../testsLibrary/ProjectAndFileTests';
+import { Key } from 'selenium-webdriver';
+import { Editor } from '../../pageobjects/ide/Editor';
+
+const driverHelper: DriverHelper = e2eContainer.get(CLASSES.DriverHelper);
+const ide: Ide = e2eContainer.get(CLASSES.Ide);
+const projectTree: ProjectTree = e2eContainer.get(CLASSES.ProjectTree);
+const editor: Editor = e2eContainer.get(CLASSES.Editor);
+
+const devfileUrl: string = 'https://raw.githubusercontent.com/eclipse/che/master/tests/e2e/files/devfiles/plugins/Java11PluginTest.yaml';
+const factoryUrl: string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=${devfileUrl}`;
+const codeNavigationClassName: string = 'String.class';
+const sampleName: string = 'console-java-simple';
+const subRootFolder: string = 'src';
+
+const fileFolderPath: string = `${sampleName}/src/main/java/org/eclipse/che/examples`;
+const tabTitle: string = 'HelloWorld.java';
+
+suite(`The 'JavaPlugin' test`, async () => {
+    suite('Create workspace', async () => {
+        test('Create workspace using factory', async () => {
+            await driverHelper.navigateToUrl(factoryUrl);
+        });
+
+        test('Wait until created workspace is started', async () => {
+            await ide.waitAndSwitchToIdeFrame();
+            await ide.waitIde(TimeoutConstants.TS_SELENIUM_START_WORKSPACE_TIMEOUT);
+            await projectTree.openProjectTreeContainer();
+            await projectTree.waitProjectImported(sampleName, subRootFolder);
+        });
+    });
+
+    suite('Test opening file', async () => {
+        // opening file that soon should give time for LS to initialize
+        projectAndFileTests.openFile(fileFolderPath, tabTitle);
+    });
+
+    suite('Language server validation', async () => {
+        test('Wait until Java LS is initialised', async () => {
+            await ide.checkLsInitializationStart('Activating');
+            await ide.waitStatusBarTextAbsence('Activating', 900_000);
+        });
+
+        test('Autocomplete', async () => {
+            await editor.moveCursorToLineAndChar(tabTitle, 10, 20);
+            await editor.pressControlSpaceCombination(tabTitle);
+            await editor.waitSuggestionContainer();
+            await editor.waitSuggestion(tabTitle, 'close() : void');
+        });
+
+        test('Error highlighting', async () => {
+            const textForErrorDisplaying: string = '$';
+            await editor.type(tabTitle, textForErrorDisplaying, 10);
+            await editor.waitErrorInLine(10, TimeoutConstants.TS_ERROR_HIGHLIGHTING_TIMEOUT);
+            await editor.performKeyCombination(tabTitle, Key.chord(Key.BACK_SPACE));
+            await editor.waitErrorInLineDisappearance(10);
+        });
+
+        test('Codenavigation', async () => {
+            await editor.moveCursorToLineAndChar(tabTitle, 9, 12);
+            await editor.performKeyCombination(tabTitle, Key.chord(Key.CONTROL, Key.F12));
+            await editor.waitEditorAvailable(codeNavigationClassName, 60_000);
+        });
+
+    });
+
+    suite('Stopping and deleting the workspace', async () => {
+        let workspaceName = 'not defined';
+        suiteSetup(async () => {
+            workspaceName = await WorkspaceNameHandler.getNameFromUrl();
+        });
+
+        test(`Stop and remove workspace`, async () => {
+            await workspaceHandling.stopAndRemoveWorkspace(workspaceName);
+        });
+    });
+
+});
