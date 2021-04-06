18f2f1b70d55ceb48d81b3d7173d8cada074eac2
Ihor Okhrimenko <iokhrime@redhat.com>
2021-04-06 16:55:07 +0300

    Create devfile for the test
    Signed-off-by: Ihor Okhrimenko <iokhrime@redhat.com>

--- tests/e2e/tests/plugins/PhpPlugin.spec.ts
+++ tests/e2e/tests/plugins/PhpPlugin.spec.ts
@@ -10,40 +10,36 @@
 import { WorkspaceNameHandler } from '../..';
 import 'reflect-metadata';
 import * as workspaceHandling from '../../testsLibrary/WorksapceHandlingTests';
-import { DriverHelper } from '../../utils/DriverHelper';
 import { e2eContainer } from '../../inversify.config';
 import { CLASSES } from '../../inversify.types';
 import { Ide, LeftToolbarButton } from '../../pageobjects/ide/Ide';
 import { TimeoutConstants } from '../../TimeoutConstants';
 import { TestConstants } from '../../TestConstants';
 import { ProjectTree } from '../../pageobjects/ide/ProjectTree';
-import { Key, By } from 'selenium-webdriver';
+import { Key } from 'selenium-webdriver';
 import { Editor } from '../../pageobjects/ide/Editor';
 import { TopMenu } from '../../pageobjects/ide/TopMenu';
 import { DebugView } from '../../pageobjects/ide/DebugView';
-import { Terminal } from '../../pageobjects/ide/Terminal';
 import { BrowserTabsUtil } from '../../utils/BrowserTabsUtil';
 
-const driverHelper: DriverHelper = e2eContainer.get(CLASSES.DriverHelper);
 const ide: Ide = e2eContainer.get(CLASSES.Ide);
 const projectTree: ProjectTree = e2eContainer.get(CLASSES.ProjectTree);
 const editor: Editor = e2eContainer.get(CLASSES.Editor);
 const topMenu: TopMenu = e2eContainer.get(CLASSES.TopMenu);
 const debugView: DebugView = e2eContainer.get(CLASSES.DebugView);
-const terminal: Terminal = e2eContainer.get(CLASSES.Terminal);
 const browserTabsUtil: BrowserTabsUtil = e2eContainer.get(CLASSES.BrowserTabsUtil);
 
-const devfileUrl: string = 'https://gist.githubusercontent.com/Ohrimenko1988/d731a6e7b2ad619ab2c6fc5c7d225a90/raw/b5e9d342805a6a322f034b125fcfd623a48cb81a/PhpPlugin.yaml';
+const devfileUrl: string = 'https://raw.githubusercontent.com/eclipse/che/master/tests/e2e/files/devfiles/plugins/PhpPluginTest.yaml';
 const factoryUrl: string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=${devfileUrl}`;
-const codeNavigationClassName: string = 'OpenDefinition.ts';
-const projectName: string = 'web-php-simple';
+const projectName: string = 'php-web-simple';
 const subRootFolder: string = 'README.md';
-const sampleBodyLocator: By = By.xpath(`//body[text()='Hello World!']`);
 
 const fileFolderPath: string = `${projectName}`;
 const tabTitle: string = 'index.php';
 
-suite(`The 'Php plugin and php-debug' tests`, async () => {
+let workspaceName = 'not defined';
+
+suite(`The 'PhpPlugin' tests`, async () => {
     suite('Create workspace', async () => {
         test('Create workspace using factory', async () => {
             await browserTabsUtil.navigateTo(factoryUrl);
@@ -54,6 +50,8 @@ suite(`The 'Php plugin and php-debug' tests`, async () => {
             await ide.waitIde(TimeoutConstants.TS_SELENIUM_START_WORKSPACE_TIMEOUT);
             await projectTree.openProjectTreeContainer();
             await projectTree.waitProjectImported(projectName, subRootFolder);
+
+            workspaceName = await WorkspaceNameHandler.getNameFromUrl();
         });
     });
 
@@ -63,23 +61,18 @@ suite(`The 'Php plugin and php-debug' tests`, async () => {
             await editor.selectTab(tabTitle);
         });
 
-        //  test('Wait until JS/Typescript LS is initialised', async () => {
-        //      await ide.checkLsInitializationStart('Initializing');
-        //      await ide.waitStatusBarTextAbsence('Initializing', 900_000);
-        //  });
-
         test('Autocomplete', async () => {
-            await editor.moveCursorToLineAndChar(tabTitle, 13, 3);
+            await editor.moveCursorToLineAndChar(tabTitle, 13, 2);
             await editor.pressControlSpaceCombination(tabTitle);
             await editor.waitSuggestionContainer();
-            await editor.waitSuggestion(tabTitle, 'echo');
+            await editor.waitSuggestion(tabTitle, 'error_log');
         });
 
         test('Error highlighting', async () => {
-            const textForErrorDisplaying: string = '*%!';
+            const textForErrorDisplaying: string = '*';
             await editor.type(tabTitle, textForErrorDisplaying, 13);
             await editor.waitErrorInLine(13, TimeoutConstants.TS_ERROR_HIGHLIGHTING_TIMEOUT);
-            await editor.type(tabTitle, Key.chord(Key.DELETE, Key.DELETE, Key.DELETE), 13);
+            await editor.type(tabTitle, Key.chord(Key.DELETE), 13);
             await editor.waitErrorInLineDisappearance(13);
         });
     });
@@ -94,16 +87,8 @@ suite(`The 'Php plugin and php-debug' tests`, async () => {
             await topMenu.selectOption('View', 'Debug');
             await ide.waitLeftToolbarButton(LeftToolbarButton.Debug);
             await debugView.clickOnDebugConfigurationDropDown();
-            await debugView.clickOnDebugConfigurationItem('Launch currently open script');
+            await debugView.clickOnDebugConfigurationItem('Launch currently open script (php-web-simple)');
             await debugView.clickOnRunDebugButton();
-        });
-
-        test('Wait debug connected', async () => {
-            await terminal.waitTab('Debug Console', 60_000);
-
-            // for make sure that debug really start
-            // (inner processes may not be displayed)
-            await driverHelper.wait(10_000);
         });
 
         test('Check breakpoint stopped', async () => {
@@ -112,11 +97,6 @@ suite(`The 'Php plugin and php-debug' tests`, async () => {
     });
 
     suite('Stopping and deleting the workspace', async () => {
-        let workspaceName = 'not defined';
-        suiteSetup(async () => {
-            workspaceName = await WorkspaceNameHandler.getNameFromUrl();
-        });
-
         test(`Stop and remove workspace`, async () => {
             await workspaceHandling.stopAndRemoveWorkspace(workspaceName);
         });
