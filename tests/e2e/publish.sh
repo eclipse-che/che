0fdb3423811d087d4bfcbf265487134d8eb7fe1f
kkanova <kkanova@redhat.com>
2021-04-08 09:07:00 +0200

    Applying review comments

--- tests/e2e/publish.sh
+++ tests/e2e/publish.sh
@@ -5,11 +5,11 @@ TIME=$(date +%s)
 if [[ $CHE_VERSION == *SNAPSHOT ]] ; then 
     echo "Building and pushing latest E2E package version $CHE_VERSION"
     VERSION=${CHE_VERSION%"-SNAPSHOT"}-$TIME
-    TAG="nightly"
+    #TAG="nightly"
 else 
     echo "Building and pushing E2E package release version $CHE_VERSION"
     VERSION=$CHE_VERSION-$TIME
-    TAG=$CHE_VERSION
+    #TAG=$CHE_VERSION
 fi
 
 npm version $VERSION
