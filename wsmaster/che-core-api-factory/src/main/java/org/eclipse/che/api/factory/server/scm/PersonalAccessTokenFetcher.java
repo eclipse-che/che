06eeb0b460a8e91d6b8f8704d42ff4c5af9f1e2a
Sergii Kabashniuk <skabashniuk@redhat.com>
2021-04-16 14:18:33 +0300

    fixup! Update wsmaster/che-core-api-factory-bitbucket-server/src/main/java/org/eclipse/che/api/factory/server/bitbucket/BitbucketServerPersonalAccessTokenFetcher.java
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- wsmaster/che-core-api-factory/src/main/java/org/eclipse/che/api/factory/server/scm/PersonalAccessTokenFetcher.java
+++ wsmaster/che-core-api-factory/src/main/java/org/eclipse/che/api/factory/server/scm/PersonalAccessTokenFetcher.java
@@ -37,8 +37,8 @@ public interface PersonalAccessTokenFetcher {
    * permissions.
    *
    * @param personalAccessToken - personal access token to check.
-   * @return - empty optional if @{@link PersonalAccessTokenFetcher} is not able to confirm or deny
-   *     that token is valid or @{@link Boolean} value if it can.
+   * @return - empty optional if {@link PersonalAccessTokenFetcher} is not able to confirm or deny
+   *     that token is valid or {@link Boolean} value if it can.
    * @throws ScmUnauthorizedException - in case if user are not authorized che server to create new
    *     token. Further user interaction is needed before calling next time this method.
    * @throws ScmCommunicationException - Some unexpected problem occurred during communication with
