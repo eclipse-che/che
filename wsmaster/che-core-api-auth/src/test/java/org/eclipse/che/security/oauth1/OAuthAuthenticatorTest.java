0f4f47eeb803d3f28bf4ff88269a121075b8a243
xbaran4 <pbaran@redhat.com>
2021-04-29 09:53:18 +0200

    start of OAuthAuthenticator tests
    Signed-off-by: xbaran4 <pbaran@redhat.com>

---
+++ wsmaster/che-core-api-auth/src/test/java/org/eclipse/che/security/oauth1/OAuthAuthenticatorTest.java
@@ -0,0 +1,9 @@
+package org.eclipse.che.security.oauth1;
+
+import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.Listeners;
+
+@Listeners({MockitoTestNGListener.class})
+public class OAuthAuthenticatorTest {
+
+}
