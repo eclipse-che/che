676ffdf5904fe38023a3b552cecc8defd230e856
Sergii Kabashniuk <skabashniuk@redhat.com>
2021-05-20 16:53:16 +0300

    Upgrade maven plugin configuration. Made pom.xml independent from che-parent

--- wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/PreviewUrlLinksVariableGeneratorTest.java
+++ wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/PreviewUrlLinksVariableGeneratorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018 Red Hat, Inc.
+ * Copyright (c) 2012-2019 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -9,7 +9,6 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-
 package org.eclipse.che.api.workspace.server;
 
 import static java.util.Collections.singletonList;
--- wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/devfile/schema/DevfileSchemaProviderTest.java
+++ wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/devfile/schema/DevfileSchemaProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2018 Red Hat, Inc.
+ * Copyright (c) 2012-2021 Red Hat, Inc.
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -9,7 +9,6 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-
 package org.eclipse.che.api.workspace.server.devfile.schema;
 
 import static org.eclipse.che.api.workspace.server.devfile.Constants.CURRENT_API_VERSION;
