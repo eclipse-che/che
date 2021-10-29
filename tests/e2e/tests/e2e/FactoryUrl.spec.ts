e36efccef84ad41d3136dbefe137e67328d2eca3
Sergey Skorik <sskoryk@redhat.com>
2021-10-29 16:31:44 +0300

    [Selenium] Update typescript factories tests (#20708)

--- tests/e2e/tests/e2e/FactoryUrl.spec.ts
+++ tests/e2e/tests/e2e/FactoryUrl.spec.ts
@@ -9,20 +9,18 @@
 //  **********************************************************************/
 
 import { e2eContainer } from '../../inversify.config';
-import { CLASSES } from '../../inversify.types';
+import { CLASSES, TYPES } from '../../inversify.types';
 import { TestConstants } from '../../TestConstants';
 import { ProjectAndFileTests } from '../../testsLibrary/ProjectAndFileTests';
-import { WorkspaceHandlingTests } from '../../testsLibrary/WorkspaceHandlingTests';
 import CheReporter from '../../driver/CheReporter';
 import { BrowserTabsUtil } from '../../utils/BrowserTabsUtil';
-import { Dashboard } from '../../pageobjects/dashboard/Dashboard';
 import { WorkspaceNameHandler } from '../../utils/WorkspaceNameHandler';
+import { TestWorkspaceUtil } from '../../utils/workspace/TestWorkspaceUtil';
 
-const workspaceHandlingTests: WorkspaceHandlingTests = e2eContainer.get(CLASSES.WorkspaceHandlingTests);
 const projectAndFileTests: ProjectAndFileTests = e2eContainer.get(CLASSES.ProjectAndFileTests);
 const browserTabsUtil: BrowserTabsUtil = e2eContainer.get(CLASSES.BrowserTabsUtil);
-const dashboard: Dashboard = e2eContainer.get(CLASSES.Dashboard);
 const workspaceNameHandler: WorkspaceNameHandler = e2eContainer.get(CLASSES.WorkspaceNameHandler);
+const testWorkspaceUtils: TestWorkspaceUtil = e2eContainer.get<TestWorkspaceUtil>(TYPES.WorkspaceUtil);
 
 const factoryUrl : string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=https://raw.githubusercontent.com/eclipse/che-devfile-registry/master/devfiles/java-maven/devfile.yaml`;
 const workspaceSampleName: string = 'console-java-simple';
@@ -38,23 +36,16 @@ suite('Workspace creation via factory url', async () => {
     });
 
     suite('Wait workspace readyness', async () => {
-        test('Register running workspace', async () => {
-            await dashboard.waitWorkspaceStartingPage();
-            workspaceName = await workspaceNameHandler.getNameFromUrl();
-            CheReporter.registerRunningWorkspace(workspaceName);
-        });
-
         projectAndFileTests.waitWorkspaceReadiness(workspaceSampleName, workspaceRootFolderName);
     });
 
     suite ('Stopping and deleting the workspace', async () => {
-        test (`Stop worksapce`, async () => {
-            await workspaceHandlingTests.stopWorkspace(workspaceName);
-        });
-        test (`Remove workspace`, async () => {
-            await workspaceHandlingTests.removeWorkspace(workspaceName);
+        test('Stop and remove workspace', async () => {
+            workspaceName = await workspaceNameHandler.getNameFromUrl();
+            CheReporter.registerRunningWorkspace(workspaceName);
+
+            await testWorkspaceUtils.cleanUpRunningWorkspace(workspaceName);
         });
     });
-
 
 });
--- tests/e2e/tests/e2e/factories/DirectUrlFactoryWithRootFolderTest.spec.ts
+++ tests/e2e/tests/e2e/factories/DirectUrlFactoryWithRootFolderTest.spec.ts
@@ -8,21 +8,19 @@
 //  * SPDX-License-Identifier: EPL-2.0
 //  **********************************************************************/
 
-import { e2eContainer } from '../../inversify.config';
-import { CLASSES } from '../../inversify.types';
-import { TestConstants } from '../../TestConstants';
-import { ProjectAndFileTests } from '../../testsLibrary/ProjectAndFileTests';
-import { WorkspaceHandlingTests } from '../../testsLibrary/WorkspaceHandlingTests';
-import CheReporter from '../../driver/CheReporter';
-import { BrowserTabsUtil } from '../../utils/BrowserTabsUtil';
-import { Dashboard } from '../../pageobjects/dashboard/Dashboard';
-import { WorkspaceNameHandler } from '../../utils/WorkspaceNameHandler';
+import { e2eContainer } from '../../../inversify.config';
+import { CLASSES, TYPES } from '../../../inversify.types';
+import { TestConstants } from '../../../TestConstants';
+import { ProjectAndFileTests } from '../../../testsLibrary/ProjectAndFileTests';
+import CheReporter from '../../../driver/CheReporter';
+import { BrowserTabsUtil } from '../../../utils/BrowserTabsUtil';
+import { WorkspaceNameHandler } from '../../../utils/WorkspaceNameHandler';
+import { TestWorkspaceUtil } from '../../../utils/workspace/TestWorkspaceUtil';
 
-const workspaceHandlingTests: WorkspaceHandlingTests = e2eContainer.get(CLASSES.WorkspaceHandlingTests);
 const projectAndFileTests: ProjectAndFileTests = e2eContainer.get(CLASSES.ProjectAndFileTests);
 const browserTabsUtil: BrowserTabsUtil = e2eContainer.get(CLASSES.BrowserTabsUtil);
-const dashboard: Dashboard = e2eContainer.get(CLASSES.Dashboard);
 const workspaceNameHandler: WorkspaceNameHandler = e2eContainer.get(CLASSES.WorkspaceNameHandler);
+const testWorkspaceUtils: TestWorkspaceUtil = e2eContainer.get<TestWorkspaceUtil>(TYPES.WorkspaceUtil);
 
 const factoryUrl : string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=https://github.com/che-samples/console-java-simple`;
 const workspaceSampleName: string = 'console-java-simple';
@@ -40,13 +38,7 @@ suite('Workspace creation via factory url', async () => {
     });
 
     suite('Wait workspace readyness', async () => {
-        test('Register running workspace', async () => {
-            await dashboard.waitWorkspaceStartingPage();
-            workspaceName = await workspaceNameHandler.getNameFromUrl();
-            CheReporter.registerRunningWorkspace(workspaceName);
-        });
-
-        projectAndFileTests.waitWorkspaceReadiness(workspaceSampleName, workspaceRootFolderName);
+        projectAndFileTests.waitWorkspaceReadiness(workspaceSampleName, workspaceRootFolderName, true, true);
     });
 
     suite('Check imported project', async () => {
@@ -56,11 +48,11 @@ suite('Workspace creation via factory url', async () => {
     });
 
     suite ('Stopping and deleting the workspace', async () => {
-        test (`Stop workspace`, async () => {
-            await workspaceHandlingTests.stopWorkspace(workspaceName);
-        });
-        test (`Remove workspace`, async () => {
-            await workspaceHandlingTests.removeWorkspace(workspaceName);
+        test('Stop and remove workspace', async () => {
+            workspaceName = await workspaceNameHandler.getNameFromUrl();
+            CheReporter.registerRunningWorkspace(workspaceName);
+
+            await testWorkspaceUtils.cleanUpRunningWorkspace(workspaceName);
         });
     });
 
