88d3e26ad3efecc0eaad7ca1078f7986bf5f9a41
Skoryk Serhii <sskoryk@redhat.com>
2022-05-10 12:52:18 +0300

    adapt XmlPlugin test to works with devfile v2

--- tests/e2e/tests/plugins/VscodeXmlPlugin.spec.ts
+++ tests/e2e/tests/plugins/VscodeXmlPlugin.spec.ts
@@ -8,31 +8,35 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 
-import { Key } from 'selenium-webdriver';
-import { e2eContainer } from '../../inversify.config';
-import { Ide } from '../../pageobjects/ide/Ide';
-import { CLASSES } from '../../inversify.types';
-import { ProjectTree } from '../../pageobjects/ide/ProjectTree';
-import { Editor } from '../../pageobjects/ide/Editor';
-import { TestConstants } from '../../TestConstants';
-import { TimeoutConstants } from '../../TimeoutConstants';
-import { WorkspaceHandlingTests } from '../../testsLibrary/WorkspaceHandlingTests';
-import { Logger } from '../../utils/Logger';
-import { BrowserTabsUtil } from '../../utils/BrowserTabsUtil';
+ import { Key } from 'selenium-webdriver';
+ import { e2eContainer } from '../../inversify.config';
+ import { CLASSES } from '../../inversify.types';
+ import { ProjectTree } from '../../pageobjects/ide/ProjectTree';
+ import { Editor } from '../../pageobjects/ide/Editor';
+ import { TestConstants } from '../../TestConstants';
+ import { Logger } from '../../utils/Logger';
+ import { WorkspaceHandlingTests } from '../../testsLibrary/WorkspaceHandlingTests';
 
-const ide: Ide = e2eContainer.get(CLASSES.Ide);
-const projectTree: ProjectTree = e2eContainer.get(CLASSES.ProjectTree);
-const editor: Editor = e2eContainer.get(CLASSES.Editor);
-const browserTabsUtil: BrowserTabsUtil = e2eContainer.get(CLASSES.BrowserTabsUtil);
-const workspaceHandlingTests: WorkspaceHandlingTests = e2eContainer.get(CLASSES.WorkspaceHandlingTests);
+ import { BrowserTabsUtil } from '../../utils/BrowserTabsUtil';
+ import { PreferencesHandler } from '../../utils/PreferencesHandler';
+ import { ProjectAndFileTests } from '../../testsLibrary/ProjectAndFileTests';
 
-const devfileUrl: string = 'https://raw.githubusercontent.com/eclipse/che/main/tests/e2e/files/devfiles/plugins/VscodeXmlPlugin.yaml';
-const factoryUrl: string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=${devfileUrl}`;
-const projectName: string = 'nodejs-web-app';
-const pathToFile: string = `${projectName}`;
-const xmlFileName: string = 'hello.xml';
+ const projectAndFileTests: ProjectAndFileTests = e2eContainer.get(CLASSES.ProjectAndFileTests);
+ const projectTree: ProjectTree = e2eContainer.get(CLASSES.ProjectTree);
+ const editor: Editor = e2eContainer.get(CLASSES.Editor);
+ const browserTabsUtil: BrowserTabsUtil = e2eContainer.get(CLASSES.BrowserTabsUtil);
+ const workspaceHandlingTests: WorkspaceHandlingTests = e2eContainer.get(CLASSES.WorkspaceHandlingTests);
+ const preferencesHandler: PreferencesHandler = e2eContainer.get(CLASSES.PreferencesHandler);
 
-suite('The "VscodeXmlPlugin" userstory', async () => {
+ const devfileUrl: string = 'https://github.com/che-samples/web-nodejs-sample/tree/xml-plugin';
+ const factoryUrl: string = `${TestConstants.TS_SELENIUM_BASE_URL}/f?url=${devfileUrl}`;
+ const projectName: string = 'web-nodejs-sample';
+ const pathToFile: string = `${projectName}`;
+ const xmlFileName: string = 'hello.xml';
+ const subRootFolder: string = 'app';
+ let workspaceName: string = 'xml-plugin';
+
+ suite('The "VscodeXmlPlugin" userstory', async () => {
     suite('Create workspace', async () => {
         test('Create workspace using factory', async () => {
             await browserTabsUtil.navigateTo(factoryUrl);
@@ -40,75 +44,70 @@ suite('The "VscodeXmlPlugin" userstory', async () => {
 
         workspaceHandlingTests.obtainWorkspaceNameFromStartingPage();
 
-        test('Wait until created workspace is started', async () => {
-            await ide.waitIde(TimeoutConstants.TS_SELENIUM_START_WORKSPACE_TIMEOUT);
-            await ide.waitNotificationAndClickOnButton('Do you trust the authors of', 'Yes, I trust', 60_000);
+        projectAndFileTests.waitWorkspaceReadiness(projectName, subRootFolder);
+
+        test('Set confirmExit preference to never', async () => {
+            await preferencesHandler.setPreferenceUsingUI('application.confirmExit', 'never');
         });
     });
 
-    suite('Check workspace readiness to work', async () => {
-        test('Wait until project is imported', async () => {
-            await projectTree.openProjectTreeContainer();
-            await projectTree.waitProjectImported(projectName, 'app');
-        });
-    });
+     suite('Check the "vscode-xml" plugin', async () => {
+         test('Check autocomplete', async () => {
+             await projectTree.expandPathAndOpenFile(pathToFile, xmlFileName);
+             await editor.waitSuggestion(xmlFileName, 'rollback', 60000, 16, 4);
+         });
 
-    suite('Check the "vscode-xml" plugin', async () => {
-        test('Check autocomplete', async () => {
-            await projectTree.expandPathAndOpenFile(pathToFile, xmlFileName);
-            await editor.waitSuggestion(xmlFileName, 'rollback', 60000, 16, 4);
-        });
+         test('Check error appearance', async () => {
+             await projectTree.expandPathAndOpenFile(pathToFile, xmlFileName);
 
-        test('Check error appearance', async () => {
-            await projectTree.expandPathAndOpenFile(pathToFile, xmlFileName);
+             await editor.type(xmlFileName, '\$\%\^\#', 16);
+             await editor.waitErrorInLine(16, xmlFileName);
+         });
 
-            await editor.type(xmlFileName, '\$\%\^\#', 16);
-            await editor.waitErrorInLine(16, xmlFileName);
-        });
+         test('Check error disappearance', async () => {
+             await editor.performKeyCombination(xmlFileName, Key.chord(Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE));
+             await editor.waitErrorInLineDisappearance(16, xmlFileName);
+         });
 
-        test('Check error disappearance', async () => {
-            await editor.performKeyCombination(xmlFileName, Key.chord(Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE));
-            await editor.waitErrorInLineDisappearance(16, xmlFileName);
-        });
+         test('Check auto-close tags', async () => {
+             await projectTree.expandPathAndOpenFile(pathToFile, xmlFileName);
 
-        test('Check auto-close tags', async () => {
-            await projectTree.expandPathAndOpenFile(pathToFile, xmlFileName);
+             await editor.type(xmlFileName, '<test>', 21);
+             await editor.waitText(xmlFileName, '<test></test>', 60000, 10000);
 
-            await editor.type(xmlFileName, '<test>', 21);
-            await editor.waitText(xmlFileName, '<test></test>', 60000, 10000);
+             await editor.performKeyCombination(xmlFileName, Key.chord(Key.CONTROL, 'z'));
+             await editor.performKeyCombination(xmlFileName, Key.chord(Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE));
+         });
 
-            await editor.performKeyCombination(xmlFileName, Key.chord(Key.CONTROL, 'z'));
-            await editor.performKeyCombination(xmlFileName, Key.chord(Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE, Key.BACK_SPACE));
-        });
+         test('To unformat the "xml" file', async () => {
+             const expectedTextBeforeFormating: string = '<constant    >Hello World!!!';
+             await projectTree.expandPathAndOpenFile(pathToFile, xmlFileName);
 
-        test('To unformat the "xml" file', async () => {
-            const expectedTextBeforeFormating: string = '<constant    >Hello World!!!';
-            await projectTree.expandPathAndOpenFile(pathToFile, xmlFileName);
+             await editor.selectTab(xmlFileName);
+             await editor.moveCursorToLineAndChar(xmlFileName, 25, 22);
+             await editor.performKeyCombination(xmlFileName, Key.chord(Key.SPACE, Key.SPACE, Key.SPACE, Key.SPACE));
+             await editor.waitText(xmlFileName, expectedTextBeforeFormating, 60000, 10000);
+         });
 
-            await editor.selectTab(xmlFileName);
-            await editor.moveCursorToLineAndChar(xmlFileName, 25, 22);
-            await editor.performKeyCombination(xmlFileName, Key.chord(Key.SPACE, Key.SPACE, Key.SPACE, Key.SPACE));
-            await editor.waitText(xmlFileName, expectedTextBeforeFormating, 60000, 10000);
-        });
+         test('To format the "xml" document', async () => {
+             const expectedTextAfterFormating: string = '<constant>Hello World!!!';
 
-        test('To format the "xml" document', async () => {
-            const expectedTextAfterFormating: string = '<constant>Hello World!!!';
+             await editor.type(xmlFileName, Key.chord(Key.CONTROL, Key.SHIFT, 'I'), 25);
+             await editor.waitText(xmlFileName, expectedTextAfterFormating, 60000, 10000);
+         });
 
-            await editor.type(xmlFileName, Key.chord(Key.CONTROL, Key.SHIFT, 'I'), 25);
-            await editor.waitText(xmlFileName, expectedTextAfterFormating, 60000, 10000);
-        });
+     });
 
-    });
+     suite('Delete workspace', async () => {
+         test('Stop and remove workspace', async () => {
+             if (TestConstants.TS_DELETE_PLUGINS_TEST_WORKSPACE === 'true') {
+                 await workspaceHandlingTests.stopAndRemoveWorkspace(workspaceName);
+                 return;
+             }
 
-    suite('Delete workspace', async () => {
-        test('Stop and remove workspace', async () => {
-            if (TestConstants.TS_DELETE_PLUGINS_TEST_WORKSPACE === 'true') {
-                await workspaceHandlingTests.stopAndRemoveWorkspace(WorkspaceHandlingTests.getWorkspaceName());
-                return;
-            }
+             Logger.info(`As far as the "TS_DELETE_PLUGINS_TEST_WORKSPACE" value is "false the workspace deletion is skipped"`);
+         });
+     });
 
-            Logger.info(`As far as the "TS_DELETE_PLUGINS_TEST_WORKSPACE" value is "false the workspace deletion is skipped"`);
-        });
-    });
+ });
 
-});
