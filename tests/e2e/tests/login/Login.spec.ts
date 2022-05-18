9209044541803d886ab14f7e085311ac3b92cfa5
Tibor Dancs <tdancs@redhat.com>
2022-05-18 13:50:48 +0200

    Added new logic for the landing page into appropriate login handler classes, updated dependencies (#21409)
    Signed-off-by: Tibor Dancs <tdancs@redhat.com>

--- tests/e2e/tests/login/Login.spec.ts
+++ tests/e2e/tests/login/Login.spec.ts
@@ -8,9 +8,12 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 
-import { TestConstants, CLASSES, ICheLoginPage, TYPES, Logger } from '../..';
 import { e2eContainer } from '../../inversify.config';
+import { CLASSES, TYPES } from '../../inversify.types';
+import { ICheLoginPage } from '../../pageobjects/login/ICheLoginPage';
+import { TestConstants } from '../../TestConstants';
 import { BrowserTabsUtil } from '../../utils/BrowserTabsUtil';
+import { Logger } from '../../utils/Logger';
 
 const browserTabsUtil: BrowserTabsUtil = e2eContainer.get(CLASSES.BrowserTabsUtil);
 const loginPage: ICheLoginPage = e2eContainer.get<ICheLoginPage>(TYPES.CheLogin);
@@ -18,6 +21,7 @@ const loginPage: ICheLoginPage = e2eContainer.get<ICheLoginPage>(TYPES.CheLogin)
 suite('Login test', async () => {
     test('Login', async () => {
         await browserTabsUtil.navigateTo(TestConstants.TS_SELENIUM_BASE_URL);
+
         await loginPage.login();
         if (TestConstants.TS_SELENIUM_LAUNCH_FULLSCREEN) {
             Logger.debug(`TS_SELENIUM_LAUNCH_FULLSCREEN is set to true, maximizing window.`);
