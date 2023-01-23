c9c6d97e8a1f976ff5cc126c0aa050c5c86ddbfb
Tibor Dancs <tdancs@redhat.com>
2023-01-23 10:30:38 +0100

    Fixing imports
    Signed-off-by: Tibor Dancs <tdancs@redhat.com>

--- tests/e2e/tests/MochaHooks.ts
+++ tests/e2e/tests/MochaHooks.ts
@@ -1,4 +1,14 @@
+/*********************************************************************
+ * Copyright (c) 2023 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
 
+import 'reflect-metadata';
 import { CLASSES } from '../inversify.types';
 import { EditorType, TestConstants } from '../TestConstants';
 import { AskForConfirmationTypeTheia, PreferencesHandlerTheia, TerminalRendererTypeTheia } from '../utils/theia/PreferencesHandlerTheia';
