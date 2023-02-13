7a9918e962bd688cb9d338fc9a623dc526f9710e
Tibor Dancs <tdancs@redhat.com>
2023-02-14 00:28:40 +0100

    Modified waitWorkspaceReadiness che-code logic, added root project presence check, implemented basic HappyPath test
    Signed-off-by: Tibor Dancs <tdancs@redhat.com>

---
+++ tests/e2e/tests/e2e_happy_path/che-code/HappyPath.spec.ts
@@ -0,0 +1,48 @@
+/*********************************************************************
+ * Copyright (c) 2023 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import { e2eContainer } from '../../../inversify.config';
+import { Workbench, ViewItem, TreeItem } from 'monaco-page-objects';
+import { CLASSES } from '../../../inversify.types';
+import { WorkspaceHandlingTests } from '../../../testsLibrary/WorkspaceHandlingTests';
+import { Logger } from '../../../utils/Logger';
+import CheReporter from '../../../driver/CheReporter';
+import { ProjectAndFileTestsCheCode } from '../../../testsLibrary/che-code/ProjectAndFileTestsCheCode';
+
+const workspaceHandlingTests: WorkspaceHandlingTests = e2eContainer.get(CLASSES.WorkspaceHandlingTests);
+const projectAndFileTests: ProjectAndFileTestsCheCode = e2eContainer.get(CLASSES.ProjectAndFileTestsCheCode);
+const stackName: string = 'Quarkus REST API';
+const sectionTitle: string = 'quarkus-quickstarts';
+const projectName: string = 'getting-started';
+
+suite(`Happy Path test`, async () => {
+    suite(`Create ${stackName} workspace`, async () => {
+        workspaceHandlingTests.createAndOpenWorkspace(stackName);
+        workspaceHandlingTests.obtainWorkspaceNameFromStartingPage();
+        test('Register running workspace', async () => {
+            CheReporter.registerRunningWorkspace(WorkspaceHandlingTests.getWorkspaceName());
+        });
+        test('Wait workspace readiness', async() => {
+            const workbench: Workbench = await projectAndFileTests.waitWorkspaceReadinessForCheCodeEditor();
+
+            const rootProjectViewItem: ViewItem = await projectAndFileTests.waitForRootProjectPresence(workbench, sectionTitle, projectName);
+            Logger.debug('Found root project.');
+            if (rootProjectViewItem instanceof TreeItem) {
+                Logger.debug(`Root project label: ${await rootProjectViewItem.getLabel()}`);
+            }
+        });
+    });
+
+    suite('Stopping and deleting the workspace', async () => {
+        test(`Stop and remove workspace`, async () => {
+            await workspaceHandlingTests.stopAndRemoveWorkspace(WorkspaceHandlingTests.getWorkspaceName());
+        });
+    });
+});
--- tests/e2e/testsLibrary/che-code/ProjectAndFileTestsCheCode.ts
+++ tests/e2e/testsLibrary/che-code/ProjectAndFileTestsCheCode.ts
@@ -15,18 +15,67 @@ import { DriverHelper } from '../../utils/DriverHelper';
 import { CLASSES } from '../../inversify.types';
 import { Logger } from '../../utils/Logger';
 import { TimeoutConstants } from '../../TimeoutConstants';
+import { Workbench, SideBarView, ViewContent, ViewSection, ViewItem } from 'monaco-page-objects';
+import { TestConstants } from '../../TestConstants';
 
 @injectable()
 export class ProjectAndFileTestsCheCode {
 
+    private static readonly ENDPOINTS_TITLE_TEXT: string = 'endpoints';
+
     constructor(
         @inject(CLASSES.DriverHelper) private readonly driverHelper: DriverHelper) {}
 
-    public async waitWorkspaceReadinessForCheCodeEditor(): Promise<void> {
+    public async waitWorkspaceReadinessForCheCodeEditor(): Promise<Workbench> {
+        const polling: number = TestConstants.TS_SELENIUM_DEFAULT_POLLING;
         try {
             await this.driverHelper.getDriver().wait(until.elementLocated(By.className('monaco-workbench')), TimeoutConstants.TS_SELENIUM_START_WORKSPACE_TIMEOUT);
+            const workbench: Workbench = new Workbench();
+            const sideBarView: SideBarView = workbench.getSideBar();
+            const sidebarViewContent: ViewContent = sideBarView.getContent();
+            let sidebarViewSections: ViewSection[];
+            while (true) {
+                sidebarViewSections = await sidebarViewContent.getSections();
+                Logger.debug(`ProjectAndFileTestsCheCode.waitWorkspaceReadinessForCheCodeEditor - Waiting for ${ProjectAndFileTestsCheCode.ENDPOINTS_TITLE_TEXT} sidebar section:`);
+                for (const section of sidebarViewSections) {
+                    Logger.trace(`Sidebar section: ${await section.getTitle()}`);
+                    if (await section.getTitle() === ProjectAndFileTestsCheCode.ENDPOINTS_TITLE_TEXT) {
+                        return workbench;
+                    }
+                }
+                await this.driverHelper.getDriver().sleep(polling);
+            }
         } catch (err) {
             Logger.error(`ProjectAndFileTestsCheCode.waitWorkspaceReadinessForCheCodeEditor - waiting for workspace readiness failed: ${err}`);
+            throw err;
+        }
+    }
+
+    public async waitForRootProjectPresence(workbench: Workbench, sectionTitle: string, rootFolderName: string): Promise<ViewItem> {
+        const polling: number = TestConstants.TS_SELENIUM_DEFAULT_POLLING;
+        try {
+            const sideBarView: SideBarView = workbench.getSideBar();
+            const sidebarViewContent: ViewContent = sideBarView.getContent();
+            let sidebarViewSections: ViewSection[];
+            let projectSidebarViewSection: ViewSection;
+            Logger.debug(`ProjectAndFileTestsCheCode.waitForRootProjectPresence - Waiting for ${sectionTitle} section in sidebar:`);
+            while (true) {
+                sidebarViewSections = await sidebarViewContent.getSections();
+                for (const section of sidebarViewSections) {
+                    Logger.trace(`\tSidebar section: ${await section.getTitle()}`);
+                    if (await section.getTitle() === sectionTitle) {
+                        projectSidebarViewSection = section;
+                        const projectRootFolderElement: ViewItem | undefined = await projectSidebarViewSection.findItem(rootFolderName);
+                        if (projectRootFolderElement === undefined) {
+                            throw new Error(`ProjectAndFileTestsCheCode.waitForRootProjectPresence - ${sectionTitle} not found.`);
+                        }
+                        return projectRootFolderElement;
+                    }
+                }
+                this.driverHelper.getDriver().sleep(polling);
+            }
+        } catch (err) {
+            Logger.error(`ProjectAndFileTestsCheCode.waitForRootProjectPresence - waiting for ${sectionTitle} in sidebar section failed: ${err}`);
             throw err;
         }
     }
