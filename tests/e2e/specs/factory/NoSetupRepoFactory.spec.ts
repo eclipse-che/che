a463fab1ae62af6a4f0cd5d93b957aae3d1aa884
Aleksandr Shmaraiev <oshmarai@redhat.com>
2023-11-30 14:50:30 +0200

    Update 'NoSetupRepoFactory' E2E test

--- tests/e2e/specs/factory/NoSetupRepoFactory.spec.ts
+++ tests/e2e/specs/factory/NoSetupRepoFactory.spec.ts
@@ -89,7 +89,7 @@ suite(
 			test(`Check that workspace cannot be created without PAT/OAuth for ${isPrivateRepo} repo`, async function (): Promise<void> {
 				await dashboard.waitLoader();
 				const loaderAlert: string = await dashboard.getLoaderAlert();
-				expect(loaderAlert).to.contain('Cause: Could not reach devfile at');
+				expect(loaderAlert).to.contain('Could not reach devfile at');
 			});
 
 			test('Check that workspace was not created', async function (): Promise<void> {
