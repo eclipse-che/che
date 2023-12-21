25713247743c704d2e67b383b65dc04b729b667d
Mykhailo Kuznietsov <mkuznets@redhat.com>
2023-12-21 14:06:33 +0000

    chore: Bump to 7.80.0-next in main
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>

--- tests/e2e/pageobjects/dashboard/UserPreferences.ts
+++ tests/e2e/pageobjects/dashboard/UserPreferences.ts
@@ -20,16 +20,16 @@ export class UserPreferences {
 	private static readonly USER_PREFERENCES_BUTTON: By = By.xpath('//button[text()="User Preferences"]');
 	private static readonly USER_PREFERENCES_PAGE: By = By.xpath('//h1[text()="User Preferences"]');
 
-    private static readonly CONTAINER_REGISTRIES_TAB: By = By.xpath('//button[text()="Container Registries"]');
-    private static readonly GIT_SERVICES_TAB: By = By.xpath('//button[text()="Git Services"]');
+	private static readonly CONTAINER_REGISTRIES_TAB: By = By.xpath('//button[text()="Container Registries"]');
+	private static readonly GIT_SERVICES_TAB: By = By.xpath('//button[text()="Git Services"]');
 
-    private static readonly PAT_TAB: By = By.xpath('//button[text()="Personal Access Tokens"]');
-    private static readonly ADD_NEW_PAT_BUTTON: By = By.xpath('//button[text()="Add Personal Access Token"]');
+	private static readonly PAT_TAB: By = By.xpath('//button[text()="Personal Access Tokens"]');
+	private static readonly ADD_NEW_PAT_BUTTON: By = By.xpath('//button[text()="Add Personal Access Token"]');
 
-    private static readonly GIT_CONFIG_PAGE: By = By.xpath('//button[text()="Gitconfig"]');
+	private static readonly GIT_CONFIG_PAGE: By = By.xpath('//button[text()="Gitconfig"]');
 
-    private static readonly SSH_KEY_TAB: By = By.xpath('//button[text()="SSH Keys"]');
-    private static readonly ADD_NEW_SSH_KEY_BUTTON: By = By.xpath('//button[text()="Add SSH Key"]');
+	private static readonly SSH_KEY_TAB: By = By.xpath('//button[text()="SSH Keys"]');
+	private static readonly ADD_NEW_SSH_KEY_BUTTON: By = By.xpath('//button[text()="Add SSH Key"]');
 
 	constructor(
 		@inject(CLASSES.DriverHelper)
@@ -37,7 +37,7 @@ export class UserPreferences {
 	) {}
 
 	async openUserPreferencesPage(): Promise<void> {
-        Logger.debug();
+		Logger.debug();
 
 		await this.driverHelper.waitAndClick(UserPreferences.USER_SETTINGS_DROPDOWN);
 		await this.driverHelper.waitAndClick(UserPreferences.USER_PREFERENCES_BUTTON);
@@ -45,45 +45,45 @@ export class UserPreferences {
 		await this.driverHelper.waitVisibility(UserPreferences.USER_PREFERENCES_PAGE);
 	}
 
-    async checkTabsAvailability(): Promise<void> {
-        Logger.debug();
+	async checkTabsAvailability(): Promise<void> {
+		Logger.debug();
 
-        await this.openContainerRegistriesTab();
-        await this.openGitServicesTab();
-        await this.openPatTab();
-        await this.openGitConfigPage();
-        await this.openSshKeyTab();
-    }
+		await this.openContainerRegistriesTab();
+		await this.openGitServicesTab();
+		await this.openPatTab();
+		await this.openGitConfigPage();
+		await this.openSshKeyTab();
+	}
 
-    async openContainerRegistriesTab(): Promise<void> {
-        Logger.debug();
+	async openContainerRegistriesTab(): Promise<void> {
+		Logger.debug();
 
-        await this.driverHelper.waitAndClick(UserPreferences.CONTAINER_REGISTRIES_TAB);
-    }
+		await this.driverHelper.waitAndClick(UserPreferences.CONTAINER_REGISTRIES_TAB);
+	}
 
-    async openGitServicesTab(): Promise<void> {
-        Logger.debug();
+	async openGitServicesTab(): Promise<void> {
+		Logger.debug();
 
-        await this.driverHelper.waitAndClick(UserPreferences.GIT_SERVICES_TAB);
-    }
+		await this.driverHelper.waitAndClick(UserPreferences.GIT_SERVICES_TAB);
+	}
 
-    async openPatTab(): Promise<void> {
-        Logger.debug();
+	async openPatTab(): Promise<void> {
+		Logger.debug();
 
-        await this.driverHelper.waitAndClick(UserPreferences.PAT_TAB);
-        await this.driverHelper.waitVisibility(UserPreferences.ADD_NEW_PAT_BUTTON);
-    }
+		await this.driverHelper.waitAndClick(UserPreferences.PAT_TAB);
+		await this.driverHelper.waitVisibility(UserPreferences.ADD_NEW_PAT_BUTTON);
+	}
 
-    async openGitConfigPage(): Promise<void> {
-        Logger.debug();
+	async openGitConfigPage(): Promise<void> {
+		Logger.debug();
 
-        await this.driverHelper.waitAndClick(UserPreferences.GIT_CONFIG_PAGE);
-    }
+		await this.driverHelper.waitAndClick(UserPreferences.GIT_CONFIG_PAGE);
+	}
 
-    async openSshKeyTab(): Promise<void> {
-        Logger.debug();
+	async openSshKeyTab(): Promise<void> {
+		Logger.debug();
 
-        await this.driverHelper.waitAndClick(UserPreferences.SSH_KEY_TAB);
-        await this.driverHelper.waitVisibility(UserPreferences.ADD_NEW_SSH_KEY_BUTTON);
-    }
+		await this.driverHelper.waitAndClick(UserPreferences.SSH_KEY_TAB);
+		await this.driverHelper.waitVisibility(UserPreferences.ADD_NEW_SSH_KEY_BUTTON);
+	}
 }
