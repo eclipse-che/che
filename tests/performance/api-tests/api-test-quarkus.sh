cfac53acfd9f235bb46efb4f4854b839eb0c3d32
Dmytro Nochevnov <dnochevn@redhat.com>
2024-01-11 01:15:02 +0200

    Fix Java sample titles/paths in tests
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- tests/performance/api-tests/api-test-quarkus.sh
+++ tests/performance/api-tests/api-test-quarkus.sh
@@ -4,7 +4,7 @@ set -e
 wget -O /tmp/api-utils.sh https://raw.githubusercontent.com/eclipse/che/main/tests/performance/api-tests/api-utils.sh
 source /tmp/api-utils.sh
 
-export TEST_DEVFILE_PATH="devfile-registry/devfiles/java11-maven-quarkus__quarkus-quickstarts/devworkspace-che-code-latest.yaml"
+export TEST_DEVFILE_PATH="devfile-registry/devfiles/java-maven-quarkus__quarkus-quickstarts/devworkspace-che-code-latest.yaml"
 export WORKSPACE_NAME="quarkus-quickstart"
 export projectName="quarkus-quickstarts/getting-started"
 export expectedCommandOutput="BUILD SUCCESS"
