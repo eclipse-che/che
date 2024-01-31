5e5f2f70867405567a066035730b353be92c1e99
Mykhailo Kuznietsov <mkuznets@redhat.com>
2024-01-31 16:41:35 +0000

    chore: Bump to 7.82.0-next in main
    Signed-off-by: Mykhailo Kuznietsov <mkuznets@redhat.com>

--- tests/e2e/constants/PLUGIN_TEST_CONSTANTS.ts
+++ tests/e2e/constants/PLUGIN_TEST_CONSTANTS.ts
@@ -12,6 +12,5 @@ export const PLUGIN_TEST_CONSTANTS: { TS_SAMPLE_LIST: string } = {
 	 * dashboard samples to check in RecommendedExtensions.spec.ts
 	 */
 	TS_SAMPLE_LIST:
-		process.env.TS_SAMPLE_LIST ||
-		'Node.js MongoDB,Node.js Express,Java Lombok,Quarkus REST API,Python,.NET,C/C++,Go,PHP,Ansible'
+		process.env.TS_SAMPLE_LIST || 'Node.js MongoDB,Node.js Express,Java Lombok,Quarkus REST API,Python,.NET,C/C++,Go,PHP,Ansible'
 };
