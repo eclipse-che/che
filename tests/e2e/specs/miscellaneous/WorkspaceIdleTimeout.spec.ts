50651f85af06cdff4e6331cbe12f2e63553d7763
Skoryk Serhii <sskoryk@redhat.com>
2024-02-13 16:36:14 +0200

    update login to oc step

--- tests/e2e/specs/miscellaneous/WorkspaceIdleTimeout.spec.ts
+++ tests/e2e/specs/miscellaneous/WorkspaceIdleTimeout.spec.ts
@@ -47,7 +47,7 @@ suite('"Check workspace idle timeout" test', function (): void {
 	let stopWorkspaceTimeout: number = 0;
 
 	suiteSetup(function (): void {
-		kubernetesCommandLineToolsExecutor.loginToOcp(OAUTH_CONSTANTS.TS_SELENIUM_OCP_USERNAME, OAUTH_CONSTANTS.TS_SELENIUM_OCP_PASSWORD);
+		kubernetesCommandLineToolsExecutor.loginToOcp();
 		shellExecutor.executeCommand('oc project openshift-devspaces');
 
 		// get current value of spec.devEnvironments.secondsOfInactivityBeforeIdling
