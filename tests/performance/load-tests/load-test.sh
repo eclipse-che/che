055c6d36f74df56b89c2ff85fed00fab69dc1de9
Skoryk Serhii <sskoryk@redhat.com>
2024-02-28 10:54:58 +0200

    use local devfile.yaml

--- tests/performance/load-tests/load-test.sh
+++ tests/performance/load-tests/load-test.sh
@@ -40,38 +40,29 @@ if [ -z $COMPLETITIONS_COUNT ]; then
   export COMPLETITIONS_COUNT=3
 fi
 
-# Checkout to user devspaces namespace
-export userName=$(kubectl config view --minify -o jsonpath='{.users[0].name}' | sed 's|/.*||')
-kubectl config set-context --current --namespace=$userName-devspaces
-
 # Delete all dw and dwt objects from test namespace
 cleanup
 
 # Delete logs
-rm dw* || true
-
-# Get Openshift DevSpaces url and path to sample devfile yaml from devfile-registry
-export devworkspaceUrl=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}' | sed 's/:6443//' | sed 's/api/devspaces.apps/')
-export testDevfilePath="devfile-registry/devfiles/TP__cpp__c-plus-plus/devworkspace-che-code-latest.yaml"
-
-echo "Download the devfile"
-curl --insecure "$devworkspaceUrl/$testDevfilePath" -o devfile.yaml
-
-echo "Start workspaces from sample devfile"
-csplit devfile.yaml /---/
-mv xx00 dwt.yaml
-mv xx01 dw.yaml
-kubectl apply -f dwt.yaml
+rm dw* || true  > /dev/null
 
 for ((i=1; i<=$COMPLETITIONS_COUNT; i++)); do
-  cat dw.yaml | sed "0,/name: cpp/s//name: dw$i/" | kubectl apply -f -
+  cat devfile.yaml | sed "0,/name: code-latest/s//name: dw$i/" | kubectl apply -f -  &
 done
+
+wait
+
+for ((i=1; i<=$COMPLETITIONS_COUNT; i++)); do
+  kubectl wait --for=condition=Ready "dw/dw$i" --timeout=120s || true &
+done
+
+wait
 
 total_time=0
 succeeded=0
 echo  "Wait for all workspaces are started and calculate average workspaces starting time"
 for ((i=1; i<=$COMPLETITIONS_COUNT; i++)); do
-  if kubectl wait --for=condition=Ready "dw/dw$i" --timeout=120s; then
+  if [ "$(kubectl get dw dw$i --template='{{.status.phase}}')" == "Running" ]; then
     start_time=$(kubectl get dw dw$i --template='{{range .status.conditions}}{{if eq .type "Started"}}{{.lastTransitionTime}}{{end}}{{end}}')
     end_time=$(kubectl get dw dw$i --template='{{range .status.conditions}}{{if eq .type "Ready"}}{{.lastTransitionTime}}{{end}}{{end}}')
     start_timestamp=$(date -d $start_time +%s)
@@ -84,13 +75,14 @@ for ((i=1; i<=$COMPLETITIONS_COUNT; i++)); do
   else
     print_error "Timeout waiting for dw$i to become ready or an error occurred."
     kubectl describe dw dw$i > dw$i-log.log
-    kubectl logs $(oc get dw dw$i --template='{{.status.devworkspaceId}}') > dw$i-pod.log || true
-fi        
+    kubectl logs $(kubectl get dw dw$i --template='{{.status.devworkspaceId}}') > dw$i-pod.log || true
+fi
 done
+
+wait
 
 print "==================== Test results ===================="
 print "Average workspace starting time for $succeeded workspaces from $COMPLETITIONS_COUNT started: $((total_time / succeeded)) seconds"
 print "$((COMPLETITIONS_COUNT - succeeded)) workspaces failed. See failed workspace pod logs in the current folder for details."
 
 trap cleanup ERR EXIT
-
