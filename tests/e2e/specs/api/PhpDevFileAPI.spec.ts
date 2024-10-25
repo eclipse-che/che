75c9f632c2329a9b3b39f6fd6c59d23695e02b82
mmusiien <mmusiien@redhat.com>
2024-10-25 04:19:05 +0300

    Add proposals to the PR

--- tests/e2e/specs/api/PhpDevFileAPI.spec.ts
+++ tests/e2e/specs/api/PhpDevFileAPI.spec.ts
@@ -10,61 +10,67 @@
 import { BASE_TEST_CONSTANTS } from '../../constants/BASE_TEST_CONSTANTS';
 import { e2eContainer } from '../../configs/inversify.config';
 import { CLASSES } from '../../configs/inversify.types';
-import {Logger} from "../../utils/Logger";
-import {DevfilesRegistryHelper} from "../../utils/DevfilesRegistryHelper";
-import {ContainerTerminal, KubernetesCommandLineToolsExecutor} from "../../utils/KubernetesCommandLineToolsExecutor";
-import {ShellExecutor} from "../../utils/ShellExecutor";
-import {DevWorkspaceConfigurationHelper} from "../../utils/DevWorkspaceConfigurationHelper";
-import {DevfileContext} from "@eclipse-che/che-devworkspace-generator/lib/api/devfile-context";
-import {ShellString} from "shelljs";
-import {expect} from "chai";
-import {API_TEST_CONSTANTS} from "../../constants/API_TEST_CONSTANTS";
-import YAML from "yaml";
-import {StringUtil} from "../../utils/StringUtil";
-import {string} from "yaml/dist/schema/common/string";
-
+import { DevfilesRegistryHelper } from '../../utils/DevfilesRegistryHelper';
+import { ContainerTerminal, KubernetesCommandLineToolsExecutor } from '../../utils/KubernetesCommandLineToolsExecutor';
+import { DevWorkspaceConfigurationHelper } from '../../utils/DevWorkspaceConfigurationHelper';
+import { DevfileContext } from '@eclipse-che/che-devworkspace-generator/lib/api/devfile-context';
+import { ShellString } from 'shelljs';
+import { expect } from 'chai';
+import { API_TEST_CONSTANTS } from '../../constants/API_TEST_CONSTANTS';
+import YAML from 'yaml';
+import { Logger } from '../../utils/Logger';
+import crypto from 'crypto';
 
 suite('PHP devfile API test', function (): void {
-    const devfilesRegistryHelper: DevfilesRegistryHelper = e2eContainer.get(CLASSES.DevfilesRegistryHelper);
-    const kubernetesCommandLineToolsExecutor: KubernetesCommandLineToolsExecutor = e2eContainer.get(CLASSES.KubernetesCommandLineToolsExecutor);
-    let containerTerminal: ContainerTerminal = e2eContainer.get(CLASSES.ContainerTerminal);
-    let devWorkspaceConfigurationHelper: DevWorkspaceConfigurationHelper;
-    let devfileContext: DevfileContext;
-    let devfileContent: string ='';
+	const devfilesRegistryHelper: DevfilesRegistryHelper = e2eContainer.get(CLASSES.DevfilesRegistryHelper);
+	const kubernetesCommandLineToolsExecutor: KubernetesCommandLineToolsExecutor = e2eContainer.get(
+		CLASSES.KubernetesCommandLineToolsExecutor
+	);
+	const devfileID: string = 'php';
+	const containerTerminal: ContainerTerminal = e2eContainer.get(CLASSES.ContainerTerminal);
+	let devWorkspaceConfigurationHelper: DevWorkspaceConfigurationHelper;
+	let devfileContext: DevfileContext;
+	let devfileContent: string = '';
 
-    suiteSetup(`Prepare login ${BASE_TEST_CONSTANTS.TEST_ENVIRONMENT}`, async function (): Promise<void> {
-        kubernetesCommandLineToolsExecutor.loginToOcp();
-    });
+	suiteSetup(`Prepare login ${BASE_TEST_CONSTANTS.TEST_ENVIRONMENT}`, function (): void {
+		kubernetesCommandLineToolsExecutor.loginToOcp();
+	});
 
-    test('Create  PHP DevSpace', async function (): Promise<void> {
-        kubernetesCommandLineToolsExecutor.namespace = 'admin-devspaces';
-        devfileContent = devfilesRegistryHelper.getDevfileContent('php');
-        const editorDevfileContent: string = devfilesRegistryHelper.obtainCheDevFileEditorFromCheConfigMap('editors-definitions');
-        kubernetesCommandLineToolsExecutor.workspaceName = YAML.parse(devfileContent).metadata.name;
+	test(`Create  ${devfileID} workspace`, async function (): Promise<void> {
+		const randomPref: string = crypto.randomBytes(4).toString('hex');
+		kubernetesCommandLineToolsExecutor.namespace = API_TEST_CONSTANTS.TS_API_TEST_NAMESPACE || 'admin-devspaces';
+		devfileContent = devfilesRegistryHelper.getDevfileContent(devfileID);
+		const editorDevfileContent: string = devfilesRegistryHelper.obtainCheDevFileEditorFromCheConfigMap('editors-definitions');
+		const uniqName: string = YAML.parse(devfileContent).metadata.name + randomPref;
+		kubernetesCommandLineToolsExecutor.workspaceName = uniqName;
 
-        devWorkspaceConfigurationHelper = new DevWorkspaceConfigurationHelper({
-            editorContent: editorDevfileContent, devfileContent: devfileContent
-        });
-        devfileContext = await devWorkspaceConfigurationHelper.generateDevfileContext();
+		devWorkspaceConfigurationHelper = new DevWorkspaceConfigurationHelper({
+			editorContent: editorDevfileContent,
+			devfileContent: devfileContent
+		});
+		devfileContext = await devWorkspaceConfigurationHelper.generateDevfileContext();
+		if (devfileContext.devWorkspace.metadata) {
+			devfileContext.devWorkspace.metadata.name = uniqName;
+		}
+		const devWorkspaceConfigurationYamlString: string =
+			devWorkspaceConfigurationHelper.getDevWorkspaceConfigurationYamlAsString(devfileContext);
+		const output: ShellString = kubernetesCommandLineToolsExecutor.applyAndWaitDevWorkspace(devWorkspaceConfigurationYamlString);
+		expect(output.stdout).contains('condition met');
+	});
 
-        const devWorkspaceConfigurationYamlString: string =
-            devWorkspaceConfigurationHelper.getDevWorkspaceConfigurationYamlAsString(devfileContext);
-        const output: ShellString = kubernetesCommandLineToolsExecutor.applyAndWaitDevWorkspace(devWorkspaceConfigurationYamlString);
-        expect(output.stdout).contains('condition met');
-    });
+	test('Check running application', function (): void {
+		const workdir: string = YAML.parse(devfileContent).commands[0].exec.workingDir;
+		const commandLine: string = YAML.parse(devfileContent).commands[0].exec.commandLine;
+		const containerName: string = YAML.parse(devfileContent).commands[0].exec.component;
+		Logger.info(`workdir from exec section of DevWorkspace file: ${workdir}`);
+		Logger.info(`commandLine from exec section of DevWorkspace file: ${commandLine}`);
+		const runCommandInBash: string = `cd ${workdir} && ${commandLine}`;
+		const output: ShellString = containerTerminal.execInContainerCommand(runCommandInBash, containerName);
+		expect(output.code).eqls(0);
+		expect(output.stdout.trim()).contains('Hello, world!');
+	});
 
-    test('Check running application', async function (): Promise<void> {
-        const workdir:string = YAML.parse(devfileContent).commands[0].exec.workingDir;
-        const commandLine:string = YAML.parse(devfileContent).commands[0].exec.commandLine;
-        const containerName:string = YAML.parse(devfileContent).commands[0].exec.component;
-        const runCommandInBash:string = `cd ${workdir} && ${commandLine}`;
-        const output: ShellString = containerTerminal.execInContainerCommand(runCommandInBash, containerName);
-        expect(output.code).eqls(0);
-        expect(output.stdout.trim()).contains('Hello, world!');
-
-    });
-
-    suiteTeardown('Delete workspace', function (): void {
-        kubernetesCommandLineToolsExecutor.deleteDevWorkspace();
-    });
+	suiteTeardown('Delete workspace', function (): void {
+		kubernetesCommandLineToolsExecutor.deleteDevWorkspace();
+	});
 });
