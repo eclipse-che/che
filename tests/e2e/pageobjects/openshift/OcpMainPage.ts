9d0a1ba191ed258c0cdff4c1130f003f2697f34c
mmusiien <mmusiien@redhat.com>
2024-11-12 22:48:55 +0200

    stabilize devconsole integration tests

--- tests/e2e/pageobjects/openshift/OcpMainPage.ts
+++ tests/e2e/pageobjects/openshift/OcpMainPage.ts
@@ -16,6 +16,7 @@ import { Logger } from '../../utils/Logger';
 import { TIMEOUT_CONSTANTS } from '../../constants/TIMEOUT_CONSTANTS';
 import { OcpImportFromGitPage } from './OcpImportFromGitPage';
 import { e2eContainer } from '../../configs/inversify.config';
+import {BrowserTabsUtil} from "../../utils/BrowserTabsUtil";
 
 @injectable()
 export class OcpMainPage {
@@ -29,7 +30,9 @@ export class OcpMainPage {
 
 	constructor(
 		@inject(CLASSES.DriverHelper)
-		private readonly driverHelper: DriverHelper
+		private readonly driverHelper: DriverHelper,
+		@inject(CLASSES.BrowserTabsUtil)
+		private readonly browserTabsUtil: BrowserTabsUtil
 	) {}
 
 	async waitOpenMainPage(): Promise<void> {
@@ -106,5 +109,13 @@ export class OcpMainPage {
 		} else {
 			Logger.debug('welcome tour modal dialog was not located');
 		}
+	}
+	async clickOnAppLauncherAndDevSpaceItem(): Promise<void> {
+		Logger.debug('click on app launcher menu');
+		const parentGUID: string = await this.browserTabsUtil.getCurrentWindowHandle();
+		await this.driverHelper.waitAndClick(By.css('nav[data-test-id=\'application-launcher\']'));
+		await this.driverHelper.waitAndClick(By.xpath('//span[contains(.,\'Red Hat OpenShift Dev Spaces\')]'));
+		await this.browserTabsUtil.waitAndSwitchToAnotherWindow(parentGUID, TIMEOUT_CONSTANTS.TS_SELENIUM_LOAD_PAGE_TIMEOUT);
+
 	}
 }
