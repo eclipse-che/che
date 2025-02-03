1464fbca2709ecf6eefb10f9fcacf61dd0c40d26
Aleksandr Shmaraiev <oshmarai@redhat.com>
2025-02-03 22:33:05 +0200

    Add changes to 'Factory' test

--- tests/e2e/specs/factory/Factory.spec.ts
+++ tests/e2e/specs/factory/Factory.spec.ts
@@ -1,5 +1,5 @@
 /** *******************************************************************
- * copyright (c) 2021-2024 Red Hat, Inc.
+ * copyright (c) 2021-2025 Red Hat, Inc.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -92,6 +92,13 @@ suite(
 			test('Select opening an existing workspace instead of creating a new one', async function (): Promise<void> {
 				await dashboard.waitExistingWorkspaceFoundAlert();
 				await dashboard.clickOpenExistingWorkspaceButton();
+			});
+		}
+
+		if (BASE_TEST_CONSTANTS.SELECT_OF_CREATION_NEW_WORKSPACE) {
+			test('Select of creating a new workspace', async function (): Promise<void> {
+				await dashboard.waitExistingWorkspaceFoundAlert();
+				await dashboard.clickOnCreateNewWorkspaceButton();
 			});
 		}
 
