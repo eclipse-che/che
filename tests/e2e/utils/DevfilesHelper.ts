880b008230e3a540cec354960c0541b34d362a74
Dmytro Nochevnov <dnochevn@redhat.com>
2025-02-26 21:42:40 +0200

    fix curl from https
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>

--- tests/e2e/utils/DevfilesHelper.ts
+++ tests/e2e/utils/DevfilesHelper.ts
@@ -24,8 +24,6 @@ export class DevfilesHelper {
 
 		// internal clusterIP is restricted by proxy when get devfile from the disconnected cluster
 		if (BASE_TEST_CONSTANTS.IS_CLUSTER_DISCONNECTED()) {
-			Logger.debug('Cluster is disconnected, using the base URL');
-
 			return `${BASE_TEST_CONSTANTS.TS_SELENIUM_BASE_URL}${devfileSampleURIPrefix}`;
 		}
 
@@ -49,7 +47,7 @@ export class DevfilesHelper {
 	public obtainDevFileContentUsingPod(podName: string, containerName: string, devFileName: string): string {
 		const clusterURL: string = this.getInternalClusterURLToDevFile(devFileName);
 		this.getShellExecutor().executeCommand(
-			`oc exec -i ${podName} -n  ${BASE_TEST_CONSTANTS.TS_PLATFORM}-${BASE_TEST_CONSTANTS.TESTING_APPLICATION_NAME()} -c ${containerName} -- sh -c 'curl -o /tmp/${devFileName}-devfile.yaml ${clusterURL}'`
+			`oc exec -i ${podName} -n  ${BASE_TEST_CONSTANTS.TS_PLATFORM}-${BASE_TEST_CONSTANTS.TESTING_APPLICATION_NAME()} -c ${containerName} -- sh -c 'curl -k -o /tmp/${devFileName}-devfile.yaml ${clusterURL}'`
 		);
 		return this.getShellExecutor()
 			.executeArbitraryShellScript(
