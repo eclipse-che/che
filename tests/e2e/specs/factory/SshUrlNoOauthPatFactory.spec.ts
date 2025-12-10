1675857caab8032a7fada6f7836ca8e95f32bd65
Serhii <sskoryk@redhat.com>
2025-12-10 15:47:07 +0200

    check stage changes, commit and push

--- tests/e2e/specs/factory/SshUrlNoOauthPatFactory.spec.ts
+++ tests/e2e/specs/factory/SshUrlNoOauthPatFactory.spec.ts
@@ -8,17 +8,7 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 
-import {
-	ActivityBar,
-	ContextMenu,
-	InputBox,
-	Key,
-	Locators,
-	NewScmView,
-	SingleScmProvider,
-	ViewControl,
-	ViewSection
-} from 'monaco-page-objects';
+import { ActivityBar, ContextMenu, Key, Locators, NewScmView, SingleScmProvider, ViewControl, ViewSection } from 'monaco-page-objects';
 import { ProjectAndFileTests } from '../../tests-library/ProjectAndFileTests';
 import { CLASSES, TYPES } from '../../configs/inversify.types';
 import { e2eContainer } from '../../configs/inversify.config';
@@ -36,6 +26,8 @@ import { ITestWorkspaceUtil } from '../../utils/workspace/ITestWorkspaceUtil';
 import { Logger } from '../../utils/Logger';
 import { DriverHelper } from '../../utils/DriverHelper';
 import { CheCodeLocatorLoader } from '../../pageobjects/ide/CheCodeLocatorLoader';
+import { ViewsMoreActionsButton } from '../../pageobjects/ide/ViewsMoreActionsButton';
+import { SourceControlView } from '../../pageobjects/ide/SourceControlView';
 
 suite(`The SshUrlNoOauthPatFactory userstory ${BASE_TEST_CONSTANTS.TEST_ENVIRONMENT}`, function (): void {
 	const projectAndFileTests: ProjectAndFileTests = e2eContainer.get(CLASSES.ProjectAndFileTests);
@@ -47,7 +39,8 @@ suite(`The SshUrlNoOauthPatFactory userstory ${BASE_TEST_CONSTANTS.TEST_ENVIRONM
 	const dashboard: Dashboard = e2eContainer.get(CLASSES.Dashboard);
 	const testWorkspaceUtil: ITestWorkspaceUtil = e2eContainer.get(TYPES.WorkspaceUtil);
 	const cheCodeLocatorLoader: CheCodeLocatorLoader = e2eContainer.get(CLASSES.CheCodeLocatorLoader);
-
+	const viewsMoreActionsButton: ViewsMoreActionsButton = e2eContainer.get(CLASSES.ViewsMoreActionsButton);
+	const sourceControlView: SourceControlView = e2eContainer.get(CLASSES.SourceControlView);
 	const webCheCodeLocators: Locators = cheCodeLocatorLoader.webCheCodeLocators;
 
 	const factoryUrl: string =
@@ -70,7 +63,6 @@ suite(`The SshUrlNoOauthPatFactory userstory ${BASE_TEST_CONSTANTS.TEST_ENVIRONM
 	const refreshButtonLabel: string = 'Refresh';
 	const pushItemLabel: string = 'Push';
 	let testRepoProjectName: string;
-	const isPrivateRepo: string = FACTORY_TEST_CONSTANTS.TS_SELENIUM_IS_PRIVATE_FACTORY_GIT_REPO ? 'private' : 'public';
 
 	async function deleteSshKeys(): Promise<void> {
 		Logger.debug('Deleting SSH keys if they are present');
@@ -117,14 +109,16 @@ suite(`The SshUrlNoOauthPatFactory userstory ${BASE_TEST_CONSTANTS.TEST_ENVIRONM
 				.undefined;
 		});
 		test('Accept the project as a trusted one', async function (): Promise<void> {
-			await projectAndFileTests.performTrustAuthorDialog();
+			await projectAndFileTests.performTrustDialogs();
+			// it needs to wait here for the Trust dialogs to be closed and the Editor to be ready
+			await driverHelper.wait(5000);
 		});
 		test('Check the project files was imported', async function (): Promise<void> {
 			expect(
 				await projectAndFileTests.getProjectTreeItem(projectSection, BASE_TEST_CONSTANTS.TS_SELENIUM_PROJECT_ROOT_FILE_NAME),
 				'Project files were not imported'
 			).not.undefined;
-			testRepoProjectName = StringUtil.getProjectNameFromGitUrl(FACTORY_TEST_CONSTANTS.TS_SELENIUM_FACTORY_GIT_REPO_URL);
+			testRepoProjectName = StringUtil.getProjectNameFromGitUrl(factoryUrl);
 		});
 
 		test('Make changes to the file', async function (): Promise<void> {
@@ -132,13 +126,14 @@ suite(`The SshUrlNoOauthPatFactory userstory ${BASE_TEST_CONSTANTS.TEST_ENVIRONM
 			await projectSection.openItem(testRepoProjectName, fileToChange);
 			await driverHelper.waitVisibility(webCheCodeLocators.Editor.inputArea);
 			await driverHelper.getDriver().findElement(webCheCodeLocators.Editor.inputArea).click();
+			await driverHelper.wait(1000);
 
 			Logger.debug('Clearing the editor with Ctrl+A');
 			await driverHelper.getDriver().actions().keyDown(Key.CONTROL).sendKeys('a').keyUp(Key.CONTROL).perform();
-			await driverHelper.wait(500);
+			await driverHelper.wait(1000);
 			Logger.debug('Deleting selected text');
 			await driverHelper.getDriver().actions().sendKeys(Key.DELETE).perform();
-			await driverHelper.wait(500);
+			await driverHelper.wait(1000);
 			Logger.debug(`Entering text: "${changesToCommit}"`);
 			await driverHelper.getDriver().actions().sendKeys(changesToCommit).perform();
 		});
@@ -165,6 +160,57 @@ suite(`The SshUrlNoOauthPatFactory userstory ${BASE_TEST_CONSTANTS.TEST_ENVIRONM
 			const changes: number = await scmProvider.getChangeCount();
 			Logger.debug(`scmProvider.getChangeCount: number of changes is "${changes}"`);
 			expect(changes).eql(1);
+		});
+
+		test('Stage the changes', async function (): Promise<void> {
+			await driverHelper.waitVisibility(webCheCodeLocators.ScmView.more);
+			viewsActionsButton = await viewsMoreActionsButton.viewsAndMoreActionsButtonIsVisible();
+			if (viewsActionsButton) {
+				await viewsMoreActionsButton.closeSourceControlGraph();
+			}
+			scmContextMenu = await scmProvider.openMoreActions();
+			await driverHelper.waitVisibility(webCheCodeLocators.ContextMenu.contextView);
+			Logger.debug('scmContextMenu.select: "Changes" -> "Stage All Changes"');
+			await scmContextMenu.select('Changes', 'Stage All Changes');
+		});
+
+		test('Commit the changes', async function (): Promise<void> {
+			Logger.info(`ScmView inputField locator: "${(webCheCodeLocators.ScmView as any).scmEditor}"`);
+			Logger.debug('Click on the Scm Editor');
+			await driverHelper
+				.getDriver()
+				.findElement((webCheCodeLocators.ScmView as any).scmEditor)
+				.click();
+			await sourceControlView.typeCommitMessage(changesToCommit);
+			await driverHelper.waitVisibility(webCheCodeLocators.ScmView.more);
+			await driverHelper.wait(timeToRefresh);
+			Logger.debug(`wait and click on: "${refreshButtonLabel}"`);
+			await driverHelper.waitAndClick(webCheCodeLocators.ScmView.actionConstructor(refreshButtonLabel));
+			// wait while changes counter will be refreshed
+			await driverHelper.wait(timeToRefresh);
+			const changes: number = await scmProvider.getChangeCount();
+			Logger.debug(`scmProvider.getChangeCount: number of changes is "${changes}"`);
+			expect(changes).eql(0);
+		});
+
+		test('Push the changes and check if the changes were pushed', async function (): Promise<void> {
+			await driverHelper.waitVisibility(webCheCodeLocators.Notification.action);
+			await driverHelper.waitVisibility(webCheCodeLocators.ScmView.more);
+			Logger.debug('scmProvider.openMoreActions');
+			scmContextMenu = await scmProvider.openMoreActions();
+			await driverHelper.waitVisibility(webCheCodeLocators.ContextMenu.itemConstructor(pushItemLabel));
+			Logger.debug(`scmContextMenu.select: "${pushItemLabel}"`);
+			await scmContextMenu.select(pushItemLabel);
+
+			await driverHelper.waitVisibility(webCheCodeLocators.ScmView.more);
+			await driverHelper.wait(timeToRefresh);
+			Logger.debug(`wait and click on: "${refreshButtonLabel}"`);
+			await driverHelper.waitAndClick(webCheCodeLocators.ScmView.actionConstructor(refreshButtonLabel));
+			const isCommitButtonDisabled: string = await driverHelper.waitAndGetElementAttribute(
+				webCheCodeLocators.Notification.action,
+				'aria-disabled'
+			);
+			expect(isCommitButtonDisabled).to.equal('true');
 		});
 
 		suiteTeardown('Delete SSH keys', async function (): Promise<void> {
