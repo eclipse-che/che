fbf37917c305358f30e0ccc3163a792b2a5da65b
Oleksii Korniienko <olkornii@redhat.com>
2026-01-28 17:03:55 +0100

    replaced stop and delete to just delete for suiteTeardown.
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- tests/e2e/specs/dashboard-samples/StartWorkspaceUsingVSCodeDesktopSshEditor.spec.ts
+++ tests/e2e/specs/dashboard-samples/StartWorkspaceUsingVSCodeDesktopSshEditor.spec.ts
@@ -136,7 +136,7 @@ suite('Check Visual Studio Code (desktop) (SSH) with all samples', function (): 
 		await dashboard.openDashboard();
 		await browserTabsUtil.closeAllTabsExceptCurrent();
 		try {
-			await dashboard.stopAndRemoveWorkspaceByUI(WorkspaceHandlingTests.getWorkspaceName());
+			await dashboard.deleteStoppedWorkspaceByUI(WorkspaceHandlingTests.getWorkspaceName());
 		} catch (e) {
 			Logger.info('Cannot find or stop DevWorkspace in suiteTeardown. Normal behaviour if test passed.');
 		}
