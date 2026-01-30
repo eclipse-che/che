c2e9cd10f8870e97986713e6a5f21e25ce754993
Sergey Skorik <sskoryk@redhat.com>
2026-01-30 15:56:26 +0200

    PredefinedNamespace test should use the latest Dev Spaces VS Code Editor image (#23733)
    * PredefinedNamespace test should use the latest Dev Spaces VS Code Editor image
    
    * update image url only if test starts on DevSpaces

--- tests/e2e/specs/miscellaneous/PredefinedNamespace.spec.ts
+++ tests/e2e/specs/miscellaneous/PredefinedNamespace.spec.ts
@@ -54,11 +54,25 @@ suite(`Create predefined workspace and check it ${BASE_TEST_CONSTANTS.TEST_ENVIR
 	// generate empty workspace DevFile and create it through oc client under a regular user
 	suiteSetup('Login', async function (): Promise<void> {
 		const devfileContent: string = 'schemaVersion: 2.2.0\n' + 'metadata:\n' + `  name: ${workspaceName}\n`;
+		const majorMinorVersion: string = BASE_TEST_CONSTANTS.TESTING_APPLICATION_VERSION.split('.').slice(0, 2).join('.');
+		const devSpacesEditorImage: string = `quay.io/redhat-user-workloads/devspaces-tenant/devspaces/code-rhel9:${majorMinorVersion}`;
 		kubernetesCommandLineToolsExecutor.loginToOcp(userName);
 		devWorkspaceConfigurationHelper = new DevWorkspaceConfigurationHelper({
 			devfileContent
 		});
 		devfileContext = await devWorkspaceConfigurationHelper.generateDevfileContext();
+
+		// update che-code-injector image to use Dev Spaces VS Code Editor (Dev Spaces only)
+		if (BASE_TEST_CONSTANTS.TESTING_APPLICATION_NAME() === 'devspaces') {
+			devfileContext.devWorkspaceTemplates.forEach((template): void => {
+				template.spec?.components?.forEach((component): void => {
+					if (component.name === 'che-code-injector' && component.container?.image) {
+						component.container.image = devSpacesEditorImage;
+					}
+				});
+			});
+		}
+
 		const devWorkspaceConfigurationYamlString: string =
 			devWorkspaceConfigurationHelper.getDevWorkspaceConfigurationYamlAsString(devfileContext);
 		kubernetesCommandLineToolsExecutor.applyWithoutNamespace(devWorkspaceConfigurationYamlString);
